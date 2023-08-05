45ed94c6ba7bdffc074c01fcec3bac3ed918afe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package dflogger/0.1.0/dflogger-0.1.0.tar.gz

---
+++ packages/dflogger/dflogger-0.1.0.tar.gz/dflogger-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dflogger',
+    packages=find_packages(),
+    version='0.1.0',
+    description='small and dataframe based machine learning experiment logger',
+    author='Sulthan Abiyyu',
+    license='MIT',
+)
