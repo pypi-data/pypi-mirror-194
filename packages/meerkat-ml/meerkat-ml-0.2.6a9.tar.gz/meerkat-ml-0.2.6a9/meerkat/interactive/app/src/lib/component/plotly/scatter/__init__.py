e6885f35e9179f6fd98d531438e92ab5be8b6716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package meerkat-ml/0.2.6a9/meerkat-ml-0.2.6a9.tar.gz

---
+++ packages/meerkat-ml/meerkat-ml-0.2.6a9.tar.gz/meerkat-ml-0.2.6a9/meerkat/interactive/app/src/lib/component/plotly/scatter/__init__.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from meerkat.dataframe import DataFrame
+from meerkat.interactive.app.src.lib.component.abstract import Component
+from meerkat.interactive.endpoint import Endpoint, EndpointProperty
+from meerkat.tools.utils import classproperty
+
+
+class ScatterPlot(Component):
+    df: DataFrame
+    x: str
+    y: str
+    title: str
+    selected: List[str] = []
+
+    on_click: EndpointProperty = None
+    on_select: Endpoint = None
+
+    def __init__(
+        self,
+        df: DataFrame,
+        *,
+        x: str,
+        y: str,
+        title: str = "",
+        selected: List[str] = [],
+        on_click: EndpointProperty = None,
+        on_select: Endpoint = None,
+    ):
+        super().__init__(
+            df=df,
+            x=x,
+            y=y,
+            title=title,
+            selected=selected,
+            on_click=on_click,
+            on_select=on_select,
+        )
+
+    @classproperty
+    def namespace(cls):
+        return "plotly"
