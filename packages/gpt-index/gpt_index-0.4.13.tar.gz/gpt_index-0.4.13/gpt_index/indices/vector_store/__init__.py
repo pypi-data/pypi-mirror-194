db73f7d362285f75faaa78a78df2289d1038d4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:33 +0000

    Add package gpt-index/0.4.13/gpt_index-0.4.13.tar.gz

---
+++ packages/gpt-index/gpt_index-0.4.13.tar.gz/gpt_index-0.4.13/gpt_index/indices/vector_store/__init__.py
@@ -0,0 +1,21 @@
+"""Vector-store based data structures."""
+
+from gpt_index.indices.vector_store.base import GPTVectorStoreIndex
+from gpt_index.indices.vector_store.vector_indices import (
+    GPTChromaIndex,
+    GPTFaissIndex,
+    GPTPineconeIndex,
+    GPTQdrantIndex,
+    GPTSimpleVectorIndex,
+    GPTWeaviateIndex,
+)
+
+__all__ = [
+    "GPTVectorStoreIndex",
+    "GPTSimpleVectorIndex",
+    "GPTFaissIndex",
+    "GPTPineconeIndex",
+    "GPTWeaviateIndex",
+    "GPTQdrantIndex",
+    "GPTChromaIndex",
+]
