41e80a8340d8389da313787820d46219136124f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:57 +0000

    Add package galaxygrad/0.0.1/galaxygrad-0.0.1.tar.gz

---
+++ packages/galaxygrad/galaxygrad-0.0.1.tar.gz/galaxygrad-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Diffusion model for galaxy generation'
+LONG_DESCRIPTION = 'Contains two jax functions ScoreNet32 and ScoreNet64. These are used to return the gradients of an arbitrary image with respect to a prior distribution of individual artifact free galaxy models. '
+
+# Setting up
+setup(
+        name="galaxygrad", 
+        version=VERSION,
+        author="Matt Sampson",
+        author_email="matt.sampson@princeton.edu",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        package_data={'':['*.eqx']},
+        include_package_data=True,
+        install_requires=['jax', 'equinox', 'einops'],
+        
+        keywords=['python', 'diffusion'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+        ]
+)
