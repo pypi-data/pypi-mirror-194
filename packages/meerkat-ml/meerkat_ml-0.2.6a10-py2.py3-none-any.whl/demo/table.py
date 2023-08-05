8677960e54e0c6cea9258fe97d8130c8aaf219f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package meerkat-ml/0.2.6a10/meerkat_ml-0.2.6a10-py2.py3-none-any.whl

---
+++ packages/meerkat-ml/meerkat_ml-0.2.6a10-py2.py3-none-any.whl/demo/table.py
@@ -0,0 +1,9 @@
+import meerkat as mk
+
+df = mk.get("imagenette", version="160px")
+
+table = mk.gui.Table(df=df)
+
+mk.gui.start(shareable=False)
+page = mk.gui.Page(component=mk.gui.html.flexcol(slots=[table]), id="table")
+page.launch()
