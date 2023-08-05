5eabd9f14da9ae1d9635c211509c196d2f864aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:06 +0000

    Add package synthdog/0.2.5/synthdog-0.2.5-py3-none-any.whl

---
+++ packages/synthdog/synthdog-0.2.5-py3-none-any.whl/synthdog/elements/__init__.py
@@ -0,0 +1,13 @@
+"""
+Donut
+Copyright (c) 2022-present NAVER Corp.
+MIT License
+"""
+from synthdog.elements.background import Background
+from synthdog.elements.content import Content
+from synthdog.elements.document import Document, CheckDocument
+from synthdog.elements.paper import Paper
+from synthdog.elements.textbox import TextBox
+from synthdog.elements.utils import *
+
+__all__ = ["Background", "Content", "Document", "Paper", "TextBox", "CheckDocument"]
