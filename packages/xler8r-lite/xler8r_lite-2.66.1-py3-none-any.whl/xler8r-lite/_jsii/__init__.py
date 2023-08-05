7abfe6325cfa33821d56868ebb9a794596211ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:01 +0000

    Add package xler8r-lite/2.66.1/xler8r_lite-2.66.1-py3-none-any.whl

---
+++ packages/xler8r-lite/xler8r_lite-2.66.1-py3-none-any.whl/xler8r-lite/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import wa-cdk-lite._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cre8ivelogix/xler8r-lite",
+    "2.66.1",
+    __name__[0:-6],
+    "xler8r-lite@2.66.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
