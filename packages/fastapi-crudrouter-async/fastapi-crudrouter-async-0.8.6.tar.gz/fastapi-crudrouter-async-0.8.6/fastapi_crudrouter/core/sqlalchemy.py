7f9e4ccee98cb75f9689703d77af625020adfb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:28 +0000

    Add package fastapi-crudrouter-async/0.8.6/fastapi-crudrouter-async-0.8.6.tar.gz

---
+++ packages/fastapi-crudrouter-async/fastapi-crudrouter-async-0.8.6.tar.gz/fastapi-crudrouter-async-0.8.6/fastapi_crudrouter/core/sqlalchemy.py
@@ -0,0 +1,278 @@
+import inspect
+from typing import Any, Callable, Generator, List, Optional, Type, Union
+
+from fastapi import Depends, HTTPException
+
+from . import NOT_FOUND, CRUDGenerator, _utils
+from ._types import DEPENDENCIES, PAGINATION
+from ._types import PYDANTIC_SCHEMA as SCHEMA
+
+try:
+    from sqlalchemy import __version__ as sqlalchemy_version
+    from sqlalchemy.exc import IntegrityError, NoResultFound
+    from sqlalchemy.ext.declarative import DeclarativeMeta as Model
+    from sqlalchemy.orm import Session
+
+    if sqlalchemy_version >= "1.4":
+        from sqlalchemy.future import select
+except ImportError:
+    Model = None
+    Session = None
+    IntegrityError = None
+    sqlalchemy_installed = False
+else:
+    sqlalchemy_installed = True
+    Session = Callable[..., Generator[Session, Any, None]]
+
+CALLABLE = Callable[..., Model]
+CALLABLE_LIST = Callable[..., List[Model]]
+
+
+class SQLAlchemyCRUDRouter(CRUDGenerator[SCHEMA]):
+    def __init__(
+        self,
+        schema: Type[SCHEMA],
+        db_model: Model,
+        db: "Session",
+        create_schema: Optional[Type[SCHEMA]] = None,
+        update_schema: Optional[Type[SCHEMA]] = None,
+        prefix: Optional[str] = None,
+        tags: Optional[List[str]] = None,
+        paginate: Optional[int] = None,
+        get_all_route: Union[bool, DEPENDENCIES] = True,
+        get_one_route: Union[bool, DEPENDENCIES] = True,
+        create_route: Union[bool, DEPENDENCIES] = True,
+        update_route: Union[bool, DEPENDENCIES] = True,
+        delete_one_route: Union[bool, DEPENDENCIES] = True,
+        delete_all_route: Union[bool, DEPENDENCIES] = True,
+        use_async: Optional[bool] = None,  # if not set, try autodetect
+        **kwargs: Any
+    ) -> None:
+        assert (
+            sqlalchemy_installed
+        ), "SQLAlchemy must be installed to use the SQLAlchemyCRUDRouter."
+
+        self.db_model = db_model
+        self.db_func = db
+        if use_async is None:
+            self.use_async = (
+                inspect.isasyncgenfunction(db) or inspect.isasyncgen(db)
+            ) and sqlalchemy_version >= "1.4"  # autodetect async mode
+        else:
+            self.use_async = use_async
+        self._pk: str = db_model.__table__.primary_key.columns.keys()[0]
+        self._pk_type: type = _utils.get_pk_type(schema, self._pk)
+
+        super().__init__(
+            schema=schema,
+            create_schema=create_schema,
+            update_schema=update_schema,
+            prefix=prefix or db_model.__tablename__,
+            tags=tags,
+            paginate=paginate,
+            get_all_route=get_all_route,
+            get_one_route=get_one_route,
+            create_route=create_route,
+            update_route=update_route,
+            delete_one_route=delete_one_route,
+            delete_all_route=delete_all_route,
+            **kwargs
+        )
+
+    def _get_all(self, *args: Any, **kwargs: Any) -> CALLABLE_LIST:
+        def route(
+            db: Session = Depends(self.db_func),
+            pagination: PAGINATION = self.pagination,
+        ) -> List[Model]:
+            skip, limit = pagination.get("skip"), pagination.get("limit")
+
+            db_models: List[Model] = (
+                db.query(self.db_model)
+                .order_by(getattr(self.db_model, self._pk))
+                .limit(limit)
+                .offset(skip)
+                .all()
+            )
+            return db_models
+
+        async def async_route(
+            db: Session = Depends(self.db_func),
+            pagination: PAGINATION = self.pagination,
+        ) -> List[Model]:
+            skip, limit = pagination.get("skip"), pagination.get("limit")
+
+            res = await db.execute(
+                select(self.db_model)
+                .order_by(getattr(self.db_model, self._pk))
+                .limit(limit)
+                .offset(skip)
+            )
+            res = res.all()
+
+            model: Model
+            db_models: List[Model] = []
+            for row in res:
+                (model,) = row
+                db_models.append(model)
+
+            return db_models
+
+        if self.use_async:
+            return async_route
+        else:
+            return route
+
+    def _get_one(self, *args: Any, **kwargs: Any) -> CALLABLE:
+        def route(
+            item_id: self._pk_type, db: Session = Depends(self.db_func)  # type: ignore
+        ) -> Model:
+            model: Model = db.query(self.db_model).get(item_id)
+
+            if model:
+                return model
+            else:
+                raise NOT_FOUND from None
+
+        async def async_route(
+            item_id: self._pk_type, db: Session = Depends(self.db_func)  # type: ignore
+        ) -> Model:
+            model: Model
+            try:
+                (model,) = (
+                    await db.execute(
+                        select(self.db_model).where(self.db_model.id == item_id)
+                    )
+                ).one()
+            except NoResultFound:
+                model = None
+
+            if model:
+                return model
+            else:
+                raise NOT_FOUND from None
+
+        if self.use_async:
+            return async_route
+        else:
+            return route
+
+    def _create(self, *args: Any, **kwargs: Any) -> CALLABLE:
+        def route(
+            model: self.create_schema,  # type: ignore
+            db: Session = Depends(self.db_func),
+        ) -> Model:
+            try:
+                db_model: Model = self.db_model(**model.dict())
+                db.add(db_model)
+                db.commit()
+                db.refresh(db_model)
+                return db_model
+            except IntegrityError:
+                db.rollback()
+                raise HTTPException(422, "Key already exists") from None
+
+        async def async_route(
+            model: self.create_schema,  # type: ignore
+            db: Session = Depends(self.db_func),
+        ) -> Model:
+            try:
+                db_model: Model = self.db_model(**model.dict())
+                db.add(db_model)
+                await db.commit()
+                await db.refresh(db_model)
+                return db_model
+            except IntegrityError:
+                await db.rollback()
+                raise HTTPException(422, "Key already exists") from None
+
+        if self.use_async:
+            return async_route
+        else:
+            return route
+
+    def _update(self, *args: Any, **kwargs: Any) -> CALLABLE:
+        def route(
+            item_id: self._pk_type,  # type: ignore
+            model: self.update_schema,  # type: ignore
+            db: Session = Depends(self.db_func),
+        ) -> Model:
+            try:
+                db_model: Model = self._get_one()(item_id, db)
+
+                for key, value in model.dict(exclude={self._pk}).items():
+                    if hasattr(db_model, key):
+                        setattr(db_model, key, value)
+
+                db.commit()
+                db.refresh(db_model)
+
+                return db_model
+            except IntegrityError as e:
+                db.rollback()
+                self._raise(e)
+
+        async def async_route(
+            item_id: self._pk_type,  # type: ignore
+            model: self.update_schema,  # type: ignore
+            db: Session = Depends(self.db_func),
+        ) -> Model:
+            try:
+                db_model: Model = await self._get_one()(item_id, db)
+
+                for key, value in model.dict(exclude={self._pk}).items():
+                    if hasattr(db_model, key):
+                        setattr(db_model, key, value)
+
+                await db.commit()
+                await db.refresh(db_model)
+
+                return db_model
+            except IntegrityError as e:
+                await db.rollback()
+                self._raise(e)
+
+        if self.use_async:
+            return async_route
+        else:
+            return route
+
+    def _delete_all(self, *args: Any, **kwargs: Any) -> CALLABLE_LIST:
+        def route(db: Session = Depends(self.db_func)) -> List[Model]:
+            db.query(self.db_model).delete()
+            db.commit()
+
+            return self._get_all()(db=db, pagination={"skip": 0, "limit": None})
+
+        async def async_route(db: Session = Depends(self.db_func)) -> List[Model]:
+            await db.execute("delete from " + self.db_model.__tablename__)
+            await db.commit()
+            return await self._get_all()(db=db, pagination={"skip": 0, "limit": None})
+
+        if self.use_async:
+            return async_route
+        else:
+            return route
+
+    def _delete_one(self, *args: Any, **kwargs: Any) -> CALLABLE:
+        def route(
+            item_id: self._pk_type, db: Session = Depends(self.db_func)  # type: ignore
+        ) -> Model:
+            db_model: Model = self._get_one()(item_id, db)
+            db.delete(db_model)
+            db.commit()
+
+            return db_model
+
+        async def async_route(
+            item_id: self._pk_type, db: Session = Depends(self.db_func)  # type: ignore
+        ) -> Model:
+            db_model: Model = await self._get_one()(item_id, db)
+            await db.delete(db_model)
+            await db.commit()
+
+            return db_model
+
+        if self.use_async:
+            return async_route
+        else:
+            return route
