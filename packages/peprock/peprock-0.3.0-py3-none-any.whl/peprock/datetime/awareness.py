299eb782a114f2aeacc8028d58e92d874ee7af83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:16 +0000

    Add package peprock/0.3.0/peprock-0.3.0-py3-none-any.whl

---
+++ packages/peprock/peprock-0.3.0-py3-none-any.whl/peprock/datetime/awareness.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import datetime  # noqa: TCH003
+import functools
+
+
+# noinspection PyTypeChecker
+@functools.singledispatch
+def is_naive(arg: datetime.date | datetime.time | datetime.datetime, /) -> bool:
+    # See https://docs.python.org/3/library/datetime.html#determining-if-an-object-is-aware-or-naive
+
+    msg: str = (
+        f"expected datetime.date | datetime.time | datetime.datetime, got {arg!r}"
+    )
+    raise TypeError(msg)
+
+
+@is_naive.register
+def _(_: datetime.date, /) -> bool:
+    return True
+
+
+@is_naive.register
+def _(d: datetime.datetime, /) -> bool:
+    return d.tzinfo is None or d.tzinfo.utcoffset(d) is None
+
+
+@is_naive.register
+def _(t: datetime.time, /) -> bool:
+    return t.tzinfo is None or t.tzinfo.utcoffset(None) is None
+
+
+def is_aware(arg: datetime.date | datetime.time | datetime.datetime, /) -> bool:
+    return not is_naive(arg)
+
+
+class EnsureAwareError(ValueError):
+    def __init__(
+        self: EnsureAwareError,
+        arg: datetime.datetime,
+        /,
+        *,
+        assumed_tz: datetime.tzinfo | None = None,
+        target_tz: datetime.tzinfo | None = None,
+    ) -> None:
+        super().__init__(
+            f"Unable to ensure awareness of '{arg!r}' using "
+            f"'{assumed_tz=}' and '{target_tz=}'",
+        )
+
+
+def ensure_aware(
+    arg: datetime.datetime,
+    /,
+    *,
+    assumed_tz: datetime.tzinfo | None = None,
+    target_tz: datetime.tzinfo | None = None,
+) -> datetime.datetime:
+    if arg.tzinfo is None:
+        if assumed_tz is None:
+            raise EnsureAwareError(
+                arg,
+                assumed_tz=assumed_tz,
+                target_tz=target_tz,
+            )
+
+        arg = arg.replace(tzinfo=assumed_tz)
+
+    if target_tz:
+        return arg.astimezone(tz=target_tz)
+
+    return arg
+
+
+__all__ = [
+    "is_naive",
+    "is_aware",
+    "EnsureAwareError",
+    "ensure_aware",
+]
