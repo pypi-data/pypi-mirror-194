ce4642991670e043038ca34bfcb4fc5f69ef5590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:45 +0000

    Add package mkdocs-link-preview-plugin/0.1.3/mkdocs-link-preview-plugin-0.1.3.tar.gz

---
+++ packages/mkdocs-link-preview-plugin/mkdocs-link-preview-plugin-0.1.3.tar.gz/mkdocs-link-preview-plugin-0.1.3/mkdocs_link_preview_plugin/plugin.py
@@ -0,0 +1,17 @@
+from mkdocs.plugins import BasePlugin
+from django.shortcuts import render
+
+"""
+A mkdocs plugin that support conversion from 
+'obsidian syntax' to 'mkdocs-material syntax'
+"""
+
+
+class LinkPreviewPlugin(BasePlugin):
+
+    def on_page_markdown(self, markdown, page, config, files):
+        ## apply conversions
+
+        return render(None, 'polls/index.html', None)
+
+
