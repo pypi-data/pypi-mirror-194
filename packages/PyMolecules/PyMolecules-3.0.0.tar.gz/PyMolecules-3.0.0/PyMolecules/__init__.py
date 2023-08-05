1e39338c0b82a2b298a5edb7256495cbf6c0b637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:35 +0000

    Add package PyMolecules/3.0.0/PyMolecules-3.0.0.tar.gz

---
+++ packages/PyMolecules/PyMolecules-3.0.0.tar.gz/PyMolecules-3.0.0/PyMolecules/__init__.py
@@ -0,0 +1,6 @@
+from PyMolecules import SingleLinkedList, DoubleLinkedList,SingleCircularLinkedList, DoubleCircularLinkedList
+
+
+__author__ = "Yokesh Bollineni"
+__version__ = "3.0.0"
+__what_is_new__ = "New class DCLL added, Bug fix in sort() of DLL class"
---
+++ packages/PyMolecules/PyMolecules-3.0.0.tar.gz/PyMolecules-3.0.0/README.txt
@@ -0,0 +1,2 @@
+PyMolecules package has 4 classes that can be used as alternative to arrays
+PyMolecules are implemented using linkedlist classes and their most common methods like insertion, deletion, searching, sorting etc
