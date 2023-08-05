ffe660212c25ec34d0e420227692941287d1726b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:12 +0000

    Add package dflogger/0.1.3/dflogger-0.1.3.tar.gz

---
+++ packages/dflogger/dflogger-0.1.3.tar.gz/dflogger-0.1.3/README.md
@@ -0,0 +1,7 @@
+# dflogger
+
+When you feel like any existing experiment tracker tools are overpowered, then dflogger is what you're looking for
+
+## Under construction 👷‍♂️🚧
+
+
---
+++ packages/dflogger/dflogger-0.1.3.tar.gz/dflogger-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dflogger',
+    packages=['dflogger'],
+    version='0.1.3',
+    description='small and dataframe based machine learning experiment logger',
+    author='Sulthan Abiyyu',
+    license='MIT',
+)
