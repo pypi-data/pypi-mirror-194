cc63ca357b52a8ccbc97a9fdd455bb088d0def56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:47 +0000

    Add package diffusionjax/0.0.2/diffusionjax-0.0.2.tar.gz

---
+++ packages/diffusionjax/diffusionjax-0.0.2.tar.gz/diffusionjax-0.0.2/diffusionjax/sde.py
@@ -0,0 +1,140 @@
+"""SDE class."""
+import abc
+from functools import partial
+import jax.numpy as jnp
+from jax import jit
+from diffusionjax.utils import batch_mul
+
+
+class SDE(abc.ABC):
+    """SDE abstract class. Functions are designed for a mini-batch of inputs."""
+
+    def __init__(self, n_steps):
+        """Construct an SDE.
+        Args:
+            n_steps: number of discretization time steps.
+        """
+        super().__init__()
+        self.n_steps = n_steps
+        self.train_ts = jnp.linspace(0, 1, self.n_steps + 1)[:-1].reshape(-1, 1)
+
+    @abc.abstractmethod
+    def sde(self, x, t):
+        r"""Return the drift and diffusion coefficients of the SDE.
+
+        Args:
+            x: a JAX tensor of the state
+            t: JAX float of the time
+
+        Returns:
+            drift: drift function of the forward SDE
+            diffusion: dispersion function of the forward SDE
+        """
+
+    @abc.abstractmethod
+    def marginal_prob(self, x, t):
+        r"""Parameters to determine the marginal distribution of the SDE,
+
+        .. math::
+            p_t(x)
+
+        Args:
+            x: a JAX tensor of the state
+            t: JAX float of the time
+        """
+
+    def discretize(self, x, t):
+        r"""Discretize the SDE in the form,
+
+        .. math::
+            x_{i+1} = x_{i} + f_i(x_i) + G_i z_i
+
+        Useful for diffusion sampling and probability flow sampling.
+        Defaults to Euler-Maryama discretization.
+
+        Args:
+            x: a JAX tensor of the state
+            t: a JAX float of the time step
+
+        Returns:
+            f, G
+        """
+        dt = 1. / self.n_steps
+        drift, diffusion = self.sde(x, t)
+        f = drift * dt
+        G = diffusion * jnp.sqrt(dt)
+        return f, G
+
+
+class OU(SDE):
+    """Time rescaled Ohrnstein Uhlenbeck (OU) SDE."""
+    def __init__(self, beta_min=0.001, beta_max=3, n_steps=1000):
+        super().__init__(n_steps)
+        self.beta_min = beta_min
+        self.beta_max = beta_max
+
+    def sde(self, x, t):
+        beta_t = self.beta_min + t * (self.beta_max - self.beta_min)
+        drift = -0.5 * batch_mul(beta_t, x)
+        diffusion = jnp.sqrt(beta_t)
+        return drift, diffusion
+
+    def log_mean_coeff(self, t):
+        return -0.5 * t * self.beta_min - 0.25 * t**2 * (self.beta_max - self.beta_min)
+
+    def mean_coeff(self, t):
+        return jnp.exp(self.log_mean_coeff(t))
+
+    def variance(self, t):
+        return 1.0 - jnp.exp(2 * self.log_mean_coeff(t))
+
+    def marginal_prob(self, x, t):
+        m = self.mean_coeff(t)
+        try:
+            mean = batch_mul(m, x)
+        except:
+            mean = m * x
+        std = jnp.sqrt(self.variance(t))
+        return mean, std
+
+    def reverse(self, score_fn):
+        """Create the reverse-time SDE/ODE
+
+        Args:
+            score_fn: A time-dependent score-based model that takes x and t and returns the score.
+        """
+        train_ts = self.train_ts
+        sde_fn = self.sde
+        discretize_fn = self.discretize
+
+        class RSDE(self.__class__):
+
+            def __init__(self):
+                self.train_ts = train_ts
+
+            def sde(self, x, t):
+                drift, diffusion = sde_fn(x, t)
+                score = score_fn(x, t)
+                drift = drift - diffusion**2 * score
+                return drift, diffusion
+
+            def discretize(self, x, t):
+                r"""Discretize the SDE in the form,
+                .. math::
+                    x_{i+1} = x_{i} + f_i(x_i) + G_i z_i
+
+                Useful for reverse diffusion sampling.
+                Defaults to Euler-Maryama discretization.
+
+                Args:
+                    x: a JAX tensor
+                    t: a JAX float representing the time step
+
+                Returns:
+                f, G
+                """
+                f, G = discretize_fn(x, t)
+                rev_f = -f + batch_mul(G**2, score_fn(x, t))
+                return rev_f, G
+
+        return RSDE()
---
+++ packages/diffusionjax/diffusionjax-0.0.2.tar.gz/diffusionjax-0.0.2/examples/example1.py
@@ -0,0 +1,209 @@
+"""Score based generative models introduction.
+
+An example using 1 dimensional image data.
+"""
+from jax import jit, vmap, grad
+import jax.random as random
+import jax.numpy as jnp
+from jax.scipy.special import logsumexp
+import matplotlib.pyplot as plt
+from diffusionjax.plot import (
+    plot_score, plot_heatmap, plot_animation)
+from diffusionjax.losses import get_loss_fn
+from diffusionjax.samplers import EulerMaruyama
+from diffusionjax.utils import (
+    MLP,
+    CNN,
+    get_score_fn,
+    update_step,
+    optimizer,
+    retrain_nn)
+from diffusionjax.sde import OU
+from mlkernels import Matern52
+import numpy as np
+import lab as B
+from functools import partial
+
+
+x_max = 5.0
+epsilon = 1e-4
+
+
+def sample_image_rgb(rng, num_samples, image_size, kernel, num_channels=1):
+    """Samples from a GMRF
+    """
+    x = np.linspace(-x_max, x_max, image_size)
+    x = x.reshape(image_size, 1)
+    C = B.dense(kernel(x))  + epsilon * B.eye(image_size)
+    x = random.multivariate_normal(rng, mean=jnp.zeros(x.shape[0]), cov=C, shape=(num_samples, num_channels))
+    x = x.transpose((0, 2, 1))
+    return x, C
+
+
+def plot_score_ax_sample(ax, sample, score, t, area_min=-1, area_max=1, fname="plot_score"):
+    @partial(jit, static_argnums=[0,])
+    def helper(score, sample, t, area_min, area_max):
+        x = jnp.linspace(area_min, area_max, 16)
+        x, y = jnp.meshgrid(x, x)
+        grid = jnp.stack([x.flatten(), y.flatten()], axis=1)
+        sample = jnp.tile(sample, (len(x.flatten()), 1, 1, 1))
+        sample.at[:, [0, 1], 0, 0].set(grid)
+        t = jnp.ones((grid.shape[0],)) * t
+        scores = score(sample, t)
+        return grid, scores
+    grid, scores = helper(score, sample, t, area_min, area_max)
+    ax.quiver(grid[:, 0], grid[:, 1], scores[:, 0, 0, 0], scores[:, 1, 0, 0])
+
+
+def plot_samples_1D(samples, image_size, fname="samples 1D.png"):
+    x = np.linspace(-x_max, x_max, image_size)
+    plt.plot(x, samples[:, :, 0].T)
+    plt.savefig(fname)
+    plt.close()
+
+
+def main():
+    num_epochs = 2000
+    rng = random.PRNGKey(2023)
+    rng, step_rng = random.split(rng, 2)
+    num_samples = 576
+    num_channels = 1
+    image_size = 64  # image size
+
+    samples, C = sample_image_rgb(rng, num_samples=num_samples, image_size=image_size, kernel=Matern52(), num_channels=num_channels)  # (num_samples, image_size, num_channels)
+    # Reshape image data
+    samples = samples.reshape(-1, image_size, num_channels)
+    plot_samples_1D(samples, image_size, "samples")
+
+    # Get sde model
+    sde = OU()
+
+    def log_hat_pt_tmp(x, t):
+        """
+        Empirical distribution score.
+
+        Args:
+            x: One location in $\mathbb{R}^2$
+            t: time
+        Returns:
+            The empirical log density, as described in the Jupyter notebook
+            .. math::
+                \hat{p}_{t}(x)
+        """
+        mean, std = sde.marginal_prob(samples[:, [0, 1], 0], t)
+        potentials = jnp.sum(-(x - mean)**2 / (2 * std**2), axis=1)
+        return logsumexp(potentials, axis=0, b=1/num_samples)
+
+    def log_hat_pt(x, t):
+        """
+        Empirical distribution score.
+
+        Args:
+            x: One location in $\mathbb{R}^2$
+            t: time
+        Returns:
+            The empirical log density, as described in the Jupyter notebook
+            .. math::
+                \hat{p}_{t}(x)
+        """
+        mean, std = sde.marginal_prob(samples, t)
+        losses = -(x - mean)**2 / (2 * std**2)
+        # Needs to be reshaped, since x is an image
+        potentials = jnp.sum(losses.reshape((losses.shape[0], -1)), axis=-1)
+        return logsumexp(potentials, axis=0, b=1/num_samples)
+
+    def nabla_log_pt(x, t):
+        """
+        Args:
+            x: One location in $\mathbb{R}^2$
+            t: time
+        Returns:
+            The true log density.
+            .. math::
+                p_{t}(x)
+        """
+        x_shape = x.shape
+        v_t = sde.variance(t)
+        m_t = sde.mean_coeff(t)
+        x = x.flatten()
+        score = - jnp.linalg.solve(m_t**2 * C + v_t * jnp.eye(x_shape[0]), x)
+        return score.reshape(x_shape)
+
+    # Get a jax grad function, which can be batched with vmap
+    nabla_log_hat_pt_tmp = jit(vmap(grad(log_hat_pt_tmp), in_axes=(0, 0), out_axes=(0)))
+    nabla_log_hat_pt = jit(vmap(grad(log_hat_pt), in_axes=(0, 0), out_axes=(0)))
+    nabla_log_pt = jit(vmap(nabla_log_pt, in_axes=(0, 0), out_axes=(0)))
+
+    # Running the reverse SDE with the empirical score
+    plot_score(score=nabla_log_hat_pt_tmp, t=0.01, area_min=-3, area_max=3, fname="empirical score")
+    sampler = EulerMaruyama(sde, nabla_log_hat_pt).get_sampler()
+    q_samples = sampler(rng, n_samples=64, shape=(image_size, num_channels))
+    plot_samples_1D(q_samples, image_size=image_size, fname="samples empirical score")
+    plot_heatmap(samples=q_samples[:, [0, 1], 0], area_min=-3, area_max=3, fname="heatmap empirical score")
+
+    # What happens when I perturb the score with a constant?
+    perturbed_score = lambda x, t: nabla_log_hat_pt(x, t) + 10.0 * jnp.ones(jnp.shape(x))
+    rng, step_rng = random.split(rng)
+    sampler = EulerMaruyama(sde, perturbed_score).get_sampler()
+    q_samples = sampler(rng, n_samples=64, shape=(image_size, num_channels))
+    plot_samples_1D(q_samples, image_size=image_size, fname="samples bounded perturbation")
+    plot_heatmap(samples=q_samples[:, [0, 1], 0], area_min=-3, area_max=3, fname="heatmap bounded perturbation")
+
+    # Running the reverse SDE with the true score
+    sampler = EulerMaruyama(sde, nabla_log_pt).get_sampler()
+    q_samples = sampler(rng, n_samples=64, shape=(image_size, num_channels))
+    plot_samples_1D(q_samples, image_size=image_size, fname="samples true score")
+    plot_heatmap(samples=q_samples[:, [0, 1], 0], area_min=-3, area_max=3, fname="heatmap true score")
+
+    # What happens when I perturb the score with a constant?
+    perturbed_score = lambda x, t: nabla_log_pt(x, t) + 10.0 * jnp.ones(jnp.shape(x))
+    rng, step_rng = random.split(rng)
+    sampler = EulerMaruyama(sde, perturbed_score).get_sampler()
+    q_samples = sampler(rng, n_samples=64, shape=(image_size, num_channels))
+    plot_samples_1D(q_samples, image_size=image_size, fname="samples true bounded perturbation")
+    plot_heatmap(samples=q_samples[:, [0, 1], 0], area_min=-3, area_max=3, fname="heatmap true bounded perturbation")
+
+    # Neural network training via score matching
+    batch_size = 16
+    score_model = MLP()  # CNN() is the natural choice
+    # Initialize parameters
+    params = score_model.init(step_rng, jnp.zeros((batch_size, image_size, num_channels)), jnp.ones((batch_size,)))
+    # Initialize optimizer
+    opt_state = optimizer.init(params)
+    # Get loss function
+    loss = get_loss_fn(
+        sde, score_model, score_scaling=True, likelihood_weighting=False,
+        reduce_mean=True, pointwise_t=False)
+    # Train with score matching
+    score_model, params, opt_state, mean_losses = retrain_nn(
+        update_step=update_step,
+        num_epochs=num_epochs,
+        step_rng=step_rng,
+        samples=samples,
+        score_model=score_model,
+        params=params,
+        opt_state=opt_state,
+        loss_fn=loss,
+        batch_size=batch_size)
+    # Get trained score
+    trained_score = get_score_fn(sde, score_model, params, score_scaling=True)
+    sampler = EulerMaruyama(sde, trained_score).get_sampler(stack_samples=False)
+    q_samples = sampler(rng, n_samples=64, shape=(image_size, num_channels))
+    rng, step_rng = random.split(rng, 2)
+    q_samples = sampler(rng, n_samples=64, shape=(image_size, num_channels))
+    plot_samples_1D(q_samples, image_size=image_size, fname="samples trained score")
+    plot_heatmap(samples=q_samples[:, [0, 1], 0], area_min=-3, area_max=3, fname="heatmap trained score")
+
+    frames = 100
+    fig, ax = plt.subplots()
+    def animate(i, ax):
+        ax.clear()
+        plot_score_ax_sample(
+            ax, q_samples[0], trained_score, t=1 - (i / frames), area_min=-5, area_max=5, fname="trained score")
+    # Plot animation of the trained score over time
+    plot_animation(fig, ax, animate, frames, "trained_score")
+
+
+if __name__ == "__main__":
+    main()
+
