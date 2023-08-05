1cbb3ebab6b9d4f4c990b876000a8a75129b365a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:39 +0000

    Add package botgus/4.0/botgus-4.0.tar.gz

---
+++ packages/botgus/botgus-4.0.tar.gz/botgus-4.0/README.txt
@@ -0,0 +1,1 @@
+www.botgus.com
---
+++ packages/botgus/botgus-4.0.tar.gz/botgus-4.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+long_description = '''
+INDICADORES DE BOT GUS
+\n
+FUNCION PARA OBTENER DATOS HISTORICOS DE BINANCE:
+\n
+MAS INFORMACION DE USO EN WWW.BOTGUS.COM
+https://github.com/jrchico/
+'''
+
+setup(name='botgus',
+      version='4.0',
+      py_modules=['botgus'],
+      description='BOTGUS',
+      long_description=long_description,
+      )
