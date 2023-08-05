925d52f868d00f93bfd25c39425fa78613bfe8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:20 +0000

    Add package wtday/0.1.2/wtday-0.1.2.tar.gz

---
+++ packages/wtday/wtday-0.1.2.tar.gz/wtday-0.1.2/README.md
@@ -0,0 +1,23 @@
+<h1 align="center">wtday
+</h1>
+<p align="center"><img src="https://img.shields.io/badge/Python-blue?style=for-the-badge&logo=python&logoColor=FFD43B"></p>
+
+A command-line tool to find out what's celebrated today in seconds
+
+### Getting Started
+
+Install it
+
+```sh
+pip install wtday
+```
+
+Then use it
+
+```sh
+wtday
+```
+
+You should get something like:
+
+![image](https://user-images.githubusercontent.com/65634520/218370196-8d183ec6-a2a6-436d-a319-ae1beb90045a.png)
---
+++ packages/wtday/wtday-0.1.2.tar.gz/wtday-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wtday',
+    version='0.1.2',
+    description='A CLI app that shows you what is celebrated today',
+    entry_points={
+        'console_scripts': [
+            'wtday = main:main'
+        ]
+    },
+    packages=find_packages(),
+    install_requires=[
+        'urllib3',
+        'beautifulsoup4',
+        'keyboard',
+        'colorama',
+        'pywin32'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
