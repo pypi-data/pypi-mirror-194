93f391752f10deec56ae7988f59cbaa30edd4bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package django-sweet-utils/1.2.4/django-sweet-utils-1.2.4.tar.gz

---
+++ packages/django-sweet-utils/django-sweet-utils-1.2.4.tar.gz/django-sweet-utils-1.2.4/django_sweet_utils/db/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class FakeDeletionQuerySet(models.query.QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
+
+
+class Manager(models.Manager):
+    """ Base manager with the following additions:
+        - 'existing()' method that filters queryset by 'is_deleted=False'
+    """
+    def get_queryset(self):
+        return FakeDeletionQuerySet(self.model, using=self._db)
+
+    def existing(self):
+        return self.filter(is_deleted=False)
+
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+
+class Model(models.Model):
+    """ Base model with the following additions:
+        - uuid4 as object id;
+        - created_at as object creation time;
+        - updated_at as object last update time;
+        - is_deleted as indicator that object is deleted or not;
+     """
+
+    id = models.UUIDField(_('ID'), default=uuid.uuid4, primary_key=True, editable=False)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True, editable=False)
+    is_deleted = models.BooleanField(_('is deleted'), default=False)
+
+    objects = Manager()
+
+    class Meta:
+        abstract = True
+
