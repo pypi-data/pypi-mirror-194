87e684e93925394717bcfc2df644c20c989075dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package v/0.3.0/v-0.3.0.tar.gz

---
+++ packages/v/v-0.3.0.tar.gz/v-0.3.0/v/file.py
@@ -0,0 +1,23 @@
+import os, shutil
+
+def ls(path):
+	"""List all files in a folder"""
+	try:
+		return os.listdir(path)
+	except Exception as e:
+		return e
+
+def create(file):
+	"""Create the specified folder"""
+	try:
+		return open(file, "x")
+	except Exception as e:
+		return e
+
+def read(file):
+	"""Get the contents of a file"""
+	try:
+		with open(file):
+			return file.read()
+	except Exception as e:
+		return e
