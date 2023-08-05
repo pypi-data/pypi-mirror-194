c8adb0b70769f9f08088253d4c1d17671cc6d0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:23 +0000

    Add package marktplaats/0.1.0/marktplaats-0.1.0-py3-none-any.whl

---
+++ packages/marktplaats/marktplaats-0.1.0-py3-none-any.whl/marktplaats/models/listing.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from marktplaats.models import ListingSeller
+from marktplaats.models.listing_location import ListingLocation
+
+
+@dataclass
+class Listing:
+    id: str
+    title: str
+    description: str
+    seller: ListingSeller
+    location: ListingLocation
+    price: float
+    link: str
+    images: list
+    category_id: int
+    attributes: list
