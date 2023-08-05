088f826680a11a9efea9f1782388c8905df59527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:16 +0000

    Add package x21/0.4.9/x21-0.4.9-cp311-cp311-win32.whl

---
+++ packages/x21/x21-0.4.9-cp311-cp311-win32.whl/x21/__init__.py
@@ -0,0 +1,17 @@
+# backwards compat
+from _x21 import __dex_23c__
+
+from ._main import (
+    __dex_22b__,
+    __dex_22c__,
+    __dex_23a__,
+    __dex_23b__,
+)
+
+__all__ = [
+    "__dex_22b__",
+    "__dex_22c__",
+    "__dex_23a__",
+    "__dex_23b__",
+    "__dex_23c__",
+]
