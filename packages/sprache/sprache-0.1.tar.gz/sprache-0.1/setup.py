143c58fb3b8750720d416b23dbd4ad86b1fe1a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:56 +0000

    Add package sprache/0.1/sprache-0.1.tar.gz

---
+++ packages/sprache/sprache-0.1.tar.gz/sprache-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sprache',
+    version='0.1',
+    description='NLP for India',
+    license='MIT',
+    author="Aditya Mandke",
+    author_email='ekdnam@gmail.com',
+    url='https://github.com/ekdnam/sprache',
+    python_requires='>=3',
+    keywords='example project',
+    install_requires=[
+          'scikit-learn',
+      ]
+)
