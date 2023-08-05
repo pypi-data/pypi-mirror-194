3728886f22b3780e921c97b087c518080939fbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package wadl-planner/1.1.0/wadl_planner-1.1.0-py3-none-any.whl

---
+++ packages/wadl-planner/wadl_planner-1.1.0-py3-none-any.whl/wadl/lib/fence.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3
+# io
+from pathlib import Path
+import csv
+import logging
+# math
+import numpy as np
+# gis
+import utm
+from shapely.geometry import Polygon
+from fastkml import kml, geometry
+
+
+class Fence(object):
+    """Holds the gps cords of the boundary of the area given by a cvs file
+
+    Args:
+        file (str)
+
+    """
+
+    def __init__(self, file):
+        self.file = Path(file)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        # get name of area
+        self.name = self.file.stem
+        # parse file
+        self.parseFile(file)
+        # build polygon
+        self.poly = Polygon(self.UTMCords)
+        # find bounding box
+        minx, miny, maxx, maxy = self.poly.bounds
+        self.logger.info(f"extends in meters {maxx - minx} by {maxy - miny}")
+
+    def parseFile(self, file):
+        # parse file as CSV
+        self.logger.info(f"Reading coordinate file {file}")
+        # stores the gps cords, utm cords, and utm zones
+        if file.suffix == "csv":
+            CSVfile = file
+        else:
+            CSVfile = file.with_suffix(".csv")
+        with open(CSVfile, 'r') as csvfile:
+            data = [(line[1], line[2])
+                    for line in csv.reader(csvfile, delimiter=',')]
+        # toss 1st line and convert to float
+        GPSData = [list(map(float, line)) for line in data[1:]]
+        # convert to utm
+        UTMData = [utm.from_latlon(cords[0], cords[1]) for cords in GPSData]
+        # store coridinate information
+        self.UTMZone = UTMData[0][2:]
+        # print(self.UTMZone)
+        self.UTMCords = np.array([[data[0], data[1]] for data in UTMData])
+        self.GPSCords = np.array(GPSData)
+
+    def plot(self, ax, color='m'):
+        """Plots the geofence in UTM
+
+        Args:
+            ax (pyplot.axis): axis object from pyplot you want to draw on
+            color ('str'): color string for the geofence.
+                default 'm' (magenta).
+
+        """
+        # plots are always in utm
+        ax.plot(*self.poly.exterior.xy, color=color)
+        # place label somewhere
+        minx, miny, maxx, maxy = self.poly.bounds
+        placement = ((minx+maxx)/2, maxy)
+        # ax.annotate(self.name, xy=placement)
+
+
+class Areas(object):
+    """Holds the gps cords of the set of areas given by a kml file
+
+    Args:
+        file (str): (lat,  lng)
+
+    Returns
+        Area: Container for a set of geofence objects
+
+    """
+
+    def __init__(self, file):
+        self.file = file
+        self.zones = []
+        self.areas = []
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        self.name = self.file.stem
+        self.parseFile(file)
+
+    def __repr__(self):
+        return self.areas.__repr__()
+
+    def __iter__(self):
+        return iter(self.areas)
+
+    def __len__(self):
+        return len(self.areas)
+
+    def parseFile(self, file):
+        with open(file, 'rt',) as f:
+            doc = f.read()
+        k = kml.KML()
+        k.from_string(doc.encode('utf-8'))
+        self.findPlacemarks(list(k.features()))
+        self.logger.info(f"{len(self.areas)} polygons in {self.file.stem}")
+
+    def findPlacemarks(self, features):
+        # parse features throughout the KML File
+        for feature in features:
+            if isinstance(feature, kml.Placemark):  # when there is no folder
+                self.findGeometries(feature)
+        for feature in features:
+            if isinstance(feature, kml.Folder):
+                self.findPlacemarks(list(feature.features()))
+            if isinstance(feature, kml.Document):
+                self.findPlacemarks(list(feature.features()))
+
+    # parse geometry
+    def findGeometries(self, placemark):
+        if hasattr(placemark, "geometry"):
+            if isinstance(placemark.geometry, geometry.LineString):
+                self.logger.info("ignoring LineString")
+            if isinstance(placemark.geometry, geometry.LinearRing):
+                # print("found LinearRing")
+                self.getUTMpoly(placemark.geometry)
+            if isinstance(placemark.geometry, geometry.MultiPolygon):
+                # print("found MultiPolygon")
+                for poly in placemark.geometry:
+                    self.getUTMpoly(poly.exterior)
+            else:
+                print(placemark.geometry, " object has no handler")
+
+    def getUTMpoly(self, ring):
+        UTM = [utm.from_latlon(cords[1], cords[0])
+               for cords in ring.coords]
+        # store coordinate information
+        zone = UTM[0][2:]
+        # print(self.UTMZone)
+        coords = np.array([[data[0], data[1]] for data in UTM])
+        self.zones.append(zone)
+        self.areas.append(Polygon(coords))
+
+    def toCSV(self):
+        for i, (zone, area) in enumerate(zip(self.zones, self.areas)):
+            cords = [utm.to_latlon(c[0], c[1], *zone)
+                     for c in area.exterior.coords]
+            outname = self.file.parent / Path(self.name+f"_{i}.csv")
+            with open(outname, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerow(["FID", "lat", "lon"])
+                for i, cord in enumerate(cords):
+                    writer.writerow([i]+list(cord))
+
+    def plot(self, ax, color='k'):
+        """Plots the geofence in UTM
+
+        Args:
+            ax (pyplot.axis): axis object from pyplot you want to draw on
+
+        """
+        for area in self.areas:
+            ax.plot(*area.exterior.xy, color=color)
---
+++ packages/wadl-planner/wadl_planner-1.1.0-py3-none-any.whl/wadl/lib/route.py
@@ -0,0 +1,326 @@
+#!bin/bash/python3
+# import warnings as warn
+import csv
+import logging
+# gis
+import utm
+# math
+import numpy as np
+# lib
+from wadl.lib.parameters import Parameters
+
+
+class RouteParameters(Parameters):
+    """Parameter container for setting route parameters
+
+    Set parameters directly like how you would for a dictionary.
+    ``routeParameters = RouteParameters``
+    ``routeParameters["Parameter"] = value``
+
+    Args:
+        limit (float): route limit in seconds
+        speed (float): route speed over coverage area in meters/seconds
+        altitude (float): altitude above ground level of the coverage area in m
+        xfer_speed (float): speed for transfer segments in m/s
+        xfer_altitude (float): altitude for transfer segments in m
+        xfer_ascend (float): ascend rate in m/s
+        xfer_decend (float): descend rate in m/s
+        land_altitude (float): altitude before landing
+
+    """
+
+    def __init__(self, default=True):
+        super(RouteParameters, self).__init__(default)
+
+    def setDefaults(self):
+        self["limit"] = 13*60  # s
+        self["speed"] = 4.0  # m/s
+        self["prio_speed"] = 3.0  # m/s
+        self["altitude"] = 35.0  # m
+        self["xfer_speed"] = 12.0  # m/s
+        self["xfer_altitude"] = 70.0  # m
+        self["xfer_ascend"] = 5.  # m/s
+        self["xfer_descend"] = 4.  # m/s
+        self["land_altitude"] = 30  # m
+
+
+class RouteSet(object):
+    """container to for a set of routes
+
+    Attributes:
+        routes (list): list of routes
+
+    Args:
+        home (list): list of homes for the routes
+        zone (tuple): tuple of UTM zone ("number", "N" or "S")
+        routeParameters (RouteParameters): desired parameters for the routes.
+
+    """
+
+    def __init__(self, home, zone, routeParameters=None):
+        # loggers
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.data = dict()
+
+        self.home = home
+
+        self.zone = zone  # store the UTM zone
+        self.routes = []
+
+        # set the parameters
+        if routeParameters is None:
+            self.routeParameters = RouteParameters()
+        else:
+            self.routeParameters = routeParameters
+
+    def __len__(self):
+        return len(self.routes)
+
+    def __iter__(self):
+        return iter(self.routes)
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def getLimit(self):
+        # get the time limit
+        return self.routeParameters['limit']
+
+    def check(self, cords):
+        """Builds the route from a UTM waypoint list and
+        runs a series of checks to verify the route is viable.
+
+        Args;
+            cords (list): list of UTM waypoints
+
+        Returns:
+            None if any check fails;
+            the Route otherwise
+
+        """
+        route = Route(cords, self.zone, self.home)
+        route.build(self.routeParameters, priority=self.data["priority"])
+        if route.check():
+            return True, route
+        else:
+            return False, route
+
+    def push(self, route):
+        # push the route into the container
+        self.routes.append(route)
+
+    def write(self, pathDir):
+        """writes the routes to a file
+
+        Args:
+            pathDir (str): location to save routes.
+
+        """
+        self.logger.info(f"writing routes to {pathDir}")
+        for i, route in enumerate(self.routes):
+            filename = pathDir / f"{i}.csv"
+            route.write(filename)
+
+
+class Route(object):
+    """A route of points
+
+    Attributes:
+        UTMcords (list): list of points in UTM
+        GPScords (list): list of points in GPS WGS84
+
+    Args:
+        cords (list): list of UTM waypoints of a route
+        home (list): list of homes for the routes
+        zone (tuple): tuple of UTM zone ("number", "N" or "S")
+
+    """
+
+    def __init__(self, cords, zone, home=None):
+        # loggers
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.UTMcords = cords  # cords in UTM
+        self.UTMZone = zone
+        self.UTM2GPS(zone)  # set path in GPS (WGS 84)
+        if home is not None:
+            self.setHome(home)  # set home pt (in GPS)
+        else:
+            self.home = home
+        self.waypoints = []
+
+        # path limits
+        self.DJIWaypointLimit = 98
+
+    def __repr__(self):
+        return self.waypoints.__repr__()
+
+    @staticmethod
+    def DistGPS(gps0, gps1, alt0=0, alt1=0):
+        # calculates the distance in meters of 2 lat/long points
+        e0, n0, _, _ = utm.from_latlon(*gps0)
+        e1, n1, _, _ = utm.from_latlon(*gps1)
+        return np.linalg.norm([e0-e1, n0-n1, alt0-alt1])
+
+    def UTM2GPS(self, zone):
+        # converts all the UTM cords to GPS
+        self.GPScords = [utm.to_latlon(*cord, *zone) for cord in self.UTMcords]
+
+    def GPS2UTM(self):
+        # converts all the GPS cords to UTM
+        self.UTMcords = [utm.from_latlon(*cord)[0:2] for cord in self.GPScords]
+
+    def setHome(self, home):
+        # resolve the home point
+        self.homeDist = np.inf
+        for h in home:
+            (dist, i) = min([(self.DistGPS(np.array(h), np.array(pt)), i)
+                             for i, pt in enumerate(self.GPScords)])
+            if dist < self.homeDist:
+                self.home = h
+                idx = i
+                self.homeDist = dist
+        self.logger.debug(f"home set to {self.home}")
+
+        # shift and wrap
+        self.GPScords = self.GPScords[idx:] + self.GPScords[1:idx+1]
+        # sync the utm
+        self.GPS2UTM()
+
+    def check(self):
+        """ run a series of checks to see if this route is feasible
+
+        Return:
+            False if one of the length checks fail
+            True otherwise
+        """
+
+        # check: under the waypoint limit
+        self.length = 0.0
+        self.ToF = 0.0
+        passed = True
+        if len(self.waypoints) > self.DJIWaypointLimit:
+            passed = False
+        # check: under the time limit
+        if self.home is None:
+            # split transfer and survey sections
+            tran_in, ToF_in = self.calcLength(self.waypoints[:2])
+            tran_out, ToF_out = self.calcLength(self.waypoints[-2:])
+            self.len_tran = tran_in + tran_out
+            self.ToF_tran = ToF_in + ToF_out
+            # survey
+            self.len_surv, self.ToF_surv = self.calcLength(
+                self.waypoints[1:-2])
+        else:
+            # split transfer and survey sections
+            tran_in, ToF_in = self.calcLength(self.waypoints[0:2])
+            tran_out, ToF_out = self.calcLength(self.waypoints[-4:])
+            self.len_tran = tran_in + tran_out
+            self.ToF_tran = ToF_in + ToF_out
+            # survey
+            self.len_surv, self.ToF_surv = self.calcLength(
+                self.waypoints[2:-4])
+        self.length = self.len_tran + self.len_surv
+        self.ToF = self.ToF_tran + self.ToF_surv
+        if self.ToF > self.limit:
+            passed = False
+        return passed
+
+    def calcLength(self, waypoints):
+        # return the length and ToF of the segment
+        length = 0
+        ToF = 0
+        if len(waypoints) < 2:
+            self.logger.debug("waypoint segment does not have at least 2 pts")
+            return 0, 0
+        for wp, nxt in zip(waypoints, waypoints[1:]):
+            dist = self.DistGPS(wp[0:2], nxt[0:2], wp[2], nxt[2])
+            length += dist
+            ToF += dist/wp[3]
+        return length, ToF
+
+    def build(self, routeParameters, priority=None):
+        # build the path
+        # unpack parameters
+        self.limit = routeParameters["limit"]
+        spd = routeParameters["speed"]
+        priSpd = routeParameters["prio_speed"]
+        alt = routeParameters["altitude"]
+        xferSpd = routeParameters["xfer_speed"]
+        xferAlt = routeParameters["xfer_altitude"]
+        xferAsc = routeParameters["xfer_ascend"]
+        xferDes = routeParameters["xfer_descend"]
+        landALt = routeParameters["land_altitude"]
+
+        if priority is None:
+            priority = set()
+
+        # take off
+        if self.home is not None:
+            Hlat, Hlng = self.home
+            self.waypoints.append([Hlat, Hlng, xferAlt, xferSpd])
+            # get higher above first point, point camera down
+        lat, lng = self.GPScords[0]
+        self.waypoints.append([lat, lng, xferAlt, xferDes])
+        # push each waypoint
+        for gpsPt, utmPt in zip(self.GPScords[:-1], self.UTMcords[:-1]):
+            lat, lng = gpsPt
+            roundedUTM = tuple(map(int, utmPt))
+            s = priSpd if roundedUTM in priority else spd
+            self.waypoints.append([lat, lng, alt, s])
+        lat, lng = self.GPScords[-1]
+        # last point to ascend to transfer
+        self.waypoints.append([lat, lng, alt, xferAsc])
+        # get higher above last point, point camera fwd
+        self.waypoints.append([lat, lng, xferAlt, xferSpd])
+        if self.home is not None:
+            # return home
+            self.waypoints.append([Hlat, Hlng, xferAlt, xferDes])
+            # land
+            self.waypoints.append([Hlat, Hlng, landALt, xferDes])
+
+    def __len__(self):
+        # number of waypoints in path
+        return len(self.UTMcords)
+
+    def plot(self, ax, color='b'):
+        # path
+        cords = np.array(self.UTMcords)
+        ax.plot(cords[:, 0], cords[:, 1], color=color)
+        # start and end
+        if len(cords) > 3:
+            ax.scatter(cords[0, 0], cords[0, 1], color=color, marker='^')
+            ax.scatter(cords[-2, 0], cords[-2, 1], color=color, marker='s')
+        if self.home is not None:
+            # plot the home point as a 'o'
+            HomeUTMx, HomeUTMy = utm.from_latlon(*self.home)[0:2]
+            ax.scatter(HomeUTMx, HomeUTMy, color=color, marker='o')
+            ax.plot([HomeUTMx, cords[0, 0]], [HomeUTMy, cords[0, 1]],
+                    color=color, linestyle="--")
+
+    def write(self, filename):
+        # writes the waypoints as a text file
+        with filename.open('w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            # take off
+            row = self.waypoints[0] + ["FALSE", "", "", ""]
+            writer.writerow(row)
+            # transfer in
+            row = self.waypoints[1] + ["FALSE", "", "", "90.0"]
+            writer.writerow(row)
+            for wp in self.waypoints[2:-3]:
+                row = wp + ["FALSE", "", "", ""]
+                writer.writerow(row)
+            # transfer out
+            row = self.waypoints[-3] + ["FALSE", "", "", "0.0"]
+            writer.writerow(row)
+            # land
+            row = self.waypoints[-2] + ["FALSE", "", "", ""]
+            writer.writerow(row)
+            row = self.waypoints[-1] + ["FALSE", "", "", ""]
+            writer.writerow(row)
