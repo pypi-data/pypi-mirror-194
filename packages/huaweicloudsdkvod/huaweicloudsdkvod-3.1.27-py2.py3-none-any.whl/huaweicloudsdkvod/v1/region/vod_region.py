6e6ace959b446c3bbfb572e5f25a2393ff346677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:26 +0000

    Add package huaweicloudsdkvod/3.1.27/huaweicloudsdkvod-3.1.27-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkvod/huaweicloudsdkvod-3.1.27-py2.py3-none-any.whl/huaweicloudsdkvod/v1/region/vod_region.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+import types
+import six
+
+from huaweicloudsdkcore.region.region import Region
+from huaweicloudsdkcore.region.provider import RegionProviderChain
+
+class VodRegion:
+    _PROVIDER = RegionProviderChain.get_default_region_provider_chain("VOD")
+
+    CN_EAST_2 = Region("cn-east-2",
+                        "https://vod.cn-east-2.myhuaweicloud.com")
+    CN_NORTH_1 = Region("cn-north-1",
+                        "https://vod.cn-north-1.myhuaweicloud.com")
+    CN_NORTH_4 = Region("cn-north-4",
+                        "https://vod.cn-north-4.myhuaweicloud.com")
+
+    static_fields = {
+        "cn-east-2": CN_EAST_2,
+        "cn-north-1": CN_NORTH_1,
+        "cn-north-4": CN_NORTH_4,
+    }
+
+    @classmethod
+    def value_of(cls, region_id, static_fields=None):
+        if not region_id:
+            raise KeyError("Unexpected empty parameter: region_id.")
+
+        fields = static_fields if static_fields else cls.static_fields
+
+        region = cls._PROVIDER.get_region(region_id)
+        if region:
+            return region
+
+        if region_id in fields:
+            return fields.get(region_id)
+
+        raise KeyError("Unexpected region_id: " + region_id)
+
+
