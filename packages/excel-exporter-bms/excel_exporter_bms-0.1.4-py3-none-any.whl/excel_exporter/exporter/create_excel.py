e4fdafee55bd75c2c15d94633d0b2a184ca1aa9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:10 +0000

    Add package excel-exporter-bms/0.1.4/excel_exporter_bms-0.1.4-py3-none-any.whl

---
+++ packages/excel-exporter-bms/excel_exporter_bms-0.1.4-py3-none-any.whl/excel_exporter/exporter/create_excel.py
@@ -0,0 +1,19 @@
+from io import BytesIO
+from openpyxl import Workbook
+from typing import Dict, List
+
+from excel_exporter.configuration.excel_configuration import ExcelConfiguration
+from excel_exporter.exporter.write_sheet import write_sheet
+
+
+def export_excel(data: List[Dict], config: ExcelConfiguration, update_time):
+    wb = Workbook()
+    for n, (ws_data, ws_config) in enumerate(
+        zip(data, config.sheets.values())
+    ):
+        ws = wb.active if n == 0 else wb.create_sheet()
+        write_sheet(ws, ws_data, ws_config, update_time)
+    virtual_file = BytesIO()
+    wb.save(virtual_file)
+    virtual_file.seek(0)
+    return virtual_file
---
+++ packages/excel-exporter-bms/excel_exporter_bms-0.1.4-py3-none-any.whl/excel_exporter/exporter/write_sheet.py
@@ -0,0 +1,23 @@
+from typing import Dict, List
+
+from openpyxl.worksheet.worksheet import Worksheet
+
+from excel_exporter.configuration.sheet import Sheet as SheetConfiguration
+from excel_exporter.exporter.apply_sheet_configs import apply_sheet_configs
+from excel_exporter.exporter.write_groups_header import write_groups_header
+from excel_exporter.exporter.write_update_time import write_update_time
+from excel_exporter.exporter.write_columns_data import write_columns_data
+from excel_exporter.exporter.write_columns_header import write_columns_header
+
+
+def write_sheet(
+    ws: Worksheet,
+    ws_data: Dict[str, List],
+    ws_config: SheetConfiguration,
+    update_time,
+):
+    write_update_time(ws, update_time, ws_config.update_date_format)
+    write_groups_header(ws, ws_config.groups, ws_config.groups_limits())
+    write_columns_header(ws, ws_config.columns)
+    write_columns_data(ws, ws_data, ws_config.columns)
+    apply_sheet_configs(ws, ws_config)
