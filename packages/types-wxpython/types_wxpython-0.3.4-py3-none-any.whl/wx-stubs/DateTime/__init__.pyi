bcfc7446170675f12cf6f4448ba30cd61efd4f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package types-wxpython/0.3.4/types_wxpython-0.3.4-py3-none-any.whl

---
+++ packages/types-wxpython/types_wxpython-0.3.4-py3-none-any.whl/wx-stubs/DateTime/__init__.pyi
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Optional, Union
+
+class TimeZone:
+    """ Class representing a time zone.
+    """
+    def __init__(self, *args, **kw) -> None:
+        """ Overloaded Implementations:
+        """
+
+    def GetOffset(self) -> int:
+        """ Return the offset of this time zone from UTC, in seconds.
+        """
+
+    def IsLocal(self) -> bool:
+        """ Return True if this is the local time zone.
+        """
+
+    @staticmethod
+    def Make(offset: int) -> 'DateTime.TimeZone':
+        """ Create a time zone with the given offset in seconds.
+        """
+
+
+
+class Tm:
+    """ Contains broken down date-time representation.
+    """
+    def GetWeekDay(self) -> 'DateTime.WeekDay':
+        """ Return the week day corresponding to this date.
+        """
+
+    def IsValid(self) -> bool:
+        """ Check if the given date/time is valid (in Gregorian calendar).
+        """
+
+
+
