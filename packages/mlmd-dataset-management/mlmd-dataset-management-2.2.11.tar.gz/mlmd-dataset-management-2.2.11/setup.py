5bd17b625a232988edc24ffde551d9564acc6cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:50 +0000

    Add package mlmd-dataset-management/2.2.11/mlmd-dataset-management-2.2.11.tar.gz

---
+++ packages/mlmd-dataset-management/mlmd-dataset-management-2.2.11.tar.gz/mlmd-dataset-management-2.2.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from distutils.dir_util import remove_tree
+# import glob
+# remove_tree(['dist']) 
+
+setup(
+    name="mlmd-dataset-management",
+    version="2.2.11",
+    description="MLMD Dataset Management",
+    long_description="MLMD Dataset Management",
+    long_description_content_type="text/markdown",
+    url="",
+    author="Thinh Nguyen",
+    author_email="nguyenlongthinh@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["azure-storage-blob","requests","ml-metadata==1.0.0","python-dotenv","google-cloud-storage","mysql-connector-python==8.0.25", "google-cloud-secret-manager", "google-api-python-client", "protobuf==3.19.5", "numpy<=1.23.0", "google-cloud-firestore==2.7.2"],
+    entry_points = {
+        'console_scripts': ['dm=cli.dataset_manager:main']
+    }
+)
