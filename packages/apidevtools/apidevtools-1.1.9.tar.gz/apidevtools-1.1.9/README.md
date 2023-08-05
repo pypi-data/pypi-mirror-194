af826730f250b2de64602258eed0f2c469a9747a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:38 +0000

    Add package apidevtools/1.1.9/apidevtools-1.1.9.tar.gz

---
+++ packages/apidevtools/apidevtools-1.1.9.tar.gz/apidevtools-1.1.9/README.md
@@ -0,0 +1,36 @@
+## Overview
+## Documentation
+## Installation
+```
+pip install apidevtools
+```
+### Package dependencies
+```
+pip install asyncpg pydantic loguru telegraph pillow numpy argon2-cffi pycryptodome telegraph httpx aiohttp
+```
+speedups
+```
+pip install aiodns ujson cchardet uvloop
+```
+### Per module dependencies
+- simpleorm
+    ```
+    pip install asyncpg pydantic loguru
+    ```
+- imgproc
+    ```
+    pip install telegraph httpx pillow numpy telegraph httpx aiohttp
+    ```
+- logman
+    ```
+    pip install loguru
+    ```
+- security
+    - hasher
+        ```
+        pip install argon2-cffi
+        ```
+    - encryptor
+        ```
+        pip install pycryptodome
+        ```
---
+++ packages/apidevtools/apidevtools-1.1.9.tar.gz/apidevtools-1.1.9/src/apidevtools/simpleorm/postgres.py
@@ -0,0 +1,134 @@
+from typing import Any
+from asyncpg.pool import create_pool as _create_pool, Pool as _Pool
+from asyncpg.connection import Connection as _Connection
+from asyncpg import exceptions as _exceptions
+from loguru._logger import Logger
+import loguru
+
+from .schema import Schema
+from .records import Records
+from .types import SchemaType, Record, Instance
+from .baseorm import BaseORM
+
+
+class PostgreSQL(BaseORM):
+    def __init__(self, database: str,
+                 host: str = 'localhost', port: str | int = 5432,
+                 user: str = 'postgres', password: str | None = None,
+                 logger: Logger = loguru.logger):
+        super().__init__(database, host, port, user, password, logger)
+
+        self.__pool: _Pool | None = None
+        self.__connection: _Connection | None = None
+
+    async def create_pool(self) -> bool:
+        try:
+            self.__pool = await _create_pool(database=self.database, host=self.host, port=self.port, user=self.user, password=self.password)
+        except OSError:
+            self.logger.error('Pool creation failed')
+        return self.__pool is not None
+
+    async def close_pool(self) -> bool:
+        try:
+            await self.__pool.expire_connections()
+            await self.__pool.close()
+            return True
+        except AttributeError:
+            self.logger.error(f'Attempting to close not acquired pool')
+        return False
+
+    async def __aenter__(self):
+        try:
+            self.__connection = await self.__pool.acquire()
+            return self.__connection
+        except AttributeError:
+            self.logger.error('Attempting to create connection with not acquired pool')
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        try:
+            await self.__pool.release(self.__connection)
+        except _exceptions.InterfaceError:
+            self.logger.error('Attempting to release not acquired connection')
+
+    async def execute(self, query: str, args: tuple[Any, ...] = ()) -> Any:
+        async with self as connection:
+            return await connection.execute(query, *args)
+
+    async def select(self, query: str, args: tuple[Any, ...] = (), schema_t: SchemaType = dict, depth: int = 0) -> Records:
+        async with self as connection:
+            records = Records(await connection.fetch(query, *args), schema_t)
+        if depth > 0 and schema_t is not dict:
+            for index, record in enumerate(records.all()):
+                for relation in record.relations():
+                    columns = ', '.join([f'"{column}"' if column != '*' else '*' for column in relation.columns])
+                    conditions = ' AND '.join(
+                        [f'"{key}" = ${index + 1}' for index, key in enumerate(list(relation.where.keys()))])
+                    query, args = f'SELECT {columns} FROM "{relation.tablename}" WHERE {conditions};', tuple(
+                        relation.where.values())
+                    instances = (await self.select(query, args, relation.rel_schema_t, depth - 1)).all()
+                    if isinstance(record, dict):
+                        record[relation.fieldname] = instances
+                    elif isinstance(record, Schema):
+                        record = relation.ext_schema_t(**dict(record))
+                        setattr(record, relation.fieldname, instances)
+                    records.records[index] = relation.ext_schema_t(**dict(record))
+        return records
+
+    async def insert(self, instance: Instance, schema_t: SchemaType = dict, tablename: str = None) -> Record:
+        instance, tablename = self.__parse_params(instance, tablename)
+        placeholders = ', '.join([f'${index + 1}' for index in range(len(instance.keys()))])
+        # ISSUE 1:
+        #   if instance with relational column passed, error raised because there is no such a column in the db
+        #   fix: get table column names from db, and take minimum length object among "columns" and "instance.keys()"
+        columns, values = str(tuple(instance.keys())).replace("'", '"'), instance.values()
+        query, args = f'INSERT INTO "{tablename}" {columns} VALUES ({placeholders}) RETURNING *;', values
+        async with self as connection:
+            records = await connection.fetch(query, *args)
+        return Records(records, schema_t).first()
+
+    async def update(self, instance: Instance, where: dict[str, Any], schema_t: SchemaType = dict, tablename: str = None) -> Records:
+        instance, tablename = self.__parse_params(instance, tablename)
+        values = ', '.join([f'"{key}" = ${index + 1}' for index, key in enumerate(instance.keys())])
+        # ISSUE 1:
+        #   if instance with relational column passed, error raised because there is no such a column in the db
+        #   fix: get table column names from db, and take minimum length object among "columns" and "instance.keys()"
+        conditions = ' AND '.join([f'"{key}" = \'{value}\'' for key, value in where.items()])
+        query, args = f'UPDATE "{tablename}" SET {values} WHERE {conditions} RETURNING *;', tuple(instance.values())
+        async with self as connection:
+            records = await connection.fetch(query, *args)
+        return Records(records, schema_t)
+
+    async def delete(self, instance: Instance, schema_t: SchemaType = dict, tablename: str = None) -> Records:
+        instance, tablename = self.__parse_params(instance, tablename)
+        # ISSUE 1:
+        #   if instance with relational column passed, error raised because there is no such a column in the db
+        #   fix: get table column names from db, and take minimum length object among "columns" and "instance.keys()"
+        conditions = ' AND '.join([f'"{key}" = ${index + 1}' for index, key in enumerate(instance.keys())])
+        query, args = f'SELECT * FROM "{tablename}" WHERE {conditions};', tuple(instance.values())
+        # ISSUE 2:
+        #   attaching removed relational children to this "records"
+        records = await self.select(query, args, schema_t)
+        for index, record in enumerate(records.all()) if schema_t is not dict else ():
+            for relation in record.relations():
+                instances = (await self.delete(dict(**relation.where), relation.rel_schema_t, relation.tablename)).all()
+                if isinstance(record, dict):
+                    record[relation.fieldname] = instances
+                elif isinstance(record, Schema):
+                    record = relation.ext_schema_t(**dict(record))
+                    setattr(record, relation.fieldname, instances)
+                records.records[index] = relation.ext_schema_t(**dict(record))
+        query, args = f'DELETE FROM "{tablename}" WHERE {conditions} RETURNING *;', tuple(instance.values())
+        async with self as connection:
+            records = await connection.fetch(query, *args)
+        # ISSUE 2 (continuation):
+        #   but returning another records
+        return Records(records, schema_t)
+
+    def __parse_params(self, instance: Instance, tablename: str) -> tuple[Record, str]:
+        if isinstance(instance, Schema):
+            tablename = instance.tablename
+            instance = dict(instance.into_db())
+        if not tablename and isinstance(instance, dict):
+            raise AttributeError('Specify "tablename" if "instance" is a "dict" type, otherwise pass '
+                                 '"Schema" type object with overwritten property "__tablename__"')
+        return instance, tablename
