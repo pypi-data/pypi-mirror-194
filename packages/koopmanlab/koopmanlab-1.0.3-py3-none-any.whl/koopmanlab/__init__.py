75eb4fc02a1031f5f74a59165e97b146bdb00d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:19 +0000

    Add package koopmanlab/1.0.3/koopmanlab-1.0.3-py3-none-any.whl

---
+++ packages/koopmanlab/koopmanlab-1.0.3-py3-none-any.whl/koopmanlab/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ["data","model"]
+
+from . import data
+from . import model
+from . import func
+from .model import koopman, koopman_vit
+from .models import KNO1d,KNO2d
+from .models import encoder_mlp, decoder_mlp, encoder_conv1d, decoder_conv1d, encoder_conv2d, decoder_conv2d
+from .models import ViT
