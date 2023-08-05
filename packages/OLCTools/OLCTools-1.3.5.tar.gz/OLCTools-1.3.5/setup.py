6c8cd435bb8118785f5e6d2afa58579fefb2c100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:41 +0000

    Add package OLCTools/1.3.5/OLCTools-1.3.5.tar.gz

---
+++ packages/OLCTools/OLCTools-1.3.5.tar.gz/OLCTools-1.3.5/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="OLCTools",
+    version="1.3.5",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/OLC-Bioinformatics/OLCTools",
+)
