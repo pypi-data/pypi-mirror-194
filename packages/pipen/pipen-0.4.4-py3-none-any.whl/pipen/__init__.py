2157c32a6a68539636178f0e5dc31a871f2af888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:44 +0000

    Add package pipen/0.4.4/pipen-0.4.4-py3-none-any.whl

---
+++ packages/pipen/pipen-0.4.4-py3-none-any.whl/pipen/__init__.py
@@ -0,0 +1,10 @@
+"""A pipeline framework for python"""
+from .pipen import Pipen
+from .proc import Proc
+
+# Use from pipen.channel import Channel instead of
+# from pipen import Channel
+# This slows down import
+# from .channel import Channel
+from .pluginmgr import plugin
+from .version import __version__
