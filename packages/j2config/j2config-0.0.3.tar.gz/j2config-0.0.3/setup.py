d7b0854263da724f8ae7504c3fcabdf30e7c654b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:56 +0000

    Add package j2config/0.0.3/j2config-0.0.3.tar.gz

---
+++ packages/j2config/j2config-0.0.3.tar.gz/j2config-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="j2config",
+    version="0.0.3",
+    author="ALIASGAR - ALI",
+    author_email="aholo2000@gmail.com",
+    description="configuration generation using jinja2 - for Networking Geeks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aliasgar1978/j2config",
+    # package_data={
+    #     'facts_finder': [
+    #         'generators/commands/*',
+    #         'modifiers/cisco/commands/*',
+    #         'modifiers/juniper/commands/*', 
+    #         ],
+    # },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=['pandas', 'jinja2', 'xlrd', 'openpyxl', 'nettoolkit'],
+)
+
