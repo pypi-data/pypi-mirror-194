ed7a367734a5b18226375d92ec1a0ed06146f45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:24 +0000

    Add package terminal-tracker/0.0.2/terminal_tracker-0.0.2.tar.gz

---
+++ packages/terminal-tracker/terminal_tracker-0.0.2.tar.gz/terminal_tracker-0.0.2/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+version="0.0.2"
+setup()
---
+++ packages/terminal-tracker/terminal_tracker-0.0.2.tar.gz/terminal_tracker-0.0.2/terminal_tracker/__init__.py
@@ -0,0 +1,3 @@
+from ._version import __version__
+from .hello import hello, print_hello
+from .searching import Preprocessing, Tags, FrequencyFile, SearchFile
