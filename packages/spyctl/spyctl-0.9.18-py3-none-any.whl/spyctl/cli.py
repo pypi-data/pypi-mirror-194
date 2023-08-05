a6f605b039ef5558c4e7e9f41d8c523c46848adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:43 +0000

    Add package spyctl/0.9.18/spyctl-0.9.18-py3-none-any.whl

---
+++ packages/spyctl/spyctl-0.9.18-py3-none-any.whl/spyctl/cli.py
@@ -0,0 +1,146 @@
+import json
+import os
+import sys
+import time
+from typing import Callable, Dict, List
+
+import yaml
+
+import spyctl.spyctl_lib as lib
+
+yaml.Dumper.ignore_aliases = lambda *args: True
+
+WARNING_MSG = "is_warning"
+WARNING_COLOR = lib.WARNING_COLOR
+COLOR_END = lib.COLOR_END
+
+
+def try_log(*args, **kwargs):
+    lib.try_log(*args, **kwargs)
+
+
+def try_print(*args, **kwargs):
+    try:
+        print(*args, **kwargs)
+        sys.stdout.flush()
+    except BrokenPipeError:
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+        sys.exit(1)
+
+
+def unsupported_output_msg(output: str) -> str:
+    return f"'--output {output}' is not supported for this command."
+
+
+YES_OPTION = False
+
+
+def set_yes_option():
+    global YES_OPTION
+    YES_OPTION = True
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        if YES_OPTION:
+            return True
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write(
+                "Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n"
+            )
+
+
+def show(obj, output, alternative_outputs: Dict[str, Callable] = {}):
+    if output == lib.OUTPUT_YAML:
+        try_print(yaml.dump(obj, sort_keys=False), end="")
+    elif output == lib.OUTPUT_JSON:
+        try_print(json.dumps(obj, sort_keys=False, indent=2))
+    elif output == lib.OUTPUT_RAW:
+        try_print(obj)
+    elif output in alternative_outputs:
+        try_print(alternative_outputs[output](obj))
+    else:
+        try_log(unsupported_output_msg(output), is_warning=True)
+
+
+def read_stdin():
+    if sys.stdin.isatty():
+        return ""
+    return sys.stdin.read().strip()
+
+
+def get_open_input(string: str):
+    if string == "-":
+        return read_stdin().strip('"')
+    if os.path.exists(string):
+        with open(string, "r") as f:
+            return f.read().strip().strip('"')
+    return string
+
+
+def handle_list(list_string: str, obj_to_str=None) -> List[str]:
+    try:
+        objs = None
+        try:
+            objs = json.loads(list_string)
+        except json.JSONDecodeError:
+            objs = yaml.load(list_string, yaml.Loader)
+            if isinstance(objs, str):
+                raise ValueError
+        if obj_to_str is not None:
+            ret = []
+            if isinstance(objs, dict):
+                objs = [obj for obj in objs.values()]
+            for obj in objs:
+                string = obj_to_str(obj)
+                if isinstance(string, list):
+                    ret.extend(string)
+                else:
+                    ret.append(string)
+            return ret
+        return objs
+    except Exception:
+        return [s.strip().strip('"') for s in list_string.split(",")]
+
+
+def time_input(args):
+    if args.within:
+        tup = args.within, int(time.time())
+        return tup
+    elif args.time_range:
+        if args.time_range[1] < args.time_range[0]:
+            err_exit("start time was before end time")
+        return tuple(args.time_range)
+    else:
+        t = args.time if args.time else time.time()
+        return t, t
+
+
+def err_exit(message: str):
+    lib.err_exit(message)
---
+++ packages/spyctl/spyctl-0.9.18-py3-none-any.whl/spyctl/commands/merge.py
@@ -0,0 +1,52 @@
+import spyctl.api as api
+import spyctl.cli as cli
+import spyctl.config.configs as cfgs
+import spyctl.filter_resource as filt
+import spyctl.resources.baselines as b
+import spyctl.resources.policies as p
+import spyctl.spyctl_lib as lib
+
+
+def handle_merge(filename, with_file, st, et, latest, output):
+    resource = lib.load_resource_file(filename)
+    resrc_kind = resource.get(lib.KIND_FIELD)
+    fingerprints = None
+    with_resource = None
+    if not with_file:
+        if latest:
+            latest_timestamp = resource.get(lib.METADATA_FIELD, {}).get(
+                lib.LATEST_TIMESTAMP_FIELD
+            )
+            if latest_timestamp is not None:
+                st = lib.time_inp(latest_timestamp)
+            else:
+                cli.err_exit(
+                    f"No {lib.LATEST_TIMESTAMP_FIELD} found in provided"
+                    f" resource {lib.METADATA_FIELD} field. Defaulting to"
+                    " 24hrs."
+                )
+        filters = lib.selectors_to_filters(resource)
+        ctx = cfgs.get_current_context()
+        machines = api.get_machines(*ctx.get_api_data())
+        machines = filt.filter_machines(
+            machines, filters, use_context_filters=False
+        )
+        muids = [m["uid"] for m in machines]
+        fingerprints = api.get_fingerprints(
+            *ctx.get_api_data(),
+            muids=muids,
+            time=(st, et),
+        )
+        fingerprints = filt.filter_fingerprints(
+            fingerprints, **filters, use_context_filters=False
+        )
+    else:
+        with_resource = lib.load_resource_file(with_file)
+    if resrc_kind == lib.BASELINE_KIND:
+        result = b.merge_baseline(resource, with_resource, fingerprints)
+    elif resrc_kind == lib.POL_KIND:
+        result = p.merge_policy(resource, with_resource, fingerprints)
+    else:
+        cli.err_exit(f"The 'merge' command is not supported for {resrc_kind}")
+    if result:
+        cli.show(result.get_obj_data(), output)
