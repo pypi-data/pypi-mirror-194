4959f75ec42cda37514e65214fd863d325b5ce30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:16 +0000

    Add package news-signals/0.1/news-signals-0.1.tar.gz

---
+++ packages/news-signals/news-signals-0.1.tar.gz/news-signals-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+with open("VERSION") as f:
+    version = f.read().strip()
+
+setup(
+    name="news-signals",
+    version=version,
+    description="A library for working with text and timeseries data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    packages=["news_signals"],
+    data_files=["LICENSE", "VERSION", "README.md"],
+    include_package_data=True
+)
