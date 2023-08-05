43e98d909eb9abc111c04f54718b4aa584d08b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package dycw-utilities/0.6.8/dycw_utilities-0.6.8-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.6.8-py3-none-any.whl/utilities/pyinstrument.py
@@ -0,0 +1,24 @@
+import datetime as dt
+from collections.abc import Iterator
+from contextlib import contextmanager
+from pathlib import Path
+from zoneinfo import ZoneInfo
+
+from beartype import beartype
+from pyinstrument.profiler import Profiler
+
+from utilities.pathlib import PathLike
+
+_CWD = Path.cwd()
+
+
+@contextmanager
+@beartype
+def profile(*, path: PathLike = _CWD) -> Iterator[None]:
+    """Profile the contents of a block."""
+    with Profiler() as profiler:
+        yield
+    now = dt.datetime.now(tz=ZoneInfo("UTC"))
+    filename = Path(path, f"profile__{now:%Y%m%dT%H%M%S}.html")
+    with filename.open(mode="w") as fh:
+        _ = fh.write(profiler.output_html())
