78fa863f1c63107a315e5eff2e1c5247f3e47776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:39 +0000

    Add package tkutil/0.0.8/tkutil-0.0.8-py3-none-any.whl

---
+++ packages/tkutil/tkutil-0.0.8-py3-none-any.whl/tkutil/__init__.py
@@ -0,0 +1,8 @@
+from tkutil.window import *
+
+
+__all__ = ["make_modal", "center", "align", "compute_center",
+           "compute_alignment", "hide_early", "relocate",
+           "get_geometry", "save_size", "save_coords",
+           "save_geometry", "restore_size", "restore_coords",
+           "restore_geometry", "WinfoUpdater"]
---
+++ packages/tkutil/tkutil-0.0.8-py3-none-any.whl/tkutil/__main__.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print("https://github.com/pyrustic/tkutil")
+
+
+if __name__ == "__main__":
+    main()
