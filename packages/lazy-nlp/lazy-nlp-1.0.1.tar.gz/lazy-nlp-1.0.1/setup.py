542f7754edf41e6fa6a687ee96ee778452456ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:41 +0000

    Add package lazy-nlp/1.0.1/lazy-nlp-1.0.1.tar.gz

---
+++ packages/lazy-nlp/lazy-nlp-1.0.1.tar.gz/lazy-nlp-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.1'
+DESCRIPTION = 'A simple Python package that allows you to do zeroshot, embeddings and build a classifier. See here for more information: https://github.com/LeonardPuettmann/lazy-nlp'
+
+# Setting up
+setup(
+    name="lazy-nlp",
+    version=VERSION,
+    author="Leonard Püttmann",
+    author_email="<leopuettmann@outlook.de>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["transformers", "torch", "sentence-transformers", "scikit-learn", "numpy"],
+    keywords=["python", "zeroshot", "nlp", "lazy-nlp"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
