6a78892911eb02bf603ed7e80d1b62461f61d5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:11 +0000

    Add package percentipedecrawler/0.0.12/percentipedecrawler-0.0.12-py3-none-any.whl

---
+++ packages/percentipedecrawler/percentipedecrawler-0.0.12-py3-none-any.whl/crawler/save.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from database import get_engine
+from models import Crawls
+from parser import strip_tags
+import markdownify
+
+
+def save(crawl_id, domain, url, html):
+    engine = get_engine()
+    with Session(engine) as session:
+        text = strip_tags(html)
+        md = markdownify.markdownify(text, heading_style="ATX")
+        crawl = Crawls(crawl_id=crawl_id, domain=domain, url=url,
+                       html=html, markdown=md, text=text)
+        session.add(crawl)
+        return session.commit()
