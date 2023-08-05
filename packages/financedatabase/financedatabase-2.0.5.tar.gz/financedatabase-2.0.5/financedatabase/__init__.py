68b533c363d60341b99ed9d9a1338d73e80c6910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:45 +0000

    Add package financedatabase/2.0.5/financedatabase-2.0.5.tar.gz

---
+++ packages/financedatabase/financedatabase-2.0.5.tar.gz/financedatabase-2.0.5/financedatabase/__init__.py
@@ -0,0 +1,8 @@
+"""Init Module"""
+from .cryptos import Cryptos  # noqa
+from .currencies import Currencies  # noqa
+from .equities import Equities  # noqa
+from .etfs import ETFs  # noqa
+from .funds import Funds  # noqa
+from .indices import Indices  # noqa
+from .moneymarkets import Moneymarkets  # noqa
