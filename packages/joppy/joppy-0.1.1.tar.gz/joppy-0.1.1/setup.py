9b2fa0388eb82c4175387a3e2eee5de8ed6149ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:08 +0000

    Add package joppy/0.1.1/joppy-0.1.1.tar.gz

---
+++ packages/joppy/joppy-0.1.1.tar.gz/joppy-0.1.1/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+
+setup(
+    name="joppy",
+    version="0.1.1",
+    packages=["joppy"],
+    # https://stackoverflow.com/a/70386281/7410886
+    package_data={
+        "joppy": ["py.typed"],
+    },
+    description="Python API for Joplin",
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/marph91/joppy",
+    author="Martin Dörfelt",
+    author_email="martin.d@andix.de",
+    license="Mozilla Public License version 2.0",
+    classifiers=[
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+    install_requires=["requests"],
+)
