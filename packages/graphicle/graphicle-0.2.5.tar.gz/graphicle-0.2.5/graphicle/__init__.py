2c7cb2bbf42757e59e9b6045133061d41c17fcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:20 +0000

    Add package graphicle/0.2.5/graphicle-0.2.5.tar.gz

---
+++ packages/graphicle/graphicle-0.2.5.tar.gz/graphicle-0.2.5/graphicle/__init__.py
@@ -0,0 +1,31 @@
+from .data import (
+    MaskArray,
+    MaskGroup,
+    PdgArray,
+    MomentumArray,
+    ColorArray,
+    HelicityArray,
+    StatusArray,
+    ParticleSet,
+    AdjacencyList,
+    Graphicle,
+)
+from . import matrix, transform, select, calculate, base
+
+__all__ = [
+    "MaskArray",
+    "MaskGroup",
+    "PdgArray",
+    "MomentumArray",
+    "ColorArray",
+    "StatusArray",
+    "HelicityArray",
+    "ParticleSet",
+    "AdjacencyList",
+    "Graphicle",
+    "matrix",
+    "transform",
+    "select",
+    "calculate",
+    "base",
+]
