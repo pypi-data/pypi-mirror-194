0809e1ac0e74263335c72a042bbd331c4cc26678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:16 +0000

    Add package EngineRemastered/0.1.0/EngineRemastered-0.1.0-py3-none-any.whl

---
+++ packages/EngineRemastered/EngineRemastered-0.1.0-py3-none-any.whl/EngineRemastered/API/__init__.py
@@ -0,0 +1,9 @@
+"""
+Engine Remastered.
+
+A description
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Brian Masse'
+__credits__ = 'Me'
---
+++ packages/EngineRemastered/EngineRemastered-0.1.0-py3-none-any.whl/EngineRemastered/__init__.py
@@ -0,0 +1,9 @@
+"""
+Engine Remastered.
+
+A description
+"""
+
+__version__ = "0.2.0"
+__author__ = 'Brian Masse'
+__credits__ = 'Me'
