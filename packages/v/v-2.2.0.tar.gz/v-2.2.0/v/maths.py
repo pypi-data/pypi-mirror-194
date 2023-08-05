9d154056d6f7e0e55293248696c48558608dc411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:59 +0000

    Add package v/2.2.0/v-2.2.0.tar.gz

---
+++ packages/v/v-2.2.0.tar.gz/v-2.2.0/v/maths.py
@@ -0,0 +1,14 @@
+import math
+
+def add(*numbers):
+	total = 0
+	for number in numbers:
+		total += number
+	return total
+
+def sub(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total -= number
+	return total
+	
