f0fb0a84edd982fafa65069e0f98658a357ce72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:07 +0000

    Add package flashquiz/0.0a1/flashquiz-0.0a1.tar.gz

---
+++ packages/flashquiz/flashquiz-0.0a1.tar.gz/flashquiz-0.0a1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flashquiz',
+    version='0.0a1',
+    description='[TEST VERSION] Quiz yourself on your flashcards',
+    author='mike-fmh',
+    author_email='mikemh@uri.edu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'pygame'
+    ]
+)
