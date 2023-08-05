c6f5b351580316e2bbe54e6ac4be21709a15a7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:11 +0000

    Add package v/2.6.0/v-2.6.0-py3-none-any.whl

---
+++ packages/v/v-2.6.0-py3-none-any.whl/v/maths.py
@@ -0,0 +1,43 @@
+import math
+
+def add(*numbers):
+	total = 0
+	for number in numbers:
+		total += number
+	return total
+
+def sub(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total -= number
+	return total
+
+def mult(*numbers):
+	total = 1
+	for number in numbers:
+		total *= number
+	return total
+
+def div(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total /= number
+	return total
+
+def int_div(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total //= number
+	return total
+	
+def rnd(number, accuracy):
+	a = number / accuracy
+	if a % 1 >= 0.5:
+		a //= 1
+		a += 1
+	else:
+		a //= 1
+	b = a * accuracy
+	if b % 1 == 0:
+		return int(b)
+	return b
