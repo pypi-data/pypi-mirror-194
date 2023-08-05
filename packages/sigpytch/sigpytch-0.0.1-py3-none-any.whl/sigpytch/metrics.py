9164968ebec49122de045890e6b5caae1fd8083f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:22 +0000

    Add package sigpytch/0.0.1/sigpytch-0.0.1-py3-none-any.whl

---
+++ packages/sigpytch/sigpytch-0.0.1-py3-none-any.whl/sigpytch/metrics.py
@@ -0,0 +1,59 @@
+"""
+BSD 3-Clause License
+
+Copyright (c) 2023, Sustainable Investment Group at UCSD
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import numpy as np
+import pandas as pd
+
+from periods import P
+
+def rolling_sharpe(daily_returns: pd.Series,
+                   risk_free_rate: pd.Series, window: int) -> pd.Series:
+    """
+    Computes the rolling Sharpe ratio for a given ticker and risk-free rate.
+    
+    Parameters:
+        daily_returns (pd.Series): Daily returns of the given ticker 
+        risk_free_rate (pd.Series): Risk-free rate (treasury yield)
+        window (int): Window size in months
+
+    Returns:
+        pd.Series: Rolling Sharpe ratio  
+    """
+    # Compute D-bar
+    avg_return = daily_returns.rolling(window = window * P.TDAYS_PER_MONTH).mean()
+    periodized_avg_return = avg_return * window * P.TDAYS_PER_MONTH
+    avg_diff_return = periodized_avg_return - risk_free_rate
+    # Compute sigma
+    volatility = daily_returns.rolling(window = window * P.TDAYS_PER_MONTH).std()
+    periodized_volatility = volatility * np.sqrt(window * P.TDAYS_PER_MONTH)
+    # Compute rolling Sharpe ratio: D-bar / sigma
+    rolling_sharpe_ratio = (avg_diff_return / periodized_volatility).dropna()
+    return rolling_sharpe_ratio
