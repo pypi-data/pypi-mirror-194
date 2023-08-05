40d30f2b605035c2f82ca26dca4e6d7f4150aef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:53 +0000

    Add package denari/1.0.3/denari-1.0.3.tar.gz

---
+++ packages/denari/denari-1.0.3.tar.gz/denari-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='denari',
+    version='1.0.3',
+    description='DenariAnalytics open souce Business Tools',
+    author='Fadil Karim',
+    author_email='insights@denarianalytics.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'plotly.graph_objects',
+        'dash'
+    ],
+)
