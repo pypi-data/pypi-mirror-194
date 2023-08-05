69eb875f4e220c6ef3acfdfed1e2c150cb527427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package kimfame/0.0.24/kimfame-0.0.24.tar.gz

---
+++ packages/kimfame/kimfame-0.0.24.tar.gz/kimfame-0.0.24/kimfame/kimfame.py
@@ -0,0 +1,6 @@
+def kimfame():
+    print("Hello, world!")
+
+
+def main():
+    print("cli test")
---
+++ packages/kimfame/kimfame-0.0.24.tar.gz/kimfame-0.0.24/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kimfame']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['faker>=17.0.0,<18.0.0']
+
+entry_points = \
+{'console_scripts': ['kimfame_cli = kimfame.kimfame:main']}
+
+setup_kwargs = {
+    'name': 'kimfame',
+    'version': '0.0.24',
+    'description': 'PyPI test project',
+    'long_description': '# Kimfame\n\nPyPI Test Project',
+    'author': 'kimfame',
+    'author_email': 'renownkim@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kimfame/kimfame',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
