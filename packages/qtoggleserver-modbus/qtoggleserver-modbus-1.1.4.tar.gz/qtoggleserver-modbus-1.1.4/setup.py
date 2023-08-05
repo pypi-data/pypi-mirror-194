96fd47712e65e57d61da2c7a81034d8a0513f419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:17 +0000

    Add package qtoggleserver-modbus/1.1.4/qtoggleserver-modbus-1.1.4.tar.gz

---
+++ packages/qtoggleserver-modbus/qtoggleserver-modbus-1.1.4.tar.gz/qtoggleserver-modbus-1.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name='qtoggleserver-modbus',
+    version='1.1.4',
+    description='Modbus client/server for qToggleServer',
+    author='Calin Crisan',
+    author_email='ccrisan@gmail.com',
+    license='Apache 2.0',
+
+    packages=find_namespace_packages(),
+
+    install_requires=[
+        'pymodbus>=3.1.3',
+        'pyserial-asyncio',
+    ]
+)
