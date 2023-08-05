d8c1f30ea5713aeea1f3254cc0f7ffc368efa5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:38 +0000

    Add package torch.redstone/0.0.4/torch.redstone-0.0.4-py3-none-any.whl

---
+++ packages/torch.redstone/torch.redstone-0.0.4-py3-none-any.whl/torch_redstone/metric.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod
+import torch
+
+from .utils import ObjectProxy
+
+
+class Metric:
+    @property
+    def name(self):
+        return getattr(self, '_name', self.__class__.__name__)
+
+    @name.setter
+    def name(self, n):
+        self._name = n
+
+    @abstractmethod
+    def __call__(self, inputs, model_return) -> torch.Tensor:
+        raise NotImplementedError
