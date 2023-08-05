330c9cfadc23c45461f4d335e75508fc761117d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:39 +0000

    Add package irll-crafting/1.0.6/irll-crafting-1.0.6.tar.gz

---
+++ packages/irll-crafting/irll-crafting-1.0.6.tar.gz/irll-crafting-1.0.6/src/crafting/examples/__init__.py
@@ -0,0 +1,22 @@
+"""#Crafting environement examples.
+
+Here is the table of available Crafting environments examples.
+
+If you built one of your own, send us a pull request so we can add it to the list!
+
+| Gym name                    | CLI name          |Interest                                              | Reference                               | 
+|:----------------------------|:------------------|:-----------------------------------------------------|:----------------------------------------|
+| "MineCrafting-Dragon-v1"    | `minecraft`       | Complex real-case hierarchies.                       |`crafting.examples.minecraft`            |
+| "TowerCrafting-v1"          | `tower`           | Polynomial growth with reuse hierarchies.            |`crafting.examples.tower`                |
+| "RecursiveCrafting-v1"      | `recursive`       | Exponential growth with reuse hierarchies.           |`crafting.examples.recursive`            |
+| "LightRecursiveCrafting-v1" | `light-recursive` | Exponential (smaller) growth with reuse hierarchies. |`crafting.examples.recursive`            |
+| "RandomCrafting-v1"         | `random`          | Random hierarchical structures.                      |`crafting.examples.random_simple`        |
+| "KeyDoorCrafting-v1"        | `keydoor`         | Gridworld like structure.                            |`crafting.examples.keydoor`              |
+
+"""
+
+from crafting.examples.keydoor import KeyDoorCraftingEnv
+from crafting.examples.minecraft.env import MineCraftingEnv
+from crafting.examples.random_simple import RandomCraftingEnv
+from crafting.examples.recursive import LightRecursiveCraftingEnv, RecursiveCraftingEnv
+from crafting.examples.tower import TowerCraftingEnv
