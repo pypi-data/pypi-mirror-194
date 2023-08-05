96713362f0153cf8866edd0d2dac956bb6e6f8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:29 +0000

    Add package streamlit-cardv2/0.0.1rc2/streamlit-cardv2-0.0.1rc2.tar.gz

---
+++ packages/streamlit-cardv2/streamlit-cardv2-0.0.1rc2.tar.gz/streamlit-cardv2-0.0.1rc2/streamlit_cardv2/frontend/build/precache-manifest.3d1d75a54aa1cab9d03088d8f3e15079.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5639d688a9ff125ab191",
+    "url": "./static/js/main.78999827.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f33ff27d4a35e474bf3c",
+    "url": "./static/js/2.a5b5137d.chunk.js"
+  },
+  {
+    "revision": "e4cca60a5b92a656bd95f822bba05374",
+    "url": "./index.html"
+  }
+];
