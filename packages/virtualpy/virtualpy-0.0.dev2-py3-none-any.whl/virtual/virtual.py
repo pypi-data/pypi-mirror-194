e68d95d9d161bcf89640af0ade34e17c72157a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:18 +0000

    Add package virtualpy/0.0.dev2/virtualpy-0.0.dev2-py3-none-any.whl

---
+++ packages/virtualpy/virtualpy-0.0.dev2-py3-none-any.whl/virtual/virtual.py
@@ -0,0 +1,6 @@
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(prog ='virtual',description ='Virtual: The Python virtualisation software that allows you to create custom systems!')
+	args = parser.parse_args()
+	print("hello world")
