4b6449f8b9e79ee047d0b1ee431d7fed7a9990bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:33 +0000

    Add package azure-cli-testsdk/0.3.0/azure-cli-testsdk-0.3.0.tar.gz

---
+++ packages/azure-cli-testsdk/azure-cli-testsdk-0.3.0.tar.gz/azure-cli-testsdk-0.3.0/HISTORY.rst
@@ -0,0 +1,51 @@
+.. :changelog:
+
+Release History
+===============
+
+0.3.0
++++++
+* Patch authentication for MSAL-based ``azure-cli-core`` (#19853)
+* Drop support for ADAL-based ``azure-cli-core`` (#19853)
+* Add resource group name prefix validator in resource group preparer (#10800)
+
+0.2.4
++++++
+* Add ManagedApplicationPreparer
+
+0.2.3
++++++
+* Minor fixes
+
+0.2.2
++++++
+* Minor fixes
+
+0.2.1
++++++
+* Minor fixes
+
+0.2.0
++++++
+* Removes dependency on azure-cli
+* Using explict import_module to import azure.cli.core components
+
+0.1.4
+++++++
+* Minor fixes.
+
+0.1.3
++++++
+* Update azure-devtools to 1.0.0
+
+0.1.2
++++++
+* Update the setup.py to limit the dependency on azure-devtools in 0.5.* range.
+
+0.1.1
++++++
+* Add additional tags to the resource group created by automation
+
+0.1.0
++++++++++
+* Initial release
---
+++ packages/azure-cli-testsdk/azure-cli-testsdk-0.3.0.tar.gz/azure-cli-testsdk-0.3.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from codecs import open
+from setuptools import setup
+
+VERSION = "0.3.0"
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'License :: OSI Approved :: MIT License',
+]
+
+DEPENDENCIES = [
+    'ConfigArgParse>=0.12.0',
+    'jmespath',
+    'vcrpy>=1.10.3',
+    'pytest'
+]
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    README = f.read()
+with open('HISTORY.rst', 'r', encoding='utf-8') as f:
+    HISTORY = f.read()
+
+setup(
+    name='azure-cli-testsdk',
+    version=VERSION,
+    description='Microsoft Azure Command-Line Tools Test SDK Module',
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='azpycli@microsoft.com',
+    url='https://github.com/Azure/azure-cli',
+    zip_safe=False,
+    classifiers=CLASSIFIERS,
+    packages=[
+        'azure.cli.testsdk',
+        'azure.cli.testsdk.scenario_tests',
+    ],
+    install_requires=DEPENDENCIES
+)
