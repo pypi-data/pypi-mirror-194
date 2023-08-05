c9ff081e6aebdaa6b79348786203f9a1099ee818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:20 +0000

    Add package whisper-cpp-cdll/0.0.3/whisper_cpp_cdll-0.0.3.tar.gz

---
+++ packages/whisper-cpp-cdll/whisper_cpp_cdll-0.0.3.tar.gz/whisper_cpp_cdll-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['whisper_cpp_cdll']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['scipy==1.9.3']
+
+setup_kwargs = {
+    'name': 'whisper-cpp-cdll',
+    'version': '0.0.3',
+    'description': 'toolkit for whisper.cpp',
+    'long_description': '# whisper.cpp.cdll\n',
+    'author': 'limdongjin',
+    'author_email': 'geniuslim27@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/limdongjin/whisper.cpp.py',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
