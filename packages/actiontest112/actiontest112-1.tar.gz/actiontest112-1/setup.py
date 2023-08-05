6a817879e704187127117bc56b619e51981d29a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package actiontest112/1/actiontest112-1.tar.gz

---
+++ packages/actiontest112/actiontest112-1.tar.gz/actiontest112-1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='actiontest112',
+    version='1',
+    python_requires='>=3.11',
+    author='Ali RajabNezhad',
+    author_email='alirn76@yahoo.com',
+    url='https://github.com/alirn76/panther',
+    description='Fast &  Friendly, Web Framework For Building Async APIs',
+    long_description='',
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
