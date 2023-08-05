8f6dd12fb13e733d440905c0ce05d7d5840ea4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:15 +0000

    Add package misal/0.1/misal-0.1.tar.gz

---
+++ packages/misal/misal-0.1.tar.gz/misal-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='misal',
+    version='0.1',
+    license='MIT',
+    author="Aditya Mandke",
+    description="NLP for Bhartiya languages",
+    author_email='ekdnam@gmail.com',
+    url='https://github.com/ekdnam/misal',
+    keywords='example project',
+    install_requires=[
+          'numpy',
+      ],
+)
