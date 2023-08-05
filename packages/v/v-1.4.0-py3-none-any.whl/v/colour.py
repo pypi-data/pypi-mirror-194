4d126bab0630c6addb7f06b2587102c3fc7709ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package v/1.4.0/v-1.4.0-py3-none-any.whl

---
+++ packages/v/v-1.4.0-py3-none-any.whl/v/colour.py
@@ -0,0 +1,23 @@
+import math
+
+class Colour:
+	def __init__(self, red=0, green=0, blue=0):
+		if type(red) == str:
+			self.hex = red
+			self.red, self.green, self.blue = hex_to_rgb(red)
+		else:
+			self.red = red
+			self.green = green
+			self.blue = blue
+			self.hex = rgb_to_hex(red, green, blue)
+	def __repr__(self):
+		return self.hex
+	def __str__(self):
+		return self.hex
+
+def rgb_to_hex(red, green, blue):
+	decimal_hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+	red_hex = decimal_hex_map[red // 16] + decimal_hex_map[red % 16]
+	green_hex = decimal_hex_map[green // 16] + decimal_hex_map[green % 16]
+	blue_hex = decimal_hex_map[blue // 16] + decimal_hex_map[blue % 16]
+	return "#" + red_hex + green_hex + blue_hex
