81dd085163ae98e17b37ca74a66b801f44f9fdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:16 +0000

    Add package gpytoolbox/0.1.0/gpytoolbox-0.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/gpytoolbox/gpytoolbox-0.1.0-cp37-cp37m-win_amd64.whl/gpytoolbox/per_face_normals.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+
+def per_face_normals(V,F,unit_norm=True):
+    """Vector perpedicular to all faces on a mesh
+    
+    Computes per face (optionally unit) normal vectors for a triangle mesh.
+
+    Parameters
+    ----------
+    V : (n,d) numpy array
+        vertex list of a triangle mesh
+    F : (m,3) numpy int array
+        face index list of a triangle mesh
+    unit_norm : bool, optional (default True)
+        Whether to normalize each face's normal before outputting
+
+    Returns
+    -------
+    N : (n,d) numpy double array
+        Matrix of per-face normals
+
+    See Also
+    --------
+    per_vertex_normals.
+
+    Examples
+    --------
+    ```python
+    from gpytoolbox import read_mesh, per_face_normals
+    v,f = read_mesh("test/unit_tests_data/bunny_oded.obj")
+    n = per_face_normals(v,f,unit_norm=True)
+    ```
+    """
+    # Computes per face (optionally unit) normal vectors for a triangle mesh
+    #
+    # Input:
+    #       V #V by 3 numpy array of mesh vertex positions
+    #       F #F by 3 int numpy array of face/edge vertex indeces into V
+    #       Optional:
+    #               'unit_norm' boolean, whether to normalize each face's 
+    #                       normal before outputting {default: true}
+    #
+    # Output:
+    #       N #F by 3 numpy array of per-face normals
+    #           
+
+    v0 = V[F[:,0],:]
+    v1 = V[F[:,1],:]
+    v2 = V[F[:,2],:]
+
+    # It's basically just a cross product
+    N = np.cross(v1-v0,v2-v0,axis=1)
+
+    if unit_norm:
+        N = N/np.tile(np.linalg.norm(N,axis=1)[:,None],(1,3))
+
+    return N
