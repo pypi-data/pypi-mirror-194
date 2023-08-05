f63e19919986c85f582c4606a6babcca3e5fb78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:19 +0000

    Add package meloetta/1.0.2/meloetta-1.0.2.tar.gz

---
+++ packages/meloetta/meloetta-1.0.2.tar.gz/meloetta-1.0.2/meloetta/actors/base.py
@@ -0,0 +1,32 @@
+import torch
+import traceback
+
+from abc import ABC, abstractmethod
+
+from typing import Any, Dict
+
+from meloetta.vector import VectorizedState
+from meloetta.room import BattleRoom
+
+
+Battle = Dict[str, Dict[str, Dict[str, Any]]]
+
+
+class Actor(ABC):
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        try:
+            return self.choose_action(*args, **kwargs)
+        except Exception as e:
+            print(traceback.format_exc())
+
+    @abstractmethod
+    def choose_action(self):
+        raise NotImplementedError
+
+    def post_match(self, room: BattleRoom):
+        pass
+
+    def get_vectorized_state(
+        self, room: BattleRoom, battle: Battle
+    ) -> Dict[str, torch.Tensor]:
+        return VectorizedState.from_battle(room, battle).to_dict()
