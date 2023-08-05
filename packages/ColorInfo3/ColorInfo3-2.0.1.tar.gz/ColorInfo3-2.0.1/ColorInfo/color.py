07cccf52114685b3e517794265c4cb28216ed0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:56 +0000

    Add package ColorInfo3/2.0.1/ColorInfo3-2.0.1.tar.gz

---
+++ packages/ColorInfo3/ColorInfo3-2.0.1.tar.gz/ColorInfo3-2.0.1/ColorInfo/color.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   color.py
+@Time    :   2023-02-25 14:42
+@Author  :   坐公交也用券
+@Version :   1.0
+@Contact :   faith01238@hotmail.com
+@Homepage : https://liumou.site
+@Desc    :   定义颜色
+"""
+Red = "\033[31m"  # 红色
+Greet = "\033[32m"  # 绿色
+Yellow = '\033[33m'  # 黄色
+Blue = '\033[34m'  # 蓝色
+RESET_ALL = '\033[0m'  # 清空颜色
