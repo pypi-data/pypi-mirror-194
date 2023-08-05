b721a47c45a08325849457ce3e1665e6ad3054d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:34 +0000

    Add package paper-qa/0.0.12/paper-qa-0.0.12.tar.gz

---
+++ packages/paper-qa/paper-qa-0.0.12.tar.gz/paper-qa-0.0.12/paperqa/utils.py
@@ -0,0 +1,35 @@
+import math
+import string
+
+
+def maybe_is_text(s, thresh=2.5):
+    if len(s) == 0:
+        return False
+    # Calculate the entropy of the string
+    entropy = 0
+    for c in string.printable:
+        p = s.count(c) / len(s)
+        if p > 0:
+            entropy += -p * math.log2(p)
+
+    # Check if the entropy is within a reasonable range for text
+    if entropy > thresh:
+        return True
+    return False
+
+
+def strings_similarity(s1, s2):
+    if len(s1) == 0 or len(s2) == 0:
+        return 0
+    # break the strings into words
+    s1 = set(s1.split())
+    s2 = set(s2.split())
+    # return the similarity ratio
+    return len(s1.intersection(s2)) / len(s1.union(s2))
+
+
+def maybe_is_truncated(s):
+    punct = [".", "!", "?", '"']
+    if s[-1] in punct:
+        return False
+    return True
---
+++ packages/paper-qa/paper-qa-0.0.12.tar.gz/paper-qa-0.0.12/tests/test_paperqa.py
@@ -0,0 +1,165 @@
+import paperqa
+import requests
+import os
+import pickle
+from paperqa.utils import strings_similarity
+from langchain.llms import OpenAI
+
+
+def test_maybe_is_text():
+    assert paperqa.maybe_is_text(
+        "This is a test. The sample conc. was 1.0 mM (at 245 ^F)"
+    )
+    assert not paperqa.maybe_is_text("\\C0\\C0\\B1\x00")
+    # get front page of wikipedia
+    r = requests.get("https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day")
+    assert paperqa.maybe_is_text(r.text)
+
+    # now force it to contain lots of weird encoding
+    bad_text = r.text.encode("latin1", "ignore").decode("utf-16", "ignore")
+    assert not paperqa.maybe_is_text(bad_text)
+
+
+def test_docs():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get front page of wikipedia
+        r = requests.get("https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day")
+        f.write(r.text)
+    llm = OpenAI(temperature=0.1, model_name="text-ada-001")
+    docs = paperqa.Docs(llm=llm)
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    assert docs.docs[doc_path]["key"] == "Wiki2023"
+    os.remove(doc_path)
+
+
+def test_evidence():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
+        f.write(r.text)
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.1, model_name="text-curie-001"))
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    evidence, _ = docs.get_evidence(
+        "For which state was he a governor", k=1, max_sources=1
+    )
+    assert "Missouri" in evidence
+    os.remove(doc_path)
+
+
+def test_query():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
+        f.write(r.text)
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.1, model_name="text-ada-001"))
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    answer = docs.query("What is Frederick Bates's greatest accomplishment?")
+    print(answer)
+    os.remove(doc_path)
+
+
+def test_docs_pickle():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get front page of wikipedia
+        r = requests.get("https://en.wikipedia.org/wiki/National_Flag_of_Canada_Day")
+        f.write(r.text)
+    llm = llm = OpenAI(temperature=0.0, model_name="text-babbage-001")
+    docs = paperqa.Docs(llm=llm)
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    docs_pickle = pickle.dumps(docs)
+    docs2 = pickle.loads(docs_pickle)
+    docs2.update_llm(llm)
+    assert len(docs.docs) == len(docs2.docs)
+    assert (
+        strings_similarity(
+            docs.get_evidence("What date is flag day in Canada?", k=1, max_sources=1)[
+                0
+            ],
+            docs2.get_evidence("What date is flag day in Canada?", k=1, max_sources=1)[
+                0
+            ],
+        )
+        > 0.75
+    )
+    os.remove(doc_path)
+
+
+def test_bad_context():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
+        f.write(r.text)
+    docs = paperqa.Docs(llm=OpenAI(model_name="text-davinci-003"))
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    answer = docs.query("What year was Barack Obama born greatest accomplishment?")
+    assert (
+        answer.answer
+        == "I cannot answer this question due to insufficient information."
+    )
+    os.remove(doc_path)
+
+
+def test_repeat_keys():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
+        f.write(r.text)
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-ada-003"))
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    try:
+        docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    except ValueError:
+        pass
+    assert len(docs.docs) == 1
+
+    # now with different paths
+    doc_path2 = "example2.txt"
+    with open(doc_path2, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        f.write(r.text)
+    docs.add(doc_path2, "WikiMedia Foundation, 2023, Accessed now")
+    assert len(docs.docs) == 2
+
+    # check keys
+    assert docs.docs[doc_path]["key"] == "Wiki2023"
+    assert docs.docs[doc_path2]["key"] == "Wiki2023a"
+
+    os.remove(doc_path)
+    os.remove(doc_path2)
+
+
+def test_pdf_reader():
+    tests_dir = os.path.dirname(os.path.abspath(__file__))
+    doc_path = os.path.join(tests_dir, "paper.pdf")
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-curie-001"))
+    docs.add(doc_path, "Wellawatte et al, XAI Review, 2023")
+    answer = docs.query("Are counterfactuals actionable (yes/no)?")
+    assert "yes" in answer.answer or "Yes" in answer.answer
+
+
+def test_prompt_length():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
+        f.write(r.text)
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-ada-001"))
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    docs.query("What is the name of the politician?", length_prompt="25 words")
+
+
+def test_doc_preview():
+    doc_path = "example.txt"
+    with open(doc_path, "w", encoding="utf-8") as f:
+        # get wiki page about politician
+        r = requests.get("https://en.wikipedia.org/wiki/Frederick_Bates_(politician)")
+        f.write(r.text)
+    docs = paperqa.Docs(llm=OpenAI(temperature=0.0, model_name="text-ada-001"))
+    docs.add(doc_path, "WikiMedia Foundation, 2023, Accessed now")
+    assert len(docs.doc_previews) == 1
