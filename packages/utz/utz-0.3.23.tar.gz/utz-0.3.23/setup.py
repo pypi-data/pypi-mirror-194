07a15829645c38fb1625f53af64fb4dfac7b14b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:32 +0000

    Add package utz/0.3.23/utz-0.3.23.tar.gz

---
+++ packages/utz/utz-0.3.23.tar.gz/utz-0.3.23/setup.py
@@ -0,0 +1,29 @@
+from utz.setup import setup
+
+setup(
+    name="utz",
+    version="0.3.23",
+    install_requires=[
+        "GitPython",
+        "joblib",
+        "pandas",
+        "python-dateutil==2.8.2",
+        "pytz",
+        "pyyaml",
+    ],
+    extras_require={
+        'pdf': [
+            'reportlab',
+            'pyPDF2',
+        ],
+        'setup': [
+            'lxml',
+            'mistune',
+        ],
+        'test': [
+            'pytest',
+            'pytest-mock',
+        ],
+    },
+    url="https://github.com/runsascoded/utz",
+)
