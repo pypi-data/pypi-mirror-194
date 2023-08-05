f50f71a52eac04a44850e2bfa2dc5bc73f60ed4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:34 +0000

    Add package UrlGetText/0.0.1/UrlGetText-0.0.1.tar.gz

---
+++ packages/UrlGetText/UrlGetText-0.0.1.tar.gz/UrlGetText-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+
+setup(name='UrlGetText',
+      version='0.0.1',
+      description='string',
+      license='MIT',
+      packages=find_packages("UrlGetText"))
