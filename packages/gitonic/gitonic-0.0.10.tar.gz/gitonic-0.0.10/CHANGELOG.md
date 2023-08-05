13b1d1202d94bc65f0397a6d1afc1c5b75e155a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:25 +0000

    Add package gitonic/0.0.10/gitonic-0.0.10.tar.gz

---
+++ packages/gitonic/gitonic-0.0.10.tar.gz/gitonic-0.0.10/CHANGELOG.md
@@ -0,0 +1,85 @@
+
+# Changelog
+
+
+# version v0.0.10 - 20230225
+
+- sort files in changes tab
+- 
+
+
+# version v0.0.9 - 20221008
+
+- git current branch in changes tab
+- support for black
+- 
+
+
+# version v0.0.8 - 20220802
+
+- bug fix, due to corrupted local env
+- double click in changes view will stage/ unstage a file
+- 
+
+
+## version v0.0.7 - 20220729
+
+- changes view height
+- bug fix, due to corrupted local env
+- bug fix, removed control-x hotkey
+- 
+
+
+## version v0.0.6 - 20220729
+
+- fix save settings 
+- set focus on commit entry after clearing fields
+- expert (dev-mode) logging tab
+- calling diff-tool will not switch to log tab
+- icon support with 
+  [`fontawesome`](https://github.com/FortAwesome/Font-Awesome)
+  and 
+  [`pytkfaicons`](https://github.com/kr-g/pytkfaicons)
+- added tooltips
+- added hotkeys
+- BUG fix: search only in first level of workspace folder for git folder
+- 
+
+
+## version v0.0.5 - 20220515
+
+- rework ui layout
+- use subprocess popen to capture stdout, and stderr
+- added "commit + push" button 
+- 
+
+
+## version v0.0.4 - 20211107
+
+- fix find git repo in workspace (with similar starting letters)
+- hopefully all childhood diseases are gone for now
+- 
+
+
+## version v0.0.3 - 20211107
+
+- thonny-gitonic plugin support
+- 
+
+
+## pre version v0.0.2 - 20211106
+
+- config parameter in config file
+- workspace not fixed anymore to ~/repo
+- refactored 
+- commit history
+- documentation in readme
+- 
+
+
+## pre version v0.0.1 - 20211106
+
+- first version 
+  - no yet released official 
+  - 
+-
---
+++ packages/gitonic/gitonic-0.0.10.tar.gz/gitonic-0.0.10/gitonic/main.py
@@ -0,0 +1,122 @@
+import sys
+import tkinter
+from tkinter import Tk
+
+from .gui import TkCmd, Tile, TileRows, TileCols, TileLabelButton
+from .gui import startup_gui, get_main, fconfigdir
+
+from .icons import get_icon
+
+from .file import FileStat
+from .singleinstance import (
+    check_instance,
+    check_and_bring_to_front,
+    create_client_socket,
+)
+
+from gitonic import set_tk_root, get_tk_root
+
+# main
+
+
+debug = False
+
+
+def quit_all(frame):
+    def quit_():
+        print("quit_all")
+        # removes all, including threads
+        # sys.exit()
+        # soft, state remains
+        # download_stop()
+        frame.quit()
+        if not debug:
+            sys.exit()
+
+    return quit_
+
+
+def minimize():
+    print("minimize")
+    get_tk_root().iconify()
+
+
+# single instance handling
+
+pnam = FileStat(fconfigdir.name).join(["socket"])
+sock = None
+
+
+def try_switch_app():
+    global sock
+    # set up server if possible
+    sock, port = check_instance(pnam.name)
+    if sock is None:
+        # open connection to server, and quit
+        sock = create_client_socket(port)
+        return sock
+
+
+def do_serv_socket(self):
+    rc = check_and_bring_to_front(sock, get_tk_root())
+    if rc is not None:
+        print("bring to front client request")
+
+
+tkcmd = None
+
+# end-of single instance
+
+
+def main_func(debug_=False):
+
+    global debug
+    debug = debug_
+
+    tk_root = set_tk_root(Tk())
+
+    if try_switch_app():
+        print("switched to server instance")
+        return
+
+    global tkcmd
+    tkcmd = TkCmd().start(tk_root, command=do_serv_socket)
+
+    mainframe = Tile(tk_root=tk_root, idn="mainframe")
+
+    main = get_main()
+
+    main_content = TileRows(
+        source=[
+            TileCols(
+                source=[
+                    TileLabelButton(
+                        caption="close app",
+                        commandtext="good bye",
+                        icon=get_icon("right-from-bracket"),
+                        command=quit_all(mainframe),
+                    ),
+                    TileLabelButton(
+                        caption="minimize",
+                        commandtext="minimize me",
+                        icon=get_icon("minimize"),
+                        hotkey="<Escape>",
+                        command=minimize,
+                    ),
+                ]
+            ),
+            main,
+        ]
+    )
+
+    mainframe.tk.protocol("WM_DELETE_WINDOW", quit_all(mainframe))
+
+    mainframe.title("gitonic")
+    mainframe.resize_grip()
+
+    mainframe.add(main_content)
+    mainframe.layout()
+
+    startup_gui()
+
+    mainframe.mainloop()
