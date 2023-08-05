19c125c807e7f6832aac697e070669b498e24835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:39 +0000

    Add package tinyticker/0.3.2/tinyticker-0.3.2.tar.gz

---
+++ packages/tinyticker/tinyticker-0.3.2.tar.gz/tinyticker-0.3.2/tinyticker/config.py
@@ -0,0 +1,71 @@
+import json
+import logging
+from pathlib import Path
+
+from .settings import CONFIG_FILE
+
+LOGGER = logging.getLogger(__name__)
+
+# remove hollow types because white on white doesn't show
+TYPES = ["candlestick", "line", "ohlc"]
+DEFAULT = {
+    "epd_model": "EPD_v3",
+    "api_key": None,
+    "flip": False,
+    "tickers": [
+        {
+            "symbol_type": "stock",
+            "symbol": "SPY",
+            "interval": "1d",
+            "lookback": None,
+            "wait_time": None,
+            "volume": False,
+            "type": "candlestick",
+            "mav": None,
+        }
+    ],
+}
+
+
+def read(config_file: Path = CONFIG_FILE) -> dict:
+    """Read the config file and return a dictionary.
+
+    Args:
+        config_file: path of the config file.
+    """
+    if config_file.is_file():
+        LOGGER.debug("Reading config file: %s", config_file)
+        with open(config_file, "r") as fd:
+            return json.load(fd)
+    else:
+        LOGGER.debug("Fallback to default values.")
+        return DEFAULT
+
+
+def write(config: dict, config_file: Path = CONFIG_FILE) -> None:
+    """Write the config file.
+
+    Args:
+        config: dictionary containing the contents of the config.
+        config_file: path of the config file.
+    """
+    config_dir = config_file.parent
+    if not config_dir.is_dir():
+        LOGGER.debug("Creating config dir: %s", config_dir)
+        config_dir.mkdir(parents=True)
+
+    LOGGER.debug("Writing config file: %s", config_file)
+    with open(config_file, "w") as fd:
+        json.dump(config, fd, indent=2)
+
+
+def write_default(config_file: Path = CONFIG_FILE) -> None:
+    """Write the default values to the config file.
+
+    Args:
+        config_file: path of the config file.
+    """
+    LOGGER.debug("Creating default config: %s", config_file)
+    if not config_file.parent.is_dir():
+        config_file.parent.mkdir(parents=True)
+    write(DEFAULT, config_file)
---
+++ packages/tinyticker/tinyticker-0.3.2.tar.gz/tinyticker-0.3.2/tinyticker/waveshare_lib/models.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from typing import Type, Union
+
+from . import epd2in13, epd2in13_V2, epd2in13_V3, epd2in13b_V3, epd2in13b_V4, epd2in13bc
+from ._base import EPDBase, EPDHighlight, EPDPartial
+
+
+@dataclass
+class EPDModel:
+    name: str
+    class_: Union[Type[EPDBase], Type[EPDHighlight], Type[EPDPartial]]
+    desc: str
+    # if the model has a colour channel for highlights i.e. red, yellow, ...
+    has_highlight: bool
+    width: int
+    height: int
+
+
+MODELS = [
+    EPDModel(
+        name="EPD",
+        class_=epd2in13.EPD,
+        desc="Black and White 2.13 inch",
+        has_highlight=False,
+        width=epd2in13.EPD_WIDTH,
+        height=epd2in13.EPD_HEIGHT,
+    ),
+    EPDModel(
+        name="EPD_v2",
+        class_=epd2in13_V2.EPD,
+        desc="Black and White 2.13 inch V2",
+        has_highlight=False,
+        width=epd2in13_V2.EPD_WIDTH,
+        height=epd2in13_V2.EPD_HEIGHT,
+    ),
+    EPDModel(
+        name="EPD_v3",
+        class_=epd2in13_V3.EPD,
+        desc="Black and White 2.13 inch V3",
+        has_highlight=False,
+        width=epd2in13_V3.EPD_WIDTH,
+        height=epd2in13_V3.EPD_HEIGHT,
+    ),
+    EPDModel(
+        name="EPDb_v3",
+        class_=epd2in13b_V3.EPD,
+        desc="Black, White and Red 2.13 inch V3",
+        has_highlight=True,
+        width=epd2in13b_V3.EPD_WIDTH,
+        height=epd2in13b_V3.EPD_HEIGHT,
+    ),
+    EPDModel(
+        name="EPDb_v4",
+        class_=epd2in13b_V4.EPD,
+        desc="Black, White and Red 2.13 inch V4",
+        has_highlight=True,
+        width=epd2in13b_V4.EPD_WIDTH,
+        height=epd2in13b_V4.EPD_HEIGHT,
+    ),
+    EPDModel(
+        name="EPDbc",
+        class_=epd2in13bc.EPD,
+        desc="Black, White and Yellow 2.13 inch",
+        has_highlight=True,
+        width=epd2in13bc.EPD_WIDTH,
+        height=epd2in13bc.EPD_HEIGHT,
+    ),
+]
+MODELS = {model.name: model for model in MODELS}
