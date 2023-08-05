0e8d2c2adbb69fa10186aa5769ebd6bd332f4991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:37 +0000

    Add package ptyx/22.6/ptyx-22.6.tar.gz

---
+++ packages/ptyx/ptyx-22.6.tar.gz/ptyx-22.6/ptyx/config.py
@@ -0,0 +1,58 @@
+# mypy: ignore-errors
+
+import os
+import sys
+
+import psutil
+
+try:
+    from ptyx.custom_config import param as custom_param
+except ImportError:
+    custom_param = {}
+
+# <default_configuration>
+param = {
+    "total": 1,
+    "formats": ["pdf", "tex", "tex+pdf", "pdf+tex"],
+    "default_formats": "pdf",
+    "tex_command": "pdflatex -interaction=nonstopmode --shell-escape --enable-write18",
+    "quiet_tex_command": "pdflatex -interaction=batchmode --shell-escape --enable-write18",
+    "sympy_is_default": True,
+    "import_paths": [],
+    "debug": False,
+    "floating_point": ",",
+    "win_print_command": "SumatraPDF.exe -print-dialog -silent -print-to-default ",
+}
+# </default_configuration>
+
+# Update parameters using `custom_config.py` param, if any.
+param.update(custom_param)
+
+
+for path in param["import_paths"]:
+    path = os.path.normpath(os.path.expanduser(path))
+    sys.path.insert(0, path)
+
+# print("Loading sympy...")
+try:
+    import sympy
+except ImportError:
+    print("** ERROR: sympy not found ! **")
+    sympy = None
+    param["sympy_is_default"] = False
+
+
+# print("Loading numpy...")
+
+try:
+    import numpy
+except ImportError:
+    print("WARNING: numpy not found.")
+    numpy = None
+
+try:
+    CPU_PHYSICAL_CORES = psutil.cpu_count(logical=False)
+except ImportError:
+    CPU_PHYSICAL_CORES = (os.cpu_count() or 1) // 2
+if not CPU_PHYSICAL_CORES:
+    CPU_PHYSICAL_CORES = 1
