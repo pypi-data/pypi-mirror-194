5a1c7415c41754a810d9d6f67e807f6ed622496e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:35 +0000

    Add package hiddifypanel/0.9.66/hiddifypanel-0.9.66.tar.gz

---
+++ packages/hiddifypanel/hiddifypanel-0.9.66.tar.gz/hiddifypanel-0.9.66/hiddifypanel/panel/restapi/__init__.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from flask_restful import Api
+
+from .resources import *
+from .update_usage import UpdateUsageResource
+
+bp = Blueprint("api", __name__, url_prefix="/<proxy_path>/<user_secret>/api/v1")
+api = Api(bp)
+
+
+def init_app(app):
+    api.add_resource(UserResource, "/users/")
+    api.add_resource(DomainResource, "/domains/")
+    api.add_resource(StrConfigResource, "/str_configs/")
+    api.add_resource(BoolConfigResource, "/bool_configs/")
+    api.add_resource(AllResource, "/all/")
+    api.add_resource(UpdateUsageResource, "/update_usage/")
+    api.add_resource(HelloResource, "/hello/")
+    
+    app.register_blueprint(bp)
---
+++ packages/hiddifypanel/hiddifypanel-0.9.66.tar.gz/hiddifypanel-0.9.66/hiddifypanel/panel/restapi/resources.py
@@ -0,0 +1,55 @@
+from flask import abort, jsonify,request
+from flask_restful import Resource
+# from flask_simplelogin import login_required
+import datetime
+from hiddifypanel.models import StrConfig,BoolConfig,User,Domain,get_hconfigs,Proxy
+from urllib.parse import urlparse
+from hiddifypanel.panel import hiddify
+class AllResource(Resource):
+    def get(self):
+        products = User.query.all() or abort(204)
+        response= jsonify(
+            {"users": [u.to_dict() for u in User.query.filter((User.usage_limit_GB>User.current_usage_GB)).filter(User.expiry_time>=datetime.date.today()).all()],
+            "domains": [hiddify.domain_dict(u) for u in Domain.query.all()],
+            "proxies": [u.to_dict() for u in Proxy.query.all()],
+            "hconfigs": get_hconfigs()
+            }
+        )
+        o = urlparse(request.base_url)
+        domain=o.hostname
+        response.headers.add('Content-disposition', f'attachment; filename=hiddify-{domain}-{datetime.datetime.now()}.json');
+
+        return response
+
+
+class UserResource(Resource):
+    def get(self):
+        products = User.query.all() or abort(204)
+        return jsonify(
+            {"users": [product.to_dict() for product in products]}
+        )
+
+class DomainResource(Resource):
+    def get(self):
+        products = Domain.query.all() or abort(204)
+        return jsonify(
+            {"domains": [hiddify.domain_dict(product) for product in products]}
+        )
+
+class BoolConfigResource(Resource):
+    def get(self):
+        products = BoolConfig.query.all() or abort(204)
+        return jsonify(
+            {"bool_configs": [product.to_dict() for product in products]}
+        )
+
+class StrConfigResource(Resource):
+    def get(self):
+        products = StrConfig.query.all() or abort(204)
+        return jsonify(
+            {"str_configs": [product.to_dict() for product in products]}
+        )
+
+class HelloResource(Resource):
+    def get(self):
+        return jsonify({"ok": True})
