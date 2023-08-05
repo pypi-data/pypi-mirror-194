941158f063485268bc06287441f5081f1a2f14b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:45 +0000

    Add package kadota/1.1.0/kadota-1.1.0.tar.gz

---
+++ packages/kadota/kadota-1.1.0.tar.gz/kadota-1.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+# Given a version number MAJOR.MINOR.PATCH, increment the:
+# MAJOR version when you make incompatible API changes
+# MINOR version when you add functionality in a backwards compatible manner
+# PATCH version when you make backwards compatible bug fixes
+
+setup(
+    name='kadota',
+    version='1.1.0',
+    description='Tools for working with Figma programmatically',
+    long_description=f"{open('README.md').read()}\n## Demo\n```\n{open('kadota/tests/test_demo.py').read()}\n```",
+    long_description_content_type='text/markdown',
+    author='Isaac Ronald Ward',
+    author_email='isaacronaldward@gmail.com',
+    license='MIT',
+    packages=['kadota'],
+    include_package_data=True,
+    install_requires=[line.strip() for line in open("requirements.txt").readlines()]
+)
