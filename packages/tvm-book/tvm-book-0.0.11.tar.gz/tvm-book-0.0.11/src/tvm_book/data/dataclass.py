faa2be7a23dc6ad936e71164dd939148e7086dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:27 +0000

    Add package tvm-book/0.0.11/tvm-book-0.0.11.tar.gz

---
+++ packages/tvm-book/tvm-book-0.0.11.tar.gz/tvm-book-0.0.11/src/tvm_book/data/dataclass.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass(eq=True, order=True)
+class TensorType:
+    shape: tuple[int]
+    dtype: str
+    name: str = "data"
+
+    @property
+    def empty(self):
+        '''空的输出张量'''
+        return np.empty(shape=self.shape,
+                        dtype=self.dtype)
+
+    @property
+    def nbytes(self):
+        '''输出张量的存储字节数'''
+        return self.empty.nbytes
+
+    @property
+    def nelement(self):
+        '''输出张量的元素个数'''
+        return self.empty.size
