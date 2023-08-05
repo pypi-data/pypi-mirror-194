c22d7d925dbf9eba49479fe801eade7178ec74e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:49 +0000

    Add package asyncgraphs/0.0.1/asyncgraphs-0.0.1.tar.gz

---
+++ packages/asyncgraphs/asyncgraphs-0.0.1.tar.gz/asyncgraphs-0.0.1/README.md
@@ -0,0 +1,5 @@
+# AsyncGraphs
+
+AsyncGraphs is a tiny ETL framework.
+It allows users to define their extract, transform and load operations and to chain them into ETL flows.
+
---
+++ packages/asyncgraphs/asyncgraphs-0.0.1.tar.gz/asyncgraphs-0.0.1/asyncgraphs/construction.py
@@ -0,0 +1,53 @@
+from typing import (
+    Any,
+    AsyncIterable,
+    Callable,
+    Iterable,
+    Optional,
+    Set,
+    TypeAlias,
+    TypeVar,
+)
+
+SourceOperation: TypeAlias = Iterable[Any] | AsyncIterable[Any]
+TransformOperation: TypeAlias = Callable[..., Any]
+TTransform = TypeVar("TTransform", bound="Transform")
+
+
+class _NodeBase:
+    def __init__(self, name: Optional[str]) -> None:
+        self.name = name or f"Node<{id(self)}>"
+        self.next_nodes: Set[Transform] = set()
+
+    def __or__(self, other: TTransform | TransformOperation) -> "Transform":
+        if callable(other):
+            other_node = Transform(None, other)
+        else:
+            other_node = other
+        self.next_nodes.add(other_node)
+        return other_node
+
+
+class Source(_NodeBase):
+    def __init__(self, name: Optional[str], operation: SourceOperation) -> None:
+        super().__init__(name)
+        self.operation = operation
+
+
+class Transform(_NodeBase):
+    def __init__(self, name: Optional[str], operation: TransformOperation) -> None:
+        super().__init__(name)
+        self.operation = operation
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.entry_nodes: Set[Source] = set()
+
+    def __or__(self, other: Source | SourceOperation) -> Source:
+        if isinstance(other, Source):
+            other_node = other
+        else:
+            other_node = Source(None, other)
+        self.entry_nodes.add(other_node)
+        return other_node
