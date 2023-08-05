6ae93d0dec37d370339d319efd0ae9090170076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package wagtail-schema.org/4.1.0/wagtail-schema.org-4.1.0.tar.gz

---
+++ packages/wagtail-schema.org/wagtail-schema.org-4.1.0.tar.gz/wagtail-schema.org-4.1.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Install wagtail-schema.org using setuptools
+"""
+
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with open('wagtailschemaorg/_version.py', 'r') as f:
+    version = '0.0.0'
+    exec(f.read())
+
+setup(
+    name='wagtail-schema.org',
+    version=version,
+    description="Add Schema.org JSON-LD to your website",
+    long_description=readme,
+    author='Tim Heap',
+    author_email='tim@takeflight.com.au',
+    url='https://github.com/takeflight/wagtail-schema.org',
+
+    install_requires=[
+        'wagtail>=2.15',
+    ],
+    extras_require={
+        'testing': ['jinja2>=2.10,<3.0', 'markupsafe==2.0.1']
+    },
+    zip_safe=False,
+    license='BSD License',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+    package_data={},
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Framework :: Django',
+        'Framework :: Django :: 3',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 2',
+        'Framework :: Wagtail :: 3',
+        'Framework :: Wagtail :: 4',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
---
+++ packages/wagtail-schema.org/wagtail-schema.org-4.1.0.tar.gz/wagtail-schema.org-4.1.0/tests/app/urls.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, re_path
+from wagtail import VERSION as WAGTAIL_VERSION
+from wagtail.admin import urls as wagtailadmin_urls
+
+if WAGTAIL_VERSION >= (3, 0):
+    from wagtail import urls as wagtail_urls
+else:
+    from wagtail.core import urls as wagtail_urls
+
+from wagtail.documents import urls as wagtaildocs_urls
+
+urlpatterns = [
+    re_path(r'^django-admin/', admin.site.urls),
+
+    re_path(r'^admin/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+
+    re_path(r'', include(wagtail_urls)),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
