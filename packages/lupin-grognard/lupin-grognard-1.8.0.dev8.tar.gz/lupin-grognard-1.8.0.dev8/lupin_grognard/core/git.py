83ae951bb3e3730ec1ee7028860064ab57d596fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:11 +0000

    Add package lupin-grognard/1.8.0.dev8/lupin-grognard-1.8.0.dev8.tar.gz

---
+++ packages/lupin-grognard/lupin-grognard-1.8.0.dev8.tar.gz/lupin-grognard-1.8.0.dev8/lupin_grognard/core/git.py
@@ -0,0 +1,27 @@
+from lupin_grognard.core.cmd import Command, run_command
+from lupin_grognard.core.config import COMMIT_DELIMITER
+
+
+class Git:
+    def get_log(
+        self, max_line_count: int = None, first_parent: bool = False
+    ) -> Command:
+        format: str = "hash>>%H%nauthor>>%cN%nauthor_mail>>%cE%nauthor_date>>%ct%ntitle>>%s%nbody>>%b<<body%n"
+        delimiter = COMMIT_DELIMITER
+        if first_parent:
+            command = f'git log --first-parent --format="{format}"{delimiter}'
+        else:
+            command = f'git log --format="{format}"{delimiter}'
+        if max_line_count:
+            max_count = f"--max-count={max_line_count}"
+            command = f"{command} {max_count}"
+        return run_command(command=command)
+
+    def get_branch_name(self) -> str:
+        return run_command(command="git branch --show-current").stdout
+
+    def get_commit_tag(self, commit_hash) -> str | None:
+        result = run_command(command=f"git describe --tags --abbrev=0 {commit_hash}")
+        if result.return_code == 0:
+            return result.stdout.strip()
+        return None
