9b23869c2af6e660ba84af01c14a318e500af8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package data-scientist-tools/1.0/data_scientist_tools-1.0.tar.gz

---
+++ packages/data-scientist-tools/data_scientist_tools-1.0.tar.gz/data_scientist_tools-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='data_scientist_tools',
+      version='1.0',
+      description='Good-to-have tools for general Data Science tasks',
+      packages=['data_scientist_tools'],
+      author_email='davidliu1007@gmail.com',
+      zip_safe=False)
