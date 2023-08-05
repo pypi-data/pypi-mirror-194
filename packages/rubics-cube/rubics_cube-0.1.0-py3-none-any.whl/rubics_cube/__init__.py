0a9a064ad47dd2e2f12b146f8baa613221bff7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:47 +0000

    Add package rubics-cube/0.1.0/rubics_cube-0.1.0-py3-none-any.whl

---
+++ packages/rubics-cube/rubics_cube-0.1.0-py3-none-any.whl/rubics_cube/__init__.py
@@ -0,0 +1,7 @@
+from .cube import Cube
+from .heuristics import AStarSolver, same_color_amount
+
+# make sure that the version is available in the package namespace
+module_path = __file__.split("__init__.py", maxsplit=1)[0]
+__version__ = open(module_path + "VERSION", encoding="utf-8").read().strip()
+del module_path
