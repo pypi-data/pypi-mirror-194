d8fa370553fc49b38cf7d4d5383968b4447fe1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:17 +0000

    Add package meta-package-manager/5.12.0/meta_package_manager-5.12.0-py3-none-any.whl

---
+++ packages/meta-package-manager/meta_package_manager-5.12.0-py3-none-any.whl/meta_package_manager/tests/test_cli_install_remove.py
@@ -0,0 +1,75 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from __future__ import annotations
+
+import pytest
+from click_extra.tests.conftest import destructive
+
+from ..pool import pool
+from .conftest import all_manager_ids_and_dummy_package
+from .test_cli import CLISubCommandTests
+
+
+@pytest.fixture
+def subcmd():
+    return "install", "arrow"
+
+
+class TestInstallRemove(CLISubCommandTests):
+    """Install and remove operations are siblings and sensible, so we regroup them under
+    the same test suite.
+
+    Install mpm with itself when we can, so we can test externally contributed
+    packaging. See:
+    https://github.com/kdeldycke/meta-package-manager/issues/527
+    """
+
+    strict_selection_match = False
+    """Install sub-command try each user-selected manager until it find one providing
+    the package we seek to install, after which the process stop.
+
+    This mean not all managers will be called, so we allow the CLI output checks to
+    partially match.
+    """
+
+    @pytest.mark.parametrize("operation", ("install", "remove"))
+    def test_no_package_id(self, invoke, operation):
+        result = invoke(operation)
+        assert result.exit_code == 2
+        assert not result.stdout
+        assert "Error: Missing argument 'PACKAGES_SPECS...'." in result.stderr
+
+    @destructive
+    @all_manager_ids_and_dummy_package
+    def test_single_manager_install_and_remove(self, invoke, manager_id, package_id):
+        result = invoke(f"--{manager_id}", "install", package_id)
+        assert result.exit_code == 0
+        self.check_manager_selection(
+            result, {manager_id}, reference_set=pool.all_manager_ids
+        )
+
+        result = invoke(f"--{manager_id}", "remove", package_id)
+        assert result.exit_code == 0
+        self.check_manager_selection(
+            result, {manager_id}, reference_set=pool.all_manager_ids
+        )
+
+
+destructive()(TestInstallRemove.test_stats)
+destructive()(TestInstallRemove.test_default_all_managers)
+destructive()(TestInstallRemove.test_manager_shortcuts)
+destructive()(TestInstallRemove.test_manager_selection)
---
+++ packages/meta-package-manager/meta_package_manager-5.12.0-py3-none-any.whl/meta_package_manager/tests/test_cli_upgrade.py
@@ -0,0 +1,75 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from __future__ import annotations
+
+import pytest
+from click_extra.tests.conftest import destructive
+
+from .conftest import default_manager_ids
+from .test_cli import CLISubCommandTests
+
+
+@pytest.fixture
+def subcmd():
+    return "upgrade", "--all"
+
+
+class TestUpgrade(CLISubCommandTests):
+    """All tests here should me marked as destructive unless --dry-run parameter is
+    passed."""
+
+    @pytest.mark.parametrize("all_option", ("--all", None))
+    def test_all_managers_dry_run_upgrade_all(self, invoke, all_option):
+        result = invoke("--dry-run", "upgrade", all_option)
+        assert result.exit_code == 0
+        if not all_option:
+            assert "assume -A/--all option" in result.stderr
+        self.check_manager_selection(result)
+
+    @destructive
+    @pytest.mark.parametrize("all_option", ("--all", None))
+    def test_all_managers_upgrade_all(self, invoke, all_option):
+        result = invoke("upgrade", all_option)
+        assert result.exit_code == 0
+        if not all_option:
+            assert "assume -A/--all option" in result.stderr
+        self.check_manager_selection(result)
+
+    @default_manager_ids
+    @pytest.mark.parametrize("all_option", ("--all", None))
+    def test_single_manager_dry_run_upgrade_all(self, invoke, manager_id, all_option):
+        result = invoke(f"--{manager_id}", "--dry-run", "upgrade", all_option)
+        assert result.exit_code == 0
+        if not all_option:
+            assert "assume -A/--all option" in result.stderr
+        self.check_manager_selection(result, {manager_id})
+
+    @destructive
+    @default_manager_ids
+    @pytest.mark.parametrize("all_option", ("--all", None))
+    def test_single_manager_upgrade_all(self, invoke, manager_id, all_option):
+        result = invoke(f"--{manager_id}", "upgrade", all_option)
+        assert result.exit_code == 0
+        if not all_option:
+            assert "assume -A/--all option" in result.stderr
+        self.check_manager_selection(result, {manager_id})
+
+
+destructive()(TestUpgrade.test_stats)
+destructive()(TestUpgrade.test_default_all_managers)
+destructive()(TestUpgrade.test_manager_shortcuts)
+destructive()(TestUpgrade.test_manager_selection)
