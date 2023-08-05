0fb10795b664d4b4251219958462cab4194f0681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package synthdog/0.0.2/synthdog-0.0.2.tar.gz

---
+++ packages/synthdog/synthdog-0.0.2.tar.gz/synthdog-0.0.2/elements/__init__.py
@@ -0,0 +1,13 @@
+"""
+Donut
+Copyright (c) 2022-present NAVER Corp.
+MIT License
+"""
+from elements.background import Background
+from elements.content import Content
+from elements.document import Document, CheckDocument
+from elements.paper import Paper
+from elements.textbox import TextBox
+from elements.utils import *
+
+__all__ = ["Background", "Content", "Document", "Paper", "TextBox", "CheckDocument"]
