10312c4a96a53148ea4fed83e83d577c0c47d5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package frpy/0.2.0/frpy-0.2.0.tar.gz

---
+++ packages/frpy/frpy-0.2.0.tar.gz/frpy-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as r:
+    long_description = r.read()
+
+setup(
+    name = 'frpy',
+    version = '0.2.0',
+    description = "An api wrapper for Free Rider HD",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+	author = 'Calculamatrise',
+    url = "https://github.com/Calculamatrise/frpy",
+	download_url = "https://github.com/Calculamatrise/frpy/archive/refs/heads/master.zip",
+	keywords = [
+		'frhd',
+		'wrapper'
+	],
+	license = 'GPL-3.0 license',
+    classifiers = [
+		"Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+	packages = find_packages(),
+    python_requires = '>=3.8',
+	install_requires = [
+        'requests'
+    ]
+)
+
+# python -m build
+# python -m twine upload --repository testpypi dist/*
