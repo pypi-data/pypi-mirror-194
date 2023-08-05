27d4af12fd610974648290412209a96111ede0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:18 +0000

    Add package fastnumbers/5.0.0/fastnumbers-5.0.0.tar.gz

---
+++ packages/fastnumbers/fastnumbers-5.0.0.tar.gz/fastnumbers-5.0.0/src/cpp/c_str_parsing.cpp
@@ -0,0 +1,216 @@
+/******************/
+/****** NOTE ******/
+/******************/
+// All functions in this file assume whitespace has been trimmed
+// from both sides of the string, and that the sign has been removed.
+
+#include "fastnumbers/c_str_parsing.hpp"
+#include "fastnumbers/third_party/fast_float.h"
+#include <algorithm>
+#include <charconv>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+
+/*********************/
+/* EXPOSED FUNCTIONS */
+/*********************/
+
+int string_contains_what(const char* str, const char* end, int base) noexcept
+{
+    const std::size_t len = static_cast<std::size_t>(end - str);
+
+    // Define possible return values
+    static constexpr int INVALID = 0;
+    static constexpr int INTEGER = 1;
+    static constexpr int FLOAT = 2;
+    static constexpr int INTLIKE_FLOAT = 3;
+
+    // If the base needs to be guessed, do so now and get it over with.
+    if (base == 0) {
+        base = detect_base(str, end);
+    }
+
+    // Negative bases are illegal. So is zero length.
+    if (base < 0 || len == 0) {
+        return INVALID;
+    }
+
+    // Special-case non-base 10.
+    if (base != 10) {
+        // Skip leading characters for non-base 10 ints.
+        if (len > 1 && str[0] == '0' && is_base_prefix(str[1], base)) {
+            str += 2;
+        }
+
+        // The rest behaves as normal.
+        const char* digit_start = str;
+        while (str != end && is_valid_digit(*str, base)) {
+            str += 1;
+        }
+        return (str == end && str != digit_start) ? INTEGER : INVALID;
+    }
+
+    // Before decimal. Keep track of the start location.
+    bool valid = false;
+    int value = INVALID;
+    const char* int_start = str;
+    consume_digits(str, len);
+    if (str != int_start) {
+        valid = true;
+        value = INTEGER;
+    }
+
+    // Decimal part of float. Keep track of number of digits read
+    // as well as beginning and end locations.
+    const char* decimal_start = str;
+    uint32_t dec_length = 0;
+    if (str != end && *str == '.') {
+        str += 1;
+        const char* dec_digits_start = str;
+        consume_digits(str, static_cast<std::size_t>(end - str));
+        dec_length = static_cast<uint32_t>(str - dec_digits_start);
+        valid = valid || dec_length > 0;
+    }
+    const char* decimal_end = str;
+
+    // If there are any decimal components then we are looking at a float.
+    if (valid && decimal_end != decimal_start) {
+        value = FLOAT;
+    }
+
+    // Exponential part of float. Parse the magnitude.
+    uint32_t expon = 0;
+    bool exp_negative = false;
+    if (valid) {
+        const char* exp_start = str;
+        if (str != end && (*str == 'e' || *str == 'E')) {
+
+            // Skip the 'e' and any sign that might be present.
+            str += 1;
+            if (str != end && is_sign(*str)) {
+                exp_negative = *str == '-';
+                str += 1;
+            }
+
+            // Parse the exponent as a digit.
+            const char* exp_digit_start = str;
+            int32_t this_char_as_digit = 0L;
+            while (str != end && (this_char_as_digit = to_digit<int32_t>(*str)) >= 0) {
+                expon = expon * 10L + this_char_as_digit;
+                str += 1;
+            }
+
+            // If all we found was e.g. "e" or "e-" then it's not valid.
+            valid = str != exp_digit_start;
+        }
+        const char* exp_end = str;
+
+        // If we found an exponent AND it was parsed validly then this is a float.
+        if (exp_end != exp_start && valid) {
+            value = FLOAT;
+        }
+    }
+
+    // If the parsing was not valid or we are not at the end of the string
+    // then the string is invalid.
+    // Othewise, do a check to see if it is an *intlike* float.
+    if (!valid || str != end) {
+        return INVALID;
+    } else if (value == FLOAT) {
+        // If we "move the decimal place" left or right depending on
+        // exponent sign and magnitude, all digits after the decimal
+        // must be zero.
+        const unsigned int_trailing_zeros
+            = number_trailing_zeros(int_start, decimal_start);
+        const unsigned dec_trailing_zeros = decimal_start == decimal_end
+            ? 0U
+            : number_trailing_zeros(decimal_start + 1, decimal_end);
+
+        if (exp_negative) {
+            if (expon <= int_trailing_zeros && dec_length == dec_trailing_zeros) {
+                value = INTLIKE_FLOAT;
+            }
+        } else {
+            if (expon >= (dec_length - dec_trailing_zeros)) {
+                value = INTLIKE_FLOAT;
+            }
+        }
+    }
+    return value;
+}
+
+void remove_valid_underscores(char* str, const char*& end, const bool based) noexcept
+{
+    // Ignore a leading negative sign
+    if (*str == '-') {
+        str += 1;
+    }
+
+    const std::size_t len = static_cast<std::size_t>(end - str);
+    std::size_t i, offset;
+
+    // The method that will be used to remove underscores is to
+    // traverse the character array, and when a valid underscore
+    // is found all characters will be shifted one to the left in
+    // order to remove that underscore. Extra characters at the
+    // end of the character array will be overwritten with \0.
+
+    // For non-based strings, parsing is "simple" -
+    // a valid underscore is surrounded by two numbers.
+    if (!based) {
+        for (i = offset = 0; i < len; i++) {
+            if (str[i] == '_' && i > 0 && i < len - 1 && is_valid_digit(str[i - 1])
+                && is_valid_digit(str[i + 1])) {
+                offset += 1;
+                continue;
+            }
+            if (offset) {
+                str[i - offset] = str[i];
+            }
+        }
+    }
+
+    // For based strings we must incorporate some state at the
+    // beginning of the string before the more simple "surrounded
+    // by two numbers" algorithm kicks in.
+    else {
+        i = offset = 0;
+
+        // Skip leading characters for non-base 10 ints.
+        if ((len - i) > 1 && str[i] == '0' && is_base_prefix(str[i + 1])) {
+            // An underscore after the prefix is allowed, e.g. 0x_d4.
+            if ((len - i > 2) && str[i + 2] == '_') {
+                i += 3;
+                offset += 1;
+            } else {
+                i += 2;
+            }
+        }
+        // No underscore in the base selector, e.g. 0_b0 is invalid.
+        else if ((len - i) > 2 && str[i] == '0' && str[i + 1] == '_' && is_base_prefix(str[i + 2])) {
+            i += 3;
+        }
+
+        // Now search for simpler valid underscores.
+        // use base 36 as the base because it is the most inclusive.
+        for (; i < len; i++) {
+            if (str[i] == '_' && i > 0 && i < len - 1 && is_valid_digit(str[i - 1], 36)
+                && is_valid_digit(str[i + 1], 36)) {
+                offset += 1;
+                continue;
+            }
+            if (offset) {
+                str[i - offset] = str[i];
+            }
+        }
+    }
+
+    // Update the end position.
+    end = str + (i - offset);
+
+    // Fill the trailing data with nul characters.
+    for (i = len - offset; i < len; i++) {
+        str[i] = '\0';
+    }
+}
---
+++ packages/fastnumbers/fastnumbers-5.0.0.tar.gz/fastnumbers-5.0.0/src/cpp/parser.cpp
@@ -0,0 +1,202 @@
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#include <Python.h>
+
+#include "fastnumbers/buffer.hpp"
+#include "fastnumbers/c_str_parsing.hpp"
+#include "fastnumbers/helpers.hpp"
+#include "fastnumbers/parser/base.hpp"
+#include "fastnumbers/parser/character.hpp"
+#include "fastnumbers/parser/numeric.hpp"
+#include "fastnumbers/parser/unicode.hpp"
+#include "fastnumbers/payload.hpp"
+#include "fastnumbers/user_options.hpp"
+
+/**
+ * \brief Remove whitespace at the end of a string
+ *
+ * To do this successfully, move the end pointer
+ * back one to not be on the '\0' character, move
+ * backwards, then push the end pointer up one
+ * as if there were still a '\0' character.
+ *
+ * \param start The beginning of the string to strip
+ * \param end The end of the string to strip - updated in-place
+ */
+inline void strip_trailing_whitespace(const char* start, const char*& end) noexcept
+{
+    while (start < end && is_whitespace(*(end - 1))) {
+        end -= 1;
+    }
+}
+
+/// Convert a 64-int with the appropriate function depending on the compiler's long size
+inline PyObject* pyobject_from_int64(const int64_t value) noexcept
+{
+    if constexpr (std::numeric_limits<long>::max() == std::numeric_limits<int64_t>::max()) {
+        return PyLong_FromLong(static_cast<long>(value));
+    } else {
+        return PyLong_FromLongLong(static_cast<long long>(value));
+    };
+}
+
+CharacterParser::CharacterParser(
+    const char* str,
+    const std::size_t len,
+    const UserOptions& options,
+    const bool explict_base_allowed
+) noexcept
+    : Parser(ParserType::CHARACTER, options, explict_base_allowed)
+    , m_start(str)
+    , m_start_orig(str)
+    , m_end_orig(str + len)
+    , m_str_len(0)
+{
+    // Store the end point of the character array
+    const char* end = m_end_orig;
+
+    // Strip leading whitespace
+    while (is_whitespace(*m_start)) {
+        m_start += 1;
+    }
+
+    // Strip trailing whitespace.
+    strip_trailing_whitespace(m_start, end);
+
+    // Remove the sign if present and remember what it represents
+    if (*m_start == '+') {
+        m_start += 1;
+    } else if (*m_start == '-') {
+        m_start += 1;
+        set_negative();
+    }
+
+    // We can only have a sign here if there are two consecutive signs.
+    // Two or more signs is illegal - let's treat it as such.
+    // Reset the start to before the first sign.
+    // All parsers will treat this as illegal now.
+    if (is_sign(*m_start)) {
+        m_start -= 1;
+        set_negative(false);
+    }
+
+    // Calculate the length of the string after accounting for
+    // whitespace and signs
+    m_str_len = static_cast<std::size_t>(end - m_start);
+}
+
+RawPayload<PyObject*> CharacterParser::as_pyint() const noexcept(false)
+{
+    // We use the fast path method even if the result overflows,
+    // so that we can determine if the integer was at least valid.
+    // If it was valid but overflowed, we use Python's parser, otherwise
+    // return an error.
+    // The only thing special handling we need is underscores or base prefixes
+    // with negative signs that caused overflow.
+    bool error;
+    bool overflow;
+    int64_t result = parse_int<int64_t>(
+        signed_start(), end(), options().get_base(), error, overflow
+    );
+    const bool underscore_error = error && has_valid_underscores();
+    const bool prefix_overflow = overflow && has_base_prefix(m_start, m_str_len);
+    if (underscore_error || prefix_overflow) {
+        Buffer buffer(signed_start(), signed_len());
+        buffer.remove_valid_underscores(options().get_base() != 10);
+        int base = options().get_base();
+        if (base == 0) {
+            base = detect_base(buffer.start(), buffer.end());
+        }
+        buffer.remove_base_prefix();
+        result = parse_int<int64_t>(buffer.start(), buffer.end(), base, error, overflow);
+    }
+    if (error) {
+        return ErrorType::BAD_VALUE;
+    }
+    if (!overflow) {
+        return pyobject_from_int64(result);
+    }
+
+    // Parse and record the location where parsing ended (including trailing whitespace)
+    // No need to do input validation with the second argument because we already know
+    // the input is valid from above.
+    PyObject* retval = PyLong_FromString(m_start_orig, nullptr, options().get_base());
+
+    // Return the value without checking python's error state
+    return retval;
+}
+
+RawPayload<PyObject*>
+CharacterParser::as_pyfloat(const bool force_int, const bool coerce) const
+    noexcept(false)
+{
+    // Perform the correct action depending on the payload's contents
+    return std::visit(
+        overloaded {
+
+            // If the payload contained a double, convert it to a PyObject*
+            [force_int, coerce](const double result) -> RawPayload<PyObject*> {
+                // force_int takes precidence,
+                // and coerce conditionally returns as an integer
+                if (force_int) {
+                    return PyLong_FromDouble(result);
+                } else if (coerce) {
+                    return Parser::float_is_intlike(result) ? PyLong_FromDouble(result)
+                                                            : PyFloat_FromDouble(result);
+                } else {
+                    return PyFloat_FromDouble(result);
+                }
+            },
+
+            // If the payload contained an error, pass the error along
+            [](const ErrorType err) -> RawPayload<PyObject*> {
+                return err;
+            },
+        },
+        as_number<double>()
+    );
+}
+
+NumberFlags CharacterParser::get_number_type() const noexcept
+{
+    // If this value is cached, use that instead of re-calculating
+    if (Parser::get_number_type() != static_cast<NumberFlags>(NumberType::UNSET)) {
+        return Parser::get_number_type();
+    }
+
+    // If the string contains an infinity or NaN then we don't need to do any
+    // other fancy processing and can return now.
+    if (quick_detect_infinity(m_start, m_str_len)) {
+        return flag_wrap(NumberType::Float | NumberType::Infinity);
+
+    } else if (quick_detect_nan(m_start, m_str_len)) {
+        return flag_wrap(NumberType::Float | NumberType::NaN);
+    }
+
+    // If the string contains a numeric representation,
+    // report which representation type is contained.
+    int value = string_contains_what(m_start, end(), options().get_base());
+
+    // If it still looks like there is no numeric representation in the string,
+    // check to see if it it contains underscores, and if so remove them and
+    // try a numeric representation again. No need to check for infinity and
+    // NaN here because those are not allowed to contain underscores.
+    if (value == 0 && has_valid_underscores()) {
+        Buffer buffer(m_start, m_str_len);
+        buffer.remove_valid_underscores(!options().is_default_base());
+        value = string_contains_what(buffer.start(), buffer.end(), options().get_base());
+    }
+
+    // Map integer values to numeric flag values
+    static constexpr NumberFlags type_mapping[] = {
+        /* 0 */ NumberType::INVALID,
+        /* 1 */ flag_wrap(NumberType::Integer),
+        /* 2 */ flag_wrap(NumberType::Float),
+        /* 3 */ flag_wrap(NumberType::Float | NumberType::IntLike),
+    };
+
+    // Return the found type
+    return type_mapping[value];
+}
