d5a8cc75a954a71010aeda79efa367225406bcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package splight-lib/2.0.20/splight_lib-2.0.20-py3-none-any.whl

---
+++ packages/splight-lib/splight_lib-2.0.20-py3-none-any.whl/remote_splight_lib/database/classmap.py
@@ -0,0 +1,67 @@
+from splight_models import (
+    Asset,
+    Attribute,
+    File,
+    Edge,
+    Graph,
+    Component,
+    Notification,
+    Query,
+    Node,
+    BlockchainContract,
+    Component,
+    HubComponent,
+    ComponentObject,
+    ComponentCommand,
+    Secret,
+    SetPoint,
+)
+
+
+CLASSMAP = {
+    Component: {
+        "path": "v2/engine/component/components"
+    },
+    Asset: {
+        "path": "v2/engine/assets"
+    },
+    Attribute: {
+        "path": "v2/engine/attributes"
+    },
+    Edge: {
+        "path": "v2/engine/graph/edges"
+    },
+    File: {
+        "path": "v2/engine/files"
+    },
+    Graph: {
+        "path": "v2/engine/graph/graphs"
+    },
+    Notification: {
+        "path": "v2/account/notifications"
+    },
+    Node: {
+        "path": "v2/engine/graph/nodes"
+    },
+    BlockchainContract: {
+        "path": "v2/backoffice/blockchain/contracts"
+    },
+    ComponentObject: {
+        "path": "v2/engine/component/objects"
+    },
+    ComponentCommand: {
+        "path": "v2/engine/component/commands"
+    },
+    Query: {
+        "path": "v2/engine/queries"
+    },
+    Secret: {
+        "path": "v2/engine/secrets"
+    },
+    HubComponent: {
+        "path": "v2/engine/hub/component-versions"
+    },
+    SetPoint: {
+        "path": "v2/engine/setpoints"
+    },
+}
