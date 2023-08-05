340e02ef8cd53292212567fa11d8c161ffdd8566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package augraphy/8.0.2/augraphy-8.0.2-py3-none-any.whl

---
+++ packages/augraphy/augraphy-8.0.2-py3-none-any.whl/augraphy/base/augmentation.py
@@ -0,0 +1,23 @@
+import random
+
+
+class Augmentation:
+    """The base class which all pipeline augmentations inherit from.
+
+    :param p: The probability that this augmentation will be run when executed as part of a pipeline.
+    :type p: float, optional
+    """
+
+    def __init__(self, p=0.5):
+        """Constructor method"""
+        self.p = p
+
+    def should_run(self):
+        """Determines whether or not the augmentation should be applied
+        by callers.
+
+        :return: True if the probability given was no smaller than the
+            random sample on the unit interval.
+        :rtype: bool
+        """
+        return random.uniform(0.0, 1.0) <= self.p
---
+++ packages/augraphy/augraphy-8.0.2-py3-none-any.whl/augraphy/utilities/foreign.py
@@ -0,0 +1,41 @@
+"""This file contains a wrapper class for augmentations from other projects.
+Currently supported:
+---
+1. Albumentations: https://github.com/albumentations-team/albumentations
+2. Imgaug: https://github.com/aleju/imgaug
+"""
+import numpy
+
+from augraphy.base.augmentation import Augmentation
+
+
+class ForeignAugmentation(Augmentation):
+    """A wrapper for augmentations from other projects.
+
+    :param foreignAugmentation: The fully-applied constructor for the foreign transform.
+    :type foreignAugmentation: object
+    :param p: The probability that augmentation will be applied.
+    :type p: float, optional
+    """
+
+    def __init__(self, foreignAugmentation, p=1):
+        self.augmentation = foreignAugmentation
+        super().__init__(p=p)
+
+    def __call__(self, image, layer=None, force=False):
+
+        image = image.copy()
+        result = self.augmentation(image=image)
+        output = self.handleForeignAugResult(result)
+        return output
+
+    def handleForeignAugResult(*res):
+        """The argument to this depends on the foreign augmentation applied.
+        If an Albumentation augmentation is used, the result is a dict with the
+        output image at key "image".
+        If an imgaug augmenter is used, the result is a numpy.ndarray.
+        """
+        if isinstance(res[1], dict):
+            return res[1]["image"]
+        else:
+            return res[1]
