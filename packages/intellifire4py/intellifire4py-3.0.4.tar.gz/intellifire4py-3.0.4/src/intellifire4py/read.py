a0f4f48dee5acd56e2767eae6dbad4c124b831ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:37 +0000

    Add package intellifire4py/3.0.4/intellifire4py-3.0.4.tar.gz

---
+++ packages/intellifire4py/intellifire4py-3.0.4.tar.gz/intellifire4py-3.0.4/src/intellifire4py/read.py
@@ -0,0 +1,32 @@
+"""IntelliFire abstract read class."""
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+from intellifire4py.model import IntelliFirePollData
+
+
+class IntelliFireDataProvider(ABC):
+    """Abstract base class to provide read logic."""
+
+    @property
+    @abstractmethod
+    def data(self) -> IntelliFirePollData:
+        """Return data to the user."""
+        return IntelliFirePollData()
+
+    @property
+    @abstractmethod
+    def is_polling_in_background(self) -> bool:
+        """Return whether api is polling."""
+        return False
+
+    @abstractmethod
+    async def start_background_polling(self, minimum_wait_in_seconds: int = 15) -> None:
+        """Abstract start polling."""
+        pass
+
+    @abstractmethod
+    def stop_background_polling(self) -> bool:
+        """Abstract stop polling."""
+        return False
