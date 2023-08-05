05488e32243c6aaf4aff571e99200e9db44fac45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:01 +0000

    Add package meent/0.7.9/meent-0.7.9.tar.gz

---
+++ packages/meent/meent-0.7.9.tar.gz/meent-0.7.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meent',
+    version='0.7.9',
+    url='https://github.com/kc-ml2/meent',
+    author='KC ML2',
+    author_email='yongha@kc-ml2.com',
+    packages=['meent'] + find_packages(include=['meent.*']),
+    install_requires=[
+        'numpy==1.23.3',
+        'jax==0.4.1',
+    ],
+    python_requires='>=3.8',
+    long_description_content_type="text/markdown",
+    package_data={
+        'meent': ['nk_data/filmetrics/*.txt', 'nk_data/matlab/*.mat'],
+    },
+)
