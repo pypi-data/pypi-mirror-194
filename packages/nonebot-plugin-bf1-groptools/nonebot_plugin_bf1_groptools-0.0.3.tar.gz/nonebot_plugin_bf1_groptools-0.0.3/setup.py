34b1b7422a28d5732903a164cbfa5e7bf23817c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:38 +0000

    Add package nonebot-plugin-bf1-groptools/0.0.3/nonebot_plugin_bf1_groptools-0.0.3.tar.gz

---
+++ packages/nonebot-plugin-bf1-groptools/nonebot_plugin_bf1_groptools-0.0.3.tar.gz/nonebot_plugin_bf1_groptools-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension
+setup(name='nonebot_plugin_bf1_groptools',
+      requires=['requests', 'nonebot'],
+      version='0.0.3',
+      description='Nonebot plugin that verifies BF1 accounts '
+                  'and automatically approves group applications and changes business cards.',
+      author='qienoob',
+      author_email='196859929@qq.com',
+        # 定义依赖哪些模块
+      # packages=find_packages(),  # 系统自动从当前目录开始找包
+      # 如果有的文件不用打包，则只能指定需要打包的文件
+      py_modules=['__init__'],  #指定目录中需要打包的py文件，注意不要.py后缀
+      license="apache 3.0",
+      )
