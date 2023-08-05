28bed228d560e60b3f790bbcac38cb6f27476c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:50 +0000

    Add package returnn/1.20230224.144852/returnn-1.20230224.144852.tar.gz

---
+++ packages/returnn/returnn-1.20230224.144852.tar.gz/returnn-1.20230224.144852/returnn/tf/frontend/frontend.py
@@ -0,0 +1,11 @@
+"""
+Frontend for exposing TensorFlow-specific functionality.
+"""
+
+from returnn.frontend_api import Frontend
+
+
+class TFFrontend(Frontend):
+    """
+    TensorFlow frontend
+    """
---
+++ packages/returnn/returnn-1.20230224.144852.tar.gz/returnn-1.20230224.144852/returnn/torch/frontend/frontend.py
@@ -0,0 +1,11 @@
+"""
+Frontend for exposing PyTorch-specific functionality.
+"""
+
+from returnn.frontend_api import Frontend
+
+
+class TorchFrontend(Frontend):
+    """
+    PyTorch frontend
+    """
