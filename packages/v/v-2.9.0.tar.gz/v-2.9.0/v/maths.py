916ed17ffda21def3e9e88ad98fc4b5374679f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:30 +0000

    Add package v/2.9.0/v-2.9.0.tar.gz

---
+++ packages/v/v-2.9.0.tar.gz/v-2.9.0/v/maths.py
@@ -0,0 +1,54 @@
+import math
+
+def add(*numbers):
+	total = 0
+	for number in numbers:
+		total += number
+	return total
+
+def sub(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total -= number
+	return total
+
+def mult(*numbers):
+	total = 1
+	for number in numbers:
+		total *= number
+	return total
+
+def div(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total /= number
+	return total
+
+def int_div(*numbers):
+	total = numbers[0]
+	for number in numbers[1:]:
+		total //= number
+	return total
+	
+def rnd(number, accuracy):
+	a = number / accuracy
+	if a % 1 >= 0.5:
+		a //= 1
+		a += 1
+	else:
+		a //= 1
+	b = a * accuracy
+	if b % 1 == 0:
+		return int(b)
+	return b
+
+def ceil(number):
+	if number % 1 == 0:
+		return int(number)
+	return int(number) + 1
+
+def floor(number):
+	return int(number)
+
+def comb(n, k):
+	return math.comb(n, k)
