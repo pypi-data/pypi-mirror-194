51305b22dc3141f4970911bd4f378cdcd9570ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:54 +0000

    Add package tmebot/1.0.1/tmebot-1.0.1-py3-none-any.whl

---
+++ packages/tmebot/tmebot-1.0.1-py3-none-any.whl/tmebot/filters/__init__.py
@@ -0,0 +1,12 @@
+from magic_filter import F
+from tapp.filters import Filter
+
+from .chat_type import ChatType
+from .text import Text
+
+__all__ = (
+    "F",
+    "Filter",
+    "ChatType",
+    "Text",
+)
