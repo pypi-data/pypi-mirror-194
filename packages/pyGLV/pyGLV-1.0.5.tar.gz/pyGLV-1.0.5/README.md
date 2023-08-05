550e12ed0bb22669adc34a0621f6dddd205a4867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:49 +0000

    Add package pyGLV/1.0.5/pyGLV-1.0.5.tar.gz

---
+++ packages/pyGLV/pyGLV-1.0.5.tar.gz/pyGLV-1.0.5/README.md
@@ -0,0 +1,51 @@
+# The pyGLV package
+
+## pyGLV (computer **G**raphics for deep **L**earning and scientific **V**isualization)
+
+A python, pure software design pattern based package that used plain and simple **E**ntities, **C**omponents and **S**ystems in a **S**cenegraph architecture from thge `pyECSS` package, in order to showcase basic, cross-platform OpenGL-based real-time computer graphics with applications to scientific visualization and deep learning.
+
+---
+
+
+> **Why GLV?**:
+>
+> This package is aimed as a basic behind-the-black-box implementation of several classic as well as modern computer graphics  methodologies, algorithms and techniques, aimed for teaching as well as framework for novel research.
+
+The following software design patterns are employed:
+
+- Decorator Pattern: *RenderDecorator, ComponentDecorator, SystemDecorator*
+- game-loop pattern (GPP non GoF): *Scene*
+
+## Installation
+
+- For `standalone` use, via `pip`
+
+  ```
+  pip install pyglv
+  ```
+
+More information can be found in [Documentation](https://pyglv.readthedocs.io) and specifically at 
+[Installation](https://pyglv.readthedocs.io/en/latest/installation.html).
+
+
+## Contributors
+
+- Prof. George Papagiannakis
+- Dr. Kamarianakis Manos
+- Geronikolakis Stratos
+- Zack Pervolarakis
+- George Kokiadis
+  
+---
+
+### *Copyright 2021-2023 Dr. George Papagiannakis,  papagian@csd.uoc.gr*
+
+### *All Rights Reserved*
+
+### *University of Crete & Foundation for Research & Technology - Hellas (FORTH)*
+
+---
+## Licensing
+
+pyGLV is licensed under the Apache License, Version 2.0. See
+LICENSE.txt for the full license text.
---
+++ packages/pyGLV/pyGLV-1.0.5.tar.gz/pyGLV-1.0.5/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+# imports from __version__
+with open(path.join(this_directory, 'pyGLV', '_version.py'), encoding='utf-8') as f:
+    exec(f.read())
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyGLV',
+    version=__version__, 
+    license='Apache 2', 
+    description = "pyGLV (computer Graphics for deep Learning and scientific Visualization)",
+    long_description=long_description,
+    # long_description= file: README.md, LICENSE.txt
+    long_description_content_type='text/markdown',
+    author = "George Papagiannakis", 
+    author_email = "papagian@csd.uoc.gr",
+    maintainer='Manos Kamarianakis',
+    maintainer_email='m.kamarianakis@gmail.com',
+    url='https://github.com/papagiannakis/pyGLV',
+    keywords = ['ECS','Scenegraph','Python design patterns','Computer Graphics'],
+    package_dir={'pyGLV':'pyGLV'},
+    packages=find_packages(exclude=["tests","tests.*", "tests/*" ]),
+    install_requires=[
+        'pip',
+        'setuptools>=61',
+        'wheel',
+        'numpy',
+        'scipy',
+        'pyECSS',
+        'imgui',
+        'PyOpenGL',
+        'PyOpenGL_accelerate',
+        'pysdl2',
+        'pysdl2-dll',
+        'ipykernel',
+    ],
+    
+
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    project_urls={
+        "Homepage" : "https://github.com/papagiannakis/pyGLV",
+        "Documentation" : "https://pyglv.readthedocs.io",
+    },
+
+    python_requires=">=3.8,<3.10",
+)
