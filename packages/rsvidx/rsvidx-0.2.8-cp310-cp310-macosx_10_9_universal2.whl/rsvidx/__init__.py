6f07704666bbb371633fe6130962ae02c9932e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:18 +0000

    Add package rsvidx/0.2.8/rsvidx-0.2.8-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/rsvidx/rsvidx-0.2.8-cp310-cp310-macosx_10_9_universal2.whl/rsvidx/__init__.py
@@ -0,0 +1,11 @@
+from rsvidx.rsvidx import Similarity
+
+"""
+RSvIDX.
+
+Vector similarity search.
+"""
+
+__version__ = "0.2.8"
+__author__ = 'RubenSystems, Ruben Ticehurst-James'
+__credits__ = 'RubenSystemsAI'
