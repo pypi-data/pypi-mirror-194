17b9734f135f07056a650219dec81c83ce8a9611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:33 +0000

    Add package dag-cbor/0.3.1/dag-cbor-0.3.1.tar.gz

---
+++ packages/dag-cbor/dag-cbor-0.3.1.tar.gz/dag-cbor-0.3.1/dag_cbor/__init__.py
@@ -0,0 +1,14 @@
+"""
+    Python implementation of the `DAG-CBOR codec <https://ipld.io/specs/codecs/dag-cbor/spec/>`_ specification.
+"""
+
+from __future__ import annotations # See https://peps.python.org/pep-0563/
+
+__version__ = "0.3.1"
+
+from .ipld import IPLDKind, IPLDScalarKind, ObjPath
+from .encoding import encode
+from .decoding import decode
+
+# explicit re-exports
+__all__ = ["encode", "decode", "IPLDKind", "IPLDScalarKind", "ObjPath"]
