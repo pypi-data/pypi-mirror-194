ac6f5c79c73773d926986333271a2f7c49a84ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:31 +0000

    Add package prowler/3.2.3/prowler-3.2.3-py3-none-any.whl

---
+++ packages/prowler/prowler-3.2.3-py3-none-any.whl/prowler/__main__.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+from prowler.lib.banner import print_banner
+from prowler.lib.check.check import (
+    bulk_load_checks_metadata,
+    bulk_load_compliance_frameworks,
+    exclude_checks_to_run,
+    exclude_services_to_run,
+    execute_checks,
+    get_checks_from_input_arn,
+    get_regions_from_audit_resources,
+    list_categories,
+    list_services,
+    print_categories,
+    print_checks,
+    print_compliance_frameworks,
+    print_compliance_requirements,
+    print_services,
+)
+from prowler.lib.check.checks_loader import load_checks_to_execute
+from prowler.lib.check.compliance import update_checks_metadata_with_compliance
+from prowler.lib.cli.parser import ProwlerArgumentParser
+from prowler.lib.logger import logger, set_logging_config
+from prowler.lib.outputs.compliance import display_compliance_table
+from prowler.lib.outputs.html import add_html_footer, fill_html_overview_statistics
+from prowler.lib.outputs.json import close_json
+from prowler.lib.outputs.outputs import extract_findings_statistics, send_to_s3_bucket
+from prowler.lib.outputs.summary_table import display_summary_table
+from prowler.providers.aws.lib.allowlist.allowlist import parse_allowlist_file
+from prowler.providers.aws.lib.quick_inventory.quick_inventory import quick_inventory
+from prowler.providers.aws.lib.security_hub.security_hub import (
+    resolve_security_hub_previous_findings,
+)
+from prowler.providers.common.audit_info import set_provider_audit_info
+from prowler.providers.common.outputs import set_provider_output_options
+
+
+def prowler():
+    # Parse Arguments
+    parser = ProwlerArgumentParser()
+    args = parser.parse()
+
+    # Save Arguments
+    provider = args.provider
+    checks = args.checks
+    excluded_checks = args.excluded_checks
+    excluded_services = args.excluded_services
+    services = args.services
+    categories = args.categories
+    checks_file = args.checks_file
+    severities = args.severity
+    compliance_framework = args.compliance
+
+    # We treat the compliance framework as another output format
+    if compliance_framework:
+        args.output_modes.extend(compliance_framework)
+
+    # Set Logger configuration
+    set_logging_config(args.log_level, args.log_file, args.only_logs)
+
+    if not args.no_banner:
+        print_banner(args)
+
+    if args.list_services:
+        print_services(list_services(provider))
+        sys.exit()
+
+    # Load checks metadata
+    logger.debug("Loading checks metadata from .metadata.json files")
+    bulk_checks_metadata = bulk_load_checks_metadata(provider)
+
+    if args.list_categories:
+        print_categories(list_categories(provider, bulk_checks_metadata))
+        sys.exit()
+
+    bulk_compliance_frameworks = {}
+    # Load compliance frameworks
+    logger.debug("Loading compliance frameworks from .json files")
+
+    # Load the compliance framework if specified with --compliance
+    # If some compliance argument is specified we have to load it
+    if (
+        args.list_compliance
+        or args.list_compliance_requirements
+        or compliance_framework
+    ):
+        bulk_compliance_frameworks = bulk_load_compliance_frameworks(provider)
+        # Complete checks metadata with the compliance framework specification
+        update_checks_metadata_with_compliance(
+            bulk_compliance_frameworks, bulk_checks_metadata
+        )
+        if args.list_compliance:
+            print_compliance_frameworks(bulk_compliance_frameworks)
+            sys.exit()
+        if args.list_compliance_requirements:
+            print_compliance_requirements(
+                bulk_compliance_frameworks, args.list_compliance_requirements
+            )
+            sys.exit()
+
+    # Load checks to execute
+    checks_to_execute = load_checks_to_execute(
+        bulk_checks_metadata,
+        bulk_compliance_frameworks,
+        checks_file,
+        checks,
+        services,
+        severities,
+        compliance_framework,
+        categories,
+        provider,
+    )
+
+    # Exclude checks if -e/--excluded-checks
+    if excluded_checks:
+        checks_to_execute = exclude_checks_to_run(checks_to_execute, excluded_checks)
+
+    # Exclude services if --excluded-services
+    if excluded_services:
+        checks_to_execute = exclude_services_to_run(
+            checks_to_execute, excluded_services, provider
+        )
+
+    # Sort final check list
+    checks_to_execute = sorted(checks_to_execute)
+
+    # If -l/--list-checks passed as argument, print checks to execute and quit
+    if args.list_checks:
+        print_checks(provider, checks_to_execute, bulk_checks_metadata)
+        sys.exit()
+
+    # Set the audit info based on the selected provider
+    audit_info = set_provider_audit_info(provider, args.__dict__)
+
+    # Once the audit_info is set and we have the eventual checks from arn, it is time to exclude the others
+    if audit_info.audit_resources:
+        audit_info.audited_regions = get_regions_from_audit_resources(
+            audit_info.audit_resources
+        )
+        checks_to_execute = get_checks_from_input_arn(
+            audit_info.audit_resources, provider
+        )
+
+    # Parse content from Allowlist file and get it, if necessary, from S3
+    if provider == "aws" and args.allowlist_file:
+        allowlist_file = parse_allowlist_file(audit_info, args.allowlist_file)
+    else:
+        allowlist_file = None
+
+    # Setting output options based on the selected provider
+    audit_output_options = set_provider_output_options(
+        provider, args, audit_info, allowlist_file, bulk_checks_metadata
+    )
+
+    # Quick Inventory for AWS
+    if provider == "aws" and args.quick_inventory:
+        quick_inventory(audit_info, args.output_directory)
+        sys.exit()
+
+    # Execute checks
+    findings = []
+    if len(checks_to_execute):
+        findings = execute_checks(
+            checks_to_execute, provider, audit_info, audit_output_options
+        )
+    else:
+        logger.error(
+            "There are no checks to execute. Please, check your input arguments"
+        )
+
+    # Extract findings stats
+    stats = extract_findings_statistics(findings)
+
+    if args.output_modes:
+        for mode in args.output_modes:
+            # Close json file if exists
+            if mode == "json" or mode == "json-asff":
+                close_json(
+                    audit_output_options.output_filename, args.output_directory, mode
+                )
+            if mode == "html":
+                add_html_footer(
+                    audit_output_options.output_filename, args.output_directory
+                )
+                fill_html_overview_statistics(
+                    stats, audit_output_options.output_filename, args.output_directory
+                )
+            # Send output to S3 if needed (-B / -D)
+            if provider == "aws" and (
+                args.output_bucket or args.output_bucket_no_assume
+            ):
+                output_bucket = args.output_bucket
+                bucket_session = audit_info.audit_session
+                # Check if -D was input
+                if args.output_bucket_no_assume:
+                    output_bucket = args.output_bucket_no_assume
+                    bucket_session = audit_info.original_session
+                send_to_s3_bucket(
+                    audit_output_options.output_filename,
+                    args.output_directory,
+                    mode,
+                    output_bucket,
+                    bucket_session,
+                )
+
+    # Resolve previous fails of Security Hub
+    if provider == "aws" and args.security_hub and not args.skip_sh_update:
+        resolve_security_hub_previous_findings(args.output_directory, audit_info)
+
+    # Display summary table
+    if not args.only_logs:
+        display_summary_table(
+            findings,
+            audit_info,
+            audit_output_options,
+            provider,
+        )
+
+        if compliance_framework and findings:
+            for compliance in compliance_framework:
+                # Display compliance table
+                display_compliance_table(
+                    findings,
+                    bulk_checks_metadata,
+                    compliance,
+                    audit_output_options.output_filename,
+                    audit_output_options.output_directory,
+                )
+
+    # If there are failed findings exit code 3, except if -z is input
+    if not args.ignore_exit_code_3 and stats["total_fail"] > 0:
+        sys.exit(3)
+
+
+if __name__ == "__main__":
+    prowler()
---
+++ packages/prowler/prowler-3.2.3-py3-none-any.whl/prowler/providers/aws/aws_provider.py
@@ -0,0 +1,158 @@
+import os
+import pathlib
+import sys
+
+from boto3 import session
+from botocore.credentials import RefreshableCredentials
+from botocore.session import get_session
+
+from prowler.config.config import aws_services_json_file
+from prowler.lib.logger import logger
+from prowler.lib.utils.utils import open_file, parse_json_file
+from prowler.providers.aws.lib.audit_info.models import AWS_Assume_Role, AWS_Audit_Info
+
+
+################## AWS PROVIDER
+class AWS_Provider:
+    def __init__(self, audit_info):
+        logger.info("Instantiating aws provider ...")
+        self.aws_session = self.set_session(audit_info)
+        self.role_info = audit_info.assumed_role_info
+
+    def get_session(self):
+        return self.aws_session
+
+    def set_session(self, audit_info):
+        try:
+            if audit_info.credentials:
+                # If we receive a credentials object filled is coming form an assumed role, so renewal is needed
+                logger.info("Creating session for assumed role ...")
+                # From botocore we can use RefreshableCredentials class, which has an attribute (refresh_using)
+                # that needs to be a method without arguments that retrieves a new set of fresh credentials
+                # asuming the role again. -> https://github.com/boto/botocore/blob/098cc255f81a25b852e1ecdeb7adebd94c7b1b73/botocore/credentials.py#L395
+                assumed_refreshable_credentials = RefreshableCredentials(
+                    access_key=audit_info.credentials.aws_access_key_id,
+                    secret_key=audit_info.credentials.aws_secret_access_key,
+                    token=audit_info.credentials.aws_session_token,
+                    expiry_time=audit_info.credentials.expiration,
+                    refresh_using=self.refresh,
+                    method="sts-assume-role",
+                )
+                # Here we need the botocore session since it needs to use refreshable credentials
+                assumed_botocore_session = get_session()
+                assumed_botocore_session._credentials = assumed_refreshable_credentials
+                assumed_botocore_session.set_config_variable(
+                    "region", audit_info.profile_region
+                )
+                return session.Session(
+                    profile_name=audit_info.profile,
+                    botocore_session=assumed_botocore_session,
+                )
+            # If we do not receive credentials start the session using the profile
+            else:
+                logger.info("Creating session for not assumed identity ...")
+                return session.Session(profile_name=audit_info.profile)
+        except Exception as error:
+            logger.critical(f"{error.__class__.__name__} -- {error}")
+            sys.exit(1)
+
+    # Refresh credentials method using assume role
+    # This method is called "adding ()" to the name, so it cannot accept arguments
+    # https://github.com/boto/botocore/blob/098cc255f81a25b852e1ecdeb7adebd94c7b1b73/botocore/credentials.py#L570
+    def refresh(self):
+        logger.info("Refreshing assumed credentials...")
+
+        response = assume_role(self.aws_session, self.role_info)
+        refreshed_credentials = dict(
+            # Keys of the dict has to be the same as those that are being searched in the parent class
+            # https://github.com/boto/botocore/blob/098cc255f81a25b852e1ecdeb7adebd94c7b1b73/botocore/credentials.py#L609
+            access_key=response["Credentials"]["AccessKeyId"],
+            secret_key=response["Credentials"]["SecretAccessKey"],
+            token=response["Credentials"]["SessionToken"],
+            expiry_time=response["Credentials"]["Expiration"].isoformat(),
+        )
+        logger.info("Refreshed Credentials:")
+        logger.info(refreshed_credentials)
+        return refreshed_credentials
+
+
+def assume_role(session: session.Session, assumed_role_info: AWS_Assume_Role) -> dict:
+    try:
+        # set the info to assume the role from the partition, account and role name
+        sts_client = session.client("sts")
+        # If external id, set it to the assume role api call
+        if assumed_role_info.external_id:
+            assumed_credentials = sts_client.assume_role(
+                RoleArn=assumed_role_info.role_arn,
+                RoleSessionName="ProwlerProAsessmentSession",
+                DurationSeconds=assumed_role_info.session_duration,
+                ExternalId=assumed_role_info.external_id,
+            )
+        # else assume the role without the external id
+        else:
+            assumed_credentials = sts_client.assume_role(
+                RoleArn=assumed_role_info.role_arn,
+                RoleSessionName="ProwlerProAsessmentSession",
+                DurationSeconds=assumed_role_info.session_duration,
+            )
+    except Exception as error:
+        logger.critical(f"{error.__class__.__name__} -- {error}")
+        sys.exit(1)
+
+    else:
+        return assumed_credentials
+
+
+def generate_regional_clients(
+    service: str, audit_info: AWS_Audit_Info, global_service: bool = False
+) -> dict:
+    try:
+        regional_clients = {}
+        # Get json locally
+        actual_directory = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+        f = open_file(f"{actual_directory}/{aws_services_json_file}")
+        data = parse_json_file(f)
+        # Check if it is a subservice
+        json_regions = data["services"][service]["regions"][
+            audit_info.audited_partition
+        ]
+        if audit_info.audited_regions:  # Check for input aws audit_info.audited_regions
+            regions = list(
+                set(json_regions).intersection(audit_info.audited_regions)
+            )  # Get common regions between input and json
+        else:  # Get all regions from json of the service and partition
+            regions = json_regions
+        # Check if it is global service to gather only one region
+        if global_service:
+            if regions:
+                if audit_info.profile_region in regions:
+                    regions = [audit_info.profile_region]
+                regions = regions[:1]
+        for region in regions:
+            regional_client = audit_info.audit_session.client(
+                service, region_name=region, config=audit_info.session_config
+            )
+            regional_client.region = region
+            regional_clients[region] = regional_client
+        return regional_clients
+    except Exception as error:
+        logger.error(
+            f"{error.__class__.__name__}[{error.__traceback__.tb_lineno}]: {error}"
+        )
+
+
+def get_aws_available_regions():
+    try:
+        actual_directory = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+        f = open_file(f"{actual_directory}/{aws_services_json_file}")
+        data = parse_json_file(f)
+
+        regions = set()
+        for service in data["services"].values():
+            for partition in service["regions"]:
+                for item in service["regions"][partition]:
+                    regions.add(item)
+        return list(regions)
+    except Exception as error:
+        logger.error(f"{error.__class__.__name__}: {error}")
+        return []
