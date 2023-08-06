d535414b9a1315f992d66723581c32fca656b12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:05 +0000

    Add package thelogger/0.2.7/thelogger-0.2.7.tar.gz

---
+++ packages/thelogger/thelogger-0.2.7.tar.gz/thelogger-0.2.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'thelogger',       
+  packages = ['thelogger', 'thelogger.notify'],  
+  version = 'v0.2.7',      
+  license='Apache 2.0',       
+  description = 'Easy logging, timing and email notifications of code execution.',  
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  author = 'Tom1919',                   
+  author_email = 'py.notify1@gmail.com',      
+  url = 'https://github.com/tom1919/TheLogger',  
+  download_url = 'https://github.com/tom1919/TheLogger/archive/refs/tags/v0.2.7.tar.gz',   
+  keywords = ['log', 'logging', 'logger', 'email', 'timimg', 'notification'],  
+  install_requires = ['pandas','tabulate'], 
+  python_requires='>3.8',
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
---
+++ packages/thelogger/thelogger-0.2.7.tar.gz/thelogger-0.2.7/thelogger/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 04:36:47 2019
+
+Easy logging, timing and email notifications of code execution.
+
+@author: tommy
+"""
+
+from .logger import logger, log, lg
+from .notify import notify
