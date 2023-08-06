14c1fe7e7e1e40d26d38774f98a5a394d1cc5ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:22 +0000

    Add package landfall/0.0.2/landfall-0.0.2.tar.gz

---
+++ packages/landfall/landfall-0.0.2.tar.gz/landfall-0.0.2/src/landfall/color.py
@@ -0,0 +1,14 @@
+"""
+Functions for using colors.
+"""
+
+import random
+
+import staticmaps
+
+
+def random_color():
+    r = random.randrange(0, 256)
+    g = random.randrange(0, 256)
+    b = random.randrange(0, 256)
+    return staticmaps.Color(r, g, b)
