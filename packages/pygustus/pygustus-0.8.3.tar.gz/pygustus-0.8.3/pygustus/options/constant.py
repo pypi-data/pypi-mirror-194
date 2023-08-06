ead88aa5979b99659d96bdddeee096e09017ded3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:46 +0000

    Add package pygustus/0.8.3/pygustus-0.8.3.tar.gz

---
+++ packages/pygustus/pygustus-0.8.3.tar.gz/pygustus-0.8.3/pygustus/options/constant.py
@@ -0,0 +1,21 @@
+# Keys for the JSON parameter file
+NAME = 'name'
+TYPE = 'type'
+VALUES = 'possible_values'
+DESCRIPTION = 'description'
+USAGE = 'usage'
+DEFAULT = 'default_value'
+DEVELOPMENT = 'development'
+EXCLUDE_APPS = 'exclude_apps' # possibilities see below
+
+# Used types
+TYPE_STRING = 'string'
+TYPE_INT = 'int'
+TYPE_FLOAT = 'float'
+TYPE_BOOL = 'bool'
+TYPE_LIST_STRING = 'list<string>'
+
+# Exclude Apps
+EXCLUDE_AUGUSTUS = 'augustus'
+EXCLUDE_ETRAINING = 'etraining'
+EXCLUDE_PYGUSTUS = 'pygustus'
