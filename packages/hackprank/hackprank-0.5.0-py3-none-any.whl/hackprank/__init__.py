2403f83066f054c189d6cd03daff679dc5b777fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:32 +0000

    Add package hackprank/0.5.0/hackprank-0.5.0-py3-none-any.whl

---
+++ packages/hackprank/hackprank-0.5.0-py3-none-any.whl/hackprank/__init__.py
@@ -0,0 +1,5 @@
+from hackprank.fakeTerminalHack import fakeTerminalHack
+from hackprank.PyHack import hack
+from hackprank.hackWeb import webHack
+from hackprank.matrix import matrixFall
+import hackprank
