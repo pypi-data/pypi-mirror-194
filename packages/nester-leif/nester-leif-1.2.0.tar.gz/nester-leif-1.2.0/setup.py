81c04bb2faf10d43805ca0c67d360de4302f57ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:39 +0000

    Add package nester-leif/1.2.0/nester-leif-1.2.0.tar.gz

---
+++ packages/nester-leif/nester-leif-1.2.0.tar.gz/nester-leif-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester-leif',
+    version='1.2.0',
+    py_modules={'nester'},
+    author='leif',
+    author_email='xiaofeixialf.6@gmail.com',
+    url='http://www.leif.com',
+    description='A simle printer of nested lists.',
+)
