21f513e85fabb4a09072343f7c2c5008574ff71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:55 +0000

    Add package flashlight-pavel-ivanov928/1.0/flashlight-pavel_ivanov928-1.0.tar.gz

---
+++ packages/flashlight-pavel-ivanov928/flashlight-pavel_ivanov928-1.0.tar.gz/flashlight-pavel_ivanov928-1.0/README.md
@@ -0,0 +1,5 @@
+This is a package with interview test.
+
+flashlight.py - solving the problem of implementing a network flashlight.
+
+tests.py - unittests for flashlight.py
---
+++ packages/flashlight-pavel-ivanov928/flashlight-pavel_ivanov928-1.0.tar.gz/flashlight-pavel_ivanov928-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md") as file:
+    read_me_description = file.read()
+
+setuptools.setup(
+    name="flashlight-pavel_ivanov928",
+    version="1.0",
+    author="Pavel Ivanov",
+    author_email="pavel@ivanov928.ru",
+    description="This is a package with interview test.",
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Splinter928/flashlight",
+    packages=['flashlight'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
