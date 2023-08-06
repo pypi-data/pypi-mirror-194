46ee6629af5a76e6fae9c0b592c42c0c91f24f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:30 +0000

    Add package spafe/0.3.2/spafe-0.3.2-py3.8.egg

---
+++ packages/spafe/spafe-0.3.2-py3.8.egg/spafe/version.py
@@ -0,0 +1,9 @@
+"""
+
+- Description : Version file.
+- Copyright (c) 2019-2023 Ayoub Malek.
+  This source code is licensed under the terms of the BSD 3-Clause License.
+  For a copy, see <https://github.com/SuperKogito/spafe/blob/master/LICENSE>.
+
+"""
+__version__ = "0.3.2"
