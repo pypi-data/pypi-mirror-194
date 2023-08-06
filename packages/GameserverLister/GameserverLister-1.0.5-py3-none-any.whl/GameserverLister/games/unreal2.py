0520d86383618954e17ac395b58a3f3ea61b0bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:33 +0000

    Add package GameserverLister/1.0.5/GameserverLister-1.0.5-py3-none-any.whl

---
+++ packages/GameserverLister/GameserverLister-1.0.5-py3-none-any.whl/GameserverLister/games/unreal2.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+from GameserverLister.common.types import Unreal2Game
+
+UNREAL2_CONFIGS: Dict[Unreal2Game, dict] = {
+    Unreal2Game.UT2003: {
+        'servers': {
+            'openspy.net': {
+                'hostname': 'utmaster.openspy.net',
+                'port': 28902
+            }
+        }
+    },
+    Unreal2Game.UT2004: {
+        'servers': {
+            'openspy.net': {
+                'hostname': 'utmaster.openspy.net',
+                'port': 28902
+            },
+            '333networks.com': {
+                'hostname': 'ut2004master.333networks.com',
+                'port': 28902
+            }
+        }
+    }
+}
