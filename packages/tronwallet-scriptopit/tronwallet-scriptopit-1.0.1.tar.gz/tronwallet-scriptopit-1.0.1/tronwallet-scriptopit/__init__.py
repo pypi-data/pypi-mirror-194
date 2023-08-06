30f4a23813101089984945985cb9f25ae5ba8acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:37 +0000

    Add package tronwallet-scriptopit/1.0.1/tronwallet-scriptopit-1.0.1.tar.gz

---
+++ packages/tronwallet-scriptopit/tronwallet-scriptopit-1.0.1.tar.gz/tronwallet-scriptopit-1.0.1/tronwallet-scriptopit/__init__.py
@@ -0,0 +1,24 @@
+# --------------------------------------------------------------------
+# Copyright (c) iEXBase. All rights reserved.
+# Licensed under the MIT License.
+# See License.txt in the project root for license information.
+# --------------------------------------------------------------------
+
+import sys
+
+import pkg_resources
+
+from eth_account import Account  # noqa: E402
+
+if sys.version_info < (3, 5):
+    raise EnvironmentError("Python 3.5 or above is required")
+
+
+__version__ = pkg_resources.get_distribution("tronapi").version
+
+__all__ = [
+    '__version__',
+    'HttpProvider',
+    'Account',
+    'Tron',
+]
