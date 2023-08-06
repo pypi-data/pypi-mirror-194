d2fb5bccf22351dd615e1add2df7cc12629018f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:54 +0000

    Add package codeforces/0.0.2/codeforces-0.0.2.tar.gz

---
+++ packages/codeforces/codeforces-0.0.2.tar.gz/codeforces-0.0.2/cf/config.py
@@ -0,0 +1,29 @@
+import click
+import json
+import os
+from rich.console import Console
+
+console = Console()
+
+
+@click.command()
+# @click.option("--username", prompt="Enter your codeforces username")
+# @click.password_option()
+@click.option("--cf_dir", prompt="Enter your codeforces directory")
+# def config(username: str, password: str, dir: str):
+def config(cf_dir: str):
+    if cf_dir.startswith("~"):
+        cf_dir = os.path.expanduser('~') + cf_dir[1:]
+    if not os.path.isdir(cf_dir):
+        console.print(f"[bold red]ERROR: [/] Directory `{cf_dir}` not found.")
+        return
+
+    data = {
+        "dir": os.path.abspath(cf_dir)
+    }
+
+    slash = "/" if os.name == "posix" else "\\\\"
+    with open(os.path.expanduser('~') + slash + "codeforces.uwu", "w") as f:
+        f.write(json.dumps(data))
+
+    console.print("[bold green]Config set![/]\n" + f"dir: {cf_dir}")
