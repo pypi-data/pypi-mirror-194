1c1f6b99196eedcc6eb8367a04c1af91e6385071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:54 +0000

    Add package db1/0.1.3/db1-0.1.3-py3-none-any.whl

---
+++ packages/db1/db1-0.1.3-py3-none-any.whl/db1/_cli/main.py
@@ -0,0 +1,52 @@
+"""Main script for the DB1 CLI."""
+
+import argparse
+
+from db1._cli.item import handle_resource_item
+
+
+def main():
+    """Main entry point for the DB1 CLI."""
+
+    parser = argparse.ArgumentParser(description="DB1 Command Line Interface (CLI).")
+
+    # Positional arguments
+    parser.add_argument(
+        "operation",
+        type=str,
+        nargs="?",
+        help="Name of operation (e.g. set, get).",
+    )
+    parser.add_argument(
+        "key",
+        type=str,
+        nargs="?",
+        help="Key of the item (e.g. some_item_key).",
+    )
+    parser.add_argument(
+        "value",
+        type=str,
+        nargs="?",
+        help="Some operations require a value (e.g. 123, hello).",
+    )
+
+    # Argument flags (optional)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        help="Print the version number and exit.",
+    )
+
+    args = vars(parser.parse_args())
+
+    if args["version"]:
+        print("0.1.3")
+        return
+
+    handle_resource_item(args)
+    return
+
+
+if __name__ == "__main__":
+    main()
