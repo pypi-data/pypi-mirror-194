1b80ed3f86dbfb49f288874a02b03bfc6acfe2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:40 +0000

    Add package llama-index/0.4.15/llama_index-0.4.15.tar.gz

---
+++ packages/llama-index/llama_index-0.4.15.tar.gz/llama_index-0.4.15/llama_index/indices/vector_store/__init__.py
@@ -0,0 +1,21 @@
+"""Vector-store based data structures."""
+
+from llama_index.indices.vector_store.base import GPTVectorStoreIndex
+from llama_index.indices.vector_store.vector_indices import (
+    GPTChromaIndex,
+    GPTFaissIndex,
+    GPTPineconeIndex,
+    GPTQdrantIndex,
+    GPTSimpleVectorIndex,
+    GPTWeaviateIndex,
+)
+
+__all__ = [
+    "GPTVectorStoreIndex",
+    "GPTSimpleVectorIndex",
+    "GPTFaissIndex",
+    "GPTPineconeIndex",
+    "GPTWeaviateIndex",
+    "GPTQdrantIndex",
+    "GPTChromaIndex",
+]
