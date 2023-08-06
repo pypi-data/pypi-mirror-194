32124f4c22c6e92bb269c268b0447d03efafa41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:19 +0000

    Add package bibkit/0.2.0/bibkit-0.2.0.tar.gz

---
+++ packages/bibkit/bibkit-0.2.0.tar.gz/bibkit-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bibkit', 'bibkit.identifiers']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bibkit',
+    'version': '0.2.0',
+    'description': 'A collection of tools for coding librarians and coders in libraries',
+    'long_description': '# bibkit\n\n',
+    'author': 'Daniel Opitz',
+    'author_email': 'daniel@toilandtrouble.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://codeberg.org/tt/bibkit',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
