0ec04853f622dec663e7c512a75cf4f5a2c90914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:08 +0000

    Add package pyalko/0.2.1/pyalko-0.2.1.tar.gz

---
+++ packages/pyalko/pyalko-0.2.1.tar.gz/pyalko-0.2.1/pyalko/const.py
@@ -0,0 +1,5 @@
+"""AL-KO Constants"""
+
+BASE_URL = "https://api.al-ko.com/v0/iot/things"
+OAUTH2_AUTHORIZE = "https://idp.al-ko.com/connect/authorize"
+OAUTH2_TOKEN = "https://idp.al-ko.com/connect/token"
