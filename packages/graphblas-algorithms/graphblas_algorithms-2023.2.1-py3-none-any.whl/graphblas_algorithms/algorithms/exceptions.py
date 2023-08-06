fa9cd7231497aa22d2580bd8d2355fb442054978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:35 +0000

    Add package graphblas-algorithms/2023.2.1/graphblas_algorithms-2023.2.1-py3-none-any.whl

---
+++ packages/graphblas-algorithms/graphblas_algorithms-2023.2.1-py3-none-any.whl/graphblas_algorithms/algorithms/exceptions.py
@@ -0,0 +1,18 @@
+class GraphBlasAlgorithmException(Exception):
+    pass
+
+
+class ConvergenceFailure(GraphBlasAlgorithmException):
+    pass
+
+
+class EmptyGraphError(GraphBlasAlgorithmException):
+    pass
+
+
+class PointlessConcept(GraphBlasAlgorithmException):
+    pass
+
+
+class Unbounded(GraphBlasAlgorithmException):
+    pass
