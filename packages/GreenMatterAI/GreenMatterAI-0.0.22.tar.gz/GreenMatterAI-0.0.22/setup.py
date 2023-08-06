123e94ace4910d86a8e94b55b2360fedd231c401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:26 +0000

    Add package GreenMatterAI/0.0.22/GreenMatterAI-0.0.22.tar.gz

---
+++ packages/GreenMatterAI/GreenMatterAI-0.0.22.tar.gz/GreenMatterAI-0.0.22/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="GreenMatterAI",
+    version="0.0.22",
+    author="GreenMatterAI",
+    description="GreenMatterAI's package",
+    packages=["GreenMatterAI"],
+    #py_modules=["GreenMatterAI"],
+    #py_modules=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "shortuuid",
+        "aws_requests_auth",
+        "boto3",
+        "pathlib",
+        "datetime",
+        "matplotlib"
+    ]
+)
