4719d7bdb4280672cd4375d9b6dd52487702b658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:18 +0000

    Add package invenio-accounts/2.1.0/invenio_accounts-2.1.0-py2.py3-none-any.whl

---
+++ packages/invenio-accounts/invenio_accounts-2.1.0-py2.py3-none-any.whl/invenio_accounts/profiles/schemas.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2022 TU Wien.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Schemas for user profiles and preferences."""
+
+
+from invenio_i18n import lazy_gettext as _
+from marshmallow import Schema, ValidationError, fields
+
+
+def validate_visibility(value):
+    """Check if the value is a valid visibility setting."""
+    if value not in ["public", "restricted"]:
+        raise ValidationError(
+            message=str(_("Value must be either 'public' or 'restricted'."))
+        )
+
+
+class UserProfileSchema(Schema):
+    """The default user profile schema."""
+
+    full_name = fields.String()
+    affiliations = fields.String()
+
+
+class UserPreferencesSchema(Schema):
+    """The default schema for user preferences."""
+
+    visibility = fields.String(validate=validate_visibility)
+    email_visibility = fields.String(validate=validate_visibility)
