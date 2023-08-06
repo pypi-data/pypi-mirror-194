0b076e03a21bcf70dd0ac58fb4893a129e702821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:06 +0000

    Add package asgi-tools/0.70.1/asgi_tools-0.70.1-cp38-cp38-win_amd64.whl

---
+++ packages/asgi-tools/asgi_tools-0.70.1-cp38-cp38-win_amd64.whl/asgi_tools/types.py
@@ -0,0 +1,22 @@
+from typing import (TYPE_CHECKING, Any, Awaitable, Callable, List, Mapping, MutableMapping, Tuple,
+                    TypeVar, Union)
+
+if TYPE_CHECKING:
+    from .request import Request
+
+TASGIMessage = Mapping[str, Any]
+TASGISend = Callable[[TASGIMessage], Awaitable[None]]
+TASGIReceive = Callable[[], Awaitable[TASGIMessage]]
+TASGIScope = MutableMapping[str, Any]
+TASGIHeaders = List[Tuple[bytes, bytes]]
+TASGIApp = Callable[[TASGIScope, TASGIReceive, TASGISend], Awaitable[Any]]
+
+TResponseContent = Union[bytes, str]
+TMiddleware = Callable[[TASGIScope, TASGIReceive, TASGISend], Awaitable[Any]]
+TJSON = Union[None, bool, int, float, str, List["TJSON"], Mapping[str, "TJSON"]]
+TExceptionHandler = Callable[["Request", BaseException], Awaitable]
+
+TV = TypeVar("TV")
+TVCallable = TypeVar("TVCallable", bound=Callable)
+TVAsyncCallable = TypeVar("TVAsyncCallable", bound=Callable[..., Awaitable])
+TVExceptionHandler = TypeVar("TVExceptionHandler", bound=TExceptionHandler)
