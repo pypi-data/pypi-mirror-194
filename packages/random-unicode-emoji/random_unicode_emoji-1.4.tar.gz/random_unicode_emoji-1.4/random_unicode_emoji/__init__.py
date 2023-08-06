bb0a02a773c9cd0b859f7e6cdc1ddb5871b13d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:17 +0000

    Add package random-unicode-emoji/1.4/random_unicode_emoji-1.4.tar.gz

---
+++ packages/random-unicode-emoji/random_unicode_emoji-1.4.tar.gz/random_unicode_emoji-1.4/random_unicode_emoji/__init__.py
@@ -0,0 +1,21 @@
+from random_unicode_emoji.random_unicode_emoji import random_emoji
+
+__author__ = 'Nic Jones'
+__email__ = 'nic@nicpjones.com'
+__version__ = '1.4'
+__doc__ = """
+Random Unicode Emoji
+====================
+A Python package and function to retrieve a random Unicode emoji.
+
+Install
+-------
+>>> pip install random-unicode-emoji
+
+Usage
+-----
+>>> # Import the Library
+>>> from random_unicode_emoji import random_emoji
+>>> # Use the Function
+>>> print(random_emoji())
+"""
---
+++ packages/random-unicode-emoji/random_unicode_emoji-1.4.tar.gz/random_unicode_emoji-1.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import re
+
+VERSIONFILE="random_unicode_emoji/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+with open("README.md", "r") as r:
+    long_description = r.read()
+    setuptools.setup(
+        name='random_unicode_emoji',
+        python_requires='>=3',
+        version=new_version,
+        license='MIT',
+        author="Nic Jones",
+        author_email='nic@nicpjones.com',
+        description='A Python package and function to retrieve a random Unicode emoji. 😄',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        url='https://github.com/NicPWNs/random-unicode-emoji',
+        download_url ='https://github.com/NicPWNs/random-unicode-emoji/releases',
+        keywords='random unicode emoji function',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+    )
