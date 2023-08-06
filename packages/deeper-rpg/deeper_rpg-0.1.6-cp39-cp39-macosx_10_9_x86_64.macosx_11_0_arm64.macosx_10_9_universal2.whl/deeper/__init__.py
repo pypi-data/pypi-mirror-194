763a93fadaec604393144a0df2577e7aca2b641d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:15 +0000

    Add package deeper-rpg/0.1.6/deeper_rpg-0.1.6-cp39-cp39-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/deeper-rpg/deeper_rpg-0.1.6-cp39-cp39-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/deeper/__init__.py
@@ -0,0 +1,7 @@
+from .deeper import *
+from .resources import *
+from .data import Data
+from .blueprint import Blueprint
+from .component import Component
+from .entity import Entity
+from .components.block import Block
