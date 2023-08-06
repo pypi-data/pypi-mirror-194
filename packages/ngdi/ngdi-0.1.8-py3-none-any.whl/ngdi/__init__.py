60558e1a12e08a499031a7e02ca6f27740633b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:05 +0000

    Add package ngdi/0.1.8/ngdi-0.1.8-py3-none-any.whl

---
+++ packages/ngdi/ngdi-0.1.8-py3-none-any.whl/ngdi/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 The NebulaGraph Authors. All rights reserved.
+
+from pkgutil import extend_path
+
+
+__path__ = extend_path(__path__, __name__)  # type: ignore
+
+from ngdi.nebula_reader import NebulaReader
+from ngdi.nebula_writer import NebulaWriter
+from ngdi.nebula_algo import NebulaAlgorithm
+from ngdi.nebula_gnn import NebulaGNN
+from ngdi.config import NebulaGraphConfig
+
+# export
+__all__ = (
+    "NebulaReader",
+    "NebulaWriter",
+    "NebulaAlgorithm",
+    "NebulaGNN",
+    "NebulaGraphConfig",
+)
