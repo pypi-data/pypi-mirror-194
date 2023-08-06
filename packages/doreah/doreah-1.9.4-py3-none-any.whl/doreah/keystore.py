82301a98e355dda3cdff1c02dada969c59d271ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package doreah/1.9.4/doreah-1.9.4-py3-none-any.whl

---
+++ packages/doreah/doreah-1.9.4-py3-none-any.whl/doreah/keystore.py
@@ -0,0 +1,88 @@
+from ._internal import DoreahConfig
+
+import yaml
+import random
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+
+config = DoreahConfig("keystore",
+	key_length=64
+)
+
+JINJAENV = Environment(
+	loader=PackageLoader('doreah', 'resources/keystore'),
+	autoescape=select_autoescape(['html', 'xml'])
+)
+
+
+
+charset = list(range(10)) + list("abcdefghijklmnopqrstuvwxyz") + list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+def randomstring(length):
+	return "".join(str(random.choice(charset)) for _ in range(length))
+
+
+class KeyStore:
+	"""Object that represents keys and their identifiers stored by a user. They are accessible via a web interface
+	and written to a permanent file."""
+
+	def __init__(self,file="keystore.yml",save_endpoint="/keys"):
+		self.file = file
+		self.keys = {}
+		self.save_endpoint = save_endpoint
+		self.load_from_file()
+
+	def __len__(self):
+		return self.keys.__len__()
+	def __getitem__(self,identifier):
+		return self.keys[identifier]
+	def __setitem__(self,identifier,value):
+		self.keys[identifier] = value
+		self.write_to_file()
+
+	def __iter__(self):
+		return (identifier for identifier in self.keys)
+
+	def check_key(self,key):
+		return (key in self.keys.values())
+
+	def check_and_identify_key(self,key):
+		for identifier in self.keys:
+			if self.keys[identifier] == key:
+				return identifier
+		return False
+
+	def generate_key(self,identifier):
+		key = randomstring(config['key_length'])
+		self.keys[identifier] = key
+		self.write_to_file()
+		return key
+
+	def add_key(self,identifier,key):
+		self.keys[identifier] = key
+		self.write_to_file()
+
+	def delete_key(self,identifier):
+		del self.keys[identifier]
+		self.write_to_file()
+
+	def update(self,dict):
+		for identifier in dict:
+			if dict[identifier] is None:
+				self.delete_key(identifier)
+			else:
+				self.add_key(identifier,dict[identifier])
+
+	def load_from_file(self):
+		try:
+			with open(self.file) as filed:
+				self.keys = yaml.safe_load(filed)
+		except FileNotFoundError:
+			self.write_to_file()
+
+	def write_to_file(self):
+		with open(self.file,"w") as filed:
+			yaml.dump(self.keys,filed)
+
+	def html(self):
+		template = JINJAENV.get_template("settings.jinja")
+		return template.render({"keystore":self})
---
+++ packages/doreah/doreah-1.9.4-py3-none-any.whl/doreah/regular.py
@@ -0,0 +1,174 @@
+from threading import Thread, Timer
+import datetime
+import random
+import time
+
+from ._internal import DEFAULT, defaultarguments, gopen, DoreahConfig
+
+
+config = DoreahConfig("regular",
+	autostart=True,
+	offset=0
+)
+
+def tz():
+	return datetime.timezone(offset=datetime.timedelta(hours=config["offset"]))
+
+### DEPRECATED
+def yearly(func):
+	"""Decorator for yearly functions. Depending on configuration, is equivalent to either :meth:`runyearly` or :meth:`repeatyearly`."""
+	if config["autostart"]: return runyearly(func)
+	else: return repeatyearly(func)
+
+def monthly(func):
+	"""Decorator for monthly functions. Depending on configuration, is equivalent to either :meth:`runmonthly` or :meth:`repeatmonthly`."""
+	if config["autostart"]: return runmonthly(func)
+	else: return repeatmonthly(func)
+
+def daily(func):
+	"""Decorator for daily functions. Depending on configuration, is equivalent to either :meth:`rundaily` or :meth:`repeatdaily`."""
+	if config["autostart"]: return rundaily(func)
+	else: return repeatdaily(func)
+
+def hourly(func):
+	"""Decorator for hourly functions. Depending on configuration, is equivalent to either :meth:`runhourly` or :meth:`repeathourly`."""
+	if config["autostart"]: return runhourly(func)
+	else: return repeathourly(func)
+###
+
+
+intervals = {
+	'yearly':{
+		'getnext':lambda now:datetime.datetime(now.year+1,1,1,tzinfo=tz()),
+		'functions':[]
+	},
+	'monthly':{
+		'getnext':lambda now:datetime.datetime(now.year,now.month + 1,1,tzinfo=tz()) if now.month != 12 else datetime.datetime(now.year+1,1,1,tzinfo=tz()),
+		'functions':[]
+	},
+	'daily':{
+		'getnext':lambda now:datetime.datetime(now.year,now.month,now.day,tzinfo=tz()) + datetime.timedelta(days=1),
+		'functions':[]
+	},
+	'hourly':{
+		'getnext':lambda now:datetime.datetime(now.year,now.month,now.day,now.hour,tzinfo=tz()) + datetime.timedelta(hours=1),
+		'functions':[]
+	},
+	# just for testing
+	'constantly':{
+		'getnext':lambda now:datetime.datetime.now(tz=tz()) + datetime.timedelta(seconds=1),
+		'functions':[]
+	}
+}
+
+# init daemon for each interval
+# spawns all runs, then goes to sleep until next time
+def doreah_regular_daemon(interval):
+	i = 0
+	while True:
+		i += 1
+		now = datetime.datetime.now(tz=tz())
+		next = intervals[interval]['getnext'](now)
+
+		wait = int(next.timestamp() - now.timestamp()) + 5
+
+		time.sleep(wait)
+		for f in intervals[interval]['functions']:
+			try:
+				t = Thread(target=f['function'],args=f['args'],kwargs=f['kwargs'])
+				t.daemon = False
+				t.setName(f"doreah-regular-{interval}-loop{i}-{f['function'].__name__}")
+				t.start()
+				#f['function'](*f['args'],**f['kwargs'])
+			except Exception as e:
+				print(e)
+			time.sleep(2)
+
+# spawns above init daemons if any functions actually need it
+def spawn_all_needed():
+
+	for interval in intervals:
+		data = intervals[interval]
+		if len(data['functions']) > 0 and not data.get('thread'):
+			data['thread'] = Thread(target=doreah_regular_daemon,kwargs={'interval':interval})
+			data['thread'].daemon = True
+			data['thread'].setName(f"doreah-regular-{interval}-init")
+			data['thread'].start()
+
+
+def run_regularly(interval,func):
+
+	functioninfo = {
+		'function':func,
+		'args':(),
+		'kwargs':{}
+	}
+
+	# run it once
+	t = Timer(5,**functioninfo)
+	t.daemon = True
+	t.start()
+
+	# add to list
+	intervals[interval]['functions'].append(functioninfo)
+	spawn_all_needed()
+
+	# return unchanged function
+	return func
+
+def repeat_regularly(interval,func):
+
+	def self_scheduling_func(*args,**kwargs):
+
+		functioninfo = {
+			'function':func,
+			'args':args,
+			'kwargs':kwargs
+		}
+
+		# execute function
+		func(*args,**kwargs)
+
+		# add original function to list, with args and kwargs from first invocation
+		intervals[interval]['functions'].append(functioninfo)
+		spawn_all_needed()
+
+	return self_scheduling_func
+
+
+def runyearly(func):
+	"""Decorator to make the function execute on first definition as well as every year."""
+	return run_regularly('yearly',func)
+
+
+def repeatyearly(func):
+	"""Decorator to make the function repeat every new year after being called once."""
+	return repeat_regularly('yearly',func)
+
+def runmonthly(func):
+	"""Decorator to make the function execute on first definition as well as every month."""
+	return run_regularly('monthly',func)
+
+def repeatmonthly(func):
+	"""Decorator to make the function repeat every new month after being called once."""
+	return repeat_regularly('monthly',func)
+
+def rundaily(func):
+	"""Decorator to make the function execute on first definition as well as every day."""
+	return run_regularly('daily',func)
+
+def repeatdaily(func):
+	"""Decorator to make the function repeat every new day after being called once."""
+	return repeat_regularly('daily',func)
+
+def runhourly(func):
+	"""Decorator to make the function execute on first definition as well as every hour."""
+	return run_regularly('hourly',func)
+
+def repeathourly(func):
+	"""Decorator to make the function repeat every new hour after being called once."""
+	return repeat_regularly('hourly',func)
+
+# just for testing
+def runoften(func):
+	return run_regularly('constantly',func)
