79545698201586767d11d89fbddd32d0abe6157c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:05 +0000

    Add package tricycle/0.2.2/tricycle-0.2.2-py3-none-any.whl

---
+++ packages/tricycle/tricycle-0.2.2-py3-none-any.whl/tricycle/__init__.py
@@ -0,0 +1,18 @@
+from ._version import __version__
+
+from ._rwlock import RWLock as RWLock
+from ._streams import (
+    BufferedReceiveStream as BufferedReceiveStream,
+    TextReceiveStream as TextReceiveStream,
+)
+from ._multi_cancel import MultiCancelScope as MultiCancelScope
+from ._service_nursery import open_service_nursery as open_service_nursery
+from ._meta import ScopedObject as ScopedObject, BackgroundObject as BackgroundObject
+
+# watch this space...
+
+_export = None
+for _export in globals().values():
+    if hasattr(_export, "__module__"):
+        _export.__module__ = __name__
+del _export
