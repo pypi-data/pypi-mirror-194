2e4998ed1987060e82d265c465581828100dafc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:26 +0000

    Add package ipyvizzu/0.14.0/ipyvizzu-0.14.0-py3-none-any.whl

---
+++ packages/ipyvizzu/ipyvizzu-0.14.0-py3-none-any.whl/ipyvizzu/__init__.py
@@ -0,0 +1,45 @@
+"""
+Build animated charts in Jupyter Notebook
+and similar environments with a simple Python syntax.
+"""
+
+from .chart import Chart
+from .animation import (
+    Animation,
+    PlainAnimation,
+    InferType,
+    Data,
+    Config,
+    Style,
+    Snapshot,
+    AnimationMerger,
+)
+from .method import Method, Animate, Feature, Store, EventOn, EventOff, Log
+from .json import RawJavaScript, RawJavaScriptEncoder
+from .template import ChartProperty, DisplayTarget, DisplayTemplate
+from .event import EventHandler
+
+__all__ = [
+    "Chart",
+    "Animation",
+    "PlainAnimation",
+    "InferType",
+    "Data",
+    "Config",
+    "Style",
+    "Snapshot",
+    "AnimationMerger",
+    "Method",
+    "Animate",
+    "Feature",
+    "Store",
+    "EventOn",
+    "EventOff",
+    "Log",
+    "RawJavaScript",
+    "RawJavaScriptEncoder",
+    "ChartProperty",
+    "DisplayTarget",
+    "DisplayTemplate",
+    "EventHandler",
+]
