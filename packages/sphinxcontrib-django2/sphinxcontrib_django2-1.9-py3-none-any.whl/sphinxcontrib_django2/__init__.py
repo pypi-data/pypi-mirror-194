fb9380140dfdead31119d1d9d7af533d02fa5227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:06 +0000

    Add package sphinxcontrib-django2/1.9/sphinxcontrib_django2-1.9-py3-none-any.whl

---
+++ packages/sphinxcontrib-django2/sphinxcontrib_django2-1.9-py3-none-any.whl/sphinxcontrib_django2/__init__.py
@@ -0,0 +1,24 @@
+"""
+This is a sphinx extension which improves the documentation of Django apps.
+"""
+__version__ = "1.9"
+
+
+def setup(app):
+    """
+    Allow this module to be used as sphinx extension.
+
+    Setup the upstream extension sphinxcontrib-django.
+
+    :param app: The Sphinx application object
+    :type app: ~sphinx.application.Sphinx
+    """
+
+    # Load upstream extension
+    app.setup_extension("sphinxcontrib_django")
+
+    return {
+        "version:": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
