99238168994996728b4ea456cd4aa6a580694bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:09 +0000

    Add package syncanysql/0.0.3/syncanysql-0.0.3.tar.gz

---
+++ packages/syncanysql/syncanysql-0.0.3.tar.gz/syncanysql-0.0.3/syncanysql/prompt.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+# 2023/2/8
+# create by: snower
+
+import os
+import sys
+from pygments.lexers import SqlLexer
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.styles import Style
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.cursor_shapes import CursorShape
+from syncanysql.executor import Executor
+from .version import version
+from .parser import SqlParser, SqlSegment
+
+sql_completer = WordCompleter(
+    [
+        "abort",
+        "action",
+        "add",
+        "after",
+        "all",
+        "alter",
+        "analyze",
+        "and",
+        "as",
+        "asc",
+        "attach",
+        "autoincrement",
+        "before",
+        "begin",
+        "between",
+        "by",
+        "cascade",
+        "case",
+        "cast",
+        "check",
+        "collate",
+        "column",
+        "commit",
+        "conflict",
+        "constraint",
+        "create",
+        "cross",
+        "current_date",
+        "current_time",
+        "current_timestamp",
+        "database",
+        "default",
+        "deferrable",
+        "deferred",
+        "delete",
+        "desc",
+        "detach",
+        "distinct",
+        "drop",
+        "each",
+        "else",
+        "end",
+        "escape",
+        "except",
+        "exclusive",
+        "exists",
+        "explain",
+        "fail",
+        "for",
+        "foreign",
+        "from",
+        "full",
+        "glob",
+        "group",
+        "having",
+        "if",
+        "ignore",
+        "immediate",
+        "in",
+        "index",
+        "indexed",
+        "initially",
+        "inner",
+        "insert",
+        "instead",
+        "intersect",
+        "into",
+        "is",
+        "isnull",
+        "join",
+        "key",
+        "left",
+        "like",
+        "limit",
+        "match",
+        "natural",
+        "no",
+        "not",
+        "notnull",
+        "null",
+        "of",
+        "offset",
+        "on",
+        "or",
+        "order",
+        "outer",
+        "plan",
+        "pragma",
+        "primary",
+        "query",
+        "raise",
+        "recursive",
+        "references",
+        "regexp",
+        "reindex",
+        "release",
+        "rename",
+        "replace",
+        "restrict",
+        "right",
+        "rollback",
+        "row",
+        "savepoint",
+        "select",
+        "set",
+        "table",
+        "temp",
+        "temporary",
+        "then",
+        "to",
+        "transaction",
+        "trigger",
+        "union",
+        "unique",
+        "update",
+        "using",
+        "vacuum",
+        "values",
+        "view",
+        "virtual",
+        "when",
+        "where",
+        "with",
+        "without",
+    ],
+    ignore_case=True,
+)
+
+style = Style.from_dict(
+    {
+        "completion-menu.completion": "bg:#008888 #ffffff",
+        "completion-menu.completion.current": "bg:#00aaaa #000000",
+        "scrollbar.background": "bg:#88aaaa",
+        "scrollbar.button": "bg:#222222",
+    }
+)
+
+class CliPrompt(object):
+    def __init__(self, manager, session_config):
+        self.manager = manager
+        self.session_config = session_config
+
+    def run(self):
+        home_config_path = os.path.join(os.path.expanduser('~'), ".syncany")
+        if not os.path.exists(home_config_path):
+            os.mkdir(home_config_path)
+        history = FileHistory(os.path.join(home_config_path, "history"))
+        session = PromptSession(
+            lexer=PygmentsLexer(SqlLexer), completer=sql_completer, style=style, history=history
+        )
+        executor = Executor(self.manager, self.session_config)
+        print("Python %s" % sys.version)
+        print("Syncany-SQL %s -- Simple and easy-to-use sql execution engine" % version)
+        lineno = 1
+        while True:
+            try:
+                text = session.prompt("> ", multiline=Condition(lambda: self.check_complete(session.app.current_buffer.text)),
+                                      cursor=CursorShape.BEAM)
+                text = text.strip()
+                if not text:
+                    continue
+                if text.lower()[:4] == "exit":
+                    return 0
+                executor.run("cli", [SqlSegment(text, lineno)])
+                try:
+                    executor.execute()
+                finally:
+                    lineno += 1
+                    executor.runners.clear()
+            except KeyboardInterrupt:
+                continue  # Control-C pressed. Try again.
+            except EOFError:
+                return 130
+            except Exception as e:
+                print(str(e))
+        return 0
+
+    def check_complete(self, content):
+        if not content.strip():
+            return False
+        if content[:4] == "exit":
+            return False
+        sql_parser = SqlParser(content)
+        try:
+            sql_parser.read_util_complete()
+        except EOFError:
+            return True
+        return False
---
+++ packages/syncanysql/syncanysql-0.0.3.tar.gz/syncanysql-0.0.3/syncanysql/taskers/set.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# 2023/2/13
+# create by: snower
+
+from ..config import CONST_CONFIG_KEYS
+
+
+class SetCommandTasker(object):
+    def __init__(self, config):
+        self.config = config
+
+    def start(self, executor, session_config, manager, arguments):
+        key, is_global = self.config["key"], False
+        if key[:6].lower() == "global":
+            key, is_global = key[6:].strip(), True
+        keys = key.split(".")[0][1:]
+        seted_config = session_config.global_config if is_global else session_config
+        if key in CONST_CONFIG_KEYS:
+            self.set_config(seted_config, key)
+        elif keys in ("databases", "imports", "sources", "defines", "variables", "options", "caches"):
+            self.set_config(seted_config, key[1:])
+        elif keys == "virtual_views":
+            self.set_config(seted_config, "databases." + key[1:])
+        elif key[:7] == "@config":
+            self.set_config(seted_config, key[8:].strip())
+        if is_global:
+            session_config.merge()
+        return []
+
+    def set_config(self, session_config, key):
+        if self.config["value"][:1] in ('"', "'"):
+            if self.config["value"][:3] in ("'''", '"""'):
+                value = self.config["value"][3:-3].strip()
+            else:
+                value = self.config["value"][1:-1].strip()
+        else:
+            if self.config["value"].lower() == 'true':
+                value = True
+            elif self.config["value"].lower() == 'false':
+                value = False
+            elif self.config["value"].lower() == 'null':
+                value = None
+            else:
+                digits = self.config["value"].split(".")
+                if digits and len(digits) <= 2 and digits[0].isdigit():
+                    value = float(self.config["value"]) if len(digits) == 2 and digits[1].isdigit() else int(self.config["value"])
+                else:
+                    value = self.config["value"].strip()
+        session_config.set(key, value)
+
+    def run(self, executor, session_config, manager):
+        return 0
+
+    def terminate(self):
+        pass
