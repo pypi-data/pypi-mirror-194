67748d7996701238e64b6535149209419c14edb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:57 +0000

    Add package bibfetch/0.0.3/bibfetch-0.0.3.tar.gz

---
+++ packages/bibfetch/bibfetch-0.0.3.tar.gz/bibfetch-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+import os
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + "/requirements.txt"
+requirements = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        requirements = f.read().splitlines()
+print(requirements)
+setup(
+    name="bibfetch",
+    version="0.0.3",
+    description="Fetch bibtex information from academic search engines.",
+    author="Zihao Ye",
+    license="GPL-3.0",
+    author_email="expye@outlook.com",
+    url="https://github.com/yzh119/bibfetch",
+    packages=["bibfetch"],
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "bibfetch = bibfetch.__main__:main",
+        ]
+    },
+)
