e8723a88af794bfabc95a0ae1d4618714de3a17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:17 +0000

    Add package llm-kira/0.6.50/llm_kira-0.6.50-py3-none-any.whl

---
+++ packages/llm-kira/llm_kira-0.6.50-py3-none-any.whl/llm_kira/creator/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2/16/23 2:36 PM
+# @FileName: __init__.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+from .engine import PromptEngine, Preset
+from .think import Hook, ThinkEngine, HookPool
