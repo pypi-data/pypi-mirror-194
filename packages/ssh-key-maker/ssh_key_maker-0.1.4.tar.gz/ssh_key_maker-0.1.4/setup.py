9e72297f7ff26bd56fb5f6a8cf60863e545bf2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:19 +0000

    Add package ssh-key-maker/0.1.4/ssh_key_maker-0.1.4.tar.gz

---
+++ packages/ssh-key-maker/ssh_key_maker-0.1.4.tar.gz/ssh_key_maker-0.1.4/setup.py
@@ -0,0 +1,19 @@
+# __Author__ = "Pranav Chandran"
+# __Date__ = 28-02-2023
+# __Time__ = 13:23
+# __FileName__ = setup.py
+from setuptools import setup
+
+setup(
+    name='ssh_key_maker',
+    version='0.1.4', #updated version
+    author='Pranav Chandran',
+    author_email='pranav.chandran@gmail.com',
+    description='Package to generate SSH keys',
+    packages=['ssh_key_maker'],
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
