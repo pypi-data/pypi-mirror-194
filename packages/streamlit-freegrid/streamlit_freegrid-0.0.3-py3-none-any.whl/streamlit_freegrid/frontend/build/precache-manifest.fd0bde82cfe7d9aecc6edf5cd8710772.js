eba6dc319dcc9565b02b0c015a80edda9dfcd98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:32 +0000

    Add package streamlit-freegrid/0.0.3/streamlit_freegrid-0.0.3-py3-none-any.whl

---
+++ packages/streamlit-freegrid/streamlit_freegrid-0.0.3-py3-none-any.whl/streamlit_freegrid/frontend/build/precache-manifest.fd0bde82cfe7d9aecc6edf5cd8710772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a450893c2355d23a18c4d36cf13de3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ecdae3b3d0119759a59",
+    "url": "./static/js/2.396cc59b.chunk.js"
+  },
+  {
+    "revision": "d2dad97fb483fe0c80be974fa4a698e1",
+    "url": "./static/js/2.396cc59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843d45d175c9cca249c9",
+    "url": "./static/js/main.ea466836.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
