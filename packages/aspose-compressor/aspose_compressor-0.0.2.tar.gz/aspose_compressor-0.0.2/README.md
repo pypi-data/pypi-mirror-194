238ce22b314e8efd76dc517d280561cc01a624ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:18 +0000

    Add package aspose-compressor/0.0.2/aspose_compressor-0.0.2.tar.gz

---
+++ packages/aspose-compressor/aspose_compressor-0.0.2.tar.gz/aspose_compressor-0.0.2/README.md
@@ -0,0 +1,8 @@
+# Aspose Compressor
+```py
+from aspose_compressor import AsposeCompressor
+
+ac = AsposeCompressor()
+file = ac.compress_video("./aspose-demo.mp4")
+file.save("./output.mp4")
+```
---
+++ packages/aspose-compressor/aspose_compressor-0.0.2.tar.gz/aspose_compressor-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup_info = {
+    "name": "aspose_compressor",
+    "version": "0.0.2",
+    "author": "CantCode023",
+    "author_email": "cantcode023@gmail.com",
+    "description": "Compress video with 100% compress size and no quality loss with only 3 lines of code.",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/CantCode023/aspose_compressor",
+    "packages": setuptools.find_packages(),
+    "install_requires": ["requests", "urllib3"],
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+    ],
+    "python_requires": '>=3.7'
+}
+
+
+setuptools.setup(**setup_info)
