e51f47a93e310f6f5acf1be7cd0801751a3d9805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:20:39 +0000

    Add package lunastro/0.0.4/lunastro-0.0.4.tar.gz

---
+++ packages/lunastro/lunastro-0.0.4.tar.gz/lunastro-0.0.4/README.md
@@ -0,0 +1,52 @@
+# lunastro
+lunastro is a python library for lunar and solar information
+
+
+Installation:
+
+To install lunastro:
+
+```python
+    pip install lunastro
+```
+<br>
+
+<h1>Functions</h1>
+
+```python
+    get_lunar_phase() # returns phase
+    get_lunar_age() # returns age of moon
+    get_lunar_age_percentage() # returns age percentage of moon
+    solardistance() # returns distance to the sun in miles
+    sunazimuth(date, latitude, longtitude) # returns the azumith of the sun in radians
+    # sunazimuth is measured from south to west
+    
+```
+
+<h1>Lunar Usage:</h1>:
+<br>
+
+```python
+
+    
+    from lunation.main import myMoon
+    # create instance of myMoon
+    moon = myMoon()
+    
+    # functions
+    phase = moon.get_lunar_phase() # returns lunar phase
+    age = moon.get_lunar_age() # returns lunar age
+    percent = moon.get_lunar_age_percent() # returns percent of lunar age as a decimal
+```
+The library can also be used to calculate distance from the sun
+<br>
+<h1> Solar Usage: </h1>
+
+```python
+
+    
+    from lunation.sun import Sun
+    # instance of Sun
+    sun = Sun()
+    distance = sun.solardistance() # returns solar distance in miles
+```
---
+++ packages/lunastro/lunastro-0.0.4.tar.gz/lunastro-0.0.4/lunastro/sun.py
@@ -0,0 +1,94 @@
+import math
+import datetime
+# start by calculating julian date
+class Sun:
+        def __init__(self):
+
+
+                self.dayMs = 1000 * 60 * 60 * 24
+                self.J1970 = 2440588
+                self.J2000 = 2451545
+
+        def toJulian(self, date):
+            return date.timestamp() / self.dayMs - 0.5 + self.J1970
+
+        def fromJulian(self, j):
+            return datetime.datetime.fromtimestamp((j + 0.5 - self.J1970) * self.dayMs)
+
+        def toDays(self, date):
+            return self.toJulian(date) - self.J2000
+
+        def get_julian_date(self):
+                date = None
+                # If no date is provided, use the current date and time
+                if date is None:
+                    date = datetime.datetime.now()
+
+                # Convert the date to a timestamp in milliseconds
+                timestamp = date.timestamp() * 1000
+
+                # Calculate the timezone offset in minutes
+                if date.utcoffset():
+                    timezone_offset = date.utcoffset().total_seconds() // 60
+                else:
+                    timezone_offset = 0
+
+                # Calculate the Julian date and return it
+                julian_date = (timestamp / 86400000) - (timezone_offset / 1440) + 2440587.5
+                return julian_date
+
+        def solardistance(self):
+                 juliandate = self.get_julian_date()
+                 # days since greenwich noon
+                 n = juliandate - 2451545 
+                 # positions
+                 meanlong = 280.460 + 0.9856474 * n
+                 # g is mean anomaly 
+                 g = 357.528 + 0.9856003 * n
+                 tmp = math.cos(g)
+                 temptwo = math.cos(2*g)
+                 # solar distance is in astronomical units
+                 solardistance = 1.00014 - 0.01671*tmp - 0.00014*temptwo
+                 return solardistance * 92955807.3 # miles
+  
+        def declination(self, l, b):
+                e = (math.pi/180) * 23.4397
+                return math.asin(math.sin(b) * math.cos(e) + math.cos(b) * math.sin(e) * math.sin(l))
+        
+        def solarmeananomaly(self, d):
+                return math.pi/180 * (357.5291 + 0.98560028 * d)
+        
+        def eclipticlongtitude(self, anomaly):
+                tmp = math.pi/180 * (1.9148 * math.sin(anomaly) + 0.02 * math.sin(2 * anomaly) + 0.0003 * math.sin(3 * anomaly))
+                sectemp = math.pi/180 * 102.9372
+                return tmp + sectemp + math.pi
+        
+        
+                
+        def currentdeclination(self, d):
+                s = self.solarmeananomaly(d)
+                longtitude = self.eclipticlongtitude(s)
+                return self.declination(longtitude, 0)
+        
+        def siderealtime(self, days, longtitudewest):
+                return math.pi/180 * (280.16 + 360.9856235 * days) - longtitudewest
+        
+        def rightAscension(self,l, b):
+                e = math.pi/180 * 23.4397
+                return math.atan2(math.sin(l) * math.cos(e) - math.tan(b) * math.sin(e), math.cos(l))
+
+                
+        def azimuth(self, h, phi, declination):
+                return math.atan2(math.sin(h), math.cos(h) * math.sin(phi) - math.tan(declination) * math.cos(phi))
+        
+        def sunazimuth(self, date, latitude, longtitude):
+                lw = math.pi/180 * - longtitude
+                phi = math.pi/180 * latitude
+                d = self.toDays(date)
+                tmp = self.currentdeclination(date)
+                h = self.siderealtime(d, lw) - tmp
+                return self.azimuth(h, phi, tmp)
+        
+                
+                
+                
