da25d76aec592413fef5b588a4586ef6414a7968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:33 +0000

    Add package pyalarmdotcomajax/0.4.13/pyalarmdotcomajax-0.4.13.tar.gz

---
+++ packages/pyalarmdotcomajax/pyalarmdotcomajax-0.4.13.tar.gz/pyalarmdotcomajax-0.4.13/setup.py
@@ -0,0 +1,8 @@
+"""Stub for legacy builds that allows pip to install an editable version of this module."""
+
+# File Version: 2023.01.27
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
