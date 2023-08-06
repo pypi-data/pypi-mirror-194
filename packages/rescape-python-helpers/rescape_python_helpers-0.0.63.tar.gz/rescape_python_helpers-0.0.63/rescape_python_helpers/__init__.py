38bacd0b7a2a2a3de464200fd1b3eb87807c68a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:55 +0000

    Add package rescape-python-helpers/0.0.63/rescape_python_helpers-0.0.63.tar.gz

---
+++ packages/rescape-python-helpers/rescape_python_helpers-0.0.63.tar.gz/rescape_python_helpers-0.0.63/rescape_python_helpers/__init__.py
@@ -0,0 +1,53 @@
+from .functional.ramda import (
+    item_str_path,
+    from_pairs,
+    to_pairs,
+    map_with_obj,
+    map_key_values,
+    map_with_obj_to_values,
+    merge_deep_all,
+    merge_all,
+    merge_deep,
+    item_path_or,
+    compact,
+    compact_dict,
+    concat,
+    default_to,
+    dump_json,
+    filter_object_or_dict,
+    flatten,
+    from_pairs_to_array_values,
+    fullname,
+    has,
+    head,
+    isalambda,
+    isint,
+    join,
+    length,
+    map_with_obj_deep,
+    map_keys_deep,
+    map_keys,
+    map_prop_value_as_index,
+    merge,
+    merge_dicts,
+    omit,
+    omit_deep,
+    dict_matches_params_deep,
+    prop,
+    prop_eq,
+    prop_eq_or,
+    prop_eq_or_in,
+    prop_eq_or_in_or,
+    prop_or
+)
+
+from .functional.memoize import memoize
+
+# Easy access ro all ramda and pyramda methods
+from .functional import ramda
+from .pyramda import *
+
+__all__ = [
+    'functional.ramda',
+    'pyramda'
+]
