1436567c13164bd6cef034acd4e955ed699b9cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:13 +0000

    Add package polarismesh-specification/0.1.0.post2/polarismesh-specification-0.1.0.post2.tar.gz

---
+++ packages/polarismesh-specification/polarismesh-specification-0.1.0.post2.tar.gz/polarismesh-specification-0.1.0.post2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="polarismesh-specification",
+    version="0.1.0.post2",
+    description="Specification for PolarisMesh",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    url="https://github.com/polarismesh/specification"
+)
