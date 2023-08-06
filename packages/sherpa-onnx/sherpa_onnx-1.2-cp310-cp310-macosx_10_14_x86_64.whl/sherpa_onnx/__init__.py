323b50da5b2883ca1654052f92724cfd6f040fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:26 +0000

    Add package sherpa-onnx/1.2/sherpa_onnx-1.2-cp310-cp310-macosx_10_14_x86_64.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.2-cp310-cp310-macosx_10_14_x86_64.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,10 @@
+from _sherpa_onnx import (
+    EndpointConfig,
+    FeatureExtractorConfig,
+    OnlineRecognizerConfig,
+    OnlineStream,
+    OnlineTransducerModelConfig,
+)
+
+from .online_recognizer import OnlineRecognizer
+__version__ = '1.2'
