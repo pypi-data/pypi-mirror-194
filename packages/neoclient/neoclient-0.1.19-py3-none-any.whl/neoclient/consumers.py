a3acf93c037dacb47ea26828f524de9e23fd8729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:26 +0000

    Add package neoclient/0.1.19/neoclient-0.1.19-py3-none-any.whl

---
+++ packages/neoclient/neoclient-0.1.19-py3-none-any.whl/neoclient/consumers.py
@@ -0,0 +1,246 @@
+from dataclasses import dataclass
+from typing import Any, Mapping, Sequence, Union
+
+from httpx import URL, Cookies, Headers, QueryParams, Timeout
+
+from . import converters
+from .errors import CompositionError
+from .models import ClientOptions, PreRequest
+from .types import (
+    CookieTypes,
+    HeaderTypes,
+    JsonTypes,
+    PathsTypes,
+    PathTypes,
+    QueriesTypes,
+    QueryTypes,
+    RequestContent,
+    RequestData,
+    RequestFiles,
+    TimeoutTypes,
+)
+
+__all__: Sequence[str] = (
+    "QueryConsumer",
+    "HeaderConsumer",
+    "CookieConsumer",
+    "PathConsumer",
+    "QueriesConsumer",
+    "HeadersConsumer",
+    "CookiesConsumer",
+    "PathsConsumer",
+    "ContentConsumer",
+    "DataConsumer",
+    "FilesConsumer",
+    "JsonConsumer",
+    "TimeoutConsumer",
+    "MountConsumer",
+    "BaseURLConsumer",
+)
+
+
+class Consumer:
+    def consume(self, target: Union[PreRequest, ClientOptions], /) -> None:
+        if isinstance(target, PreRequest):
+            self.consume_request(target)
+        elif isinstance(target, ClientOptions):
+            self.consume_client(target)
+        else:
+            raise CompositionError(
+                f"Consumer {type(self).__name__!r} does not support consumption of type {type(target)}"
+            )
+
+    def consume_request(self, _: PreRequest, /) -> None:
+        raise CompositionError(
+            f"Consumer {type(self).__name__!r} does not support consumption of type {PreRequest}"
+        )
+
+    def consume_client(self, _: ClientOptions, /) -> None:
+        raise CompositionError(
+            f"Consumer {type(self).__name__!r} does not support consumption of type {ClientOptions}"
+        )
+
+
+@dataclass(init=False)
+class QueryConsumer(Consumer):
+    key: str
+    value: str
+
+    def __init__(self, key: str, value: QueryTypes) -> None:
+        self.key = key
+        self.value = converters.convert_query_param(value)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.params = request.params.set(self.key, self.value)
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.params = client.params.set(self.key, self.value)
+
+
+@dataclass(init=False)
+class HeaderConsumer(Consumer):
+    key: str
+    value: str
+
+    def __init__(self, key: str, value: HeaderTypes) -> None:
+        self.key = key
+        self.value = converters.convert_header(value)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.headers[self.key] = self.value
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.headers[self.key] = self.value
+
+
+@dataclass(init=False)
+class CookieConsumer(Consumer):
+    key: str
+    value: str
+
+    def __init__(self, key: str, value: CookieTypes) -> None:
+        self.key = key
+        self.value = converters.convert_cookie(value)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.cookies[self.key] = self.value
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.cookies[self.key] = self.value
+
+
+@dataclass(init=False)
+class PathConsumer(Consumer):
+    key: str
+    value: str
+
+    def __init__(self, key: str, value: PathTypes) -> None:
+        self.key = key
+        self.value = converters.convert_path_param(value)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.path_params[self.key] = self.value
+
+
+@dataclass(init=False)
+class QueriesConsumer(Consumer):
+    params: QueryParams
+
+    def __init__(self, params: QueriesTypes, /) -> None:
+        self.params = converters.convert_query_params(params)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.params = request.params.merge(self.params)
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.params = client.params.merge(self.params)
+
+
+@dataclass(init=False)
+class HeadersConsumer(Consumer):
+    headers: Headers
+
+    def __init__(self, headers: HeaderTypes, /) -> None:
+        self.headers = converters.convert_headers(headers)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.headers.update(self.headers)
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.headers.update(self.headers)
+
+
+@dataclass(init=False)
+class CookiesConsumer(Consumer):
+    cookies: Cookies
+
+    def __init__(self, cookies: CookieTypes, /) -> None:
+        self.cookies = converters.convert_cookies(cookies)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.cookies.update(self.cookies)
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.cookies.update(self.cookies)
+
+
+@dataclass(init=False)
+class PathsConsumer(Consumer):
+    path_params: Mapping[str, str]
+
+    def __init__(self, path_params: PathsTypes, /) -> None:
+        self.path_params = converters.convert_path_params(path_params)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.path_params.update(self.path_params)
+
+
+@dataclass
+class ContentConsumer(Consumer):
+    content: RequestContent
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.content = self.content
+
+
+@dataclass
+class DataConsumer(Consumer):
+    data: RequestData
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.data = self.data
+
+
+@dataclass
+class FilesConsumer(Consumer):
+    files: RequestFiles
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.files = self.files
+
+
+@dataclass
+class JsonConsumer(Consumer):
+    json: JsonTypes
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.json = self.json
+
+
+@dataclass(init=False)
+class TimeoutConsumer(Consumer):
+    timeout: Timeout
+
+    def __init__(self, timeout: TimeoutTypes, /) -> None:
+        self.timeout = converters.convert_timeout(timeout)
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.timeout = self.timeout
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.timeout = self.timeout
+
+
+@dataclass
+class StateConsumer(Consumer):
+    key: str
+    value: Any
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.state[self.key] = self.value
+
+
+@dataclass
+class MountConsumer(Consumer):
+    path: str
+
+    def consume_request(self, request: PreRequest, /) -> None:
+        request.url = request.url.copy_with(path=self.path + request.url.path)
+
+
+@dataclass
+class BaseURLConsumer(Consumer):
+    base_url: str
+
+    def consume_client(self, client: ClientOptions, /) -> None:
+        client.base_url = URL(self.base_url)
---
+++ packages/neoclient/neoclient-0.1.19-py3-none-any.whl/neoclient/models.py
@@ -0,0 +1,477 @@
+import urllib.parse
+from dataclasses import dataclass
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    MutableMapping,
+    Optional,
+    Sequence,
+    Set,
+    Union,
+)
+
+import httpx
+from httpx import URL, Cookies, Headers, QueryParams, Timeout
+from httpx._auth import Auth
+from httpx._config import DEFAULT_MAX_REDIRECTS, DEFAULT_TIMEOUT_CONFIG
+
+from . import converters, utils
+from .errors import IncompatiblePathParameters, MissingStateError
+from .types import (
+    AsyncByteStream,
+    AuthTypes,
+    CookiesTypes,
+    CookieTypes,
+    DefaultEncodingTypes,
+    EventHooks,
+    HeadersTypes,
+    HeaderTypes,
+    JsonTypes,
+    MethodTypes,
+    PathsTypes,
+    QueriesTypes,
+    RequestContent,
+    RequestData,
+    RequestExtensions,
+    RequestFiles,
+    ResponseContent,
+    ResponseExtensions,
+    SyncByteStream,
+    TimeoutTypes,
+    URLTypes,
+)
+
+__all__: Sequence[str] = (
+    "Client",
+    "Request",
+    "Response",
+    "PreRequest",
+    "OperationSpecification",
+)
+
+
+class State:
+    _state: MutableMapping[str, Any]
+
+    def __init__(self, state: Optional[MutableMapping[str, Any]] = None):
+        if state is None:
+            state = {}
+
+        super().__setattr__("_state", state)
+
+    def __repr__(self) -> str:
+        context: str = ", ".join(
+            f"{key}={value!r}" for key, value in self._state.items()
+        )
+        return f"{type(self).__name__}({context})"
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, type(self)):
+            return self._state == value._state
+        else:
+            return False
+
+    def _set(self, key: str, value: Any) -> None:
+        self._state[key] = value
+
+    def _get(self, key: str) -> Any:
+        if key in self._state:
+            return self._state[key]
+        else:
+            raise MissingStateError(key=key)
+
+    def _del(self, key: str) -> None:
+        del self._state[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self._set(key, value)
+
+    def __setattr__(self, key: str, value: Any) -> None:
+        self._set(key, value)
+
+    def __getitem__(self, key: str) -> Any:
+        try:
+            return self._get(key)
+        except MissingStateError as missing_state_error:
+            raise KeyError(key) from missing_state_error
+
+    def __getattr__(self, key: str) -> Any:
+        try:
+            return self._get(key)
+        except MissingStateError as missing_state_error:
+            raise AttributeError(
+                f"{type(self).__name__!r} object has no attribute {key!r}"
+            ) from missing_state_error
+
+    def __delitem__(self, key: str) -> None:
+        self._del(key)
+
+    def __delattr__(self, key: str) -> None:
+        self._del(key)
+
+    def get(self, key: str, default: Any = None) -> Any:
+        return self._state.get(key, default)
+
+
+class Request(httpx.Request):
+    state: State
+
+    def __init__(
+        self,
+        method: MethodTypes,
+        url: URLTypes,
+        *,
+        params: Optional[QueriesTypes] = None,
+        headers: Optional[HeaderTypes] = None,
+        cookies: Optional[CookieTypes] = None,
+        content: Optional[RequestContent] = None,
+        data: Optional[RequestData] = None,
+        files: Optional[RequestFiles] = None,
+        json: Optional[Any] = None,
+        stream: Union[SyncByteStream, AsyncByteStream, None] = None,
+        extensions: Optional[RequestExtensions] = None,
+        state: Optional[State] = None,
+    ):
+        super().__init__(
+            method=method,
+            url=url,
+            params=params,
+            headers=headers,
+            cookies=cookies,
+            content=content,
+            data=data,
+            files=files,
+            json=json,
+            stream=stream,
+            extensions=extensions,
+        )
+
+        if state is not None:
+            self.state = state
+        else:
+            self.state = State()
+
+    @classmethod
+    def from_httpx_request(cls, request: httpx.Request, /) -> "Request":
+        if hasattr(request, "_content"):
+            return cls(
+                method=request.method,
+                url=request.url,
+                headers=request.headers,
+                extensions=request.extensions,
+                content=request.content,
+            )
+        else:
+            return cls(
+                method=request.method,
+                url=request.url,
+                headers=request.headers,
+                extensions=request.extensions,
+                stream=request.stream,
+            )
+
+
+class Response(httpx.Response):
+    state: State
+
+    def __init__(
+        self,
+        status_code: int,
+        *,
+        headers: Optional[HeaderTypes] = None,
+        content: Optional[ResponseContent] = None,
+        text: Optional[str] = None,
+        html: Optional[str] = None,
+        json: Any = None,
+        stream: Union[SyncByteStream, AsyncByteStream, None] = None,
+        request: Optional[httpx.Request] = None,
+        extensions: Optional[ResponseExtensions] = None,
+        history: Optional[List[httpx.Response]] = None,
+        default_encoding: Union[str, Callable[[bytes], str]] = "utf-8",
+        state: Optional[State] = None,
+    ):
+        super().__init__(
+            status_code=status_code,
+            headers=headers,
+            content=content,
+            text=text,
+            html=html,
+            json=json,
+            stream=stream,
+            request=request,
+            extensions=extensions,
+            history=history,
+            default_encoding=default_encoding,
+        )
+
+        if state is not None:
+            self.state = state
+        else:
+            self.state = State()
+
+    @classmethod
+    def from_httpx_response(cls, response: httpx.Response, /) -> "Response":
+        if hasattr(response, "_content"):
+            return cls(
+                status_code=response.status_code,
+                headers=response.headers,
+                request=response.request,
+                content=response.content,
+            )
+        else:
+            return cls(
+                status_code=response.status_code,
+                headers=response.headers,
+                request=response.request,
+                stream=response.stream,
+            )
+
+
+DEFAULT_TRUST_ENV: bool = True
+DEFAULT_ENCODING: str = "utf-8"
+DEFAULT_FOLLOW_REDIRECTS: bool = False
+DEFAULT_BASE_URL: URLTypes = ""
+DEFAULT_EVENT_HOOKS: EventHooks = {"request": [], "response": []}
+
+
+@dataclass(init=False)
+class ClientOptions:
+    auth: Optional[AuthTypes]
+    params: QueryParams
+    headers: Headers
+    cookies: Cookies
+    timeout: Timeout
+    follow_redirects: bool
+    max_redirects: int
+    event_hooks: EventHooks
+    base_url: URL
+    trust_env: bool
+    default_encoding: DefaultEncodingTypes
+
+    def __init__(
+        self,
+        auth: Optional[AuthTypes] = None,
+        params: Optional[QueriesTypes] = None,
+        headers: Optional[HeadersTypes] = None,
+        cookies: Optional[CookiesTypes] = None,
+        timeout: TimeoutTypes = DEFAULT_TIMEOUT_CONFIG,
+        follow_redirects: bool = DEFAULT_FOLLOW_REDIRECTS,
+        max_redirects: int = DEFAULT_MAX_REDIRECTS,
+        event_hooks: Optional[EventHooks] = None,
+        base_url: URLTypes = DEFAULT_BASE_URL,
+        trust_env: bool = DEFAULT_TRUST_ENV,
+        default_encoding: DefaultEncodingTypes = DEFAULT_ENCODING,
+    ) -> None:
+        self.auth = auth
+        self.params = (
+            converters.convert_query_params(params)
+            if params is not None
+            else QueryParams()
+        )
+        self.headers = (
+            converters.convert_headers(headers) if headers is not None else Headers()
+        )
+        self.cookies = (
+            converters.convert_cookies(cookies) if cookies is not None else Cookies()
+        )
+        self.timeout = (
+            converters.convert_timeout(timeout) if timeout is not None else Timeout()
+        )
+        self.follow_redirects = follow_redirects
+        self.max_redirects = max_redirects
+        self.event_hooks = (
+            event_hooks if event_hooks is not None else DEFAULT_EVENT_HOOKS
+        )
+        self.base_url = URL(base_url)
+        self.trust_env = trust_env
+        self.default_encoding = default_encoding
+
+    def build(self) -> httpx.Client:
+        return httpx.Client(
+            auth=self.auth,
+            params=self.params,
+            headers=self.headers,
+            cookies=self.cookies,
+            timeout=self.timeout,
+            follow_redirects=self.follow_redirects,
+            max_redirects=self.max_redirects,
+            event_hooks=self.event_hooks,
+            base_url=self.base_url,
+            trust_env=self.trust_env,
+            default_encoding=self.default_encoding,
+        )
+
+    def is_default(self) -> bool:
+        return all(
+            (
+                self.auth == None,
+                self.params == QueryParams(),
+                self.headers == Headers(),
+                self.cookies == Cookies(),
+                self.timeout == DEFAULT_TIMEOUT_CONFIG,
+                self.follow_redirects == DEFAULT_FOLLOW_REDIRECTS,
+                self.max_redirects == DEFAULT_MAX_REDIRECTS,
+                self.event_hooks == DEFAULT_EVENT_HOOKS,
+                self.base_url == DEFAULT_BASE_URL,
+                self.trust_env == DEFAULT_TRUST_ENV,
+                self.default_encoding == DEFAULT_ENCODING,
+            )
+        )
+
+    @classmethod
+    def from_client(cls, client: httpx.Client, /) -> "ClientOptions":
+        return cls(
+            auth=client.auth,
+            params=client.params,
+            headers=client.headers,
+            cookies=client.cookies,
+            timeout=client.timeout,
+            follow_redirects=client.follow_redirects,
+            max_redirects=client.max_redirects,
+            event_hooks=client.event_hooks,
+            base_url=client.base_url,
+            trust_env=client.trust_env,
+            default_encoding=client._default_encoding,
+        )
+
+
+@dataclass(init=False)
+class PreRequest:
+    method: str
+    url: URL
+    params: QueryParams
+    headers: Headers
+    cookies: Cookies
+    content: Optional[RequestContent]
+    data: Optional[RequestData]
+    files: Optional[RequestFiles]
+    json: Optional[JsonTypes]
+    timeout: Optional[Timeout]
+    path_params: MutableMapping[str, str]
+    state: State
+
+    def __init__(
+        self,
+        method: MethodTypes,
+        url: URLTypes,
+        *,
+        params: Optional[QueriesTypes] = None,
+        headers: Optional[HeadersTypes] = None,
+        cookies: Optional[CookiesTypes] = None,
+        content: Optional[RequestContent] = None,
+        data: Optional[RequestData] = None,
+        files: Optional[RequestFiles] = None,
+        json: Optional[JsonTypes] = None,
+        timeout: Optional[TimeoutTypes] = None,
+        path_params: Optional[PathsTypes] = None,
+        state: Optional[State] = None,
+    ) -> None:
+        ...
+        self.method = method if isinstance(method, str) else method.decode()
+        self.url = URL(url)
+        self.params = (
+            converters.convert_query_params(params)
+            if params is not None
+            else QueryParams()
+        )
+        self.headers = (
+            converters.convert_headers(headers) if headers is not None else Headers()
+        )
+        self.cookies = (
+            converters.convert_cookies(cookies) if cookies is not None else Cookies()
+        )
+        self.content = content
+        self.data = data
+        self.files = files
+        self.json = json
+        self.timeout = (
+            converters.convert_timeout(timeout) if timeout is not None else None
+        )
+        self.path_params = (
+            converters.convert_path_params(path_params)
+            if path_params is not None
+            else {}
+        )
+        self.state = state if state is not None else State()
+
+    def build_request(self, client: Optional[httpx.Client]) -> Request:
+        url: str = self._get_formatted_url()
+
+        extensions: Dict[str, Any] = {}
+
+        if client is None:
+            if self.timeout is not None:
+                extensions["timeout"] = self.timeout.as_dict()
+
+            return Request(
+                self.method,
+                url,
+                params=self.params,
+                headers=self.headers,
+                cookies=self.cookies,
+                content=self.content,
+                data=self.data,
+                files=self.files,
+                json=self.json,
+                extensions=extensions,
+                state=self.state,
+            )
+        else:
+            httpx_request: httpx.Request = client.build_request(
+                self.method,
+                url,
+                params=self.params,
+                headers=self.headers,
+                cookies=self.cookies,
+                content=self.content,
+                data=self.data,
+                files=self.files,
+                json=self.json,
+                timeout=self.timeout,
+                extensions=extensions,
+            )
+
+            request: Request = Request.from_httpx_request(httpx_request)
+
+            request.state = self.state
+
+            return request
+
+    def merge(self, pre_request: "PreRequest", /) -> "PreRequest":
+        return self.__class__(
+            method=pre_request.method,
+            url=pre_request.url,
+            params=self.params.merge(pre_request.params),
+            headers=httpx.Headers({**self.headers, **pre_request.headers}),
+            cookies=httpx.Cookies({**self.cookies, **pre_request.cookies}),
+            content=(
+                pre_request.content if pre_request.content is not None else self.content
+            ),
+            data=(pre_request.data if pre_request.data is not None else self.data),
+            files=(pre_request.files if pre_request.files is not None else self.files),
+            json=(pre_request.json if pre_request.json is not None else self.json),
+            timeout=(
+                pre_request.timeout if pre_request.timeout is not None else self.timeout
+            ),
+            path_params={
+                **self.path_params,
+                **pre_request.path_params,
+            },
+        )
+
+    def _get_formatted_url(self) -> str:
+        return urllib.parse.unquote(str(self.url)).format(**self.path_params)
+
+    def validate(self):
+        url: str = urllib.parse.unquote(str(self.url))
+
+        expected_path_params: Set[str] = utils.parse_format_string(url)
+        actual_path_params: Set[str] = set(self.path_params.keys())
+
+        # Validate path params are correct
+        if expected_path_params != actual_path_params:
+            raise IncompatiblePathParameters(
+                f"Expected {tuple(expected_path_params)}, got {tuple(actual_path_params)}"
+            )
