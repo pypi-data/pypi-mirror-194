a9aba08bc04f992c98506790c4d5dea83e60f3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:27 +0000

    Add package similarity-engine-utils/0.0.6/similarity_engine_utils-0.0.6-py3-none-any.whl

---
+++ packages/similarity-engine-utils/similarity_engine_utils-0.0.6-py3-none-any.whl/se_utils/backend/milvus/conf.py
@@ -0,0 +1,9 @@
+DEFAULT_SCHEMA_URL = 'https://raw.githubusercontent.com/dsakovych/similarity-engine-utils/main/examples/schema_yp.json'
+
+DEFAULT_SEARCH_PARAMS = {
+    "metric_type": "L2",
+    "params": {
+        "nprobe": 1
+    },
+    "offset": 0
+}
