01d2ac9986568b0bd8ccd35311941162d983e978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package pydantic-duality/0.2.1/pydantic_duality-0.2.1.tar.gz

---
+++ packages/pydantic-duality/pydantic_duality-0.2.1.tar.gz/pydantic_duality-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pydantic_duality']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.9.2', 'typing-extensions>=4.4.0']
+
+setup_kwargs = {
+    'name': 'pydantic-duality',
+    'version': '0.2.1',
+    'description': 'Automatically generate two versions of your pydantic models: one with Extra.forbid and one with Extra.ignore',
+    'long_description': 'None',
+    'author': 'Stanislav Zmiev',
+    'author_email': 'szmiev2000@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/Ovsyanka83/pydantic-duality',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
