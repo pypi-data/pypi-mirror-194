caffd1a1b9cd49be0f54dc864b48f40470dd1016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package remotemanager/0.5.12/remotemanager-0.5.12-py3-none-any.whl

---
+++ packages/remotemanager/remotemanager-0.5.12-py3-none-any.whl/remotemanager/serialisation/__init__.py
@@ -0,0 +1,16 @@
+from remotemanager.serialisation.serialyaml import serialyaml
+from remotemanager.serialisation.serialjson import serialjson
+
+__all__ = ['serialyaml', 'serialjson']
+
+try:
+    from remotemanager.serialisation.serialdill import serialdill  # noqa: F401
+    __all__.append('serialdill')
+except ImportError:
+    pass
+
+try:
+    from remotemanager.serialisation.serialjsonpickle import serialjsonpickle  # noqa: F401, E501
+    __all__.append('serialjsonpickle')
+except ImportError:
+    pass
