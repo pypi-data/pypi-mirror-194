989ddc23b00ec3dc2f5aade8565ba4d7fe59d4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:15 +0000

    Add package slack-bots/0.1.0/slack_bots-0.1.0-py3-none-any.whl

---
+++ packages/slack-bots/slack_bots-0.1.0-py3-none-any.whl/slack_bots/slack_event_bot.py
@@ -0,0 +1,16 @@
+
+import logging
+
+import slackeventsapi
+
+from .slack_bot import SlackBot
+
+_LOG = logging.getLogger(__name__)
+
+
+class SlackEventBot(SlackBot):
+
+    """Event-based bot for Slack."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
