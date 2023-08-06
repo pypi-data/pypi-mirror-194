4287005eb5dae5027dc3c89d945d9e39587cdc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:27 +0000

    Add package elevenlabslib/0.2.1/elevenlabslib-0.2.1-py3-none-any.whl

---
+++ packages/elevenlabslib/elevenlabslib-0.2.1-py3-none-any.whl/elevenlabslib/__init__.py
@@ -0,0 +1,5 @@
+from .ElevenLabsUser import ElevenLabsUser
+from .ElevenLabsVoice import ElevenLabsVoice
+from .ElevenLabsSample import ElevenLabsSample
+from .ElevenLabsHistoryItem import ElevenLabsHistoryItem
+__all__ = ["ElevenLabsUser", "ElevenLabsVoice", "ElevenLabsSample", "ElevenLabsHistoryItem"]
