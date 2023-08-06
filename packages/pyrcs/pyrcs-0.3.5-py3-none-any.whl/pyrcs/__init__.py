1b11806479b0dcf621e172b5c5a7ecbc721a5c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:20 +0000

    Add package pyrcs/0.3.5/pyrcs-0.3.5-py3-none-any.whl

---
+++ packages/pyrcs/pyrcs-0.3.5-py3-none-any.whl/pyrcs/__init__.py
@@ -0,0 +1,45 @@
+"""Package initialization."""
+
+import datetime
+import json
+
+import pkg_resources
+
+from .collector import *
+
+with open(pkg_resources.resource_filename(__name__, "data/metadata.json"), mode='r') as metadata_file:
+    metadata = json.load(metadata_file)
+
+__project__ = metadata['Project']
+__pkgname__ = metadata['Package']
+
+__author__ = metadata['Author']
+__affiliation__ = metadata['Affiliation']
+__author_email__ = metadata['Email']
+
+__description__ = metadata['Description']
+
+__copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
+
+__version__ = metadata['Version']
+__license__ = metadata['License']
+
+__first_release_date__ = metadata['First release']
+
+__all__ = [
+    'LineData',
+    'OtherAssets',
+    'Electrification',
+    'ELRMileages',
+    'LineNames',
+    'LocationIdentifiers',
+    'LOR',
+    'TrackDiagrams',
+    'Bridges',
+    'Depots',
+    'Features',
+    'SignalBoxes',
+    'Stations',
+    'Tunnels',
+    'Viaducts',
+]
