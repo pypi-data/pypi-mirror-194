e5e47604086018bbc06f93ac61de304310640256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:31 +0000

    Add package ssh-pocket/1.2.1/ssh-pocket-1.2.1.tar.gz

---
+++ packages/ssh-pocket/ssh-pocket-1.2.1.tar.gz/ssh-pocket-1.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name="ssh-pocket",
+    version="1.2.1",
+    author="Dawn",
+    author_email="congminh292k@gmail.com",
+    description="Easy peasy ssh choosey",
+    keywords="ssh management tool",
+    license="MIT",
+    url="https://github.com/mmmdawn/ssh-pocket",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux"
+    ],
+    dependencies=[
+        "inquirerpy ~= 0.3.4"
+    ],
+    install_requires=[
+      "inquirerpy ~= 0.3.4"
+    ],
+    packages=["pocket"],
+    entry_points={"console_scripts": ["s=pocket.__main__:main", "si=pocket.import_ssh:main"]},
+    python_requires=">=3.6",
+)
