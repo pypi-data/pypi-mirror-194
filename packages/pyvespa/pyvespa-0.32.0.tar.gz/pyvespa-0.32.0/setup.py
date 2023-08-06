f51fdeb91ce927dedbff036872b9e807dfd6d007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:37 +0000

    Add package pyvespa/0.32.0/pyvespa-0.32.0.tar.gz

---
+++ packages/pyvespa/pyvespa-0.32.0.tar.gz/pyvespa-0.32.0/setup.py
@@ -0,0 +1,55 @@
+import os
+import setuptools
+
+
+def get_target_version():
+    pyvespa_version = os.environ.get("PYVESPA_VERSION", 0.32)
+    build_nr = os.environ.get("SD_EVENT_ID", "0")
+    return "{}.{}".format(pyvespa_version, build_nr)
+
+
+min_python = "3.6"
+
+setuptools.setup(
+    name="pyvespa",
+    version=get_target_version(),
+    description="Python API for vespa.ai",
+    keywords="vespa, search engine, data science",
+    author="Thiago G. Martins",
+    maintainer="Kristian Aune",
+    maintainer_email="kraune@yahooinc.com",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "pandas",
+        "docker",
+        "jinja2",
+        "cryptography",
+        "aiohttp",
+        "tenacity",
+    ],
+    extras_require={
+        "ml": ["transformers", "torch<1.13", "tensorflow", "tensorflow_ranking", "keras_tuner"],
+        "full": ["onnxruntime", "transformers", "torch<1.13", "tensorflow", "tensorflow_ranking", "keras_tuner"],
+    },
+    python_requires=">=3.6",
+    zip_safe=False,
+    package_data={"vespa": ["py.typed"]},
+    data_files=[
+        (
+            "templates",
+            [
+                "vespa/templates/hosts.xml",
+                "vespa/templates/services.xml",
+                "vespa/templates/schema.txt",
+                "vespa/templates/query_profile.xml",
+                "vespa/templates/query_profile_type.xml",
+                "vespa/templates/validation-overrides.xml"
+            ],
+        )
+    ],
+)
