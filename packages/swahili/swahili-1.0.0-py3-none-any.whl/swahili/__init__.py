29750d436c5ff15decc51b8f69b840d0ca2fee24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:29 +0000

    Add package swahili/1.0.0/swahili-1.0.0-py3-none-any.whl

---
+++ packages/swahili/swahili-1.0.0-py3-none-any.whl/swahili/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__author__ = 'Emmanuel Minga'
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+__all__ = [
+    'Maandishi',
+    'Neno',
+    'Sentensi',
+    'Mwana',
+    'OrodhaManeno',
+]
