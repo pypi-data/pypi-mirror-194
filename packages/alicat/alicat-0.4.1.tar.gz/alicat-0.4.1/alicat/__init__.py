95a9b5b7e9fad2160ea9ec5f1d7cfdb0ed648897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:38 +0000

    Add package alicat/0.4.1/alicat-0.4.1.tar.gz

---
+++ packages/alicat/alicat-0.4.1.tar.gz/alicat-0.4.1/alicat/__init__.py
@@ -0,0 +1,52 @@
+"""Python driver for Alicat mass flow controllers.
+
+Distributed under the GNU General Public License v2
+Copyright (C) 2019 NuMat Technologies
+"""
+from alicat.serial import FlowController, FlowMeter, command_line  # noqa
+
+
+def run():
+    """CLI interface, accessible when installed through pip."""
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Control an Alicat mass "
+                                     "flow controller from the command line.")
+    parser.add_argument('port', nargs='?', default='/dev/ttyUSB0', help="The "
+                        "target serial port. Default '/dev/ttyUSB0'.")
+    parser.add_argument('--address', '-a', default='A', type=str, help="The "
+                        "device address, A-Z. Should only be used if multiple "
+                        "flow controllers are connected to one port or if"
+                        "device ID is not A.")
+    parser.add_argument('--set-gas', '-g', default=None, type=str,
+                        help="Sets the gas type. Supported gas types are: "
+                             "'Air', 'Ar', 'CH4', 'CO', 'CO2', 'C2H6', 'H2', "
+                             "'He', 'N2', 'N2O', 'Ne', 'O2', 'C3H8', "
+                             "'n-C4H10', 'C2H2', 'C2H4', 'i-C2H10', 'Kr', "
+                             "'Xe', 'SF6', 'C-25', 'C-10', 'C-8', 'C-2', "
+                             "'C-75', 'A-75', 'A-25', 'A1025', 'Star29', "
+                             "'P-5'")
+    parser.add_argument('--set-flow-rate', '-f', default=None, type=float,
+                        help="Sets the target flow rate of the controller.")
+    parser.add_argument('--set-pressure', '-p', default=None, type=float,
+                        help="Sets the target pressure of the controller.")
+    parser.add_argument('--stream', '-s', action='store_true',
+                        help="Sends a constant stream of flow controller "
+                             "data, formatted as a tab-separated table.")
+    parser.add_argument("--lock", "-l", action="store_true",
+                        help="Locks device display.")
+    parser.add_argument("--unlock", "-u", action="store_true",
+                        help="Unlocks device display.")
+    parser.add_argument("--hold", "-hd", action="store_true",
+                        help="Holds the valve at the present value.")
+    parser.add_argument("--cancel-hold", "-c", action="store_true",
+                        help="Cancel valve hold.")
+    parser.add_argument("--reset-totalizer", "-r", action="store_true",
+                        help="Reset current value of totalizer to zero.")
+    args = parser.parse_args()
+
+    command_line(args)
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/alicat/alicat-0.4.1.tar.gz/alicat-0.4.1/alicat/mock.py
@@ -0,0 +1,52 @@
+"""Mock for offline testing of `FlowController`s."""
+
+from random import choice, random
+from time import sleep
+from unittest.mock import MagicMock
+
+
+class FlowController(MagicMock):
+    """Mocks an Alicat MFC for offline testing."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the device client."""
+        self.controlpoint = choice(['flow', 'pressure'])
+        self.setpoint = 10
+        self.gas = 'N2'
+
+    def get(self):
+        """Get the current state of the flow controller."""
+        sleep(random() * 0.25)
+        return {
+            'setpoint': self.setpoint,
+            'control_point': self.control_point,
+            'gas': self.gas,
+            'mass_flow': self.setpoint * (0.95 + 0.1 * random()),
+            'pressure': random() * 50.0,
+            'temperature': random() * 50.0,
+            'total_flow': 0.0,
+            'volumetric_flow': 0.0,
+        }
+
+    def _set_setpoint(self, setpoint):
+        self.setpoint = setpoint
+
+    def _set_control_point(self, control_point):
+        self.control_point = control_point
+
+    def _get_control_point(self):
+        return self.control_point
+
+    def set_flow_rate(self, flowrate):
+        """Set the target setpoint."""
+        self._set_control_point('flow')
+        self._set_setpoint(flowrate)
+
+    def set_gas(self, gas):
+        """Set the gas type."""
+        self.gas = gas
+
+    def set_pressure(self, pressure):
+        """Set the target pressure."""
+        self._set_control_point('pressure')
+        self._set_setpoint(pressure)
