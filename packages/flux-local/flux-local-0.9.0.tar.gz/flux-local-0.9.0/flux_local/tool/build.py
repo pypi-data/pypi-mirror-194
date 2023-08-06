4de4453ae7e749c992353dd6a8046b187213a46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:45 +0000

    Add package flux-local/0.9.0/flux-local-0.9.0.tar.gz

---
+++ packages/flux-local/flux-local-0.9.0.tar.gz/flux-local-0.9.0/flux_local/tool/build.py
@@ -0,0 +1,56 @@
+"""Flux-local build action."""
+
+import logging
+import pathlib
+import tempfile
+
+
+from flux_local import git_repo
+
+from .visitor import ContentOutput, HelmVisitor
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class BuildAction:
+    """Flux-local build action."""
+
+    async def run(  # type: ignore[no-untyped-def]
+        self,
+        path: pathlib.Path,
+        enable_helm: bool,
+        skip_crds: bool,
+        skip_secrets: bool,
+        **kwargs,  # pylint: disable=unused-argument
+    ) -> None:
+        """Async Action implementation."""
+
+        query = git_repo.ResourceSelector(path=git_repo.PathSelector(path=path))
+        query.kustomization.namespace = None
+        query.kustomization.skip_crds = skip_crds
+        query.kustomization.skip_secrets = skip_secrets
+        query.helm_release.enabled = enable_helm
+        query.helm_release.namespace = None
+
+        content = ContentOutput()
+        query.kustomization.visitor = content.visitor()
+        helm_visitor = HelmVisitor()
+        query.helm_repo.visitor = helm_visitor.repo_visitor()
+        query.helm_release.visitor = helm_visitor.release_visitor()
+        await git_repo.build_manifest(selector=query)
+
+        if enable_helm:
+            with tempfile.TemporaryDirectory() as helm_cache_dir:
+                await helm_visitor.inflate(
+                    pathlib.Path(helm_cache_dir),
+                    content.visitor(),
+                    skip_crds,
+                    skip_secrets,
+                )
+
+        keys = list(content.content)
+        keys.sort()
+        for key in keys:
+            for line in content.content[key]:
+                print(line)
