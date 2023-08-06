61740cd8184f3f8a917aa08ba7ac714d1e7f8a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:00 +0000

    Add package dnres/0.2.0/dnres-0.2.0.tar.gz

---
+++ packages/dnres/dnres-0.2.0.tar.gz/dnres-0.2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2.0'
+DESCRIPTION = 'Managing and sharing data and results of analysis.'
+LONG_DESCRIPTION = 'A package that facilitates modular type of data analysis providing a tagging system for managing generated data and resuls.'
+
+# Setting up
+setup(
+    name="dnres",
+    version=VERSION,
+    author="Dimitrios Kioroglou",
+    author_email="<d.kioroglou@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['rich'],
+    keywords=['python', 'data analysis', 'bioinformatics', 'reporting', 'analysis results'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
