fd672937372992ec53d6a3584985c6d8cd7ab0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:43 +0000

    Add package Droxic/0.2/Droxic-0.2.tar.gz

---
+++ packages/Droxic/Droxic-0.2.tar.gz/Droxic-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Droxic',
+    version='0.2',
+    description='A simple library to help with Droxification',
+    py_modules=['Droxic'],
+    author='Hamza Khan',
+    author_email='hamzamania@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
