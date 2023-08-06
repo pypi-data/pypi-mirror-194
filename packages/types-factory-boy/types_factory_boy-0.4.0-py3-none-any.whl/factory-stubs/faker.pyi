e914712c8d587e76b217d1a4fa721f5b30f7c367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:21 +0000

    Add package types-factory-boy/0.4.0/types_factory_boy-0.4.0-py3-none-any.whl

---
+++ packages/types-factory-boy/types_factory_boy-0.4.0-py3-none-any.whl/factory-stubs/faker.pyi
@@ -0,0 +1,24 @@
+from typing import Any, ContextManager, Generic, TypeVar
+
+import faker
+import faker.providers
+
+from . import declarations
+
+T = TypeVar("T")  # Type of the instance
+V = TypeVar("V")  # Type of the attribute
+
+class Faker(Generic[T, V], declarations.BaseDeclaration[T, V]):
+    provider: str
+    def __init__(
+        self, provider: str, **kwargs: declarations.BaseDeclaration[T, Any] | Any
+    ) -> None: ...
+    @classmethod
+    def override_default_locale(cls, locale: str) -> ContextManager[None]: ...
+    @classmethod
+    @classmethod
+    def _get_faker(cls, locale: str | None = ...) -> faker.Faker: ...
+    @classmethod
+    def add_provider(
+        cls, provider: faker.providers.BaseProvider, locale: str | None = ...
+    ) -> None: ...
