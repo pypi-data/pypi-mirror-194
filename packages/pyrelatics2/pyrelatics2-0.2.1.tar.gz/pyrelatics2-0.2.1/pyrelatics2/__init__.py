84e0f3c29e864b71a7564de5f2cfe5013ca87c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:17 +0000

    Add package pyrelatics2/0.2.1/pyrelatics2-0.2.1.tar.gz

---
+++ packages/pyrelatics2/pyrelatics2-0.2.1.tar.gz/pyrelatics2-0.2.1/pyrelatics2/__init__.py
@@ -0,0 +1,4 @@
+from .client import ClientCredential, RelaticsWebservices
+from .exceptions import TokenRequestError
+from .result_classes import ExportResult, ImportResult
+from .version import __version__
