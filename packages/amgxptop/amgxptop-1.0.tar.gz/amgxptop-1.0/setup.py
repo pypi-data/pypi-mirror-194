5713956fc7cbd2d68f40d64b7b8df4600619d2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:49 +0000

    Add package amgxptop/1.0/amgxptop-1.0.tar.gz

---
+++ packages/amgxptop/amgxptop-1.0.tar.gz/amgxptop-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0'
+DESCRIPTION = 'Request Package LMAO'
+
+# Setting up
+setup(
+    name="amgxptop",
+    version=VERSION,
+    author="amgxp",
+    author_email="<Amgxpp@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['vardxg'],
+    keywords=['requests'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
