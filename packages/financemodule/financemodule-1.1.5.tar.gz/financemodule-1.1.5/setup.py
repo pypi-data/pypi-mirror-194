dbd7f3387172c7bff8fde26a3a1c80b33609e4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:35 +0000

    Add package financemodule/1.1.5/financemodule-1.1.5.tar.gz

---
+++ packages/financemodule/financemodule-1.1.5.tar.gz/financemodule-1.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+# read from README.RST
+def readme():
+    with open('readme.md') as f:
+        return f.read()
+
+
+setup(
+    name='financemodule',
+    author= "AnandRaj",
+    package_data={'financemodule': ['*']},
+    long_description=readme(),
+    long_description_content_type='text/markdown'
+)
