433dbc71a51bd6b4079e250fc5ce1b35d2df89f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:45 +0000

    Add package phamt/0.1.7/phamt-0.1.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/phamt/phamt-0.1.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl/phamt/test/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+####################################################################################################
+# phamt/test/__init__.py
+# Initialization of the phamt library tests.
+# By Noah C. Benson
+
+from .phamt import TestPHAMT
