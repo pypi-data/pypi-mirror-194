05fcf9c771349f4193668841a2b54a462c61dbe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:47 +0000

    Add package pyvrp/0.0.3a4/pyvrp-0.0.3a4-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyvrp/pyvrp-0.0.3a4-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/Individual_bindings.cpp
@@ -0,0 +1,59 @@
+#include "Individual.h"
+
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <sstream>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_Individual, m)
+{
+    py::class_<Individual>(m, "Individual")
+        .def(py::init<ProblemData &, PenaltyManager &, XorShift128 &>(),
+             py::arg("data"),
+             py::arg("penalty_manager"),
+             py::arg("rng"),
+             py::keep_alive<1, 2>(),  // keep data and penalty_manager alive
+             py::keep_alive<1, 3>())  // at least until individual is freed
+        .def(py::init<ProblemData &,
+                      PenaltyManager &,
+                      std::vector<std::vector<int>>>(),
+             py::arg("data"),
+             py::arg("penalty_manager"),
+             py::arg("routes"),
+             py::keep_alive<1, 2>(),  // keep data and penalty_manager alive
+             py::keep_alive<1, 3>())  // at least until individual is freed
+        .def("cost", &Individual::cost)
+        .def("num_routes", &Individual::numRoutes)
+        .def("get_routes",
+             &Individual::getRoutes,
+             py::return_value_policy::reference_internal)
+        .def("get_neighbours",
+             &Individual::getNeighbours,
+             py::return_value_policy::reference_internal)
+        .def("is_feasible", &Individual::isFeasible)
+        .def("has_excess_capacity", &Individual::hasExcessCapacity)
+        .def("has_time_warp", &Individual::hasTimeWarp)
+        .def(
+            "__copy__",
+            [](Individual const &individual) { return Individual(individual); })
+        .def(
+            "__deepcopy__",
+            [](Individual const &individual, py::dict) {
+                return Individual(individual);
+            },
+            py::arg("memo"))
+        .def("__hash__",
+             [](Individual const &individual)
+             { return std::hash<Individual>()(individual); })
+        .def(pybind11::self == pybind11::self)  // this is __eq__
+        .def("__str__",
+             [](Individual const &individual)
+             {
+                 std::stringstream stream;
+                 stream << individual;
+                 return stream.str();
+             });
+}
---
+++ packages/pyvrp/pyvrp-0.0.3a4-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/crossover/crossover.h
@@ -0,0 +1,44 @@
+#ifndef CROSSOVER_H
+#define CROSSOVER_H
+
+#include "Individual.h"
+#include "PenaltyManager.h"
+#include "ProblemData.h"
+#include "XorShift128.h"
+
+#include <functional>
+#include <vector>
+
+namespace crossover
+{
+/**
+ * Greedily inserts the unplanned clients into non-empty routes.
+ */
+void greedyRepair(std::vector<std::vector<int>> &routes,
+                  std::vector<int> const &unplanned,
+                  ProblemData const &data);
+}  // namespace crossover
+
+typedef std::function<Individual(
+    std::pair<Individual const *, Individual const *> const &,
+    ProblemData const &,
+    PenaltyManager &,
+    XorShift128 &)>
+    CrossoverOperator;
+
+/**
+ * Performs two SREX crossovers of the given parents (binary tournament). This
+ * was one of ORTEC's DIMACS contributions.
+ * <br />
+ * Yuichi Nagata and Shigenobu Kobayashi. "A memetic algorithm for the pickup
+ * and delivery problem with time windows using selective route exchange
+ * crossover". In: International Conference on Parallel Problem Solving from
+ * Nature. Springer. 2010, pp. 536–545.
+ */
+Individual selectiveRouteExchange(
+    std::pair<Individual const *, Individual const *> const &parents,
+    ProblemData const &data,
+    PenaltyManager const &penaltyManager,
+    XorShift128 &rng);
+
+#endif  // CROSSOVER_H
