8b007ad6efb331c83700e1df3b18dd8ae5d4a8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:20:17 +0000

    Add package denari/1.0.14/denari-1.0.14.tar.gz

---
+++ packages/denari/denari-1.0.14.tar.gz/denari-1.0.14/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='denari',
+    version='1.0.14',
+    description='DenariAnalytics OpenSouce Business and Tax Tools',
+    author='Fadil Karim',
+    author_email='insights@denarianalytics.com',
+    packages=['TaxTools','NarcoAnalytics','Montana'],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'plotly',
+        'dash'
+    ],
+    package_data={
+    'TaxTools': ['UK Tax Tables/*.csv',
+                 'UK Tax Tables/2020-2021/*.csv',
+                 'UK Tax Tables/2021-2022/*.csv',
+                 'UK Tax Tables/2022-2023/*.csv',
+                 'UK Tax Tables/2023-2024/*.csv']
+                 }
+
+)
