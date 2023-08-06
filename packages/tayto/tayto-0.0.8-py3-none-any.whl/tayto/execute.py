8e684cb72b6f3b91b8049aed5110d962b009c423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:12 +0000

    Add package tayto/0.0.8/tayto-0.0.8-py3-none-any.whl

---
+++ packages/tayto/tayto-0.0.8-py3-none-any.whl/tayto/execute.py
@@ -0,0 +1,21 @@
+import sys, os
+from tayto import linux
+
+version = ''.join(sys.version.split('.')[:2])
+home = os.getcwd()
+path, mode, *_ = sys.argv[1:]
+folder, filename, ext = linux.path_break(path)
+# ~/.config/Code/User/keybindings.json
+# /home/quang/.local/lib/python3.10/site-packages/tayto/execute.py
+
+
+if mode == 'exe':
+  if ext == 'py':
+    os.system(f'python3 {path}')
+  elif ext == 'sh':
+    os.system(f'chmod +x {path}')
+    os.system(f'/usr/bin/bash {path}')
+  elif ext == 'js':
+    os.system(f'node {path}')
+
+if mode == 'test': pass
