752c1ccfe178c736ebdccbf9baa0139c8580f953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:05 +0000

    Add package aind-segmentation-evaluation/0.1.2/aind_segmentation_evaluation-0.1.2.tar.gz

---
+++ packages/aind-segmentation-evaluation/aind_segmentation_evaluation-0.1.2.tar.gz/aind_segmentation_evaluation-0.1.2/demo.py
@@ -0,0 +1,39 @@
+"""
+Created on Tue Jan 3 15:00:00 2023
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+"""
+
+import os
+
+from aind_segmentation_evaluation.run_evaluation import (
+    graph_based_eval,
+    voxel_based_eval,
+)
+
+if __name__ == "__main__":
+
+    # Initializations
+    shape = (148, 226, 282)
+    data_dir = "./resources"
+    path_to_pred_volume = os.path.join(data_dir, "pred_volume.tif")
+    path_to_target_volume = os.path.join(data_dir, "target_volume.tif")
+    target_graphs_dir = os.path.join(data_dir, "target_graphs")
+
+    # Evaluation
+    graph_stats = graph_based_eval(
+        shape,
+        target_graphs_dir=target_graphs_dir,
+        path_to_pred_volume=path_to_pred_volume,
+        output="tif",
+        output_dir=data_dir,
+    )
+
+    # Write out results
+    print("Graph-based evaluation...")
+    for key in graph_stats.keys():
+        print("   " + key + ":", graph_stats[key])
+    print("")
+
