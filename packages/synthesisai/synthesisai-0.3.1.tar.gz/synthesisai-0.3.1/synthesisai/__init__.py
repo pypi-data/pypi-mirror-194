065a1280c7311c7bbb16d694069e062f132c5b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:40 +0000

    Add package synthesisai/0.3.1/synthesisai-0.3.1.tar.gz

---
+++ packages/synthesisai/synthesisai-0.3.1.tar.gz/synthesisai-0.3.1/synthesisai/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for synthesisai."""
+
+__author__ = """Synthesis AI"""
+__email__ = "anish@synthesis.ai"
+__version__ = '0.3.1'
+
+from .dataset import Grouping, SaiDataset
+from .modality import Modality
