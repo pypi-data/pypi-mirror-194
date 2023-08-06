ac3581daa58a2e7c1059c80931064b407a6dc7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:06 +0000

    Add package simplecloud/0.0.0.1/simplecloud-0.0.0.1.tar.gz

---
+++ packages/simplecloud/simplecloud-0.0.0.1.tar.gz/simplecloud-0.0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+long_description = 'API and CLI to manage a remote filesystem'
+
+setup(
+    name='simplecloud',
+    version='0.0.0.1',
+    author='Garrett Charles',
+    author_email='realcomputerscientist@gmail.com',
+    description='Local Network Cloud',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cloud = cloud.cloud_cli:main',
+            'cloud-server = cloud.cloud_api:main'
+        ]
+    },
+    keywords='',
+    install_requires=requirements,
+    zip_safe=False
+)
