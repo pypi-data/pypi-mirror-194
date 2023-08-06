a07a651e2aa6ae5a6f0064b83289ef6a05097d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:19 +0000

    Add package random-unicode-emoji/1.1/random_unicode_emoji-1.1.tar.gz

---
+++ packages/random-unicode-emoji/random_unicode_emoji-1.1.tar.gz/random_unicode_emoji-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='random_unicode_emoji',
+    version='1.1',
+    license='MIT',
+    author="Nic Jones",
+    author_email='nic@nicpjones.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/NicPWNs/random-unicode-emoji',
+    keywords='random unicode emoji',
+)
