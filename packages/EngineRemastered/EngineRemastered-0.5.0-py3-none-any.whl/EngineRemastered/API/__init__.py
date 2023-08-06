4abe5bb3a248da60b64abefafb95cc5a4ad1e981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package EngineRemastered/0.5.0/EngineRemastered-0.5.0-py3-none-any.whl

---
+++ packages/EngineRemastered/EngineRemastered-0.5.0-py3-none-any.whl/EngineRemastered/API/__init__.py
@@ -0,0 +1,9 @@
+"""
+Engine Remastered.
+
+A description
+"""
+
+__version__ = "0.5.0"
+__author__ = 'Brian Masse'
+__credits__ = 'Me'
---
+++ packages/EngineRemastered/EngineRemastered-0.5.0-py3-none-any.whl/EngineRemastered/API/main.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import pkg_resources
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+builds = os.path.dirname(parent)
+
+import _EngineRemastered as _ER
+
+class EngineRemasteredApp:
+
+    def __init__(this):
+        #start the app
+        # Main/app/GraphicsPipeline/shader.frag
+
+        # one of these will lead to the site packages folder, where we can access the right data
+        p = ""
+        for path in sys.path:
+            if "site-packages" in path:
+                p = path
+                break
+
+        this.app = _ER.EngineRemasteredApp()
+        this.app.setPath( p )
+        this.app.initApp()
+
+    def addObject( this, object ):
+        this.app.createObj(object.name, "base.png", object.scale[0], object.scale[1], object.scale[1], object.transform[0], object.transform[1], object.transform[2], object.color[0] / 255, object.color[1] / 255, object.color[2] / 255 )
+
+    def startApp(this):
+        this.app.runApp()
+
+class EngineObject:
+    def __init__(this, name, scale, transform, color):
+        this.name = name
+        this.scale = scale
+        this.transform = transform
+        this.color = color
+
+
+# app = EngineRemasteredApp()
+
+# name = "monkey.ply"
+# scale = (1, 1, 1)
+# transform = (1, 1, 1)
+# color = (255, 1, 1)
+
+# object = EngineObject(name, scale, transform, color)
+
+# app.addObject(object)
+# app.startApp()
