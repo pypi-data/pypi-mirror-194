985a7ef312d38a06442dfcc3186a9cacf527c9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:51 +0000

    Add package bot-alert-rate/0.0.2/bot_alert_rate-0.0.2.tar.gz

---
+++ packages/bot-alert-rate/bot_alert_rate-0.0.2.tar.gz/bot_alert_rate-0.0.2/bot_alert_rate/constants.py
@@ -0,0 +1,13 @@
+CHAIN_IDS = {1, 56, 42161, 137, 43114, 10, 250}
+QUERY_PAYLOAD = {
+    "query": "{records{   contract_interaction_count   contract_creation_count   erc_approval_all_count   erc_approval_count   erc_transfer_count   large_value_transfer_count   transfer_count   tx_count   tx_with_input_data_count }}"
+}
+DATASET_IDS = {
+    1: "sq_786661e4d8b140cfb22f77cae19de1c5",
+    56: "sq_14d40ded55034637868a1b857917f10b",
+    42161: "sq_e0f899885862445995cb5286bdfd1b26",
+    137: "sq_f4ef7e179b594f9ca66578f017b227f1",
+    43114: "Avalanche will be supported in Q2, 2023. Please use custom scan count type.",
+    10: "Optimism will be supported in Q2, 2023. Please use custom scan count type.",
+    250: "Fantom will be suppored in Q3, 2023. Please use custom scan count type.",
+}
