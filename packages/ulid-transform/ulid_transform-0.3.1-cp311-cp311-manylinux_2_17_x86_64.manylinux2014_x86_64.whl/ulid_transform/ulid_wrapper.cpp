47c150fa8902aff3d567bca87301e764c3618cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:13 +0000

    Add package ulid-transform/0.3.1/ulid_transform-0.3.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ulid-transform/ulid_transform-0.3.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ulid_transform/ulid_wrapper.cpp
@@ -0,0 +1,25 @@
+#include "ulid_wrapper.h"
+#include "ulid.hh"
+
+using namespace std;
+
+std::string _cpp_ulid() {
+  ulid::ULID ulid;
+  ulid::EncodeNowRand(ulid);
+  return ulid::Marshal(ulid);
+}
+
+std::string _cpp_ulid_at_time(double epoch_time) {
+  time_t encoded_time = static_cast<time_t>(epoch_time*1000);
+  ulid::ULID ulid;
+  ulid::EncodeTime(encoded_time, ulid);
+  ulid::EncodeEntropyRand(ulid);
+  return ulid::Marshal(ulid);
+}
+
+std::string _cpp_ulid_to_bytes(std::string ulid_string) {
+  ulid::ULID ulid = ulid::Unmarshal(ulid_string.c_str());
+  std::vector<uint8_t> data = ulid::MarshalBinary(ulid);
+  std::string str(reinterpret_cast<char *>(data.data()), data.size());
+  return str;
+}
