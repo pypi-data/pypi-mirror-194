48aad10ad13bcd807a02df3588a0371c88913898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:01 +0000

    Add package fastweb3/0.0.0.dev0/fastweb3-0.0.0.dev0-py3-none-any.whl

---
+++ packages/fastweb3/fastweb3-0.0.0.dev0-py3-none-any.whl/fastclf/__version__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#
+# fastclf - fastclf is a package that makes it easy to test different models on different datasets.
+#
+# Hans Park
+# rhcproc@gmail.com
+#
+# License: MIT
+#
+
+__title__ = 'fastweb3'
+__license__ = 'MIT'
+__version__ = '0.0.0dev'
+__author__ = 'Hans Park'
+__contact__ = 'rhcproc@gmail.com'
+__url__ = 'https://github.com/rhcproc/fastweb3'
+__description__ = (
+    '')
