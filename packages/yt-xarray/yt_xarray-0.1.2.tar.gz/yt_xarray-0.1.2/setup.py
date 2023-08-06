a4a8705209e33bd36a8dd8dee3ed9049892a3286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:14 +0000

    Add package yt-xarray/0.1.2/yt_xarray-0.1.2.tar.gz

---
+++ packages/yt-xarray/yt_xarray-0.1.2.tar.gz/yt_xarray-0.1.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as reqs_file:
+    requirements = reqs_file.read().strip().split("\n")
+
+
+test_requirements = [
+    "pytest>=3",
+]
+
+setup(
+    author="Chris Havlin",
+    author_email="chris.havlin@gmail.com",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="interface between yt and xarray",
+    install_requires=requirements,
+    license="MIT license",
+    long_description="tools for connecting yt and xarray",
+    include_package_data=True,
+    keywords="yt_xarray",
+    name="yt_xarray",
+    packages=find_packages(include=["yt_xarray", "yt_xarray.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/data-exp-lab/yt_xarray",
+    version="0.1.2",
+    zip_safe=False,
+)
