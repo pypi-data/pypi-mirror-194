1c64008c6ddc33b22c61733d47e9b4612b5c922d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:25 +0000

    Add package betfair-parser/0.1.20/betfair_parser-0.1.20.tar.gz

---
+++ packages/betfair-parser/betfair_parser-0.1.20.tar.gz/betfair_parser-0.1.20/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['betfair_parser',
+ 'betfair_parser.spec',
+ 'betfair_parser.spec.api',
+ 'betfair_parser.spec.streaming']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fsspec>=2022', 'msgspec>=0.12']
+
+setup_kwargs = {
+    'name': 'betfair-parser',
+    'version': '0.1.20',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Bradley McElroy',
+    'author_email': 'bradley.mcelroy@live.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
