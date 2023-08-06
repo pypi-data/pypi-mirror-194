2b1f416ca3b474c5f35d289a694d1ab7c9239b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:57 +0000

    Add package amoniak/1.0.0/amoniak-1.0.0.tar.gz

---
+++ packages/amoniak/amoniak-1.0.0.tar.gz/amoniak-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+INSTALL_REQUIRES = [
+    'empowering',
+    'erppeek',
+    'pymongo<3',
+    'rq',
+    'modeldict',
+    'times',
+    'raven',
+    'click'
+]
+
+setup(
+    name='amoniak',
+    version='1.0.0',
+    packages=['amoniak', 'amoniak.caching'],
+    url='http://gisce.net',
+    license='MIT',
+    install_requires=INSTALL_REQUIRES,
+    entry_points="""
+        [console_scripts]
+        amoniak=amoniak.runner:amoniak
+    """,
+    author='GISCE-TI, S.L.',
+    author_email='ti@gisce.net',
+    description='AMON Tools for GISCE-ERP'
+)
