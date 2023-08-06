82e076e28b95e38056aa0d41c35a721700e3d9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:33 +0000

    Add package fuzzy-sql/2.0.4b0/fuzzy-sql-2.0.4b0.tar.gz

---
+++ packages/fuzzy-sql/fuzzy-sql-2.0.4b0.tar.gz/fuzzy-sql-2.0.4b0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+
+
+setup(
+    name="fuzzy-sql",
+    version="2.0.4-beta",
+    description="A generator of random SQL SELECT queries mainly to compare responses from a real dataset against that from a synthetic dataset.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Samer Kababji",
+    author_email="skababji@ehealthinformation.ca",
+    classifiers=[
+        "Development Status :: 4 - Beta", "Programming Language :: Python :: 3.8","Operating System :: OS Independent","License :: OSI Approved :: MIT License"
+    ],
+    keywords="sql, synthetic, clinical trials, generative, testing, fuzzy, fuzzing",
+    project_urls = {
+        'Home': 'https://github.com/skababji-ehil/fuzzy_sql',
+        'Documentation': 'https://fuzzy-sql.readthedocs.io/en/latest/index.html#'
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "pandas",
+        "Jinja2",
+        "seaborn",
+        "scikit-learn",
+        "jupyter",
+        "jsonschema",
+        "multiprocess",
+        "scipy",
+        "matplotlib"
+    ],
+    extras_require={
+        "dev": ["wheel","dvc", "Sphinx","sphinxcontrib-bibtex"],
+    },
+
+)
