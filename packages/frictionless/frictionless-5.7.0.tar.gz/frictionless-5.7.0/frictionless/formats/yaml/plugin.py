2ca2f2df9264cd05903deba7c03c1bc1a8b226f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:31 +0000

    Add package frictionless/5.7.0/frictionless-5.7.0.tar.gz

---
+++ packages/frictionless/frictionless-5.7.0.tar.gz/frictionless-5.7.0/frictionless/formats/yaml/plugin.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from ...system import Plugin
+from ...detector import Detector
+from .control import YamlControl
+from .parser import YamlParser
+
+if TYPE_CHECKING:
+    from ...resource import Resource
+
+
+class YamlPlugin(Plugin):
+    """Plugin for Yaml"""
+
+    # Hooks
+
+    def create_parser(self, resource):
+        if resource.format == "yaml":
+            return YamlParser(resource)
+
+    def detect_resource(self, resource: Resource):
+        if resource.format == "yaml":
+            resource.mediatype = "text/yaml"
+
+    def detect_resource_type(self, resource: Resource):
+        if resource.format == "yaml":
+            return Detector.detect_metadata_type(resource.normpath) or "json"
+
+    def select_control_class(self, type):
+        if type == "yaml":
+            return YamlControl
