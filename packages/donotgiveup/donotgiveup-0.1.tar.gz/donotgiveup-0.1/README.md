473ad4424808c6e87378a2b1e67aed17b5e8e917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:41 +0000

    Add package donotgiveup/0.1/donotgiveup-0.1.tar.gz

---
+++ packages/donotgiveup/donotgiveup-0.1.tar.gz/donotgiveup-0.1/README.md
@@ -0,0 +1,1 @@
+This is not a leap year so you have a day less
---
+++ packages/donotgiveup/donotgiveup-0.1.tar.gz/donotgiveup-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='donotgiveup',
+    version='0.1',
+    description='Let us try this another time',
+    py_modules=['test_pb2', 'test_pb2_grpc'],
+    author='Amir Kidwai',
+    author_email='findamirkidwai@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
