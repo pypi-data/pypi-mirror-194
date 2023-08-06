156229c5887a50e9d753cf4a0f871b857836fecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:19 +0000

    Add package dpdata/0.2.14/dpdata-0.2.14.tar.gz

---
+++ packages/dpdata/dpdata-0.2.14.tar.gz/dpdata-0.2.14/dpdata/__init__.py
@@ -0,0 +1,19 @@
+from . import lammps, md, vasp
+from .system import LabeledSystem, MultiSystems, System
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    from .__about__ import __version__
+
+# BondOrder System has dependency on rdkit
+try:
+    # prevent conflict with dpdata.rdkit
+    import rdkit as _
+
+    USE_RDKIT = True
+except ModuleNotFoundError:
+    USE_RDKIT = False
+
+if USE_RDKIT:
+    from .bond_order_system import BondOrderSystem
