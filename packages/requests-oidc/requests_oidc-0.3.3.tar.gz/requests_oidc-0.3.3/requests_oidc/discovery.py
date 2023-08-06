cd03ead9e43e49b18f104d74b6bdd0206c2ef15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:14 +0000

    Add package requests-oidc/0.3.3/requests_oidc-0.3.3.tar.gz

---
+++ packages/requests-oidc/requests_oidc-0.3.3.tar.gz/requests_oidc-0.3.3/requests_oidc/discovery.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Self
+
+import requests
+
+
+@dataclass
+class ServerDetails:
+    oidc_url: str
+    auth_url: str
+    token_url: str
+    device_url: str
+
+    @classmethod
+    def discover(cls, oidc_url: str) -> Self:
+        res = requests.get(oidc_url)
+        res.raise_for_status()
+        data = res.json()
+
+        return cls(
+            oidc_url=oidc_url,
+            auth_url=data["authorization_endpoint"],
+            token_url=data["token_endpoint"],
+            device_url=data["device_authorization_endpoint"],
+        )
