ce169fb0d8ac5030db8c09b8b34d148244b123fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:03 +0000

    Add package ufit/1.6.4/ufit-1.6.4.tar.gz

---
+++ packages/ufit/ufit-1.6.4.tar.gz/ufit-1.6.4/ufit/gui/datasetitem.py
@@ -0,0 +1,10 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# ufit, a universal scattering fitting suite
+#
+# Copyright (c) 2013-2021, Georg Brandl and contributors.  All rights reserved.
+# Licensed under a 2-clause BSD license, see LICENSE.
+# *****************************************************************************
+
+# for pickle compatibility
+from ufit.gui.scanitem import ScanDataItem as DatasetItem
