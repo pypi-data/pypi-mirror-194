0a1d24ee5370b3154bca6af0aff35b22df50960c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:06 +0000

    Add package blifparser/2.0.1/blifparser-2.0.1.tar.gz

---
+++ packages/blifparser/blifparser-2.0.1.tar.gz/blifparser-2.0.1/blifparser/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Imports modules and packages for the developer.
+"""
+
+from ._version import __version__
+
+try:
+    from . import keywords
+    from . import utils
+    from . import blifparser
+
+except ImportError:
+    from .blifparser import keywords    # type: ignore
+    from .blifparser import utils       # type: ignore
+    from .blifparser import blifparser  # type: ignore
+
+if __name__ == "__main__":
+    blifparser.main()
---
+++ packages/blifparser/blifparser-2.0.1.tar.gz/blifparser-2.0.1/blifparser/_version.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__version__ = "2023-03-01 2.0.1"
