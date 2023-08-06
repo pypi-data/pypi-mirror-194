cb1704e5e8635974bdd15a433f594b93ba743363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:15 +0000

    Add package nextrade/0.1.6/nextrade-0.1.6.tar.gz

---
+++ packages/nextrade/nextrade-0.1.6.tar.gz/nextrade-0.1.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nextrade",
+    version="0.1.6",
+    author="nxt",
+    author_email="nxt@nextrade.com",
+    description="Trade volume analysis tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nextrade/library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'requests',
+        'plotly',
+    ],
+    python_requires='>=3.7',
+)
