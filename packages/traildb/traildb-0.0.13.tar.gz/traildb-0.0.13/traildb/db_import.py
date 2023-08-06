d0840b67d1e9bcdad2821f5a5d37c5f63652c9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:05 +0000

    Add package traildb/0.0.13/traildb-0.0.13.tar.gz

---
+++ packages/traildb/traildb-0.0.13.tar.gz/traildb-0.0.13/traildb/db_import.py
@@ -0,0 +1,32 @@
+import pymongo
+import os
+import json
+from mlflow.tracking import MlflowClient
+
+class trail_init:
+    def __init__(self, URI, database):
+        client = pymongo.MongoClient(URI)
+        self.db = client[database]
+
+
+
+    def log_experiment(self, mlflowrun, parent, data_meta):
+        tags = {k: v for k, v in mlflowrun.data.tags.items() if not k.startswith("mlflow.")}
+        artifacts = [f.path for f in MlflowClient().list_artifacts(mlflowrun.info.run_id, "model")]
+        d = {}
+        d['run_id'] = mlflowrun.info.run_id
+        d['artifacts'] = artifacts
+        d['params'] = mlflowrun.data.params
+        d['metrics'] = mlflowrun.data.metrics
+        d['tags'] = tags
+        d['parent'] = parent
+        d['data'] = data_meta
+        #print(d)
+        #with open('data.json', 'w') as fp:
+        #    json.dump(d, fp)
+        #DB_import.JSON_to_db(d)
+        collection = self.db['experiments']
+        collection.insert_one(d)
+
+    def get_run_info(self):
+        pass
