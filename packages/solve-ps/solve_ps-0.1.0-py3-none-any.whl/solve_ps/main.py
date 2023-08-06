e303be5e4dd455713971cfdbc0d51f9742f7f351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:45 +0000

    Add package solve-ps/0.1.0/solve_ps-0.1.0-py3-none-any.whl

---
+++ packages/solve-ps/solve_ps-0.1.0-py3-none-any.whl/solve_ps/main.py
@@ -0,0 +1,15 @@
+import click
+from solve_ps.subs.run import run
+from solve_ps.subs.get import get
+from solve_ps.subs.diff import diff
+from solve_ps.subs.tc import tc
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(run)
+cli.add_command(get)
+cli.add_command(diff)
+cli.add_command(tc)
+cli()
