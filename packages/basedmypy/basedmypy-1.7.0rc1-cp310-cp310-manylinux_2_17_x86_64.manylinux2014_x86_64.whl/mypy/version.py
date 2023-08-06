06070caedbb293f14ecd66c7968ae0329cfeb9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:20 +0000

    Add package basedmypy/1.7.0rc1/basedmypy-1.7.0rc1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/basedmypy/basedmypy-1.7.0rc1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "1.1.0"
+based_version_info = VersionInfo(major=1, minor=7, patch=0, release_level='rc', iteration=1, mypy_version='1.1.0', mypy_release_level='final')
+__based_version__ = "1.7.0rc1"
