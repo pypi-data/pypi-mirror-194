fbeb06344b1a7d5ded9b959dad6206ab4a0943a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:25 +0000

    Add package nightchanges/0.1/nightchanges-0.1.tar.gz

---
+++ packages/nightchanges/nightchanges-0.1.tar.gz/nightchanges-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='nightchanges',
+    version='0.1',
+    description='Come on Zayn Malik',
+    py_modules=['first'],
+    author='Amir Kidwai',
+    author_email='findamirkidwai@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
