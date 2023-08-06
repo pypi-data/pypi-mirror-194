1a6d513bdd634c80130a5c661321478eed6c4565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:20 +0000

    Add package credittomodels/0.0.21/credittomodels-0.0.21.tar.gz

---
+++ packages/credittomodels/credittomodels-0.0.21.tar.gz/credittomodels-0.0.21/credittomodels/User.py
@@ -0,0 +1,14 @@
+
+
+class User(object):
+
+    def __init__(self, user_id: int, user_name: str, password: str, jwt_token: str,
+                 key: str, token_creation_time: str, role_id: int):
+
+        self.user_id = user_id
+        self.user_name = user_name
+        self.password = password
+        self.jwt_token = jwt_token
+        self.key = key
+        self.token_creation_time = token_creation_time
+        self.role_if = role_id
