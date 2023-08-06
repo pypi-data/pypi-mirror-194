73f9a3706770a5b2e9464939c185f4b135177640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package pyxxl/0.3.0a2/pyxxl-0.3.0a2-py3-none-any.whl

---
+++ packages/pyxxl/pyxxl-0.3.0a2-py3-none-any.whl/pyxxl/ctx.py
@@ -0,0 +1,43 @@
+import logging
+from contextvars import ContextVar
+from typing import Any, Optional
+
+from pyxxl.schema import RunData
+
+_global_vars: ContextVar[dict] = ContextVar("pyxxl_vars", default={})
+
+
+class GlobalVars:
+    @staticmethod
+    def _set_var(name: str, obj: Any) -> None:
+        _global_vars.get()[name] = obj
+
+    @staticmethod
+    def _get_var(name: str) -> Any:
+        return _global_vars.get()[name]
+
+    @staticmethod
+    def try_get(name: str) -> Optional[Any]:
+        return _global_vars.get().get(name)
+
+    @staticmethod
+    def set_xxl_run_data(data: RunData) -> None:
+        GlobalVars._set_var("xxl_kwargs", data)
+
+    @property
+    def xxl_run_data(self) -> RunData:
+        return self._get_var("xxl_kwargs")
+
+    @staticmethod
+    def set_task_logger(logger: logging.Logger) -> None:
+        GlobalVars._set_var("task_logger", logger)
+
+    @property
+    def logger(self) -> logging.Logger:  # pragma: no cover
+        return self._get_var("task_logger")
+
+    def clear(self) -> None:
+        _global_vars.get().clear()
+
+
+g = GlobalVars()
