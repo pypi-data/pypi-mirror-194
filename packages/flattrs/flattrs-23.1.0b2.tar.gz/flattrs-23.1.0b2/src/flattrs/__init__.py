79f6d6e42fd6266f31a2d07fa489f5766e7e197f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:12 +0000

    Add package flattrs/23.1.0b2/flattrs-23.1.0b2.tar.gz

---
+++ packages/flattrs/flattrs-23.1.0b2.tar.gz/flattrs-23.1.0b2/src/flattrs/__init__.py
@@ -0,0 +1,52 @@
+from typing import Final
+
+from .converters import Converter
+from .types import (
+    Float,
+    Float32,
+    Float64,
+    Int8,
+    Int8Enum,
+    Int16,
+    Int16Enum,
+    Int32,
+    Int32Enum,
+    Int64,
+    Int64Enum,
+    Uint8,
+    Uint8Enum,
+    Uint16,
+    Uint16Enum,
+    Uint32,
+    Uint32Enum,
+    Uint64,
+    Uint64Enum,
+    UnionVal,
+)
+
+global_converter = Converter()
+dumps: Final = global_converter.dumps
+loads: Final = global_converter.loads
+
+__all__ = [
+    "Float",
+    "Float32",
+    "Float64",
+    "Int16",
+    "Int16Enum",
+    "Int32",
+    "Int32Enum",
+    "Int64",
+    "Int64Enum",
+    "Int8",
+    "Int8Enum",
+    "Uint16",
+    "Uint16Enum",
+    "Uint32",
+    "Uint32Enum",
+    "Uint64",
+    "Uint64Enum",
+    "Uint8",
+    "Uint8Enum",
+    "UnionVal",
+]
