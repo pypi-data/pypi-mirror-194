b43174366fa3f333d49a66536821175afec73034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:32 +0000

    Add package Droxic/0.1/Droxic-0.1-py3-none-any.whl

---
+++ packages/Droxic/Droxic-0.1-py3-none-any.whl/Droxic.py
@@ -0,0 +1,22 @@
+
+# Description: This is the main file for the Droxic project. It is the entry point for the program. It is a simple library to help with Droxification which is a process of converting a Python program into a Drox program.
+class Droxic:
+    def __init__(self) -> None:
+        pass
+        
+    def Droxify(self, path: str) -> None:
+        # Description: This function will Droxify the Python program at the given path.
+        # Path: Droxic.py
+        # Compare this snippet from setup.py:
+        # from setuptools import setup
+        # 
+        # setup(
+        #     name='Droxic',
+        #     version='0.1',
+        #     description='A simple library to help with Droxification',
+        #     py_modules=['Droxic'],
+        #     author='Hamza Khan',
+        #
+        pass
+    
+    
