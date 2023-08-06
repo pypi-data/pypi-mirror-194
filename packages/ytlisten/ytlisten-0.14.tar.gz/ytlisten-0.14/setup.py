bd80803b6bae6be59d30ac235b2c214bcce7c1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:40 +0000

    Add package ytlisten/0.14/ytlisten-0.14.tar.gz

---
+++ packages/ytlisten/ytlisten-0.14.tar.gz/ytlisten-0.14/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='ytlisten',
+    version='0.14',
+    description='A command-line tool to search and listen to YouTube audio',
+    author='Chris Ismael',
+    author_email='chris.ismael@gmail.com',
+    url='https://github.com/ismaelc/ytlisten',
+    packages=['ytlisten'],
+    install_requires=[
+        'google-api-python-client',
+        'python-vlc',
+        'pytube'
+    ]
+)
