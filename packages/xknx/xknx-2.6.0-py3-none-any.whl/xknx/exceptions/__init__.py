d9918b7e74d7a50c096e00878e133a836febba75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:53 +0000

    Add package xknx/2.6.0/xknx-2.6.0-py3-none-any.whl

---
+++ packages/xknx/xknx-2.6.0-py3-none-any.whl/xknx/exceptions/__init__.py
@@ -0,0 +1,43 @@
+"""Module for XKNX Exception handling."""
+# flake8: noqa
+from .exception import (
+    CommunicationError,
+    ConfirmationError,
+    ConversionError,
+    CouldNotParseAddress,
+    CouldNotParseKNXIP,
+    CouldNotParseTelegram,
+    DataSecureError,
+    DeviceIllegalValue,
+    IncompleteKNXIPFrame,
+    InvalidSecureConfiguration,
+    KNXSecureValidationError,
+    ManagementConnectionError,
+    ManagementConnectionRefused,
+    ManagementConnectionTimeout,
+    SecureException,
+    TunnellingAckError,
+    UnsupportedCEMIMessage,
+    XKNXException,
+)
+
+__all__ = [
+    "CommunicationError",
+    "ConfirmationError",
+    "ConversionError",
+    "CouldNotParseAddress",
+    "CouldNotParseKNXIP",
+    "CouldNotParseTelegram",
+    "DataSecureError",
+    "DeviceIllegalValue",
+    "ManagementConnectionError",
+    "ManagementConnectionRefused",
+    "ManagementConnectionTimeout",
+    "IncompleteKNXIPFrame",
+    "InvalidSecureConfiguration",
+    "KNXSecureValidationError",
+    "SecureException",
+    "TunnellingAckError",
+    "UnsupportedCEMIMessage",
+    "XKNXException",
+]
