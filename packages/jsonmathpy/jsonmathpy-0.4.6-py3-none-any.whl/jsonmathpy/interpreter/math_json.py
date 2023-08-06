1c9633a69f9930a0186d6c5a90a154be06ccf73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:59 +0000

    Add package jsonmathpy/0.4.6/jsonmathpy-0.4.6-py3-none-any.whl

---
+++ packages/jsonmathpy/jsonmathpy-0.4.6-py3-none-any.whl/jsonmathpy/interpreter/math_json.py
@@ -0,0 +1,81 @@
+class MathJSON:
+    def __init__(self, dict):
+        self.dict = dict
+        
+    def __add__(self, other):
+        return MathJSON({
+            "operation": "ADD",
+            "arguments": [self.dict, other.dict]
+        })
+    
+    def __mul__(self, other):
+        return MathJSON({
+            "operation": "MULTIPLY",
+            "arguments": [self.dict, other.dict]
+        })
+    
+    def __sub__(self, other):
+        return MathJSON({
+            "operation": "SUBTRACTION",
+            "arguments": [self.dict, other.dict]
+        })
+
+    def __pow__(self, other):
+        return MathJSON({
+            "operation": "POWER",
+            "arguments": [self.dict, other.dict]
+        })
+    
+    def __truediv__(self, other):
+        return MathJSON({
+            "operation": "DIVISION",
+            "arguments": [self.dict, other.dict]
+        })
+
+    def func(self, variables):
+        return MathJSON({
+            "operation": "FUNCTION",
+            "arguments": [self.dict, variables.dict]
+        })
+
+    def integrate(self, measure):
+        return MathJSON({
+            "operation": "INTEGRAL",
+            "arguments": [self.dict, measure.dict]
+        })
+
+    def differentiate(self, measure):
+        return MathJSON({
+            "operation": "DIFFERENTIAL",
+            "arguments": [self.dict, measure.dict]
+        })
+
+    def function(self, measure):
+        return MathJSON({
+            "operation": "FUNCTION",
+            "arguments": [self.dict, measure.dict]
+        })
+
+    def build_int(self, integer):
+        return MathJSON({
+            "operation": "BUILD_INT",
+            "arguments": str(integer)
+        })
+
+    def build_float(self, float):
+        return MathJSON({
+            "operation": "BUILD_FLOAT",
+            "arguments": str(float)
+        })
+
+    def build_tensor(self, tensor_repr):
+        return MathJSON({
+            "operation": "BUILD_TENSOR",
+            "arguments": str(tensor_repr)
+        })
+
+    def build_variable(self, variable_repr):
+        return MathJSON({
+            "operation": "BUILD_VARIABLE",
+            "arguments": str(variable_repr)
+        })
