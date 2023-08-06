8b59d3f13e724173bb391f31a8beb2ec0cf1884c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:51 +0000

    Add package Bridges3lab/1.0.3/Bridges3lab-1.0.3-py3-none-any.whl

---
+++ packages/Bridges3lab/Bridges3lab-1.0.3-py3-none-any.whl/canisius_cyb600_example/main.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes(now.strftime("%H:%M:%S"), "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
