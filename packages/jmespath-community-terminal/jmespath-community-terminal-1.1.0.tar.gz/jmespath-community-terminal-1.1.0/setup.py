df0088ac0a6530dc0ca6dcd55e7e7aed367bce57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:54 +0000

    Add package jmespath-community-terminal/1.1.0/jmespath-community-terminal-1.1.0.tar.gz

---
+++ packages/jmespath-community-terminal/jmespath-community-terminal-1.1.0.tar.gz/jmespath-community-terminal-1.1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+import sys
+import io
+
+from setuptools import setup, find_packages
+
+
+requires = [
+    'jmespath-community>=1.1.0',
+    'Pygments>=2.0,<3.0',
+    'urwid==1.2.2'
+]
+
+
+setup(
+    name='jmespath-community-terminal',
+    version='1.1.0',
+    description='JMESPath Terminal',
+    long_description=io.open('README.rst', encoding='utf-8').read(),
+    author='James Saryerwinnie, Springcomp',
+    author_email='js@jamesls.com, springcomp@users.noreply.github.com',
+    url='https://github.com/jmespath-community/jmespath.terminal',
+    scripts=['bin/jpterm'],
+    py_modules=['jpterm'],
+    install_requires=requires,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
