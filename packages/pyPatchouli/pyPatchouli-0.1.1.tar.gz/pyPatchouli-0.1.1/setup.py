1c7cb8b77bb61d6d1bc62801be3d56c73fcb4d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:27 +0000

    Add package pyPatchouli/0.1.1/pyPatchouli-0.1.1.tar.gz

---
+++ packages/pyPatchouli/pyPatchouli-0.1.1.tar.gz/pyPatchouli-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md",encoding="utf-8") as f:
+    long_description=f.read()
+
+setup(
+    name="pyPatchouli",
+    version="0.1.1",
+    install_requires=[],
+    description="Patchouli is LINE app's history text parser.",
+    long_description=long_description,
+    license="MIT",
+    keywords='line patchouliv history parser',
+    author="nakasyou",
+    url="https://github.com/nakasyou/Patchouli",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
