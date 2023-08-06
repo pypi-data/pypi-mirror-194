4d4baaaf5a6b5285e2be9bb5bf4ed28cf07dd3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:43 +0000

    Add package requests-toolkit-stable/0.14.0/requests-toolkit-stable-0.14.0.tar.gz

---
+++ packages/requests-toolkit-stable/requests-toolkit-stable-0.14.0.tar.gz/requests-toolkit-stable-0.14.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r",encoding='UTF-8') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "lxml",
+    'requests',
+    "xmltodict",
+    'tqdm',
+    'aiohttp',
+    'pandas'
+]
+
+setup(
+    name="requests-toolkit-stable",
+    version="0.14.0",
+    author="Tao Xiang",
+    author_email="tao.xiang@tum.de",
+    description="A package of APIs using requests.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leoxiang66/requests-toolkit",
+    packages=find_packages(),
+    # py_modules=['timedd']
+    install_requires=requirements,
+    classifiers=[
+	"Programming Language :: Python :: 3.8",
+	"License :: OSI Approved :: MIT License",
+    ],
+)
