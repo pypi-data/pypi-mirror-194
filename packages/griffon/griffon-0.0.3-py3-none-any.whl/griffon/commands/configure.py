0aea9d7558a62711daa1d73663f4b4bcb3700f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:38 +0000

    Add package griffon/0.0.3/griffon-0.0.3-py3-none-any.whl

---
+++ packages/griffon/griffon-0.0.3-py3-none-any.whl/griffon/commands/configure.py
@@ -0,0 +1,37 @@
+"""
+
+"""
+import configparser
+import logging
+import os
+
+import click
+
+from griffon import GRIFFON_CONFIG_DIR, GRIFFON_RC_FILE
+
+logger = logging.getLogger("rich")
+
+
+@click.group(name="configure")
+@click.pass_context
+def configure_grp(ctx):
+    """Configure operations."""
+    pass
+
+
+@configure_grp.command(name="setup", help="Create ~/.griffon and .griffonrc config file")
+def setup():
+    """stub"""
+    if not os.path.exists(os.path.expanduser(GRIFFON_CONFIG_DIR)):
+        os.makedirs(os.path.expanduser(GRIFFON_CONFIG_DIR))
+        logger.warning(f"{GRIFFON_CONFIG_DIR} created")
+    else:
+        logger.warning(f"{GRIFFON_CONFIG_DIR} already exists")
+
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.optionxform = str
+    config.add_section("default")
+    config["default"]["format"] = "text"
+    config.add_section("exclude")
+    with open(os.path.expanduser(GRIFFON_RC_FILE), "w") as configfile:
+        config.write(configfile)
