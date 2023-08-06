5c4c8209a8ca33efb3c54f6cbdcd9536eb9cd7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:52 +0000

    Add package drivesecrets/0.0.6/drivesecrets-0.0.6.tar.gz

---
+++ packages/drivesecrets/drivesecrets-0.0.6.tar.gz/drivesecrets-0.0.6/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['drivesecrets']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'drivesecrets',
+    'version': '0.0.6',
+    'description': 'Save secrets in your google drive to secure your Colaboratory notebooks.',
+    'long_description': 'None',
+    'author': 'Kevin Katz',
+    'author_email': 'contact@kevinkatz.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
