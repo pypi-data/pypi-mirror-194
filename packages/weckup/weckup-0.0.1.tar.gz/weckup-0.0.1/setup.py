b24d1a8eed77b39c359ff0893349901254f60725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:50 +0000

    Add package weckup/0.0.1/weckup-0.0.1.tar.gz

---
+++ packages/weckup/weckup-0.0.1.tar.gz/weckup-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['weckup']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'weckup',
+    'version': '0.0.1',
+    'description': 'weckup',
+    'long_description': 'None',
+    'author': 'HE Yaowen',
+    'author_email': 'he.yaowen@hotmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
