323011e9715d84b7659fd5d8a6aa7b815d9327ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:06 +0000

    Add package quenouille/1.5.1/quenouille-1.5.1.tar.gz

---
+++ packages/quenouille/quenouille-1.5.1.tar.gz/quenouille-1.5.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="quenouille",
+    version="1.5.1",
+    description="A library of multithreaded iterator workflows.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/medialab/quenouille",
+    license="MIT",
+    author="Guillaume Plique",
+    author_email="kropotkinepiotr@gmail.com",
+    keywords="url",
+    python_requires=">=3.5",
+    packages=find_packages(exclude=["test"]),
+    package_data={"docs": ["README.md"]},
+    zip_safe=True,
+)
