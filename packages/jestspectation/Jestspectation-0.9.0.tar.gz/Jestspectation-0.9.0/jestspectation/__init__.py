2455c8c3cf5737c84feeefecbc2a9f2ae339036c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:25 +0000

    Add package jestspectation/0.9.0/Jestspectation-0.9.0.tar.gz

---
+++ packages/jestspectation/Jestspectation-0.9.0.tar.gz/Jestspectation-0.9.0/jestspectation/__init__.py
@@ -0,0 +1,49 @@
+"""
+Jestspectation
+==============
+
+Pattern matching helper classes designed to be similar to Jest's expect
+matchers, but modified to suit a Pythonic style of programming.
+"""
+
+from .__any import Any, Anything
+from .__containers import (
+    DictContainingKeys,
+    DictContainingValues,
+    DictContainingItems,
+    ListContaining,
+    SetContaining,
+)
+from .__float_approx import FloatApprox
+from .__equals import Equals, Is
+from .logicals import And, Not, Or, Xor
+from .__strings import (
+    StringMatchingRegex,
+    StringContaining,
+    TextLike,
+    LinesLike,
+)
+from .__config import configure
+
+
+__all__ = [
+    'And',
+    'Any',
+    'Anything',
+    'configure',
+    'DictContainingKeys',
+    'DictContainingValues',
+    'DictContainingItems',
+    'Equals',
+    'FloatApprox',
+    'Is',
+    'LinesLike',
+    'ListContaining',
+    'Not',
+    'Or',
+    'SetContaining',
+    'StringContaining',
+    'StringMatchingRegex',
+    'TextLike',
+    'Xor',
+]
