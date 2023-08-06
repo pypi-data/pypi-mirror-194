18870abe2b9ac8161f8bdaec669c1e20d24ee8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:23 +0000

    Add package changelogged/0.7.0/changelogged-0.7.0-py3-none-any.whl

---
+++ packages/changelogged/changelogged-0.7.0-py3-none-any.whl/changelogger/app/__init__.py
@@ -0,0 +1,36 @@
+from os import getcwd
+
+import typer
+from git.exc import InvalidGitRepositoryError
+from git.repo import Repo
+from rich import print
+
+from changelogger.app.manage import app as manage_app
+from changelogger.app.unreleased import app as unrealeased_app
+from changelogger.conf import settings
+
+app = typer.Typer()
+app.add_typer(manage_app)
+app.add_typer(unrealeased_app)
+
+
+@app.callback()
+def changelogger():
+    """Automated management of your CHANGELOG.md and other versioned files,
+    following the principles of Keep a Changelog and Semantic Versioning."""
+    if not settings.CHANGELOG_PATH.exists():
+        print(
+            "[bold red]Error: [/bold red]"
+            f"Could not find changelog file [bold]{settings.CHANGELOG_PATH}[/bold]."
+        )
+        exit(1)
+
+    if settings.HAS_DEFAULTS:
+        try:
+            _ = Repo(getcwd()).git_dir
+        except InvalidGitRepositoryError:
+            print(
+                "[bold red]Error: [/bold red]"
+                "Must be in a git repo to use the default behavior."
+            )
+            exit(1)
