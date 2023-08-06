aa9ebbc302f642a3c7bacf9dd2369472aefcd2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:16 +0000

    Add package alteredcarbon/0.1/alteredcarbon-0.1.tar.gz

---
+++ packages/alteredcarbon/alteredcarbon-0.1.tar.gz/alteredcarbon-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alteredcarbon',
+    version='0.1',
+    description='A library for performing addition and subtraction',
+    py_modules=['add', 'sub'],
+    url='https://github.com/cumbersomeamir',
+    author='Amir Kidwai',
+    author_email='findamirkidwai@gmail.com',
+)
+
