fea1a7b071bdf679c319ef8fa709e1ad9f6cf699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package CIMLA/0.0.13/CIMLA-0.0.13.tar.gz

---
+++ packages/CIMLA/CIMLA-0.0.13.tar.gz/CIMLA-0.0.13/CIMLA/ML/BaseModels.py
@@ -0,0 +1,13 @@
+class baseML(object):
+    def __init__(self):
+        self.model = None
+        pass
+
+    def train(self, data):
+        pass
+
+    def predict(self, x):
+        pass
+
+    def save(self, path):
+        pass
