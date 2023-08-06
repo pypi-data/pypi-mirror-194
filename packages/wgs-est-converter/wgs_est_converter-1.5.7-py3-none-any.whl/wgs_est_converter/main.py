f69c77d94760e7a0f150df1251a03edcf3930757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package wgs-est-converter/1.5.7/wgs_est_converter-1.5.7-py3-none-any.whl

---
+++ packages/wgs-est-converter/wgs_est_converter-1.5.7-py3-none-any.whl/wgs_est_converter/main.py
@@ -0,0 +1,14 @@
+import pyproj
+from shapely.geometry import Point
+from shapely.ops import transform
+import warnings
+warnings.filterwarnings("ignore") #ignore warnings
+
+project = pyproj.Transformer.from_proj(
+    pyproj.Proj(init='epsg:4326'), # source coordinate system
+    pyproj.Proj(init='epsg:3301')) # destination coordinate system
+
+def transformer(x,y):
+    p = Point(x,y)
+    transformed_point = transform(project.transform, p)
+    return (transformed_point.x,transformed_point.y)
