ab103874f0e1220d53fbfa0c0e24e3515dcc3e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:45 +0000

    Add package governance/1.0.0/governance-1.0.0-py2-none-any.whl

---
+++ packages/governance/governance-1.0.0-py2-none-any.whl/governance/__init__.py
@@ -0,0 +1,10 @@
+from .governance import governance
+
+governance = governance()
+set_api_key = governance.set_api_key
+set_server_url = governance.set_server_url
+set_agent_id = governance.set_agent_id
+set_project_id = governance.set_project_id
+set_stage_id = governance.set_stage_id
+set_agent_token = governance.set_agent_token
+submit = governance.submit
