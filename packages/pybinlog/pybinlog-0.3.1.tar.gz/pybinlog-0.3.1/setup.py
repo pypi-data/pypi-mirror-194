e27d2d4aaba965beb03e3a9494626a66587ec379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:54 +0000

    Add package pybinlog/0.3.1/pybinlog-0.3.1.tar.gz

---
+++ packages/pybinlog/pybinlog-0.3.1.tar.gz/pybinlog-0.3.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+descr = "Utility functions to parse Ardupilot's log file in bin format. "
+#long_descr = "Contains file converter from Ardupilot's log files in bin format to CSV files for each message. Supports command line operation and gui as well."
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='pybinlog',
+    description=descr,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tayyabkhalil-313/pybinlog',
+    author='Tayyab Khalil',
+    author_email='tayyabkhalilpm@gmail.com',
+    download_url='https://github.com/tayyabkhalil-313/pybinlog',
+    license='MIT License',
+    install_requires=['pymavlink', 'PyQt5'],
+    packages=find_packages(),
+    version="0.3.1",
+    entry_points = {
+        'console_scripts': [
+            'bin2csv=utils.bin2csv:main',
+            'bin2csvgui=utils.bin2csvgui:main',
+        ],
+    },
+)
