399d91d3e7fd582b38572dcd78f432fec80f6ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:39 +0000

    Add package algolibs/0.1.1/algolibs-0.1.1-py3-none-any.whl

---
+++ packages/algolibs/algolibs-0.1.1-py3-none-any.whl/algolibs/stack.py
@@ -0,0 +1,35 @@
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.minstack or val <= self.getMin():
+            self.minstack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.getMin():
+            self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def get_min(self) -> int:
+        if self.minstack:
+            return self.minstack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.get_min()
