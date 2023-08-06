993ec7ba73b8424dbfaee4808e9afdb7f259ae04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:41 +0000

    Add package miaou/0.1.0/miaou-0.1.0.tar.gz

---
+++ packages/miaou/miaou-0.1.0.tar.gz/miaou-0.1.0/miaou/scanner/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+#
+# author: philip1134
+# date: 2023-02-27
+#
+
+
+from miaou.scanner.selenium_scanner import SeleniumScanner
+
+
+class ScannerFactory:
+    """scanner factory"""
+
+    _map = {
+        "selenium": SeleniumScanner,
+    }
+
+    @classmethod
+    def get(cls, scanner, config={}):
+        """get scanner by scanner type"""
+
+        if isinstance(scanner, str):
+            return cls._map.get(scanner, SeleniumScanner)(config)
+        else:
+            # maybe customized scanner
+            return scanner
+
+# end
---
+++ packages/miaou/miaou-0.1.0.tar.gz/miaou-0.1.0/test/test.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+#
+# author: philip1134
+# date: 2023-02-27
+#
+
+
+import miaou
+
+miaou.generate(
+    site_url="http://localhost:21250",
+    username="admin",
+    password="Lton2008@",
+    combined_print=True,
+    output_path=".",
+    config={"driver": "firefox"}
+)
+
+
+# end
