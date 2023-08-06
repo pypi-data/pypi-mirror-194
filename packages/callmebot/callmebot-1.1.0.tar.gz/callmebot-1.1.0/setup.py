f4f9cb1df7904331ad6a7c60c1a79d731b080117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:05 +0000

    Add package callmebot/1.1.0/callmebot-1.1.0.tar.gz

---
+++ packages/callmebot/callmebot-1.1.0.tar.gz/callmebot-1.1.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from setuptools import setup
+
+version = '1.1.0'
+
+setup(
+    name='callmebot',
+    packages=['callmebot'],
+    install_requires=[
+        'requests',
+        'typer',
+        'rich',
+    ],
+    version=version,
+    description='CallMeBot Python Client',
+    long_description='CallMeBot Python Client',
+    author='Jordi Petit',
+    author_email='jpetit@cs.upc.edu',
+    url='https://github.com/jutge-org/callmebot',
+    download_url='https://github.com/jutge-org/callmebot/tarball/{}'.format(version),
+    keywords=['callmebot'],
+    license='Apache',
+    zip_safe=False,
+    include_package_data=True,
+    setup_requires=['setuptools'],
+    entry_points={
+        'console_scripts': [
+            'callmebot=callmebot:cmd.main',
+        ]
+    },
+    scripts=[
+    ]
+)
