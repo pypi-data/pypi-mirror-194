e99862e5c20d38f690a4fe06640a926e4f02f811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:14 +0000

    Add package alvonOnvif/0.0.8/alvonOnvif-0.0.8.tar.gz

---
+++ packages/alvonOnvif/alvonOnvif-0.0.8.tar.gz/alvonOnvif-0.0.8/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="alvonOnvif",
+    version="0.0.8",
+    author="Deepak Singh",
+    description="Custom Onvif",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["alvonOnvif"],
+    download_url="",
+    keywords=["opencv", "onvif", "alvon", "stream", "logging", "onvif-stream"],
+    install_requires=["onvif-zeep", "opencv-python", "mediapipe", "scipy"]
+)
