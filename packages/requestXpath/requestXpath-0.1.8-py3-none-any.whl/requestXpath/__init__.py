1699affb10f0d47f509b9964ea97e340a758907c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:55 +0000

    Add package requestXpath/0.1.8/requestXpath-0.1.8-py3-none-any.whl

---
+++ packages/requestXpath/requestXpath-0.1.8-py3-none-any.whl/requestXpath/__init__.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------
+   File Name:     prequest
+   Description :   Network Requests Class
+   Author :        penr
+   date:          2023/02/16
+-------------------------------------------------
+   Change Activity:
+                   2023/02/16:
+-------------------------------------------------
+"""
+from .requestXpath import prequests
+from .pxpath import Xpath
+
+prequests = prequests()
+
+__author__ = 'penr'
