fa2c2f3896530295a8d97ac1fc9def5e85af452d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:16 +0000

    Add package polygonutils/0.1/polygonutils-0.1.tar.gz

---
+++ packages/polygonutils/polygonutils-0.1.tar.gz/polygonutils-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='polygonutils',
+    version='0.1',
+    description='Paquete para calcular área y longitud de diferentes polígonos',
+    author='Tu nombre',
+    author_email='tuemail@tudominio.com',
+    url='https://github.com/tuusuario/tuproyecto',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
