ac4724f7324cdb0bfff95cc7f87ae0e6cd3faec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:58 +0000

    Add package KiwiCoder/0.2.3.3/KiwiCoder-0.2.3.3.tar.gz

---
+++ packages/KiwiCoder/KiwiCoder-0.2.3.3.tar.gz/KiwiCoder-0.2.3.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+if __name__ == "__main__":
+    setup(
+        packages=['kiwi'],
+        package_dir={'': 'src'},
+        long_description=long_description,
+    )
