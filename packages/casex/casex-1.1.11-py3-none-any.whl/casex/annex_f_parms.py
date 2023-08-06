267c1c302ab1a10f5baa862b2023d234248013aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:45 +0000

    Add package casex/1.1.11/casex-1.1.11-py3-none-any.whl

---
+++ packages/casex/casex-1.1.11-py3-none-any.whl/casex/annex_f_parms.py
@@ -0,0 +1,249 @@
+"""
+This class provides support for redoing some of the computations found in Annex F :cite:`a-JARUS_AnnexF`.
+"""
+import math
+import warnings
+from dataclasses import dataclass
+
+import numpy as np
+
+from casex import AircraftSpecs, enums, BallisticDescent2ndOrderDragApproximation, constants
+
+
+class AnnexFParms:
+    """This class contains the following parameters for the 5 size classes in the iGRC table:
+
+    Parameters
+    ----------
+    impact_angle : float
+        [deg] The impact angle of the descending aircraft, measured relative to the ground.
+
+    Attributes
+    ----------
+    wingspan : float
+        Characteristic dimension of the aircraft. See Annex F :cite:`a-JARUS_AnnexF`
+        for more detailed explanation on what that is.
+    critical_area_target : float
+        [m^2] Size of the largest critical area for each size class.
+    cruise_speed : float
+        [m/s]Maximum cruise speed for each size class.
+    mass : float
+        [kg] Assumed biggest mass for each size class.
+    KE_critical : float
+        [J] Non-lethal energy during slide.
+    friction_coefficient = 0.5 : float
+        [-] The friction coefficient is assumed constant at 0.5 throughout Annex F :cite:`a-JARUS_AnnexF`.
+    glide_reduce = 0.7 : float
+        [-] Reduction in glide speed relative to cruise speed.
+    glide_speed : float
+        [m/s] The glide speed resulting from multiplying the cruise speed by glide_reduce.
+    aircraft : :class:`AircraftSpecs`
+        The class containing information about the aircraft.
+    scenario_angles = [9, 35, 80] : float array
+        [deg] The three impact angles for the three descent scenarios. The 80 degrees is not actually used but
+        recomputed for each ballistic descent.
+    terminal_velocity : float
+        [m/s] Terminal velocity for aircraft.
+    ballistic_frontal_area : float
+        [m^2] Assumed frontal area used in ballistic computations.
+    ballistic_drag_coefficient = 0.7 : float
+        [-] Drag coefficient used for ballistic descent.
+    ballistic_descent_altitude : float
+        [m] Assumed altitude for beginning of ballistic descent.
+    ballistic_impact_velocity :float
+        [m/s] Assumed horizontal velocity for beginning of ballistic descent.
+    ballistic_impact_angle : float
+        [deg] Computed impact angle with 0 being horizontal.
+    ballistic_distance : float
+        [m] Computed horizontal distance traveled during ballistic descent.
+    ballistic_impact_KE : float
+        [J] Computed kinetic energy of aircraft just prior to impact.
+    ballistic_descent_time : float
+        [s] Computed descent time for ballistic descent.
+    impact_angle : float
+        [deg] The impact angle of the aircraft when crashing, measure relative to horizontal.
+    aircraft_type : :class:`enums.AircraftType`
+        The type of aircraft. This parameters is not currently used.
+    horizontal_COR : float
+        [-] The coefficient of restitution for a near horizontal impact.
+    vertical_COR : float
+        [-] The coefficient of restitution for a vertical impact. The actual COR is determined
+        as a first order interpolation between `horizontal_COR` for 0 degrees and `vertical_COR`
+        for 90 degrees.
+    """
+
+    # This dataclass make the programming and plotting more smooth in allowing for looping for virtually all values.
+    @dataclass
+    class CAParameters:
+        wingspan: float
+        critical_area_target: float
+        cruise_speed: float
+        iGRC_impact_angle : float
+        ballistic_frontal_area: float
+        mass: float
+        KE_critical: float
+        ballistic_descent_altitude: float
+        terminal_velocity: float = 0
+        ballistic_impact_velocity: float = 0
+        ballistic_impact_angle: float = 0
+        ballistic_distance: float = 0
+        ballistic_impact_KE: float = 0
+        ballistic_descent_time: float = 0
+        glide_speed: float = 0
+        aircraft: AircraftSpecs = None
+
+    def __init__(self, impact_angle):
+        self.glide_reduce = 0.7
+        self.friction_coefficient = 0.5
+        self.ballistic_drag_coefficient = 0.7
+        self.scenario_angles = np.array([10, 35, 80])
+
+        self.impact_angle = impact_angle
+
+        # Set aircraft type, the type has no effect in this example, but must be given a value.
+        self.aircraft_type = enums.AircraftType.GENERIC
+
+        # Setup the parameters used in the plotting.
+        self.CA_parms = []
+        #                                      Width   CA       Speed    iGRC angle  Drag area   Mass     lethal KE   Altitude
+        self.CA_parms.append(self.CAParameters(1,      6.5,     25,      35,         0.1,        3,       290/0.5,    50))
+        self.CA_parms.append(self.CAParameters(3,      200,     35,      35,         0.5,        50,      290,        100))
+        self.CA_parms.append(self.CAParameters(8,      2000,    75,      35,         2.0,        400,     290,        200))
+        self.CA_parms.append(self.CAParameters(20,     20000,   150,     35,         8.0,        5000,    290,        500))
+        self.CA_parms.append(self.CAParameters(40,     66000,   200,     35,         14,         10000,   290,        1000))
+    
+        # Upper and low value for coefficient of restitution (over 10 to 90 degree impact angles).
+        self.Deg10_COR = 0.8
+        self.Deg90_COR = 0.6
+
+        self.recompute_parameters()
+
+    def recompute_parameters(self):
+        BDM = BallisticDescent2ndOrderDragApproximation()
+
+        # Compute the parameters for each of the 5 size classes.
+        for k in range(5):
+            # Compute glide speed based on cruise speed
+            self.CA_parms[k].glide_speed = self.glide_reduce * self.CA_parms[k].cruise_speed
+
+            # Define the aircraft.
+            self.CA_parms[k].aircraft = AircraftSpecs(self.aircraft_type, self.CA_parms[k].wingspan, 1,
+                                                      self.CA_parms[k].mass)
+
+            # Set parameters into aircraft.
+            self.CA_parms[k].aircraft.set_ballistic_frontal_area(self.CA_parms[k].ballistic_frontal_area)
+            self.CA_parms[k].aircraft.set_ballistic_drag_coefficient(self.ballistic_drag_coefficient)
+            self.CA_parms[k].aircraft.set_friction_coefficient(self.friction_coefficient)
+
+            if k == 0:
+                # The 1 m column uses 0.8 as CoR in all cases.
+                self.CA_parms[k].aircraft.set_coefficient_of_restitution(0.8)
+            else:
+                # The other columns uses a CoR depending on angle.
+                self.CA_parms[k].aircraft.set_coefficient_of_restitution(
+                    self.CA_parms[k].aircraft.COR_from_impact_angle(self.impact_angle, [self.scenario_angles[0], 90],
+                                                                    [self.Deg10_COR, self.Deg90_COR]))
+
+            # Compute terminal velocity.
+            self.CA_parms[k].terminal_velocity = self.CA_parms[k].aircraft.terminal_velocity()
+
+            # Compute ballistic descent values.
+            BDM.set_aircraft(self.CA_parms[k].aircraft)
+            p = BDM.compute_ballistic_distance(self.CA_parms[k].ballistic_descent_altitude,
+                                               self.CA_parms[k].cruise_speed, 0)
+            self.CA_parms[k].ballistic_impact_velocity = p[1]
+            self.CA_parms[k].ballistic_impact_angle = p[2] * 180 / np.pi
+            self.CA_parms[k].ballistic_distance = p[0]
+            self.CA_parms[k].ballistic_descent_time = p[3]
+            self.CA_parms[k].ballistic_impact_KE = 0.5 * self.CA_parms[k].mass * np.power(p[1], 2)        
+
+    @staticmethod
+    def iGRC(pop_dens, CA, TLOS=1E-6, width = 0, use_obstacle_reduction = False, use_integer_reduction = False):
+        """Compute the finale integer iGRC as described in Annex F :cite:`a-JARUS_AnnexF`.
+        
+        This method computes the integer and the raw iGRC values for a given population density and
+        size of critical area. The TLOS, target level of safety, can also be set, but the default value
+        is :math:`10^{-6}` as described in Annex F :cite:`a-JARUS_AnnexF`.
+        
+        .. note:: This method converts the population density to ppl/m^2 as needed for the equation.
+                    This is because the unit for the input is ppl/km^2, since this is typically
+                    how the density is known.
+        
+        Parameters
+        ----------
+        pop_dens : float
+            [ppl/km^2] Population density
+        CA : float
+            [m^2] Size of the critical area.
+        TLOS : float, optional
+            [fatalities per flight hour] Target level of safety (the default is 1e-6).
+            This value is described in more detail in Annex F :cite:`a-JARUS_AnnexF`.
+        use_obstacle_reduction : bool, optional
+            If True, the obstacle reduction (see obstacle_reduction_factor()) is applied to the iGRC value. This requires width to be set.
+            Default value is False.
+        width : float, optional
+            Width of the aircraft. This is needed if use_obstacle_reduction is set to True. Otherwise, it is ignored.
+            Default value is 0.
+        use_integer_reduction: bool, optional
+            if True, the 0.3 reduction in iGRC value is applied.
+            
+        Returns
+        -------
+        iGRC : integer
+            The intrinsic ground risk class (as an integer). This is the raw value rounded up to nearest integer.
+        raw iGRC : float
+            The raw iGRC before rounding up.
+        """
+        if use_obstacle_reduction:
+            if width == 0:
+                warnings.warn("width is not set. Using value of 1 m.")
+                width = 1
+            pop_dens = pop_dens * AnnexFParms.obstacle_reduction_factor(pop_dens, width)
+        
+        # Note that the 1E-6 here is the conversion from km^2 to m^2.
+        raw_iGRC_value = 1 - math.log10(TLOS / (pop_dens * 1E-6 * CA))
+        
+        if use_integer_reduction:
+            raw_iGRC_value = raw_iGRC_value - 0.3
+            
+        # The raw iGRC value may be rounded to one decimal.
+        raw_iGRC_value = round(raw_iGRC_value * 10) / 10
+
+        return math.ceil(raw_iGRC_value), raw_iGRC_value
+
+    @staticmethod
+    def obstacle_reduction_factor(pop_dens, width):
+        """Compute the obstacle reduction factor used in the iGRC in Annex F :cite:`a-JARUS_AnnexF`.
+        
+        The obstacle reduction factor as shown in the table below:
+
+        +---------------------------------+--------------------+---------------------+
+        |                                 | 1 m < width <= 3 m | 3 m < width <= 20 m |
+        +---------------------------------+--------------------+---------------------+
+        | 1,500 <= pop density < 100,000  |    120/200         |     700/2000        |
+        +---------------------------------+--------------------+---------------------+
+
+        and 1 if either pop density or width is outside ranges in the table.
+        
+        Parameters
+        ----------
+        pop_dens : float
+            [ppl/km^2] Population density
+        width : float
+            [m] Width of aircraft.
+            
+        Returns
+        -------
+        obstacle_reduction_factor : float
+            The reduction factor for use in iGRC.
+        """
+
+        obstacle_reduction_factor = 1
+
+        if 1500 <= pop_dens < 100000:
+            if 1 < width <= 3:
+                obstacle_reduction_factor = 120 / 200
+            elif 3 < width <= 20:
+                obstacle_reduction_factor = 700 / 2000
+            
+        return obstacle_reduction_factor
---
+++ packages/casex/casex-1.1.11-py3-none-any.whl/casex/annex_f_tables.py
@@ -0,0 +1,349 @@
+"""
+This class allows for recreating some of the tables in Annex F :cite:`a-JARUS_AnnexF`.
+"""
+import numpy as np
+import math
+from casex import enums, FrictionCoefficients, CriticalAreaModels, AircraftSpecs, AnnexFParms
+
+class AnnexFTables:
+    """This class contains methods for generating tradeoff tables and variations over the nominal iGRC table.
+
+    Parameters
+    ----------
+    none.
+
+    Attributes
+    ----------
+    none.
+    """
+ 
+    @staticmethod
+    def iGRC_tables(show_with_obstacles = True,
+                    show_ballistic = False,
+                    show_integer_reduction = True,
+                    show_CFIT_angle = False,
+                    show_additional_pop_density = False):
+        """Compute the iGRC tables Annex F :cite:`a-JARUS_AnnexF`.
+        
+        Produces console output to show the iGRC tables in a variety of forms.
+        The default setup (none of the input booleans are set), will produce the
+        nominal iGRC table.
+        
+        Parameters
+        ----------
+        show_with_obstacles : bool, optional
+            If true, show iGRC values when obstacles are present. Default True.
+        show_ballistic : bool, optional
+            If true, show iGRC values for ballistic descent (typically rotorcraft). This superseeds show_CFIT_angle. Default False.
+        show_integer_reduction : bool, optional
+            If true, subtract 0.3 from all values. Default True.
+        show_CFIT_angle : bool, optional
+            If true, show for 9 degree impact angle instead of 35 degree. Default False.
+        show_additional_pop_density : bool, optional
+            If true, show additional population density rows. Default False.
+            
+        Returns
+        -------
+        console_output : list of strings
+            The output to show.
+        """
+        
+        # Data on person size.
+        person_radius = 0.3
+        person_height = 1.8
+        
+        # Set impact angle.
+        impact_angle = 10 if show_CFIT_angle else 35
+        
+        # Instantiate necessary classes.
+        CA = CriticalAreaModels(person_radius, person_height)
+        AFP = AnnexFParms(impact_angle)
+        
+        # Instantiate and add data to AircraftSpecs class.
+        # Values are not relevant here, since they will be changed below.
+        aircraft = AircraftSpecs(enums.AircraftType.GENERIC, 1, 1, 1)
+        
+        # Friction coefficient from Annex F.
+        aircraft.set_friction_coefficient(AFP.friction_coefficient)
+        
+        # Set parameters for fuel
+        aircraft.set_fuel_type(enums.FuelType.GASOLINE)
+        aircraft.set_fuel_quantity(0)
+        
+        # Set list of population density bands in the table.
+        pop_density = np.array([0.1, 10, 100, 1500, 15000, 100000])
+        
+        if show_additional_pop_density:
+            pop_density = np.append(pop_density, [30, 300, 2000, 2500, 3000, 10000, 20000, 50000])
+        
+        pop_density = np.sort(pop_density)
+        
+        # Initialize variables.,
+        iGRC_table = np.zeros((len(pop_density), 5))
+        impact_angles = np.zeros((5))
+        p = np.zeros((5, 5))
+        
+        # Loop over columns in the iGRC table.
+        for j in range(0, 5):
+            
+            # Set mass according to Annex F.
+            aircraft.set_mass(AFP.CA_parms[j].mass)
+        
+            # First, set width and speed to nominal values.
+            aircraft.set_width(AFP.CA_parms[j].wingspan)
+            
+            if show_ballistic:
+                impact_speed = AFP.CA_parms[j].ballistic_impact_velocity
+                impact_angle = AFP.CA_parms[j].ballistic_impact_angle
+            else:
+                impact_speed = AFP.CA_parms[j].glide_speed if show_CFIT_angle else AFP.CA_parms[j].cruise_speed
+        
+            # Get CoR using the aircraft class.
+            if j > 1:
+                aircraft.set_coefficient_of_restitution(aircraft.COR_from_impact_angle(impact_angle))
+            else:
+                aircraft.set_coefficient_of_restitution(0.9)
+        
+            # Compute the CA.
+            p[j] = CA.critical_area(enums.CriticalAreaModel.JARUS, aircraft, impact_speed, impact_angle, 0, -1)
+            
+            # Store the impact angle (only relevant for ballistic).
+            impact_angles[j] = impact_angle
+        
+        # Compute the iGRC values.
+        for i in range(len(pop_density)):
+            for j in range(0, 5):
+                iGRC_table[i][j] = AFP.iGRC(pop_density[i],
+                                            p[j][0],
+                                            use_obstacle_reduction = show_with_obstacles,
+                                            width = AFP.CA_parms[j].wingspan,
+                                            use_integer_reduction = show_integer_reduction)[1]
+        console_output = []
+        
+        s = "Final iGRC"
+        s = s + " (ballistic)" if show_ballistic else s + " ({:d} deg)".format(impact_angle)
+        console_output.append(s)
+            
+        console_output.append("                          1 m       3 m       8 m      20 m      40 m")
+        console_output.append("--------------------+-------------------------------------------------")
+        
+        for i in range(len(pop_density)):
+            
+            if (pop_density[i] > 1):
+                s = "{:7.0f} [ppl/km^2]  | ".format(pop_density[i])
+            else:
+                s = "Controlled (< 0.1)  | "
+         
+            for j in range(0, 5):
+                s = s + "{:7.1f}   ".format(iGRC_table[i][j])
+            
+            console_output.append(s)
+            
+        console_output.append("--------------------+-------------------------------------------------")
+        
+        s = "     CA size [m^2]  |"
+        for j in range(0, 5):
+            s = s + "{:8.0f}  ".format(p[j][0])
+        console_output.append(s)
+        
+        s = "Slide distance [m]  |"
+        for j in range(0, 5):
+            s = s + "{:8.0f}  ".format(p[j][2] / AFP.CA_parms[j].wingspan)
+        console_output.append(s)
+        
+        if show_ballistic:
+            s = "Impact angle [deg]  |"
+            for j in range(0, 5):
+                s = s + "{:8.0f}  ".format(impact_angles[j])
+            console_output.append(s)
+            
+            s = "Altitude [m]        |    "
+            for k in range(5):
+                s = s + "{:4.0f}      ".format(AFP.CA_parms[k].ballistic_descent_altitude)
+            console_output.append(s)
+            
+            s = "Impact angle [deg]  |    "
+            for k in range(5):
+                s = s + "{:4.1f}      ".format(AFP.CA_parms[k].ballistic_impact_angle)
+            console_output.append(s)
+        
+            s = "Impact vel [m/s]    |   "    
+            for k in range(5):
+                s = s + "{:5.1f}     ".format(AFP.CA_parms[k].ballistic_impact_velocity)
+            console_output.append(s)
+        
+            s = "Drag area [m^2]     |    "
+            for k in range(5):
+                s = s + "{:4.1f}      ".format(AFP.CA_parms[k].ballistic_frontal_area)
+            console_output.append(s)
+                
+        console_output.append("--------------------+-------------------------------------------------")
+    
+        return console_output
+    
+    
+    @staticmethod
+    def iGRC_tradeoff_tables(tradeoff_type, show_integer_iGRC, show_relative):
+        """Compute the iGRC tradeoff tables Annex F :cite:`a-JARUS_AnnexF`.
+        
+        Produces console output to show the iGRC tradeoff tables.
+        
+        The tables can be shown with integer iGRC values (rounded up to nearest integer) and with one
+        decimal. The latter makes it easier to determine how close the iGRC value is to the lower integer.
+        
+        The tables can also be shown relative to the nominal table. Here positive values are iGRC values above the nominal
+        and negative below nominal.
+        
+        Parameters
+        ----------
+        tradeoff_type : integer
+            If 0, produces the nominal iGRC table.
+            For n = 1 through 6 produces the corresponding Tn table, as described in Annex F.
+            For any other value, an error text is produced.
+        show_integer_iGRC : bool
+            If True, show the iGRC values rounded up to nearest integer. Otherwise, show with one decimal.
+            This only applies to actual iGRC values. Relative values are always shown with one decimal.
+        show_relative: bool
+            If True, the table shows the difference between nominal iGRC values and values for the tradeoff table.
+            The numbers are always shown with one decimal. 
+            
+        Returns
+        -------
+        console_output : list of strings
+            The output to show.
+        """
+        
+        console_output = []
+        
+        # Data on person size.
+        person_radius = 0.3
+        person_height = 1.8
+        
+        # Instantiate necessary classes.
+        CA = CriticalAreaModels(person_radius, person_height)
+        
+        # The trade-off tables are only valid for impact angle 35 degrees.
+        impact_angle = 35
+        
+        # Instantiate the Annex F parameter class.
+        AFP = AnnexFParms(impact_angle)
+        
+        # Instantiate and add data to AircraftSpecs class.
+        # Note that the values here are not important, since they will be changed below.
+        aircraft = AircraftSpecs(enums.AircraftType.GENERIC, 1, 1, 1)
+        
+        # Friction coefficient from Annex F
+        aircraft.set_friction_coefficient(AFP.friction_coefficient)
+        
+        # Set parameters for fuel.
+        aircraft.set_fuel_type(enums.FuelType.GASOLINE)
+        aircraft.set_fuel_quantity(0)
+        
+        # Get CoR using the aircraft class.
+        aircraft.set_coefficient_of_restitution(aircraft.COR_from_impact_angle(impact_angle))
+        
+        # Set the appropriate changes for each type of trade-off.
+        if tradeoff_type == 0:
+            width_factor        = 1
+            impact_speed_factor = 1
+            Dpop_factor         = 1
+        elif tradeoff_type == 1:
+            width_factor        = 1     
+            impact_speed_factor = 1.4  
+            Dpop_factor         = 0.5                             
+        elif tradeoff_type == 2:
+            width_factor        = 2   
+            impact_speed_factor = 1        
+            Dpop_factor         = 0.5                             
+        elif tradeoff_type == 3:
+            width_factor        = 0.5 
+            impact_speed_factor = 1      
+            Dpop_factor         = 2                            
+        elif tradeoff_type == 4:
+            width_factor        = 0.5
+            impact_speed_factor = 1.4
+            Dpop_factor         = 1                               
+        elif tradeoff_type == 5:
+            width_factor        = 1      
+            impact_speed_factor = 0.75 
+            Dpop_factor         = 1.7                       
+        elif tradeoff_type == 6:
+            width_factor        = 1.7
+            impact_speed_factor = 0.75   
+            Dpop_factor         = 1                            
+        else:
+            return "tradeoff_type value not legal."
+            
+        if tradeoff_type > 0:
+            console_output.append("Modified raw iGRC table for trade-off scenario T{:1}".format(tradeoff_type))
+        else:
+            console_output.append("Original raw iGRC table")
+            
+        if show_relative:
+            console_output.append("Table show difference to the original raw iGRC table.")
+        
+        s = "             Max dim   |  "
+        for j in range(0, 5):
+            s = s + "{:4.1f} m    ".format(AFP.CA_parms[j].wingspan * width_factor)
+        console_output.append(s)                                                                    
+        
+        s = "Dpop         Max speed |"        
+        for j in range(0, 5):
+            s = s + "{:4} m/s  ".format(int(AFP.CA_parms[j].cruise_speed * impact_speed_factor))
+        console_output.append(s)
+        
+        console_output.append("-----------------------+--------------------------------------------------")
+        
+        for Dpop in [0.1, 10, 100, 1500, 15000, 100000, 1000000]:
+            
+            if (Dpop > 1):
+                s = "{:7} ppl/km^2       |   ".format(int(Dpop * Dpop_factor))
+            else:
+                s = "      Controlled       |   "
+        
+            # Loop over columns in the iGRC table.
+            for j in range(0, 5):
+                
+                # Set mass according to Annex F.
+                aircraft.set_mass(AFP.CA_parms[j].mass)
+        
+                # First, set width and speed to nominal values.
+                aircraft.set_width(AFP.CA_parms[j].wingspan)
+                impact_speed = AFP.CA_parms[j].cruise_speed
+        
+                # Compute the AC.
+                p_original = CA.critical_area(enums.CriticalAreaModel.JARUS, aircraft, impact_speed, impact_angle, 0, -1)
+                
+                # Then set to modified values.
+                aircraft.set_width(AFP.CA_parms[j].wingspan * width_factor)
+                impact_speed = AFP.CA_parms[j].cruise_speed * impact_speed_factor
+            
+                # And again, compute AC.
+                p_trade_off = CA.critical_area(enums.CriticalAreaModel.JARUS, aircraft, impact_speed, impact_angle, 0, -1)
+        
+                # Compute the raw iGRC according to Annex F.
+                raw_iGRC_original = AnnexFParms.iGRC(Dpop,
+                                                     p_original[0],
+                                                     use_integer_reduction=True,
+                                                     width=AFP.CA_parms[j].wingspan, 
+                                                     use_obstacle_reduction=True) 
+                raw_iGRC_trade_off = AnnexFParms.iGRC(Dpop * Dpop_factor,
+                                                      p_trade_off[0],
+                                                      use_integer_reduction=True,
+                                                      width=AFP.CA_parms[j].wingspan, 
+                                                      use_obstacle_reduction=True) 
+        
+                if show_relative:
+                    s = s + "{:4.1f}      ".format(raw_iGRC_trade_off[1] - raw_iGRC_original[1])
+                else:
+                    if show_integer_iGRC:
+                        s = s + "{:4.0f}      ".format(raw_iGRC_trade_off[0])
+                    else:
+                        s = s + "{:4.1f}      ".format(raw_iGRC_trade_off[1])
+                
+            console_output.append(s)
+            
+        if show_relative:
+            console_output.append("Negative number means lower iGRC relative to nominal and positive means higher.")
+            
+        return console_output
