a880fbf852e040097c65e4c9fea0f9e356dcfa92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:20 +0000

    Add package pykidgui/2.1.5/pykidgui-2.1.5-py3-none-any.whl

---
+++ packages/pykidgui/pykidgui-2.1.5-py3-none-any.whl/pykidgui/pykidgui.py
@@ -0,0 +1,220 @@
+"""
+ _______________________________________________________________________
+| ######  #     # #    #    ###   ######   #####    #     #   ###       | 
+| #     #  #   #  #   #      #    #     # #         #     #    #        |
+| #     #   # #   #  #       #    #     # #         #     #    #        |
+| ######     #    ###        #    #     # #    ###  #     #    #        |
+| #          #    #  #       #    #     # #      #  #     #    #        |
+| #          #    #   #      #    #     # #      #  #     #    #        |
+| #          #    #    #    ###   ######   #####    #####     ###       |
+|_______________________________________________________________________|
+
+"""
+
+version  = '2.0.0'
+author = 'github@RonanBasto'
+
+import tkinter as tk
+from tkinter import *
+
+
+
+class Gui(tk.Tk):
+    def __init__(self, title, size):
+        super().__init__()
+        self.title(title)
+        self.geometry(size)
+        self.menu_bar = tk.Menu(self)
+        self.config(menu=self.menu_bar)
+        self.widgets = {}
+        self.canvas = Canvas(self)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+
+        self.scrollbar = Scrollbar(self, orient=VERTICAL, command=self.canvas.yview)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox('all')))
+
+        self.frame = Frame(self.canvas)
+        self.canvas.create_window((0,0), window=self.frame, anchor='nw')
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+    def add_menu(self, menu_name, menu_items):
+        # Cria um novo menu
+        new_menu = tk.Menu(self.menu_bar, tearoff=0)
+        # Adiciona cada item ao novo menu
+        for item in menu_items:
+            # Verifica se o item é um separador
+            if item == "-":
+                new_menu.add_separator()
+            else:
+                # Extrai o nome e a ação do item
+                item_name, item_action = item
+                # Adiciona o item ao menu com a ação associada
+                new_menu.add_command(label=item_name, command=item_action)
+        # Adiciona o novo menu à barra de menu
+        self.menu_bar.add_cascade(label=menu_name, menu=new_menu)
+        
+    def add_image(self, img_path, **kwargs):
+        img = tk.PhotoImage(file=img_path)
+        img_label = ImageLabel(self, image=img, **kwargs)
+        img_label.image = img
+        img_label.pack()
+        return img_label
+    def add_scrollbar(self):
+        scrollbar = Scrollbar(self.frame, orient=VERTICAL)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        return scrollbar
+    
+    def update_image_position(self):
+        self.place(x=self.x, y=self.y)
+        
+    def open_file_dialog(self, file_types):
+                        file = filedialog.askopenfile(mode="r", filetypes=file_types)
+                        if file:
+                            return file.name
+        
+    def add_spinbox(self, label, from_, to, callback):
+        var = tk.StringVar()
+        spinbox = tk.Spinbox(self, from_=from_, to=to, textvariable=var, command=callback)
+        spinbox.pack()
+        label_widget = tk.Label(self, text=label)
+        label_widget.pack()
+
+    def add_checkbox(self, text, callback):
+        var = tk.BooleanVar()
+        checkbox = tk.Checkbutton(self, text=text, variable=var, command=lambda: callback(var.get()))
+        checkbox.pack()
+        
+    def add_text(self, text_name, prompt=""):
+        text_var = tk.StringVar()
+        new_text = tk.Entry(self, textvariable=text_var)
+        new_text.pack()
+        self.widgets[text_name] = new_text, text_var
+        return text_var
+
+    def add_button(self, button_name, on_click, text=""):
+        new_button = tk.Button(self, text=text, command=lambda: self.new_on_click(button_name, on_click))
+        new_button.pack()
+        self.widgets[button_name] = new_button
+
+
+    def add_listbox(self, listbox_name, items):
+        new_listbox = tk.Listbox(self)
+        for item in items:
+            new_listbox.insert(tk.END, item)
+        new_listbox.pack()
+        self.widgets[listbox_name] = new_listbox
+
+    def add_text_area(self, text_area_name):
+        text_area = tk.Text(self)
+        text_area.pack()
+        self.widgets[text_area_name] = text_area
+        return text_area
+        
+    def add_canvas(self, canvas_name, width, height):
+        new_canvas = tk.Canvas(self, width=width, height=height)
+        new_canvas.pack()
+        self.widgets[canvas_name] = new_canvas
+        return new_canvas
+
+    def add_canvas_items(self, canvas_name, maps):
+        canvas = self.widgets[canvas_name]
+        for m in maps:
+            shape = m['shape']
+            coords = m['coords']
+            fill = m.get('fill', '')
+            outline = m.get('outline', '')
+            width = m.get('width', 1)
+            tags = m.get('tags', '')
+            if shape == 'rectangle':
+                canvas.create_rectangle(coords, fill=fill, outline=outline, width=width, tags=tags)
+            elif shape == 'oval':
+                canvas.create_oval(coords, fill=fill, outline=outline, width=width, tags=tags)
+            elif shape == 'line':
+                canvas.create_line(coords, fill=fill, width=width, tags=tags)
+            elif shape == 'polygon':
+                canvas.create_polygon(coords, fill=fill, outline=outline, width=width, tags=tags)
+
+    def update_canvas_items(self, canvas_name, maps):
+        canvas = self.widgets[canvas_name]
+        canvas.delete('all')
+        self.add_canvas_items(canvas_name, maps)
+
+    def clear_canvas(self, canvas_name):
+        canvas = self.widgets[canvas_name]
+        canvas.delete('all')
+
+    def new_on_click(self, button_name, on_click):
+        if isinstance(self.widgets[button_name], tuple):
+            _, text_var = self.widgets[button_name]
+            on_click(text_var.get())
+        elif isinstance(self.widgets[button_name], tk.Listbox):
+            selected_index = self.widgets[button_name].curselection()
+            on_click(selected_index)
+        else:
+            on_click()
+            
+    def add_entry(self, entry_name):
+        entry = tk.Entry(self)
+        entry.pack()
+        return entry
+        
+    def get_entry_value(self, entry):
+        return entry.get()
+
+    def add_label(self, text):
+        label = Label(self.frame, text=text)
+        label.pack()
+    
+class Canvas_Map(tk.Canvas):
+    def __init__(self, master, map_data, cell_size=50):
+        super().__init__(master, width=len(map_data[0]) * cell_size, height=len(map_data) * cell_size)
+        self.map_data = map_data
+        self.cell_size = cell_size
+        self.cells = []
+        self.draw_map()
+
+    def draw_map(self):
+        for y, row in enumerate(self.map_data):
+            for x, value in enumerate(row):
+                x1, y1 = x * self.cell_size, y * self.cell_size
+                x2, y2 = x1 + self.cell_size, y1 + self.cell_size
+                cell = self.create_rectangle(x1, y1, x2, y2, fill=value)
+                self.cells.append(cell)
+
+
+class GUI_FRAME:
+    def __init__(self, title, geometry):
+        self.root = tk.Tk()
+        self.root.title(title)
+        self.root.geometry(geometry)
+        self.frames = {}
+
+    def add_frame(self, frame_name, **kwargs):
+        frame = tk.Frame(self.root, **kwargs)
+        self.frames[frame_name] = frame
+        frame.pack()
+
+    def run(self):
+        self.root.mainloop()
+
+class ImageLabel(tk.Label):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.image = None
+        self.x = 0
+        self.y = 0
+
+    def set_image(self, img_path):
+        self.image = tk.PhotoImage(file=img_path)
+        self.config(image=self.image)
+
+    def update_position(self):
+        self.place(x=self.x, y=self.y)
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.update_position()
---
+++ packages/pykidgui/pykidgui-2.1.5-py3-none-any.whl/pykidgui/test.py
@@ -0,0 +1,147 @@
+from pykidgui import *
+
+
+
+map_data = [
+    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#333", "#333", "#333", "#333", "#333", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
+]
+
+root = tk.Tk()
+canvas_map = Canvas_Map(root, map_data)
+canvas_map.pack()
+root.mainloop()
+
+
+# onclick ex
+'''
+def hello_world():
+    print("Hello, World!")
+
+my_gui = Gui("Janela","500x500")
+
+# Adiciona um botão à janela principal
+my_gui.add_button("hello_button", hello_world, text="Say Hello!")
+'''
+
+
+# entry ex
+my_gui = Gui("Janela","500x500")
+my_entry = my_gui.add_entry("entry1")
+
+def on_click():
+    entry_value = my_gui.get_entry_value(my_entry)
+    print(f"Valor digitado: {entry_value}")
+    
+my_gui.add_button("button1",on_click, "Clique aqui")
+my_gui.mainloop()
+
+
+#listbox
+
+
+my_gui = Gui("Janela","500x500")
+my_gui.add_listbox("opcoes", ["opção 1", "opção 2", "opção 3"])
+
+
+selected_index = my_gui.widgets["opcoes"].curselection()
+if selected_index:
+    selected_option = my_gui.widgets["opcoes"].get(selected_index)
+    print(selected_option)
+else:
+    print("Nenhuma opção selecionada.")
+
+
+my_gui.mainloop()
+
+# area de texto
+
+my_gui = Gui("Janela","500x500")
+my_gui.add_text_area("texto1")
+my_gui.mainloop()
+
+#processer Bar
+
+
+from pykidgui import *
+
+my_gui = Gui("Janela","500x500")
+
+# Adicionar um canvas com uma largura e altura definidas
+canvas = my_gui.add_canvas("canvas", width=500, height=500)
+
+# Criar um mapa para uma linha que começa na posição (50, 50) e termina na posição (250, 250)
+line_map = {'shape': 'line', 'coords': [50, 50, 250, 250], 'fill': 'black', 'width': 2}
+line_map1 = {'shape': 'line', 'coords': [100,150, 250, 250], 'fill': 'red', 'width': 2}
+# Adicionar a linha ao canvas
+my_gui.add_canvas_items("canvas", [line_map])
+my_gui.add_canvas_items("canvas", [line_map1])
+
+# Executar o loop principal da GUI
+my_gui.mainloop()
+
+
+def on_checkbox_clicked(checked):
+    print("Checkbox marcado" if checked else "Checkbox desmarcado")
+
+my_gui = Gui("Exemplo de Checkbox", "300x200")
+my_gui.add_checkbox("Marque aqui", on_checkbox_clicked)
+my_gui.add_checkbox("Marque aqui", on_checkbox_clicked)
+my_gui.mainloop()
+
+checkbox_values = []
+
+my_gui = Gui("Exemplo de Checkbox", "300x200")
+
+def on_checkbox_clicked(value):
+    checkbox_values.append(value)
+    print(checkbox_values)
+
+my_gui.add_checkbox("Checkbox 1", on_checkbox_clicked)
+my_gui.add_checkbox("Checkbox 2", on_checkbox_clicked)
+my_gui.add_checkbox("Checkbox 3", on_checkbox_clicked)
+
+my_gui.mainloop()
+
+
+def minha_funcao_callback(value):
+ 
+    print(value)
+    
+gui = Gui("Minha janela", "400x300")
+gui.add_spinbox("Idade", 0, 100,minha_funcao_callback)
+gui.mainloop()
+
+
+
+def minha_funcao1():
+ 
+    print("novo arquivo")
+
+def minha_funcao2():
+ 
+    print("abrir arquivo")
+
+def minha_funcao3():
+ 
+    print("sair")
+            
+gui = Gui("Minha janela", "400x400")
+gui.add_menu("Arquivo", [("Novo", minha_funcao1), ("Abrir", minha_funcao2), "-", ("Sair", minha_funcao3)])
+gui.add_menu("Arquivo2", [("Novo2", minha_funcao1), ("Abrir2", minha_funcao2), "-", ("Sair2", minha_funcao3)])
+gui.mainloop()
+
+gui = Gui("Minha janela", "500x500")
+img1 = gui.add_image("test.png")
+img1.set_position(15,100)
+gui.mainloop()
+
+gui = Gui("Minha janela", "500x500")
+gui.add_label("Hello, world!")
+scrollbar = gui.add_scrollbar()
+gui.mainloop()
+
