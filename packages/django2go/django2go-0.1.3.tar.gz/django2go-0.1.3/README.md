eebe3e484e6fdb8ab4f281a781f97915fea744be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:14 +0000

    Add package django2go/0.1.3/django2go-0.1.3.tar.gz

---
+++ packages/django2go/django2go-0.1.3.tar.gz/django2go-0.1.3/README.md
@@ -0,0 +1,71 @@
+# Django Model 2 Go Struct
+
+for gorm or other ORM operation.
+
+Depends in go code:
+
+https://github.com/jinzhu/gorm
+
+https://github.com/guregu/null
+
+## Install
+
+```bash
+git clone /django2go.git
+cd django2go
+python setup.py install .
+```
+
+## Usage:
+
+settings.py
+
+```python
+INSTALLED_APPS = [
+    ...
+    'django2go',
+    ...
+]
+
+```
+
+run CMD:
+
+`python manage.py model2go app_name`
+
+Example generated go code: [testapp.go](./test_project/testapp.go)
+
+`python manage.py model2go app_name --use_column_name` To use database column name.
+
+Django model:
+
+```python
+
+class Model1(models.Model):
+    field1 = models.CharField(max_length=200)
+    field_with_under_score = models.IntegerField()
+    fieldWithUpperCase = models.IntegerField()
+    fieldWith_Case = models.IntegerField()
+
+```
+
+Generated go struct:
+
+```go
+
+type Model2 struct {
+    Id              int64       `json:"id" gorm:"primary_key"`
+    Field1          string      `json:"field1"`
+    FieldWithUnderScore int64       `json:"field_with_under_score"`
+    Fieldwithuppercase int64       `json:"fieldWithUpperCase"`
+    FieldwithCase   int64       `json:"fieldWith_Case"`
+}
+
+// TableName 使用指定的数据库表名
+func (Model2) TableName() string {
+    
+    return TABLE_PREFIX + "model2"
+    
+}
+
+```
---
+++ packages/django2go/django2go-0.1.3.tar.gz/django2go-0.1.3/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import six
+from setuptools import find_packages, setup
+
+if six.PY2:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+        README = readme.read()
+else:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf8') as readme:
+        README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django2go',
+    version='0.1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Translate django model to go struct.',
+    long_description=README,
+    author='treeoph',
+    author_email="treeoph@gmail.com",
+    long_description_content_type='text/markdown',
+    data_files=[
+        ('django2go/templates', [])],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 3.2',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
