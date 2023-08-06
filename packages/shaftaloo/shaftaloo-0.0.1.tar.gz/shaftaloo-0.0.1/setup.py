a61f37c919b0018d1b64ca8804e9efe4594c8d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:20 +0000

    Add package shaftaloo/0.0.1/shaftaloo-0.0.1.tar.gz

---
+++ packages/shaftaloo/shaftaloo-0.0.1.tar.gz/shaftaloo-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "shaftaloo",
+    description= "An open-source python package",
+    url= "https://github.com/mohammadamint/shaftaloo/",
+    version = "0.0.1",
+    packages= find_packages(),
+    license="European Union Public License 1.2",
+    python_requires = ">=3.6.0",
+    package_data={"": ["*.txt", "*.dat", "*.doc", "*.rst","*.xlsx"]},
+    install_requires = [
+        "pandas >= 1.3.3",
+        "numpy >= 1.21.2",
+        "xlsxwriter >= 1.3.7",
+        "matplotlib >= 3.3.4",
+        "openpyxl >= 3.0.6",
+        "tqdm",
+    ],
+
+
+)
