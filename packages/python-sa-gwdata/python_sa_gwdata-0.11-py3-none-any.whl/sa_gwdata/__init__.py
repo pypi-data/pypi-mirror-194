f5bd9a43073ebb099400d5e386a45e2f56fc0807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:41 +0000

    Add package python-sa-gwdata/0.11/python_sa_gwdata-0.11-py3-none-any.whl

---
+++ packages/python-sa-gwdata/python_sa_gwdata-0.11-py3-none-any.whl/sa_gwdata/__init__.py
@@ -0,0 +1,8 @@
+from sa_gwdata.waterconnect import *
+from sa_gwdata.waterconnect_funcs import *
+from sa_gwdata.sarig import *
+from sa_gwdata.identifiers import *
+
+from .version import version
+
+__version__ = version()
