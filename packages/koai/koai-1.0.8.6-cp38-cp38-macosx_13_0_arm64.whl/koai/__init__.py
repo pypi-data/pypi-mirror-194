e44df863e767ece81629053be15ef6e1d4fcc120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:05 +0000

    Add package koai/1.0.8.6/koai-1.0.8.6-cp38-cp38-macosx_13_0_arm64.whl

---
+++ packages/koai/koai-1.0.8.6-cp38-cp38-macosx_13_0_arm64.whl/koai/__init__.py
@@ -0,0 +1,7 @@
+from . import benchmarks
+from . import models
+from .benchmarks import finetune, TaskInfo
+
+__version__ = "0.0.1.6"
+
+__all__ = ["benchmarks", "finetune", "TaskInfo"]  # more functions will be added soon...
