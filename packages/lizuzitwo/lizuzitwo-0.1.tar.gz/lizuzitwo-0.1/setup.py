9c928fcb05cc985b9fb3d21e952d98d5a59fbe83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package lizuzitwo/0.1/lizuzitwo-0.1.tar.gz

---
+++ packages/lizuzitwo/lizuzitwo-0.1.tar.gz/lizuzitwo-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lizuzitwo',
+    version='0.1',
+    description='A library for performing addition and subtraction',
+    packages=find_packages(),
+    url='https://github.com/cumbersomeamir',
+    author='Amir Kidwai',
+    author_email='findamirkidwai@gmail.com',
+)
+
