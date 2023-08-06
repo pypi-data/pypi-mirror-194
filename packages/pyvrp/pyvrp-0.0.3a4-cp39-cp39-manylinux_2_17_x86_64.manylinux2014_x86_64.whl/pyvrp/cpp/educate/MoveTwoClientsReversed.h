fc0e93ae4c824dbf252a8a6280cd646d9b77df16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:49 +0000

    Add package pyvrp/0.0.3a4/pyvrp-0.0.3a4-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyvrp/pyvrp-0.0.3a4-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/educate/MoveTwoClientsReversed.h
@@ -0,0 +1,20 @@
+#ifndef MOVETWOCLIENTSREVERSED_H
+#define MOVETWOCLIENTSREVERSED_H
+
+#include "LocalSearchOperator.h"
+#include "Node.h"
+
+/**
+ * Inserts U -> X after V (as V -> X -> U), if that is an improving move.
+ */
+class MoveTwoClientsReversed : public LocalSearchOperator<Node>
+{
+    using LocalSearchOperator::LocalSearchOperator;
+
+public:
+    int evaluate(Node *U, Node *V) override;
+
+    void apply(Node *U, Node *V) override;
+};
+
+#endif  // MOVETWOCLIENTSREVERSED_H
