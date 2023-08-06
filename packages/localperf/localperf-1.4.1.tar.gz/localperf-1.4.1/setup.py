cbc9d9f333365da379b05a2eeb46691153c89349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:57 +0000

    Add package localperf/1.4.1/localperf-1.4.1.tar.gz

---
+++ packages/localperf/localperf-1.4.1.tar.gz/localperf-1.4.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="localperf",
+    url="https://github.com/tboulet/Local-Python-Performance", 
+    author="Timothé Boulet",
+    author_email="timothe.boulet0@gmail.com",
+    
+    packages=find_namespace_packages(),
+    requires=[
+        "numpy",
+        "joblib",
+        "matplotlib",
+        "tqdm",
+    ],
+    version="1.4.1",
+    license="MIT",
+    description="Measure of python performance in local.",
+    long_description=open('README.md').read(),      
+    long_description_content_type="text/markdown",
+)
