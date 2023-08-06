3aaa5dfd4b3c84450dd71d5eef0fbabc18ac636b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:57 +0000

    Add package CIMLA/0.0.5/CIMLA-0.0.5.tar.gz

---
+++ packages/CIMLA/CIMLA-0.0.5.tar.gz/CIMLA-0.0.5/CIMLA/main.py
@@ -0,0 +1,23 @@
+import argparse
+from ConfigParser import parser
+"""
+TODOs:
+1. Move post processor to CIMLA_pipeline --> helps making it available as a module
+"""
+
+def main():
+
+    flag = argparse.ArgumentParser()
+    flag.add_argument('--config', type = str, default = None, help = 'path to config file', required = True)
+    config_file = flag.parse_args().config
+    CIMLA_pipeline, postProcessor, confs = parser.parse(config_file)
+    CIMLA_pipeline.run(attr_data_split = confs['attribution']['data_split'],
+                       attr_data_group = confs['attribution']['data_group'],
+                       attr_data_size = confs['attribution']['data_size'],
+                       global_type = confs['attribution']['global_type'])
+    print("* Post-processing ...")
+    postProcessor.process()
+    #postProcessor.clear()
+
+if __name__ == "__main__":
+    main()
