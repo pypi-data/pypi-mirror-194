996f1da09a23301105c90325c8834f63006f9063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:18 +0000

    Add package wizata-dsapi/0.0.3/wizata-dsapi-0.0.3.tar.gz

---
+++ packages/wizata-dsapi/wizata-dsapi-0.0.3.tar.gz/wizata-dsapi-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='wizata-dsapi',
+    version='0.0.3',
+    description='Wizata Data Science Toolkit',
+    author='Wizata S.A.',
+    author_email='info@wizata.com',
+    packages=['wizata-dsapi-entities'],
+)
