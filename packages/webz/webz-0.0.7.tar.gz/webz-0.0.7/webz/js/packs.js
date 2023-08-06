8c7bde8ee2781c355f03d68498fb59a91e5effd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:12 +0000

    Add package webz/0.0.7/webz-0.0.7.tar.gz

---
+++ packages/webz/webz-0.0.7.tar.gz/webz-0.0.7/webz/js/packs.js
@@ -0,0 +1,17 @@
+var scripts = [
+    //"/webz/js/jquery/1.10.2/jquery.min.js",
+    //"/webz/js/vue/vue.js",
+    //"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    "https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js",
+    "https://cdn.staticfile.org/vue/2.6.12/vue.min.js",
+    "https://cdn.staticfile.org/twitter-bootstrap/5.2.3/js/bootstrap.bundle.min.js"
+]
+var styles=[
+    //"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+    "https://cdn.staticfile.org/twitter-bootstrap/5.2.3/css/bootstrap.min.css"
+]
+
+addScripts(scripts)
+addScript("/webz/js/utils.js")
+
+addStyles(styles)
