da54ca0e71d9ba71c46823af8966c82fbb03c365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package dploot/2.1.17/dploot-2.1.17.tar.gz

---
+++ packages/dploot/dploot-2.1.17.tar.gz/dploot-2.1.17/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="dploot",
+    version="2.1.17",
+    author="zblurx",
+    author_email="seigneuret.thomas@pm.me",
+    description="DPAPI looting remotely in Python",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/zblurx/dploot",
+    license="MIT",
+    install_requires=[
+        "impacket>=0.10.0",
+        "cryptography>=3.5",
+        "pyasn1>=0.4.8",
+        "lxml==4.9.2"
+    ],
+    python_requires='>=3.6',
+    packages=[
+        "dploot",
+        "dploot.lib",
+        "dploot.action",
+        "dploot.triage",
+],
+    entry_points={
+        "console_scripts": ["dploot=dploot.entry:main"],
+    },
+)
