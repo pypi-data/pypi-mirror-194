9ff5be04849165110ed554fc8ca2224be1004859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:47 +0000

    Add package parcels/2.4.1/parcels-2.4.1.tar.gz

---
+++ packages/parcels/parcels-2.4.1.tar.gz/parcels-2.4.1/parcels/particlefile/particlefileaos.py
@@ -0,0 +1,32 @@
+"""Module controlling the writing of ParticleSets to NetCDF file"""
+import numpy as np
+
+from parcels.particlefile.baseparticlefile import BaseParticleFile
+
+__all__ = ['ParticleFileAOS']
+
+
+class ParticleFileAOS(BaseParticleFile):
+    """Initialise trajectory output.
+
+    :param name: Basename of the output file. This can also be a Zarr store.
+    :param particleset: ParticleSet to output
+    :param outputdt: Interval which dictates the update frequency of file output
+                     while ParticleFile is given as an argument of ParticleSet.execute()
+                     It is either a timedelta object or a positive double.
+    :param chunks: Tuple (trajs, obs) to control the size of chunks in the zarr output.
+    :param write_ondelete: Boolean to write particle data only when they are deleted. Default is False
+    """
+
+    def __init__(self, name, particleset, outputdt=np.infty, chunks=None, write_ondelete=False):
+        super(ParticleFileAOS, self).__init__(name=name, particleset=particleset, outputdt=outputdt,
+                                              chunks=chunks, write_ondelete=write_ondelete)
+
+    def __del__(self):
+        super(ParticleFileAOS, self).__del__()
+
+    def _reserved_var_names(self):
+        """
+        returns the reserved dimension names not to be written just once.
+        """
+        return ['time', 'lat', 'lon', 'depth', 'id']
