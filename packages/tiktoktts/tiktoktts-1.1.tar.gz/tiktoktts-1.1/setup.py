dfb67dde3b6991e64fc86b7f353fa5ba06ce90fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:49 +0000

    Add package tiktoktts/1.1/tiktoktts-1.1.tar.gz

---
+++ packages/tiktoktts/tiktoktts-1.1.tar.gz/tiktoktts-1.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='tiktoktts',
+version='1.1',
+description='Make a TikTok tts voices.',
+url='https://github.com/gamertube583yt',
+author='FlipFlapsRU',
+author_email='ruslankidulin@gmail.com',
+license='MIT',
+packages=setuptools.find_packages(),
+zip_safe=False)
