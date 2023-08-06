bec29900601b26d6a8556fde5041fa804e56bf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:01 +0000

    Add package kinetics-dfba/0.1.3/kinetics_dfba-0.1.3.tar.gz

---
+++ packages/kinetics-dfba/kinetics_dfba-0.1.3.tar.gz/kinetics_dfba-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kinetics_dfba',
+    version = '0.1.3',
+    keywords='dfba with kinetics',
+    description = 'dfba algorithm with kinetics to simulate metabolic activities of microbiota',
+    long_description=open('../README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license = 'MIT License',
+    url = 'https://gitee.com/Xu_Billy/d-fba-package',
+    author = 'Xu Billy',
+    author_email = 'xu_tian@stu.scu.edu.cn',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
