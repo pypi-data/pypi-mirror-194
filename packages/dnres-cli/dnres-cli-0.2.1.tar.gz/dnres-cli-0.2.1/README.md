77fe08b455dbff1fda3a8dfe729bcfd3692e5f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:20 +0000

    Add package dnres-cli/0.2.1/dnres-cli-0.2.1.tar.gz

---
+++ packages/dnres-cli/dnres-cli-0.2.1.tar.gz/dnres-cli-0.2.1/README.md
@@ -0,0 +1,67 @@
+# dnres-cli
+
+Command line utility to be used along with [dnres](https://github.com/DKioroglou/dnres) python package.
+
+# Usage
+
+## Parse config
+
+```console
+$ dnres config.ini
+```
+
+## Print contents of stored/tagged data
+
+```console
+$ dnres config.ini cat --help 
+```
+
+For files, the following filetypes are supported for printing:  
+
+* `.csv` 
+* `.tsv` 
+* `.xls` 
+* `.xlsx`
+
+For python objects, the following classes are supported: 
+
+* `dict` 
+* `list` 
+* `tuple` 
+* `set` 
+* `str`
+
+If filetype or class is not supported, the filepath of the stored data gets printed.
+
+## Print absolute filepath of stored/tagged data
+
+```console
+$ dnres config.ini ls --help
+```
+
+## Other commands
+
+The following `dnres` methods are exposed as cli commands: 
+
+* `info`
+* `tag`
+* `set-info`
+* `remove-from-db`
+* `remove-tag`
+
+Information on these commands/methods can be found [here](https://dnres.readthedocs.io/en/latest/source/dnres.html).  
+To get help on these commands run:  
+
+```console
+$ dnres config.ini COMMAND --help
+```
+
+## Installation
+
+```
+pip install dnres-cli
+```
+
+## License
+
+BSD 3
---
+++ packages/dnres-cli/dnres-cli-0.2.1.tar.gz/dnres-cli-0.2.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2.1'
+DESCRIPTION = 'Command line interface for dnres package.'
+LONG_DESCRIPTION = 'A package that provides a command line interface to be used along with dnres python package.'
+
+# Setting up
+setup(
+    name="dnres-cli",
+    version=VERSION,
+    entry_points='''
+        [console_scripts]
+        dnres=src.dnres_cli:dnres
+    ''',
+    author="Dimitrios Kioroglou",
+    author_email="<d.kioroglou@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['dnres', 'click', 'pandas'],
+    include_package_data=True,
+    keywords=['python', 'data analysis', 'bioinformatics', 'reporting', 'analysis results'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
