e5c2de192ae12afcdcfd93b45f6ad850b0057b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:38 +0000

    Add package cs-events/0.2.1/cs_events-0.2.1-py3-none-any.whl

---
+++ packages/cs-events/cs_events-0.2.1-py3-none-any.whl/cs_events/__init__.py
@@ -0,0 +1,21 @@
+"""
+Provides C#-style event handling mechanism.
+"""
+
+from ._collections import EventHandlerCollection, EventHandlerDict, EventHandlerList
+from ._event import Event, EventHandler, accessors, event
+from ._events import events
+
+
+__all__ = [
+    "accessors",
+    "event",
+    "Event",
+    "EventHandler",
+    "EventHandlerCollection",
+    "EventHandlerDict",
+    "EventHandlerList",
+    "events",
+]
+__version__ = "0.2.1"
+__author__ = "Daniel Jeong"
