95b85c82065dafd6aeb42bc2266270d4f246a5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:06 +0000

    Add package XAISuite/1.0.2/XAISuite-1.0.2.tar.gz

---
+++ packages/XAISuite/XAISuite-1.0.2.tar.gz/XAISuite-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='XAISuite',
+      version='1.0.2', 
+      description='XAISuite: Training and Explanation Generation Utilities for Machine Learning Models',
+      long_description=open("README.md", "r", encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      keywords="XAI Explainable AI Explanation Machine Learning Models",
+      url="https://github.com/11301858/XAISuite",
+      author = "Shreyan Mitra",
+      install_requires=[
+        "omnixai==1.2.4",
+        "dash>=2.7.1",
+        "dash_bootstrap_components>=1.3.0"
+      ],
+       packages=["xaisuite", "demo", "xaisuitegui", "xaisuitecli"],
+      )
