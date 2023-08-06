0b59b1c0a63ff467c1667ce933511e3a6deeebc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:34 +0000

    Add package ecs-composex/0.23.5/ecs_composex-0.23.5-py3-none-any.whl

---
+++ packages/ecs-composex/ecs_composex-0.23.5-py3-none-any.whl/ecs_composex/ecs/managed_sidecars/aws_cw_agent.py
@@ -0,0 +1,30 @@
+#  SPDX-License-Identifier: MPL-2.0
+#  Copyright 2020-2022 John Mille <john@compose-x.io>
+
+"""
+Simple class to manage AWS XRay sidecar
+"""
+
+from ecs_composex.common.cfn_params import Parameter
+from ecs_composex.ecs.managed_sidecars import ManagedSidecar
+
+CW_IMAGE_PARAMETER = Parameter(
+    "CloudwatchAgentImage",
+    Type="String",
+    Default="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:latest",
+)
+
+CW_AGENT_NAME = "cloudwatch-agent"
+CW_AGENT_DEFINITION = {
+    "image": CW_IMAGE_PARAMETER.Default,
+    "ports": [
+        {"target": 25888, "protocol": "tcp"},
+        {"target": 25888, "protocol": "udp"},
+    ],
+    "deploy": {
+        "resources": {"limits": {"cpus": 0.1, "memory": "256M"}},
+    },
+    "labels": {"container_name": CW_AGENT_NAME},
+}
+
+CW_AGENT_SERVICE = ManagedSidecar(CW_AGENT_NAME, CW_AGENT_DEFINITION)
