1acb51e3d0da262d1ad7dccd9ef939b679b47b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:06 +0000

    Add package django-fees/0.2.1/django-fees-0.2.1.tar.gz

---
+++ packages/django-fees/django-fees-0.2.1.tar.gz/django-fees-0.2.1/fees/context_processors.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from fees.models import Package
+
+
+def purchaser_plan(request):
+    # let's presume the purchaser is set in the request, otherwise use request.user as purchaser instance
+    purchaser = getattr(request, 'purchaser', request.user)
+
+    try:
+        plan = purchaser.plan
+        package = purchaser.plan.package
+    except (ObjectDoesNotExist, AttributeError):
+        plan = None
+        package = Package.get_fallback_package()
+
+    return {
+        'plan': plan,
+        'package': package,
+        'expiration': plan.expiration if plan else None
+    }
---
+++ packages/django-fees/django-fees-0.2.1.tar.gz/django-fees-0.2.1/fees/mixins.py
@@ -0,0 +1,58 @@
+import datetime
+from django.db.models import F
+from django.utils.translation import ugettext_lazy as _
+
+
+class PurchaserMixin(object):
+    created_attribute = 'created'
+
+    def is_package_quota_available(self, quota):
+        package = self.package
+        return quota in package.get_quotas() if package is not None else False
+
+    @property
+    def package(self):
+        from fees.models import Package
+        return Package.get_current_package(self)
+
+    @property
+    def plan(self):
+        return self.plan_history\
+            .active()\
+            .order_by(
+                '-modified',
+                # F('expiration').desc(nulls_last=True)
+            ).first()
+
+    @plan.setter
+    def plan(self, plan):
+        if plan.purchaser and plan.purchaser != self:
+            raise ValueError(_('Purchaser of the plan is already set: %s') % plan.purchaser)
+        plan.purchaser = self
+        plan.save()
+
+    @property
+    def plan_history(self):
+        return self.plan_set.all()
+
+    @property
+    def is_in_free_trial(self):
+        if not self.package or not self.plan:
+            return False
+
+        trial_duration = self.package.trial_duration
+
+        if not trial_duration or trial_duration <= 0:
+            return False
+
+        created_date = getattr(self, self.created_attribute, None)
+        plan_expiration_date = self.plan.expiration
+
+        if created_date is not None and plan_expiration_date is not None:
+            created_date = created_date.date() if isinstance(created_date, datetime.datetime) else created_date
+            delta = plan_expiration_date - created_date
+
+            if delta.days <= trial_duration:
+                return True
+
+        return False
