ea519a928078ead715ec7a3d9ac81589ab174092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package masterthermconnect/2.1.0b5/masterthermconnect-2.1.0b5-py3.10.egg

---
+++ packages/masterthermconnect/masterthermconnect-2.1.0b5-py3.10.egg/masterthermconnect/__init__.py
@@ -0,0 +1,25 @@
+"""Python API wrapper for Mastertherm Connect."""
+from masterthermconnect.__version__ import __version__
+from masterthermconnect.controller import MasterthermController
+from masterthermconnect.exceptions import (
+    MasterthermAuthenticationError,
+    MasterthermConnectionError,
+    MasterthermResponseFormatError,
+    MasterthermTokenInvalid,
+    MasterthermUnsupportedRole,
+    MasterthermUnsupportedVersion,
+    MasterthermEntryNotFound,
+    MasterthermPumpError,
+)
+
+__all__ = [
+    "__version__",
+    "MasterthermController",
+    "MasterthermAuthenticationError",
+    "MasterthermConnectionError",
+    "MasterthermResponseFormatError",
+    "MasterthermTokenInvalid",
+    "MasterthermUnsupportedRole",
+    "MasterthermUnsupportedVersion",
+    "MasterthermPumpError",
+]
