068a9fd5f5fd91c5d7760f9429383800a43b2fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:23 +0000

    Add package gituptools/0.7/gituptools-0.7-py3-none-any.whl

---
+++ packages/gituptools/gituptools-0.7-py3-none-any.whl/gituptools/__init__.py
@@ -0,0 +1,19 @@
+"""Welcome to Gituptools.
+
+100% standard library helper tool for packaging Python from Gitlab CICD
+pipelines.  This package pulls as much metadata from the pipeline as
+possible to help fill in the setup command.
+
+"""
+__all__ = ('setup', 'Gitlab')
+
+import setuptools
+
+from .gitlab import Gitlab
+
+
+def setup(**kwargs):
+    """Gituptools setup wrapper."""
+    if Gitlab: # noqa
+        kwargs = {**Gitlab.kwargs, **kwargs}
+    setuptools.setup(**kwargs)
