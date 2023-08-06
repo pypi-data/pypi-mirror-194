a475d686558b437b6c52e427901272d45ebfd440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:15 +0000

    Add package macicons/0.0.7/macicons-0.0.7.tar.gz

---
+++ packages/macicons/macicons-0.0.7.tar.gz/macicons-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="macicons",
+    version="0.0.7",
+    author="Cangyuan Li",
+    author_email="everest229@gmail.com",
+    description="CLI utility to change Mac Icons.",
+    url="https://github.com/CangyuanLi/macicons",
+    packages=["macicons"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "macicons=macicons.cli:main"
+        ]
+    },
+    install_requires=["tqdm"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS"
+    ]
+)
