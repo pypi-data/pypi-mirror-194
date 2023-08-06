7da67b5a3f3bbe45383910ed9e463da158be72c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package mpf-monitor/0.56.1/mpf-monitor-0.56.1.tar.gz

---
+++ packages/mpf-monitor/mpf-monitor-0.56.1.tar.gz/mpf-monitor-0.56.1/mpfmonitor/_version.py
@@ -0,0 +1,7 @@
+# mpf-monitor
+__version__ = '0.56.1'
+__short_version__ = '0.56'
+__bcp_version__ = '1.1'
+__config_version__ = '5'
+
+version = f"MPF Monitor v{__version__} (config_version={__config_version__}, BCP v{__bcp_version__})"
