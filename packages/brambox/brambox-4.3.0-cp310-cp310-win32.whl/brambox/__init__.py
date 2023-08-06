28ade1709b7576a4fb3be8ae4f71dd8b9241a82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:47 +0000

    Add package brambox/4.3.0/brambox-4.3.0-cp310-cp310-win32.whl

---
+++ packages/brambox/brambox-4.3.0-cp310-cp310-win32.whl/brambox/__init__.py
@@ -0,0 +1,11 @@
+#
+# BRAMBOX: Basic Recipes for Annotation Munching toolBOX
+# Copyright EAVISE
+#
+from . import io
+from . import stat
+from . import util
+from . import eval
+
+from . import _version
+__version__ = _version.get_versions()['version']
