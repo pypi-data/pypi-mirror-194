79849c044be6d595b13f1ea4c0d2101c4bd823b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:33 +0000

    Add package zeekscript/1.2.1/zeekscript-1.2.1-pp39-pypy39_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/zeekscript/zeekscript-1.2.1-pp39-pypy39_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/zeekscript/error.py
@@ -0,0 +1,10 @@
+"""Exception hierarchy for the zeekscript package."""
+
+class Error(Exception):
+    """Base class for all zeekscript errors."""
+
+class FileError(Error):
+    """System errors while processing script files"""
+
+class ParserError(Error):
+    """A hard parsing error, producing no parse tree."""
