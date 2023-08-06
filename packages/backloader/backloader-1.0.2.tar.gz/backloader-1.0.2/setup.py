e19f99b10cba32e56a4ebdc3fd7cdb6b626eb4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package backloader/1.0.2/backloader-1.0.2.tar.gz

---
+++ packages/backloader/backloader-1.0.2.tar.gz/backloader-1.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="backloader",
+    version="1.0.2",
+    author="Aiden Kundert",
+    author_email="aidenkundert060@gmail.com",
+    description="If you have something that requires importing lots of packages (5,000): baS.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://replit.com/@-jpg/backloader#setup.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
