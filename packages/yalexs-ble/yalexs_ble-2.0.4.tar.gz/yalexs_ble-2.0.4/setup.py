41dfa7bc83e7c2cade2d647599535910fc16432b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:46 +0000

    Add package yalexs-ble/2.0.4/yalexs_ble-2.0.4.tar.gz

---
+++ packages/yalexs-ble/yalexs_ble-2.0.4.tar.gz/yalexs_ble-2.0.4/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['yalexs_ble']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['async-timeout>=3.0.1',
+ 'bleak-retry-connector>=2.9.0',
+ 'bleak>=0.19.0',
+ 'pycryptodome>=3.15.0,<4.0.0']
+
+extras_require = \
+{'docs': ['Sphinx>=5.0,<6.0',
+          'sphinx-rtd-theme>=1.0,<2.0',
+          'myst-parser>=0.18,<0.19']}
+
+setup_kwargs = {
+    'name': 'yalexs-ble',
+    'version': '2.0.4',
+    'description': 'Bluetooth control of Yale and August locks',
+    'long_description': '# Yale Access BLE\n\n<p align="center">\n  <a href="https://github.com/bdraco/yalexs-ble/actions?query=workflow%3ACI">\n    <img src="https://img.shields.io/github/workflow/status/bdraco/yalexs-ble/CI/main?label=CI&logo=github&style=flat-square" alt="CI Status" >\n  </a>\n  <a href="https://yalexs-ble.readthedocs.io">\n    <img src="https://img.shields.io/readthedocs/yalexs-ble.svg?logo=read-the-docs&logoColor=fff&style=flat-square" alt="Documentation Status">\n  </a>\n  <a href="https://codecov.io/gh/bdraco/yalexs-ble">\n    <img src="https://img.shields.io/codecov/c/github/bdraco/yalexs-ble.svg?logo=codecov&logoColor=fff&style=flat-square" alt="Test coverage percentage">\n  </a>\n</p>\n<p align="center">\n  <a href="https://python-poetry.org/">\n    <img src="https://img.shields.io/badge/packaging-poetry-299bd7?style=flat-square&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAASCAYAAABrXO8xAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJJSURBVHgBfZLPa1NBEMe/s7tNXoxW1KJQKaUHkXhQvHgW6UHQQ09CBS/6V3hKc/AP8CqCrUcpmop3Cx48eDB4yEECjVQrlZb80CRN8t6OM/teagVxYZi38+Yz853dJbzoMV3MM8cJUcLMSUKIE8AzQ2PieZzFxEJOHMOgMQQ+dUgSAckNXhapU/NMhDSWLs1B24A8sO1xrN4NECkcAC9ASkiIJc6k5TRiUDPhnyMMdhKc+Zx19l6SgyeW76BEONY9exVQMzKExGKwwPsCzza7KGSSWRWEQhyEaDXp6ZHEr416ygbiKYOd7TEWvvcQIeusHYMJGhTwF9y7sGnSwaWyFAiyoxzqW0PM/RjghPxF2pWReAowTEXnDh0xgcLs8l2YQmOrj3N7ByiqEoH0cARs4u78WgAVkoEDIDoOi3AkcLOHU60RIg5wC4ZuTC7FaHKQm8Hq1fQuSOBvX/sodmNJSB5geaF5CPIkUeecdMxieoRO5jz9bheL6/tXjrwCyX/UYBUcjCaWHljx1xiX6z9xEjkYAzbGVnB8pvLmyXm9ep+W8CmsSHQQY77Zx1zboxAV0w7ybMhQmfqdmmw3nEp1I0Z+FGO6M8LZdoyZnuzzBdjISicKRnpxzI9fPb+0oYXsNdyi+d3h9bm9MWYHFtPeIZfLwzmFDKy1ai3p+PDls1Llz4yyFpferxjnyjJDSEy9CaCx5m2cJPerq6Xm34eTrZt3PqxYO1XOwDYZrFlH1fWnpU38Y9HRze3lj0vOujZcXKuuXm3jP+s3KbZVra7y2EAAAAAASUVORK5CYII=" alt="Poetry">\n  </a>\n  <a href="https://github.com/ambv/black">\n    <img src="https://img.shields.io/badge/code%20style-black-000000.svg?style=flat-square" alt="black">\n  </a>\n  <a href="https://github.com/pre-commit/pre-commit">\n    <img src="https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white&style=flat-square" alt="pre-commit">\n  </a>\n</p>\n<p align="center">\n  <a href="https://pypi.org/project/yalexs-ble/">\n    <img src="https://img.shields.io/pypi/v/yalexs-ble.svg?logo=python&logoColor=fff&style=flat-square" alt="PyPI Version">\n  </a>\n  <img src="https://img.shields.io/pypi/pyversions/yalexs-ble.svg?style=flat-square&logo=python&amp;logoColor=fff" alt="Supported Python versions">\n  <img src="https://img.shields.io/pypi/l/yalexs-ble.svg?style=flat-square" alt="License">\n</p>\n\nBluetooth control of Yale and August locks\n\n## Installation\n\nInstall this via pip (or your favourite package manager):\n\n`pip install yalexs-ble`\n\n## Fork history\n\nThanks to:\n\nhttps://github.com/Friendly0Fire/augustpy\nhttps://github.com/terrcin/augustctl\nhttps://github.com/ethitter/augustctl\nhttps://github.com/robson83/augustctl\n\n## Contributors ✨\n\nThanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):\n\n<!-- prettier-ignore-start -->\n<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->\n<!-- markdownlint-disable -->\n<!-- markdownlint-enable -->\n<!-- ALL-CONTRIBUTORS-LIST:END -->\n<!-- prettier-ignore-end -->\n\nThis project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!\n\n## Credits\n\nThis package was created with\n[Cookiecutter](https://github.com/audreyr/cookiecutter) and the\n[browniebroke/cookiecutter-pypackage](https://github.com/browniebroke/cookiecutter-pypackage)\nproject template.\n',
+    'author': 'J. Nick Koston',
+    'author_email': 'nick@koston.org',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/bdraco/yalexs-ble',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
