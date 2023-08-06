4a714d7a36d23302592351656c55fef9cfe3a423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:41 +0000

    Add package pyrestsdk/0.3.7/pyrestsdk-0.3.7-py2.py3-none-any.whl

---
+++ packages/pyrestsdk/pyrestsdk-0.3.7-py2.py3-none-any.whl/pyrestsdk/request/supports_types/_supports_invoke_collection_request.py
@@ -0,0 +1,19 @@
+from typing import TypeVar, List
+
+from pyrestsdk.type.model import Entity
+from pyrestsdk.type.exception import UnexpectedReturnType
+from pyrestsdk.request.supports_types._supports_base_invoke_request import SupportsBaseInvokeRequest
+
+T = TypeVar("T", bound=Entity)
+
+class SupportsInvokeCollectionRequest(SupportsBaseInvokeRequest):
+
+    @property
+    def invoke_request(self) -> List[T]:
+
+        _return = self.Send(self.input_object)
+
+        if not isinstance(_return, list) or _return is None:
+            raise UnexpectedReturnType(type(_return), List[self.generic_type])
+
+        return _return
---
+++ packages/pyrestsdk/pyrestsdk-0.3.7-py2.py3-none-any.whl/pyrestsdk/type/model/_header_option.py
@@ -0,0 +1,7 @@
+"""Houses Header Option"""
+
+from pyrestsdk.type.model._option import Option
+
+
+class HeaderOption(Option):
+    """Header Option Type"""
