99af554351caeb1ee53f75a5974a2c6919fba394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:33 +0000

    Add package random-unicode-emoji/1.2/random_unicode_emoji-1.2.tar.gz

---
+++ packages/random-unicode-emoji/random_unicode_emoji-1.2.tar.gz/random_unicode_emoji-1.2/README.rst
@@ -0,0 +1,16 @@
+Random Unicode Emoji
+====================
+
+Description
+-----------
+
+A simple Python package to retrieve a random Unicode emoji. 😄
+
+
+Usage
+-----
+
+>>> # Import the Library
+>>> import random_unicode_emoji
+>>> # Use the Function
+>>> emoji = random_emoji()
---
+++ packages/random-unicode-emoji/random_unicode_emoji-1.2.tar.gz/random_unicode_emoji-1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import re
+
+VERSIONFILE="random_unicode_emoji/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+with open("README.rst", "r") as r:
+    long_description = r.read()
+    setuptools.setup(
+        name='random_unicode_emoji',
+        python_requires='>=3',
+        version=new_version,
+        license='MIT',
+        author="Nic Jones",
+        author_email='nic@nicpjones.com',
+        description='A simple Python package to retrieve a random Unicode emoji. 😄',
+        long_description=long_description,
+        packages=setuptools.find_packages(),
+        url='https://github.com/NicPWNs/random-unicode-emoji',
+        download_url ='https://github.com/NicPWNs/random-unicode-emoji/releases',
+        keywords='random unicode emoji',
+    )
