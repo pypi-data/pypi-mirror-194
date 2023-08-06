a1f44c46841594df199af0a7dc19579410ec1e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:25 +0000

    Add package rdvackc/0.0.1/rdvackc-0.0.1.tar.gz

---
+++ packages/rdvackc/rdvackc-0.0.1.tar.gz/rdvackc-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from distutils.core import setup
+setup(
+    name="rdvackc",
+    version="0.0.1",
+    description="An Uber Rides Clustering",
+    author="Lagisetty Ravikiran",
+    author_email="kiranlravi8@gmail.com",
+    py_modules=["rdvackc"],
+    package_dir={"":"src"},
+    include_package_data=True,
+    )
