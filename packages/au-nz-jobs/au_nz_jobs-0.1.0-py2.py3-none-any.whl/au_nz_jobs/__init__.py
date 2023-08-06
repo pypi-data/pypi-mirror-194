c0f02b9fe534a84cc2ed8fa014a83d19b94b0f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:11 +0000

    Add package au-nz-jobs/0.1.0/au_nz_jobs-0.1.0-py2.py3-none-any.whl

---
+++ packages/au-nz-jobs/au_nz_jobs-0.1.0-py2.py3-none-any.whl/au_nz_jobs/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for au-nz-jobs."""
+
+__author__ = """Robert Tu"""
+__email__ = 'tsy0716@gmail.com'
+__version__ = '0.1.0'
+
+# imports
+from au_nz_jobs.downloader import Job, Jobs
+from au_nz_jobs.save_jobs import save_jobs, save_jobs_sqlite
