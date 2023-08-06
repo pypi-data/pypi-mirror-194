c95c11e942fc25c89a2c5c6d94dfeecc28eedad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:56 +0000

    Add package alphamed-federated/0.4.1/alphamed_federated-0.4.1-py3-none-any.whl

---
+++ packages/alphamed-federated/alphamed_federated-0.4.1-py3-none-any.whl/alphafed/loggers.py
@@ -0,0 +1,27 @@
+import logging
+
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+
+__all__ = ['logger', 'task_logger']
+
+if 'alphafed' in logging.Logger.manager.loggerDict.keys():
+    logger = logging.getLogger('alphafed')
+    logfile = '/logs/alphafed.log'
+    handler = ConcurrentRotatingFileHandler(logfile, "a", 512*1024, 5)
+    logger.addHandler(handler)
+
+else:
+    format = '%(asctime)s|%(levelname)s|%(module)s|%(funcName)s|%(lineno)d:\n%(message)s'
+    logging.basicConfig(level=logging.DEBUG,
+                        filename='alphafed.log',
+                        filemode='w',
+                        format=format)
+    logger = logging.getLogger(__name__)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(console_handler)
+
+if 'task' in logging.Logger.manager.loggerDict.keys():
+    task_logger = logging.getLogger('task')
+else:
+    task_logger = logger
