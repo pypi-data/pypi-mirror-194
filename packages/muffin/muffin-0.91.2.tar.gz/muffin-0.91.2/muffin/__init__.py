ee85b6fc6887e0640c007b63d8e3a72824cbbad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:35 +0000

    Add package muffin/0.91.2/muffin-0.91.2.tar.gz

---
+++ packages/muffin/muffin-0.91.2.tar.gz/muffin-0.91.2/muffin/__init__.py
@@ -0,0 +1,50 @@
+"""Muffin is a web framework to build ASGI applications."""
+
+# Package information
+# ===================
+
+__version__ = "0.91.2"
+__project__ = "muffin"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+from asgi_tools import ASGINotFound  # noqa
+from asgi_tools import (ASGIConnectionClosed, ASGIError, ASGIMethodNotAllowed, Request, Response,
+                        ResponseError, ResponseFile, ResponseHTML, ResponseJSON, ResponseRedirect,
+                        ResponseSSE, ResponseStream, ResponseText, ResponseWebSocket)
+from asgi_tools.tests import ASGITestClient as TestClient  # noqa
+
+
+class MuffinException(ASGIError):
+
+    """Base class for Muffin Errors."""
+
+    pass
+
+
+CONFIG_ENV_VARIABLE = "MUFFIN_CONFIG"
+
+
+from .app import Application  # noqa
+from .handler import Handler  # noqa
+
+__all__ = (
+    "ASGIConnectionClosed",
+    "ASGIError",
+    "ASGIMethodNotAllowed",
+    "ASGINotFound",
+    "Application",
+    "Handler",
+    "Request",
+    "Response",
+    "ResponseError",
+    "ResponseFile",
+    "ResponseHTML",
+    "ResponseJSON",
+    "ResponseRedirect",
+    "ResponseSSE",
+    "ResponseStream",
+    "ResponseText",
+    "ResponseWebSocket",
+    "TestClient",
+)
