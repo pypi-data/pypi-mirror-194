9864d0dfb4ed272ab29177e650417adf1f33c64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:08 +0000

    Add package gnocchiclient/7.0.8/gnocchiclient-7.0.8.tar.gz

---
+++ packages/gnocchiclient/gnocchiclient-7.0.8.tar.gz/gnocchiclient-7.0.8/tools/install_deps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+sudo apt-get update -y && sudo apt-get install -qy gnupg software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get update -y && sudo apt-get install -qy \
+        locales \
+        git \
+        wget \
+        curl \
+        python3 \
+        python3-dev \
+        python3-pip \
+        python3.6 \
+        python3.6-dev \
+        python3.6-distutils \
+        python3.9 \
+        python3.9-dev \
+
+sudo rm -rf /var/lib/apt/lists/*
+
+export LANG=en_US.UTF-8
+sudo update-locale
+sudo locale-gen $LANG
+
+sudo python3 -m pip install -U pip tox virtualenv
+sudo python3.6 -m pip install -U pip tox virtualenv
