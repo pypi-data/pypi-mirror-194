28dd420f59fafbecfa2894c304bd34d4f206d20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:08 +0000

    Add package cenfind/0.11.3/cenfind-0.11.3.tar.gz

---
+++ packages/cenfind/cenfind-0.11.3.tar.gz/cenfind-0.11.3/src/cenfind/core/detectors.py
@@ -0,0 +1,109 @@
+import contextlib
+import os
+from pathlib import Path
+from typing import Tuple, List
+import functools
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from csbdeep.utils import normalize
+from spotipy.utils import normalize_fast2d
+from spotipy.model import SpotNet
+from stardist.models import StarDist2D
+
+from cenfind.core.data import Field
+from cenfind.core.outline import resize_image
+from cenfind.core.outline import Centre, Contour
+
+np.random.seed(1)
+tf.random.set_seed(2)
+
+
+@functools.lru_cache(maxsize=None)
+def get_model(model):
+    path = Path(model)
+    if not path.is_dir():
+        raise (FileNotFoundError(f"{path} is not a directory"))
+
+    return SpotNet(None, name=path.name, basedir=str(path.parent))
+
+
+def extract_foci(
+    data: Field,
+    foci_model_file: Path,
+    channel: int,
+    prob_threshold=0.5,
+    min_distance=2,
+    **kwargs,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Detect centrioles as row, col, row major
+    :param data:
+    :param foci_model_file:
+    :param channel:
+    :param prob_threshold:
+    :param min_distance:
+    :param kwargs:
+    :return:
+    """
+    data = data.channel(channel)
+    with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+        data = normalize_fast2d(data)
+        model = get_model(foci_model_file)
+        mask_preds, points_preds = model.predict(
+            data, prob_thresh=prob_threshold, min_distance=min_distance, verbose=False
+        )
+    return mask_preds, points_preds
+
+
+def extract_nuclei(
+    field: Field, channel: int, factor: int, model: StarDist2D = None, annotation=None
+) -> Tuple[List[Centre], List[Contour]]:
+    """
+    Extract the nuclei from the nuclei image
+    :param field:
+    :param channel:
+    :param factor: the factor related to pixel size
+    :param model:
+    :param annotation: a mask with pixels labelled for each centre
+
+    :return: List of Contours.
+
+    """
+
+    if annotation is not None:
+        nuclei_detected = annotation
+    elif model is not None:
+        data = field.channel(channel)
+        data_resized = resize_image(data, factor)
+        with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+            labels, coords = model.predict_instances(normalize(data_resized))
+        nuclei_detected = cv2.resize(
+            labels, dsize=data.shape, fx=1, fy=1, interpolation=cv2.INTER_NEAREST
+        )
+
+    else:
+        raise ValueError("Please provide either an annotation or a model")
+
+    labels_id = np.unique(nuclei_detected)
+
+    cnts = []
+    for nucleus_id in labels_id:
+        if nucleus_id == 0:
+            continue
+        sub_mask = np.zeros_like(nuclei_detected, dtype="uint8")
+        sub_mask[nuclei_detected == nucleus_id] = 255
+        contour, hierarchy = cv2.findContours(
+            sub_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        cnts.append(contour[0])
+
+    contours = tuple(cnts)
+    contours = [
+        Contour(c, "Nucleus", c_id, confidence=-1) for c_id, c in enumerate(contours)
+    ]
+
+    centres = [c.centre for c in contours]
+
+    return centres, contours
---
+++ packages/cenfind/cenfind-0.11.3.tar.gz/cenfind-0.11.3/src/cenfind/publication/train_all_dataset_unet.py
@@ -0,0 +1,33 @@
+from numpy.random import seed
+
+seed(1)
+
+import tensorflow as tf
+
+tf.random.set_seed(2)
+
+from datetime import datetime
+
+from spotipy.model import SpotNet
+
+from cenfind.core.data import Dataset
+from cenfind.core.constants import datasets, PREFIX_REMOTE
+from cenfind.cli.train_spotnet import fetch_all_fields, config_unet
+
+
+def main():
+    path_datasets = [PREFIX_REMOTE / ds for ds in datasets]
+    dss = [Dataset(path) for path in path_datasets]
+
+    all_train_x, all_train_y, all_test_x, all_test_y = fetch_all_fields(dss)
+
+    time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    model_unet = SpotNet(config_unet, name=time_stamp, basedir='models/dev/unet')
+    model_unet.train(all_train_x, all_train_y, validation_data=(all_test_x, all_test_y), epochs=100)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
