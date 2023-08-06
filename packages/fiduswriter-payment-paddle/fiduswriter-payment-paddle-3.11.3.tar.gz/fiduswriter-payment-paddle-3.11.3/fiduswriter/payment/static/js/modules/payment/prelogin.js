a963a46a054d673327fb2c9035b549c58c38ae5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:19 +0000

    Add package fiduswriter-payment-paddle/3.11.3/fiduswriter-payment-paddle-3.11.3.tar.gz

---
+++ packages/fiduswriter-payment-paddle/fiduswriter-payment-paddle-3.11.3.tar.gz/fiduswriter-payment-paddle-3.11.3/fiduswriter/payment/static/js/modules/payment/prelogin.js
@@ -0,0 +1,14 @@
+export class PaymentPrelogin {
+    constructor({page}) {
+        this.preloginPage = page
+    }
+
+    init() {
+        this.preloginPage.footerLinks.push(
+            {
+                text: gettext("Pricing"),
+                link: "/pricing/"
+            }
+        )
+    }
+}
