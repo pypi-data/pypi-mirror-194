20f2049e2c9623c11c52ee584ef98959a885bfec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:08 +0000

    Add package wizata/0.0.2/wizata-0.0.2.tar.gz

---
+++ packages/wizata/wizata-0.0.2.tar.gz/wizata-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='wizata',
+    version='0.0.2',
+    description='Wizata Data Science Toolkit',
+    author='Wizata S.A.',
+    author_email='info@wizata.com',
+    packages=['entities'],
+)
