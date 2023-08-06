80c8dd63fdae78101fb8cc8ab1e6fee39382a657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:45 +0000

    Add package Spinloader/0.0.1/Spinloader-0.0.1.tar.gz

---
+++ packages/Spinloader/Spinloader-0.0.1.tar.gz/Spinloader-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Spinloader",
+    version="0.0.1",
+    description= "A CLI animator that does a little more than just render animations",
+    long_description="A CLI animator that does a little more than just render animations",
+    packages = find_packages()
+)
