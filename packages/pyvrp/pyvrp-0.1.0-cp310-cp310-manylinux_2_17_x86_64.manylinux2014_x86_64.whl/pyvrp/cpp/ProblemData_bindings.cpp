74993160d7d680cb60d6c7a63e938f491f3e22f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:28 +0000

    Add package pyvrp/0.1.0/pyvrp-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyvrp/pyvrp-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/ProblemData_bindings.cpp
@@ -0,0 +1,46 @@
+#include "ProblemData.h"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_ProblemData, m)
+{
+    py::class_<ProblemData::Client>(m, "Client")
+        .def_readonly("x", &ProblemData::Client::x)
+        .def_readonly("y", &ProblemData::Client::y)
+        .def_readonly("service_duration", &ProblemData::Client::serviceDuration)
+        .def_readonly("demand", &ProblemData::Client::demand)
+        .def_readonly("tw_early", &ProblemData::Client::twEarly)
+        .def_readonly("tw_late", &ProblemData::Client::twLate);
+
+    py::class_<ProblemData>(m, "ProblemData")
+        .def(py::init<std::vector<std::pair<int, int>> const &,
+                      std::vector<int> const &,
+                      int,
+                      int,
+                      std::vector<std::pair<int, int>> const &,
+                      std::vector<int> const &,
+                      std::vector<std::vector<int>> const &>(),
+             py::arg("coords"),
+             py::arg("demands"),
+             py::arg("nb_vehicles"),
+             py::arg("vehicle_cap"),
+             py::arg("time_windows"),
+             py::arg("service_durations"),
+             py::arg("duration_matrix"))
+        .def_property_readonly("num_clients", &ProblemData::numClients)
+        .def_property_readonly("num_vehicles", &ProblemData::numVehicles)
+        .def_property_readonly("vehicle_capacity",
+                               &ProblemData::vehicleCapacity)
+        .def("client",
+             &ProblemData::client,
+             py::arg("client"),
+             py::return_value_policy::reference)
+        .def("depot", &ProblemData::depot, py::return_value_policy::reference)
+        .def("dist", &ProblemData::dist, py::arg("first"), py::arg("second"))
+        .def("distance_matrix",
+             &ProblemData::distanceMatrix,
+             py::return_value_policy::reference);
+}
