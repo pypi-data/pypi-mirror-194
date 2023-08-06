9bd3e9cfe64a723bb4c58b6c463319c1993d5a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:59 +0000

    Add package easycrystallography/0.3.0/easyCrystallography-0.3.0-py3-none-any.whl

---
+++ packages/easycrystallography/easyCrystallography-0.3.0-py3-none-any.whl/easyCrystallography/io/cif/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'github.com/wardsimon'
+__version__ = '0.0.1'
+
+
+#  SPDX-FileCopyrightText: 2023 easyCrystallography contributors <crystallography@easyscience.software>
+#  SPDX-License-Identifier: BSD-3-Clause
+#  © 2022-2023  Contributors to the easyCore project <https://github.com/easyScience/easyCrystallography>
+
+from .lattice import Lattice
+from .spacegroup import SpaceGroup
+from .atoms import Atoms
