450d24ed1a80678448d9f9c2486e46a9a5e4ca80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:07 +0000

    Add package foc-common/0.0.7/foc_common-0.0.7.tar.gz

---
+++ packages/foc-common/foc_common-0.0.7.tar.gz/foc_common-0.0.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+NAME = "foc_common"
+VERSION = "0.0.7"
+DESCRIPTION = "A description"
+REQUIRES_PYTHON = ">=3.6.0"
+
+
+setuptools.setup(
+  name=NAME,
+  version=VERSION,
+  description=DESCRIPTION,
+  packages=setuptools.find_packages(),
+  package_dir={"foc_common": "foc_common"},
+  include_package_data=True,
+)
