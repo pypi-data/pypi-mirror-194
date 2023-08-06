ddf187407a629c98b9835e063ba10e10833510c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:07 +0000

    Add package deep-serialization/0.1.12/deep_serialization-0.1.12.tar.gz

---
+++ packages/deep-serialization/deep_serialization-0.1.12.tar.gz/deep_serialization-0.1.12/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['deep_serialization']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['deep-serialization = '
+                     'deep_serialization:deep_serialization']}
+
+setup_kwargs = {
+    'name': 'deep-serialization',
+    'version': '0.1.12',
+    'description': 'Automatic python objects JSON serialization',
+    'long_description': '# Deep Serializer\n\nUtil can serialize almost python objects to json.\n',
+    'author': 'Vyacheslav Tamarin',
+    'author_email': 'vyacheslav.tamarin@yandex.ru',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
