4799d8794c7c643a9d9ac1feb3394f04302e40af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:54 +0000

    Add package wizata-dsapi/0.0.8/wizata-dsapi-0.0.8.tar.gz

---
+++ packages/wizata-dsapi/wizata-dsapi-0.0.8.tar.gz/wizata-dsapi-0.0.8/wizata_dsapi_tools/wizata_dsapi_client.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class WizataDSAPIClient:
+
+    def __init__(self):
+        self.domain = None
+        self.user = None
+        self.password = None
+
+    def __url(self):
+        return "https://" + self.user + ":" + self.password + "@" + self.domain + "/dsapi/"
+
+    def __header(self):
+        return {'Content-Type': 'application/json'}
+
+    def __request_process(self, method, route):
+        response = requests.request(method, self.__url() + route, headers=self.__header())
+        return response
+
+    def get_ds_functions(self):
+        return self.__request_process("GET", "get_ds_functions")
