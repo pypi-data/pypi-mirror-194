c8cc5ba87a41b375ea5bdcf1ada03e507ae7733f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:06 +0000

    Add package helperforpy/0.1.0/helperforpy-0.1.0.tar.gz

---
+++ packages/helperforpy/helperforpy-0.1.0.tar.gz/helperforpy-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helperforpy',
+    version='0.1.0',
+    description='Helperforpy package',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+)
