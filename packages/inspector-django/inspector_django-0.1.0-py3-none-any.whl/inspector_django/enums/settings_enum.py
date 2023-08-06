4e9a706b6c48c00d8006dd0f113a7b7f5496ee51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:51 +0000

    Add package inspector-django/0.1.0/inspector_django-0.1.0-py3-none-any.whl

---
+++ packages/inspector-django/inspector_django-0.1.0-py3-none-any.whl/inspector_django/enums/settings_enum.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SettingKeys:
+    INGESTION_KEY = 'inspector_ingestion_key'
+    INSPECTOR_TRANSPORT = 'inspector_transport'
+    MONITORING_QUERY = 'inspector_monitoring_query'
+    MONITORING_REQUEST = 'inspector_monitoring_request'
