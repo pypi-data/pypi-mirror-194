ad6c0847a0403b8780433a090a5b5c500451642e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:43 +0000

    Add package srj-basic-calculator2/0.0.1/srj-basic-calculator2-0.0.1.tar.gz

---
+++ packages/srj-basic-calculator2/srj-basic-calculator2-0.0.1.tar.gz/srj-basic-calculator2-0.0.1/srjbasiccalc/calc.py
@@ -0,0 +1,14 @@
+def add_numbers(num1,num2):
+    return num1 + num2
+
+
+def minus_numbers(num1,num2):
+    return num1 - num2
+
+
+def multiply_numbers(num1,num2):
+    return num1 * num2
+
+
+def devide_numbers(num1,num2):
+    return num1 / num2
