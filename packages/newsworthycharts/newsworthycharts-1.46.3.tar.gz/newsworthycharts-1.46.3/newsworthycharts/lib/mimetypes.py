30bdd928017a217f3fe1b1f648865a51b721b843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:26 +0000

    Add package newsworthycharts/1.46.3/newsworthycharts-1.46.3.tar.gz

---
+++ packages/newsworthycharts/newsworthycharts-1.46.3.tar.gz/newsworthycharts-1.46.3/newsworthycharts/lib/mimetypes.py
@@ -0,0 +1,11 @@
+""" Mime type data
+"""
+
+MIME_TYPES = {
+    'dw': "text/plain",
+    'eps': "application/postscript",
+    'pdf': "application/pdf",
+    'png': "image/png",
+    'svg': "image/svg+xml",
+    'webp': "image/webp",
+}
