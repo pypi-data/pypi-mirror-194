8ee8017f29e16db74a6451bc86c3e44fe1965e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:14 +0000

    Add package bemserver-api-client/0.12.0/bemserver_api_client-0.12.0-py3-none-any.whl

---
+++ packages/bemserver-api-client/bemserver_api_client-0.12.0-py3-none-any.whl/bemserver_api_client/resources/services/check_missing.py
@@ -0,0 +1,13 @@
+"""BEMServer API client check missing service resources
+
+/st_check_missings_by_campaigns/ endpoints
+"""
+from ..base import BaseResources
+
+
+class ST_CheckMissingByCampaignResources(BaseResources):
+    endpoint_base_uri = "/st_check_missings_by_campaigns/"
+
+    def get_full(self, *, etag=None, **kwargs):
+        endpoint = f"{self.endpoint_base_uri}full"
+        return self._req.getall(endpoint, etag=etag, params=kwargs)
