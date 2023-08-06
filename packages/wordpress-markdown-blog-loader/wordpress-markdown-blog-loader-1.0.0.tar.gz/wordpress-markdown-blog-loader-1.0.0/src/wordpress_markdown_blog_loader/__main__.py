2cbe898b8e0e8a5ab29c3f0684b5426d9df7a52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:50 +0000

    Add package wordpress-markdown-blog-loader/1.0.0/wordpress-markdown-blog-loader-1.0.0.tar.gz

---
+++ packages/wordpress-markdown-blog-loader/wordpress-markdown-blog-loader-1.0.0.tar.gz/wordpress-markdown-blog-loader-1.0.0/src/wordpress_markdown_blog_loader/__main__.py
@@ -0,0 +1,31 @@
+import logging
+import os
+import click
+from wordpress_markdown_blog_loader import upload, download, new
+
+
+@click.group()
+def main():
+    """
+    Wordpress CLI
+    """
+    logging.basicConfig(
+        level=os.getenv("LOG_LEVEL", "INFO"), format="%(levelname)s: %(message)s"
+    )
+
+
+@main.group()
+def posts():
+    """
+    Wordpress posts up- and download
+    """
+
+
+posts.add_command(upload.command)
+posts.add_command(download.command)
+posts.add_command(new.command)
+posts.add_command(new.update_banner_command)
+
+
+if __name__ == "__main__":
+    main()
