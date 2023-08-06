96c3327ef7ce2385a08c37e4736ec377fc991e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:09 +0000

    Add package rsome/1.1.4/rsome-1.1.4.tar.gz

---
+++ packages/rsome/rsome-1.1.4.tar.gz/rsome-1.1.4/rsome/__init__.py
@@ -0,0 +1,13 @@
+from .math import norm
+from .math import square
+from .math import sumsqr
+from .math import quad
+from .math import expcone
+from .math import exp
+from .math import log
+from .math import pexp
+from .math import plog
+from .math import entropy
+from .math import kldiv
+from .math import maxof, minof
+from .subroutines import E
