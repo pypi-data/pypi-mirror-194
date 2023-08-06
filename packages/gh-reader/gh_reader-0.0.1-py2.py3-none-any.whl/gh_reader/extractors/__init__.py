1635443a773e07ea1abdd89435f18787730375a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:50 +0000

    Add package gh-reader/0.0.1/gh_reader-0.0.1-py2.py3-none-any.whl

---
+++ packages/gh-reader/gh_reader-0.0.1-py2.py3-none-any.whl/gh_reader/extractors/__init__.py
@@ -0,0 +1,12 @@
+from . import (
+    commits,
+    issue_comments,
+    issue_events,
+    issue_labels,
+    issues,
+    issues_pr,
+    labels,
+    pull_requests,
+    repository,
+    stargazers
+)
