d0adcba5274d9b1e3010867ec34a854116d22000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:25 +0000

    Add package pymodelextractor/0.26.1/pymodelextractor-0.26.1.tar.gz

---
+++ packages/pymodelextractor/pymodelextractor-0.26.1.tar.gz/pymodelextractor-0.26.1/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pymodelextractor',
+ 'pymodelextractor.exceptions',
+ 'pymodelextractor.factories',
+ 'pymodelextractor.learners',
+ 'pymodelextractor.learners.observation_table_learners',
+ 'pymodelextractor.learners.observation_table_learners.translators',
+ 'pymodelextractor.learners.observation_tree_learners',
+ 'pymodelextractor.teachers',
+ 'pymodelextractor.tests',
+ 'pymodelextractor.tests.learners_tests',
+ 'pymodelextractor.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pythautomata==0.35.1', 'scikit-learn>=1.2.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pymodelextractor',
+    'version': '0.26.1',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Federico VIlensky',
+    'author_email': 'fedevilensky@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
