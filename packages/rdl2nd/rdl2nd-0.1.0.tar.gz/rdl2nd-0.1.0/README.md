884f22da1607ca3b554b4cfad9e85ea79cdedcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:41 +0000

    Add package rdl2nd/0.1.0/rdl2nd-0.1.0.tar.gz

---
+++ packages/rdl2nd/rdl2nd-0.1.0.tar.gz/rdl2nd-0.1.0/README.md
@@ -0,0 +1,10 @@
+# SystemRDL to NaturalDocs
+
+[[_TOC_]]
+
+## Description
+
+It generates registers memory map documentation from [SystemRDL][] input files to [NaturalDocs][] output.
+
+[SystemRDL]: https://www.accellera.org/downloads/standards/systemrdl
+[NaturalDocs]: https://www.naturaldocs.org
---
+++ packages/rdl2nd/rdl2nd-0.1.0.tar.gz/rdl2nd-0.1.0/lib/rdl2nd/field.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Copyright 2023 xlogic <https://xlogic.dev> and contributors to the project
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileContributor: 2023 Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
+
+class Field:
+    def __init__(self):
+        print('Hello from field class')
