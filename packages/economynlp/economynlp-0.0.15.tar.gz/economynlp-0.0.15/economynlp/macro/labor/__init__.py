698b8c22a7d4afd4b59a218f0f496138d5bb94df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:28 +0000

    Add package economynlp/0.0.15/economynlp-0.0.15.tar.gz

---
+++ packages/economynlp/economynlp-0.0.15.tar.gz/economynlp-0.0.15/economynlp/macro/labor/__init__.py
@@ -0,0 +1,4 @@
+from economynlp.macro.labor.unemployment import *
+from economynlp.macro.labor.human_capital import *
+from economynlp.macro.labor.minimum_wage import *
+from economynlp.macro.labor.employment_structure import *
