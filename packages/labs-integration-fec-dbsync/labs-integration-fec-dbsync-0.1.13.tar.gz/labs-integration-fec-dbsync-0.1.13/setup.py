c37779626e5e4735b9f96ae063ff092306ddb92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:20 +0000

    Add package labs-integration-fec-dbsync/0.1.13/labs-integration-fec-dbsync-0.1.13.tar.gz

---
+++ packages/labs-integration-fec-dbsync/labs-integration-fec-dbsync-0.1.13.tar.gz/labs-integration-fec-dbsync-0.1.13/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='labs-integration-fec-dbsync',
+    version='0.1.13',
+    include_package_data=True,
+    install_requires=['cx_Oracle==6.3.1','mysql-connector'],
+    packages=find_packages(),
+    zip_safe=False,
+    author="Kuber Mehrotra",
+    author_email="kuber.mehrotra@kuliza.com",
+)
