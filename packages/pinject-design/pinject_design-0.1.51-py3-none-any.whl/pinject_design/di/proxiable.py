dc5a010f11eae6147e480783e56647d4a3fae77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:24 +0000

    Add package pinject-design/0.1.51/pinject_design-0.1.51-py3-none-any.whl

---
+++ packages/pinject-design/pinject_design-0.1.51-py3-none-any.whl/pinject_design/di/proxiable.py
@@ -0,0 +1,69 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Generic, TypeVar, Any
+
+T = TypeVar("T")
+
+
+class IProxyContext(Generic[T]):
+
+    @abstractmethod
+    def getattr(self, tgt: T, name: str):
+        pass
+
+    @abstractmethod
+    def call(self, tgt: T, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def pure(self, tgt: T) -> "DelegatedVar[T]":
+        pass
+
+    @abstractmethod
+    def getitem(self, tgt: T, key) -> Any:
+        pass
+
+    @abstractmethod
+    def eval(self, tgt: T):
+        pass
+
+    @abstractmethod
+    def alias_name(self):
+        pass
+
+    @abstractmethod
+    def iter(self, tgt: T):
+        pass
+    @abstractmethod
+    def dir(self,tgt):
+        pass
+
+
+
+@dataclass
+class DelegatedVar(Generic[T]):
+    # Generic Var Type that delegates all implementations to cxt.
+    # todo add 'hook capability'
+    value: T
+    cxt: IProxyContext[T]
+
+    def __getattr__(self, item):
+        return self.cxt.getattr(self.value, item)
+
+    def __call__(self, *args, **kwargs):
+        return self.cxt.call(self.value, *args, **kwargs)
+
+    def __getitem__(self, key):
+        return self.cxt.getitem(self.value, key)
+
+    def eval(self)->T:
+        """this should return the semantic result of calculation."""
+        return self.cxt.eval(self.value)
+
+    def __str__(self):
+        return f"{self.cxt.alias_name()}({self.value},{self.cxt})"
+
+    def __iter__(self):
+        return self.cxt.iter(self.value)
+
+
