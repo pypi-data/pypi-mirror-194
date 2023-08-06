a945635ccd71432769b68cef2f36db9d7586fc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:12 +0000

    Add package langchain/0.0.99rc0/langchain-0.0.99rc0.tar.gz

---
+++ packages/langchain/langchain-0.0.99rc0.tar.gz/langchain-0.0.99rc0/langchain/chat_models/base.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from langchain.schema import ChatGeneration, ChatMessage, ChatResult
+
+
+class BaseChat(ABC):
+    def generate(
+        self, messages: List[ChatMessage], stop: Optional[List[str]] = None
+    ) -> ChatResult:
+        """Top Level call"""
+        # Nothing here now, but future proofing.
+        return self._generate(messages, stop=stop)
+
+    @abstractmethod
+    def _generate(
+        self, messages: List[ChatMessage], stop: Optional[List[str]] = None
+    ) -> ChatResult:
+        """Top Level call"""
+
+    def run(
+        self, messages: List[ChatMessage], stop: Optional[List[str]] = None
+    ) -> ChatMessage:
+        res = self.generate(messages, stop=stop)
+        return res.generations[0].message
+
+
+class SimpleChat(BaseChat):
+    role: str = "assistant"
+
+    def _generate(
+        self, messages: List[ChatMessage], stop: Optional[List[str]] = None
+    ) -> ChatResult:
+        output_str = self._call(messages, stop=stop)
+        message = ChatMessage(text=output_str, role=self.role)
+        generation = ChatGeneration(message=message)
+        return ChatResult(generations=[generation])
+
+    @abstractmethod
+    def _call(
+        self, messages: List[ChatMessage], stop: Optional[List[str]] = None
+    ) -> str:
+        """Simpler interface."""
