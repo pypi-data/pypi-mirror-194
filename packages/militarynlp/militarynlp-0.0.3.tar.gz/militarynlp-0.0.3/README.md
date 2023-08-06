91b5631865b20ce9ce9850cfd93019f3264afcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:46 +0000

    Add package militarynlp/0.0.3/militarynlp-0.0.3.tar.gz

---
+++ packages/militarynlp/militarynlp-0.0.3.tar.gz/militarynlp-0.0.3/README.md
@@ -0,0 +1,29 @@
+
+# Python Narrative packages
+- [politicsnlp](https://pypi.org/project/politicsnlp/) for Politics
+- [religionnlp](https://pypi.org/project/religionnlp/) for Religion
+- [lawnlp](https://pypi.org/project/lawnlp/) for Law
+- [economynlp](https://pypi.org/project/economynlp/) for Economy
+- [cpanlp](https://pypi.org/project/cpanlp/) for Business
+- [militarynlp](https://pypi.org/project/militarynlp/) for Military
+
+## Communicate effectively within a common Military framework
+  
+
+<img src="https://raw.githubusercontent.com/python-narrative/militarynlp/main/militarynlp.jpg" width = "220" height = "100" 
+alt="logo" align=center />
+
+## Install & Import
+#### Dependencies
+- numpy
+- pandas
+  
+For detailed installation instructions, see the
+[documentation](https://cpanlp.com/documentation).
+```python
+pip install militarynlp
+import militarynlp as mp
+```
+
+
+Check out: https://cpanlp.com
---
+++ packages/militarynlp/militarynlp-0.0.3.tar.gz/militarynlp-0.0.3/militarynlp/special_military_operations.py
@@ -0,0 +1,29 @@
+class SpecialMilitaryOperations:
+    """
+Special military operations, also known as special operations, refer to military activities that are conducted by specially trained personnel using unconventional tactics and techniques. Some common features of special military operations include:
+
+Highly trained personnel: Special military operations are carried out by highly trained and skilled personnel who are often drawn from elite units.
+Unconventional tactics and techniques: Special military operations involve the use of unconventional tactics and techniques that are not typically employed in conventional military operations.
+Secrecy and discretion: Special military operations are often carried out in secret and with a high degree of discretion, to avoid detection and minimize the risk of retaliation.
+High risk and high reward: Special military operations are often high risk, but also offer the potential for high rewards in terms of achieving strategic objectives.
+Rapid deployment: Special military operations often require rapid deployment, with personnel and equipment capable of being rapidly deployed to the target location.
+
+    """
+    def __init__(self, objective, personnel):
+        self.objective = objective
+        self.personnel = personnel
+
+    def use_unconventional_tactics(self):
+        print("Using unconventional tactics and techniques to achieve the " + self.objective)
+
+    def maintain_secrecy(self):
+        print("Maintaining secrecy and discretion to avoid detection and minimize the risk of retaliation")
+
+    def offer_high_reward(self):
+        print("Offering the potential for high rewards in terms of achieving strategic objectives")
+
+    def require_rapid_deployment(self):
+        print("Requiring rapid deployment of highly trained personnel and specialized equipment to the target location")
+
+    def involve_high_risk(self):
+        print("Involving high risk due to the unconventional tactics and the need for secrecy")
