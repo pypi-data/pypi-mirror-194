94eb52c4ad2d4f72b3f69390fb8e7bc20a2c88ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:45 +0000

    Add package byunlp/0.0.2/byunlp-0.0.2.tar.gz

---
+++ packages/byunlp/byunlp-0.0.2.tar.gz/byunlp-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name="byunlp",
+      version="0.0.2",
+      description="Automatic conjugation of Korean verbs",
+      author='SungJoo Byun',
+      author_email='byunsj@snu.ac.kr',
+      url=None,
+      license='MIT',
+      packages=['byunlp'],
+      insall_requires=['soynlp']
+     )
+      
