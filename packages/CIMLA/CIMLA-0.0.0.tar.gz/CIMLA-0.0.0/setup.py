8ae3ae7e3a2d91fce1f5a97fbdbf0748c391cf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package CIMLA/0.0.0/CIMLA-0.0.0.tar.gz

---
+++ packages/CIMLA/CIMLA-0.0.0.tar.gz/CIMLA-0.0.0/setup.py
@@ -0,0 +1,42 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.0'
+PACKAGE_NAME = 'CIMLA'
+AUTHOR = 'Payam Dibaeinia'
+AUTHOR_EMAIL = 'dibaein2@illinois.edu'
+URL = 'https://github.com/PayamDiba/CIMLA'
+
+LICENSE = 'MIT License'
+DESCRIPTION = 'Counterfactual Inference by Machine Learning and Attribution Models'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'dask==21.10.0',
+      'h5py==2.10.0',
+      'numpy==1.21.2',
+      'pandas==1.3.3',
+      'scipy==1.7.1',
+      'tensorflow==2.2.0',
+      'dask-ml==1.9.0',
+      'pytables==3.6.1',
+      'shap==0.39.0',
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      entry_points ={'console_scripts': [ 'cimla = CIMLA.main:main']},
+      python_requires='==3.8.12',
+      )
