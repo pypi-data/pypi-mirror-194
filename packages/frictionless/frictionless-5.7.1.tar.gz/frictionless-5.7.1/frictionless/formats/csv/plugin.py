1de8711cde6e3b7f61d2c8fa2616ed5ec38c2939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:47 +0000

    Add package frictionless/5.7.1/frictionless-5.7.1.tar.gz

---
+++ packages/frictionless/frictionless-5.7.1.tar.gz/frictionless-5.7.1/frictionless/formats/csv/plugin.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
+from ...system import Plugin
+from .control import CsvControl
+from .parser import CsvParser
+
+if TYPE_CHECKING:
+    from ...resource import Resource
+
+
+class CsvPlugin(Plugin):
+    """Plugin for CSV"""
+
+    # Hooks
+
+    def create_parser(self, resource: Resource):
+        if resource.format in ["csv", "tsv"]:
+            return CsvParser(resource)
+
+    def detect_resource(self, resource: Resource):
+        if resource.format in ["csv", "tsv"]:
+            resource.mediatype = f"text/{resource.format}"
+
+    def detect_resource_type(self, resource: Resource):
+        if resource.format in ["csv", "tsv"]:
+            return "table"
+
+    def select_control_class(self, type: Optional[str]):
+        if type == "csv":
+            return CsvControl
