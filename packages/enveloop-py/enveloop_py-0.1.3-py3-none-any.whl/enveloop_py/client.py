76dbdc9038d86eaa9a10eea7f2e11f8830dd0d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:38 +0000

    Add package enveloop-py/0.1.3/enveloop_py-0.1.3-py3-none-any.whl

---
+++ packages/enveloop-py/enveloop_py-0.1.3-py3-none-any.whl/enveloop_py/client.py
@@ -0,0 +1,37 @@
+import requests
+
+from .message_response import MessageResponse
+from .template_response import TemplateResponse
+
+class Client:
+    """Client class."""
+
+    def __init__(self, api_key=None, api_host=None, ssl=None):
+        """Initialize the client."""
+        self._api_key = api_key
+        self._endpoint = 'https://{}'.format(api_host) if ssl else 'http://{}'.format(api_host)
+
+    def send_message(self, template=None, to=None, from_address=None, subject=None, template_variables={}):
+        data = {
+            'to': to,
+            'from': from_address,
+            'subject': subject,
+            'template': template,
+            'templateVariables': template_variables
+        }
+
+        res = requests.post(
+            '{}/messages'.format(self._endpoint),
+            json=data,
+            headers={'Authorization': 'token {}'.format(self._api_key)}
+        )
+
+        return MessageResponse(res)
+
+    def template_info(self, template=None):
+        res = requests.get(
+            '{}/templates/{}'.format(self._endpoint, template),
+            headers={'Authorization': 'token {}'.format(self._api_key)}
+        )
+
+        return TemplateResponse(res)
