f53a882bbadb2e8d934a0147f5ad64bbdc306a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:04 +0000

    Add package mpyl/0.0.6/mpyl-0.0.6-py3-none-any.whl

---
+++ packages/mpyl/mpyl-0.0.6-py3-none-any.whl/mpyl/__init__.py
@@ -0,0 +1,11 @@
+"""
+.. include:: ../../README.md
+
+# How do I?
+
+## ..report the outcome of a pipeline run
+To report the outcome to Github, see `mpyl.reporting.targets.github`
+## ..create a custom step
+For creating your own step, see `mpyl.steps`
+
+"""
