0a528e8e27d188ec6e1b71f7578690f7ba2c0cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:00 +0000

    Add package peaqevcore/12.2.4/peaqevcore-12.2.4.tar.gz

---
+++ packages/peaqevcore/peaqevcore-12.2.4.tar.gz/peaqevcore-12.2.4/peaqevcore/services/hourselection/initializers/hourselectionfactory.py
@@ -0,0 +1,10 @@
+from .hoursbase import Hours
+from .price_aware_hours import PriceAwareHours
+from .regular_hours import RegularHours
+
+class HourselectionFactory:
+    @staticmethod
+    def create(hub) -> Hours:
+        if hub.options.price.price_aware is False:
+            return RegularHours(hub)
+        return PriceAwareHours(hub)
