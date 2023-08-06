cfe56530734d6cab8527d162588895759012a1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:31 +0000

    Add package RanDepict/1.1.5/RanDepict-1.1.5.tar.gz

---
+++ packages/RanDepict/RanDepict-1.1.5.tar.gz/RanDepict-1.1.5/RanDepict/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+RanDepict Python Package.
+This repository contains RanDepict,
+an easy-to-use utility to generate a big variety of
+chemical structure depictions (random depiction styles and image augmentations).
+
+
+Example:
+--------
+>>> from RanDepict import RandomDepictor
+>>> smiles = "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
+>>> with RandomDepictor() as depictor:
+>>>    image = depictor(smiles)
+
+Have a look in the RanDepictNotebook.ipynb for more examples.
+
+For comments, bug reports or feature ideas,
+please raise an issue on the Github repository.
+
+"""
+
+__version__ = "1.1.5"
+
+__all__ = [
+    "RanDepict",
+]
+
+
+from .randepict import RandomDepictor, RandomDepictorConfig, DepictionFeatureRanges, RandomMarkushStructureCreator
