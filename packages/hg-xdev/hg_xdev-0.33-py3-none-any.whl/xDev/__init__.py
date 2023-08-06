3db36f225be1a3c57253711a2b2227b0cf0e539a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:27 +0000

    Add package hg-xdev/0.33/hg_xdev-0.33-py3-none-any.whl

---
+++ packages/hg-xdev/hg_xdev-0.33-py3-none-any.whl/xDev/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Date: 2023-01-03
+Description: 
+LastEditTime: 2023-03-01
+LastEditors: xushuwei
+'''
+
+
+__all__ = [
+    'xConan',
+    'xFrame',
+]
+
+__version__ = '0.33'
