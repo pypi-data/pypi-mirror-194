168fed5143fee54e5c85aed6f286c9e23309c326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:21:00 +0000

    Add package therapi/0.0.2/therapi-0.0.2.tar.gz

---
+++ packages/therapi/therapi-0.0.2.tar.gz/therapi-0.0.2/therapi/__init__.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class BaseAPIConsumer:
+    base_url = None
+
+    def __init__(self, base_url=None):
+        if base_url:
+            self.base_url = base_url
+
+        if not self.base_url:
+            raise ValueError(
+                "A base URL is required. Specify it as a class member, or when initializing your class instance."
+            )
+
+    def construct_url(self, *url_parts):
+        parts = [self.base_url] + [part.strip("/") for part in url_parts]
+        return "/".join(parts)
+
+    def base_request(self, method, path):
+        return requests.request(method, self.construct_url(path))
