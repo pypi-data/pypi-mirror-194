6334a1dd9a0e2e6cc6a4a3dca925766d4b62b151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:09 +0000

    Add package lightning-Horovod/0.1.0.dev0/lightning_Horovod-0.1.0.dev0-py3-none-any.whl

---
+++ packages/lightning-Horovod/lightning_Horovod-0.1.0.dev0-py3-none-any.whl/lightning_horovod/__about__.py
@@ -0,0 +1,17 @@
+__version__ = "0.1.0.dev0"
+__author__ = "Lightning-AI et al."
+__author_email__ = "name@lightning.ai"
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright (c) 2021-2023, {__author__}."
+__homepage__ = "https://github.com/Lightning-AI/lightning-Horovod"
+__docs__ = "PyTorch Lightning Strategy for Horovod."
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
