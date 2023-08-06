9337a2b4e86776ed6129b40ff31d649f4ca98f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:38 +0000

    Add package pyslinger/0.1.1/pyslinger-0.1.1.tar.gz

---
+++ packages/pyslinger/pyslinger-0.1.1.tar.gz/pyslinger-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pyslinger',
+    version='0.1.1',
+    description='Python package to ping slack',
+    author='satya.penamakuri',
+    author_email='satya.penamakuri@gmail.com',
+    packages=['pyslinger'],
+    install_requires=[],
+)
