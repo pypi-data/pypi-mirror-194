2c37ee95849b14b0229668ea43a5e013284dd188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:48 +0000

    Add package pitchmark/0.2.0/pitchmark-0.2.0-py3-none-any.whl

---
+++ packages/pitchmark/pitchmark-0.2.0-py3-none-any.whl/pitchmark/osm/__init__.py
@@ -0,0 +1,8 @@
+"""A subpackage for parsing OpenStreetMap files."""
+
+# read version from installed package
+from importlib.metadata import version
+
+__version__ = version("pitchmark")
+
+from pitchmark.osm.osm import golf_tags, GolfHandler
