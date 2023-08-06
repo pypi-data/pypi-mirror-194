4e6f9768b9fa9f323e06a8d39ba8e5798c95d514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:58 +0000

    Add package django-multitenancy-manager/0.0.2a21/django-multitenancy-manager-0.0.2a21.tar.gz

---
+++ packages/django-multitenancy-manager/django-multitenancy-manager-0.0.2a21.tar.gz/django-multitenancy-manager-0.0.2a21/multitenancy/admin/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from multitenancy.admin.views import (
+    adminViews,
+    authViews
+)
+
+#app_name = "multitenancy"
+
+urlpatterns = [
+    path(r"", adminViews.AdminIndexView.as_view(), name="admin_dashboard"),
+    path("teams/all", adminViews.TeamsIndexView.as_view(), name="teams_index"),    
+    path("acounts/login/", authViews.LoginView.as_view(), name="accounts_login"),
+    path("page-not-found/", authViews.pageNotFound.as_view(), name="page_not_found"),
+
+]
+handler404 = 'multitenancy.admin.views.authViews.pageNotFound'
