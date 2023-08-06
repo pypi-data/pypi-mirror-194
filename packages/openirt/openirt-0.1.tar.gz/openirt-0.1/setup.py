389fefd365ed504f8324e760ed23b909863d0d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:15 +0000

    Add package openirt/0.1/openirt-0.1.tar.gz

---
+++ packages/openirt/openirt-0.1.tar.gz/openirt-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='openirt',
+    packages = find_packages(include=['openirt']),
+    url='https://gitlab.com/pleased/...',
+    version='0.1',
+    description='Item response theory toolkit',
+    author='Johan Hay',
+    license='MIT',
+)
