838149c978e2a2ad6de5ef14161f3b2ba38466e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:01 +0000

    Add package wedoc/0.1.23/wedoc-0.1.23-py3-none-any.whl

---
+++ packages/wedoc/wedoc-0.1.23-py3-none-any.whl/wedoc/__init__.py
@@ -0,0 +1,25 @@
+from wedoc.api import Document
+from wedoc.api import Spreadsheet
+from wedoc.api import Form
+from wedoc.client import WedocClientBase
+
+
+class WedocClient(WedocClientBase):
+    doc: Document = Document()
+    workbook: Spreadsheet = Spreadsheet()
+    form: Form = Form()
+
+    def __init__(self, corpid, corpsecret) -> None:
+        """
+        对企业微信自建应用初始化
+
+        :param corpid: 企业 ID 标识
+        :param corpsecret: 企业自建应用密钥
+
+        使用示例::
+
+        >>> from wedoc import WedocClient
+        >>> client = WedocClient("corpid", "corpsecret")
+        >>> client.doc.create_doc("doc_name", "doc_type")
+        """
+        super().__init__(corpid, corpsecret)
