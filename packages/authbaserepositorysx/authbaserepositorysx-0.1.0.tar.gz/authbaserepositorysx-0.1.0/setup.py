c7eaeb24c702cbf357c99705aabe2c07d4c8389b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:34 +0000

    Add package authbaserepositorysx/0.1.0/authbaserepositorysx-0.1.0.tar.gz

---
+++ packages/authbaserepositorysx/authbaserepositorysx-0.1.0.tar.gz/authbaserepositorysx-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['authbaserepositorysx']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi-jwt-auth>=0.5.0,<0.6.0', 'sqlalchemy==1.4.41']
+
+setup_kwargs = {
+    'name': 'authbaserepositorysx',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
