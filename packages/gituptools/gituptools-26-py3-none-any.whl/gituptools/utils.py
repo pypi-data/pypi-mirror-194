3e4041130def0b13a6c5a424506a303625941731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:25 +0000

    Add package gituptools/26/gituptools-26-py3-none-any.whl

---
+++ packages/gituptools/gituptools-26-py3-none-any.whl/gituptools/utils.py
@@ -0,0 +1,34 @@
+"""Gituptools utilities module."""
+__all__ = ('attribution',)
+
+import functools
+import os
+import typing
+
+# --------------------------------------------------------------------------- #
+
+FOLDER: str = os.path.abspath(os.path.dirname(__file__))
+STATIC: str = os.path.join(FOLDER, 'static')
+
+klass = typing.Optional[type]
+file_name = typing.Optional[str]
+kallable = typing.Union[type, typing.Callable]
+
+# --------------------------------------------------------------------------- #
+
+
+@functools.lru_cache(maxsize=1)
+def load_variable_file(fh: str) -> list:
+    """Load and split a variable file into a list of slugs."""
+    file: str = f'vars.{fh!s}.txt'
+    with open(os.path.join(STATIC, file), mode='r') as f:
+        return f.read().strip().splitlines()
+
+
+def attribution(cls: klass = None, file: file_name = None) -> kallable:
+    """Attribute all variables in a file to a class."""
+    if callable(cls) and file:
+        for var in load_variable_file(file):
+            setattr(cls, var, os.getenv(var))
+        return cls
+    return functools.partial(attribution, file=file)
