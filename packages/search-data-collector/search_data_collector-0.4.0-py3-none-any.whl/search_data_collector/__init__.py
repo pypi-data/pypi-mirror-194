934b958f858c4e5d1bb360c59f58cb649499aea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:37 +0000

    Add package search-data-collector/0.4.0/search_data_collector-0.4.0-py3-none-any.whl

---
+++ packages/search-data-collector/search_data_collector-0.4.0-py3-none-any.whl/search_data_collector/__init__.py
@@ -0,0 +1,11 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+__version__ = "0.4.0"
+__license__ = "MIT"
+
+
+from .search_data_collector import SearchDataCollector
+
+__all__ = ["SearchDataCollector"]
