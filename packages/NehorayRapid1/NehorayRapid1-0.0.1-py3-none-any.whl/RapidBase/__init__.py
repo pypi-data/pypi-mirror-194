7d23b90f1bb8a4b1a5fb4656810d6854814beea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:46 +0000

    Add package NehorayRapid1/0.0.1/NehorayRapid1-0.0.1-py3-none-any.whl

---
+++ packages/NehorayRapid1/NehorayRapid1-0.0.1-py3-none-any.whl/RapidBase/__init__.py
@@ -0,0 +1,23 @@
+##Basic_Operations_Timing
+from Basic_Operations_Timing.Numpy_basic_operations import *
+
+
+## GANS
+from MISC_REPOS.GANS.GAN_Discriminators.GAN_Discriminators import *
+from MISC_REPOS.GANS.GAN_CoGAN_test import *
+from MISC_REPOS.GANS.GAN_test import *
+from MISC_REPOS.GANS.GAN_BiCycleGAN_test import *
+from MISC_REPOS.GANS.GAN_CoGAN_test import *
+from MISC_REPOS.GANS.GAN_CycleGAN_test import *
+from MISC_REPOS.GANS.GAN_DCGAN_test import *
+from MISC_REPOS.GANS.GAN_DRGAN_test import *
+from MISC_REPOS.GANS.GAN_EBGAN_test import *
+from MISC_REPOS.GANS.GAN_ESRGAN_test import *
+from MISC_REPOS.GANS.GAN_LSGAN_test import *
+from MISC_REPOS.GANS.GAN_pix2pix_test import *
+from MISC_REPOS.GANS.GAN_SRGAN_test import *
+from MISC_REPOS.GANS.GAN_StarGAN_test import *
+from MISC_REPOS.GANS.GAN_test import *
+from MISC_REPOS.GANS.GAN_WGANGP_test import *
+from MISC_REPOS.GANS.GAN_WGAN_test import *
+from MISC_REPOS.GANS.GAN_ACGAN_test import *
