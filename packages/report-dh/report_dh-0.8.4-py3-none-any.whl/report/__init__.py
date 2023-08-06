d2d7bebbf17fde9830c3710bdf3ec50290bf17cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:49 +0000

    Add package report-dh/0.8.4/report_dh-0.8.4-py3-none-any.whl

---
+++ packages/report-dh/report_dh-0.8.4-py3-none-any.whl/report/__init__.py
@@ -0,0 +1,22 @@
+from item import *
+
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--report", action="store", default=None,
+                     help="path to the script to be executed before running tests")
+
+def pytest_runtestloop(session):
+    from _internal import Launch, Data
+
+    script_path = session.config.getoption("--report")
+    if script_path:
+        Data.parse()
+        Launch.start_launch()
+
+def pytest_sessionfinish(session, exitstatus):
+    from _internal import Launch
+
+    script_path = session.config.getoption("--report")
+    if script_path:
+        Launch.finish_launch()
