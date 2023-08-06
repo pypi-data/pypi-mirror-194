fbf9b394f6ecd42156525936e143cf39710dccc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:29 +0000

    Add package foldergenie/1.0.0/foldergenie-1.0.0.tar.gz

---
+++ packages/foldergenie/foldergenie-1.0.0.tar.gz/foldergenie-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Directory structure creation'
+LONG_DESCRIPTION = 'A package that allows to create complex folder structures'
+
+# Setting up
+setup(
+    name="foldergenie",
+    version=VERSION,
+    author="Skadoosh (Hariprasad)",
+    author_email="<hariprasad19036@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['foldergenie=foldergenie:foldergenie'],
+    },
+    install_requires=[],
+    keywords=['python', 'folder', 'file', 'directory', 'organize'],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
