583a73fee61742bc294ef6aff5f875ceb500e3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:27 +0000

    Add package fixieai/0.1.6/fixieai-0.1.6-py3-none-any.whl

---
+++ packages/fixieai/fixieai-0.1.6-py3-none-any.whl/fixieai/__init__.py
@@ -0,0 +1,23 @@
+from fixieai.agents import AgentQuery
+from fixieai.agents import AgentResponse
+from fixieai.agents import CodeShotAgent
+from fixieai.agents import Embed
+from fixieai.agents import Message
+from fixieai.client import FixieClient
+from fixieai.client import get_agents
+from fixieai.client import get_client
+from fixieai.client import get_embeds
+from fixieai.client import query
+
+__all__ = [
+    "AgentQuery",
+    "AgentResponse",
+    "Embed",
+    "Message",
+    "CodeShotAgent",
+    "FixieClient",
+    "get_agents",
+    "get_client",
+    "get_embeds",
+    "query",
+]
