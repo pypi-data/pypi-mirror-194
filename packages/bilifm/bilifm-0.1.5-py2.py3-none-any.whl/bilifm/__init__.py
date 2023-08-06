5702b26271a0b0a9b91bcc14743bce18d882ef85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:37 +0000

    Add package bilifm/0.1.5/bilifm-0.1.5-py2.py3-none-any.whl

---
+++ packages/bilifm/bilifm-0.1.5-py2.py3-none-any.whl/bilifm/__init__.py
@@ -0,0 +1,24 @@
+import typer
+
+from bilifm.audio import Audio
+from bilifm.user import User
+
+app = typer.Typer()
+
+
+@app.command()
+def bv(bv: str):
+    audio = Audio(bv)
+    audio.download()
+
+
+@app.command()
+def uid(uid: str):
+    user = User(uid)
+
+    for video in user.videos:
+        bv = video["bvid"]
+        audio = Audio(bv)
+        audio.download()
+
+    typer.echo("Download complete")
