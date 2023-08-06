56204edf5936e675ecff5f25edd09f3afe1fdd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:10 +0000

    Add package USB-Imager/2.2.1/usb_imager-2.2.1-py3-none-any.whl

---
+++ packages/USB-Imager/usb_imager-2.2.1-py3-none-any.whl/usbimager/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""GUI-Application to write bootable disk images to USB keys."""
+
+
+APP_NAME = "usbimager"
+APP_TITLE = "USB-Imager"
+
+__version__ = "2.2.1"
+
+APP_LICENSE = "GPLv3+"
+APP_COPYRIGHT = "© 2021-2022"
+
+APP_ORGANISATION = "Secu-Design®"
+APP_ID = "com.secu-design.app.usbimager"
+APP_AUTHOR = "Skynet-Devel"
+APP_AUTHOR_EMAIL = "skynet.devel@secu-design.com"
+
+APP_URL_GITLAB = "https://gitlab.com/secu-design/usb-imager"
+APP_URL_PYPI = "https://pypi.org/project/usb-imager"
+
+APP_ABOUT = f"""\
+Copyright {APP_COPYRIGHT} by {APP_ORGANISATION}.
+Released under the {APP_LICENSE} license.
+
+Author:  {APP_AUTHOR}
+E-mail:  {APP_AUTHOR_EMAIL}
+
+Websites:
+{APP_URL_GITLAB}
+{APP_URL_PYPI}
+
+Please send me feedback if you like
+USB-Imager or if you find bugs.
+"""
