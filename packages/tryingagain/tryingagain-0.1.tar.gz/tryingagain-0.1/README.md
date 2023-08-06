d0929dfe30db8fb865479924509f0e8e37b2ea90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:18 +0000

    Add package tryingagain/0.1/tryingagain-0.1.tar.gz

---
+++ packages/tryingagain/tryingagain-0.1.tar.gz/tryingagain-0.1/README.md
@@ -0,0 +1,1 @@
+I feel I have cracked the implementation using first principles but I can't wait to see two python scripts being used simultanously
---
+++ packages/tryingagain/tryingagain-0.1.tar.gz/tryingagain-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tryingagain',
+    version='0.1',
+    description='A library for performing addition and subtraction',
+    py_modules=find_packages(),
+    url='https://github.com/cumbersomeamir',
+    author='Amir Kidwai',
+    author_email='findamirkidwai@gmail.com',
+)
+
