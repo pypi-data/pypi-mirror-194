2ce95cfae28b08c53a423bcdc8d3eb59284dc77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:17 +0000

    Add package la-nlp/0.1.0/la_nlp-0.1.0.tar.gz

---
+++ packages/la-nlp/la_nlp-0.1.0.tar.gz/la_nlp-0.1.0/la_nlp/aspect_sentiment_pipe.py
@@ -0,0 +1,26 @@
+from la_nlp import components, config
+from spacy import load as load_model
+from spacy.language import Language
+
+ASPECTS = config.get_aspects()
+
+NLP = load_model('en_core_web_lg')
+
+def make_doc(text, nlp=NLP, aspects=ASPECTS):
+    keywords = []
+    for keywords_list in aspects.values():
+        keywords.extend(keywords_list)
+    return nlp(text, component_cfg={'aspect_sentiment_pipe':{'base_aspects':aspects, 'base_keywords':keywords}})
+
+@Language.component('aspect_sentiment_pipe')
+def aspect_sentiment_pipe(doc, base_aspects, base_keywords):
+    doc = components.contains_aspect(doc, base_keywords)
+    doc = components.aspects(doc, base_aspects)
+    doc = components.keywords(doc, base_keywords)
+    doc = components.keyword_aspects(doc, base_aspects)
+    doc = components.parent_span(doc)
+    doc = components.parent_span_sentiment(doc)
+    doc = components.aspect_sentiments(doc, base_aspects)
+    return doc
+
+NLP.add_pipe('aspect_sentiment_pipe')
---
+++ packages/la-nlp/la_nlp-0.1.0.tar.gz/la_nlp-0.1.0/la_nlp/scratch2.py
@@ -0,0 +1,5 @@
+from la_nlp import scratch
+
+scratch.x = 10
+
+print(scratch.x)
