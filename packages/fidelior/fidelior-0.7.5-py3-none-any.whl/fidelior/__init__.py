2c3477a1a47983055d1cf4772459991b84726836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:19 +0000

    Add package fidelior/0.7.5/fidelior-0.7.5-py3-none-any.whl

---
+++ packages/fidelior/fidelior-0.7.5-py3-none-any.whl/fidelior/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+FIDELIOR
+--------
+
+To get started, visit the project page at https://gitlab.com/nleht/fidelior 
+
+For differences with the previous version:
+    >>> help(fidelior.core)
+
+FIDELIOR (c) by Nikolai G. Lehtinen
+
+FIDELIOR is licensed under a
+Creative Commons Attribution-NoDerivatives 4.0 International License.
+
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by-nd/4.0/>.
+"""
+
+__version__ = '0.7.5'
+print('Welcome to FIDELIOR, v.',__version__)
+print('(c) by Nikolai G. Lehtinen; licensed under CC BY-ND 4.0.')
+
+from .core import *
+
+print('Make finite-difference coding great again!')
